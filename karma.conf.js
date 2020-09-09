d4b3b3eba62acdf587d3eb757755ecf2b82fa4fd
taulinger <fam.aulinger@gmx.de>
2020-09-09 13:19:45 +0200

    lint

--- karma.conf.js
+++ karma.conf.js
@@ -1,28 +1,28 @@
 const webpackConfig = require('./webpack.test.config.js');
 
 module.exports = function (config) {
-  config.set({
-    frameworks: ['jasmine'],
-    // list of files / patterns to load in the browser
-    files: [
-      { pattern: 'test/**/*.test.js', watched: false },
-    ],
-    // preprocess matching files before serving them to the browser
-    preprocessors: {
-      'test/**/*.test.js': ['webpack'],
-    },
-    reporters: ['spec', 'coverage-istanbul'],
-    // port: 9876,
-    // colors: true,
-    // logLevel: config.LOG_INFO,
-    // autoWatch: true,
-    browsers: ['ChromeHeadless', 'FirefoxHeadless'],
-    // singleRun: true,
-    concurrency: Infinity,
-    webpack: webpackConfig,
-    coverageIstanbulReporter: {
-      dir: 'coverage/%browser%',
-      reports: ['text-summary', 'html']
-    },
-  });
+	config.set({
+		frameworks: ['jasmine'],
+		// list of files / patterns to load in the browser
+		files: [
+			{ pattern: 'test/**/*.test.js', watched: false },
+		],
+		// preprocess matching files before serving them to the browser
+		preprocessors: {
+			'test/**/*.test.js': ['webpack'],
+		},
+		reporters: ['spec', 'coverage-istanbul'],
+		// port: 9876,
+		// colors: true,
+		// logLevel: config.LOG_INFO,
+		// autoWatch: true,
+		browsers: ['ChromeHeadless', 'FirefoxHeadless'],
+		// singleRun: true,
+		concurrency: Infinity,
+		webpack: webpackConfig,
+		coverageIstanbulReporter: {
+			dir: 'coverage/%browser%',
+			reports: ['text-summary', 'html']
+		},
+	});
 };
