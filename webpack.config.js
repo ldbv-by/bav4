302eb565ab2ea44aa37332b84c61581e978284f4
Thomas Aulinger <fam.aulinger@gmx.de>
2020-09-12 12:51:45 +0200

    lint

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,37 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ],
-            },
-            {
-                test: /\.svg$/,
-                use: 'svg-url-loader',
-            }
-        ],
-    },
-    devtool: 'inline-source-map',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html',
-        })
-    ],
+	entry: './src/main.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				],
+			},
+			{
+				test: /\.svg$/,
+				use: 'svg-url-loader',
+			}
+		],
+	},
+	devtool: 'inline-source-map',
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'src/index.html',
+		})
+	],
 
-    // OPTIONAL
-    // Reload On File Change
-    watch: false,
-    devServer: {
-        contentBase: './dist',
-    },
+	// OPTIONAL
+	// Reload On File Change
+	watch: false,
+	devServer: {
+		contentBase: './dist',
+	},
 };
--- webpack.test.config.js
+++ webpack.test.config.js
@@ -1,28 +1,28 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+	mode: 'development',
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ],
-            },
-            {
-                test: /\.svg$/,
-                use: 'svg-url-loader',
-            },
-            {
-                test: /\.js/,
-                include: /src/,
-                exclude: /node_modules|\.spec\.js$/,
-                use: "@jsdevtools/coverage-istanbul-loader"
-            }
-        ],
-    },
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+				],
+			},
+			{
+				test: /\.svg$/,
+				use: 'svg-url-loader',
+			},
+			{
+				test: /\.js/,
+				include: /src/,
+				exclude: /node_modules|\.spec\.js$/,
+				use: '@jsdevtools/coverage-istanbul-loader'
+			}
+		],
+	},
 
 };
