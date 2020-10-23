8756ef39f3cebeb49534cd2c130722beba9f36b2
taulinger <fam.aulinger@gmx.de>
2020-10-23 15:50:10 +0200

    layout depends on device

--- src/components/menue/sidePanel/SidePanel.js
+++ src/components/menue/sidePanel/SidePanel.js
@@ -3,6 +3,8 @@ import { styleMap } from 'lit-html/directives/style-map.js';
 import BaElement from '../../BaElement';
 import './sidePanel.css';
 import { closeSidePanel } from './store/sidePanel.action';
+import { $injector } from '../../../injection';
+
 
 /**
  *  
@@ -11,22 +13,75 @@ import { closeSidePanel } from './store/sidePanel.action';
  */
 export class SidePanel extends BaElement {
 
+	constructor() {
+		super();
 
+		const { EnvironmentService } = $injector.inject('EnvironmentService');
+		this.environmentService = EnvironmentService;
+	}
+
+	/**
+	 * @override
+	 */
+	onAfterRender() {
+		this.getElementsByClassName('tabcontent')[0].style.display = 'block';
+		this.getElementsByClassName('tablink')[0].classList.add('tablink-active');
+	}
+
+
+	/**
+	 * @override
+	 */
 	createView() {
 
 		const { open } = this.state;
+		const { mobile } = this.environmentService;
+		const getOverlayClass = () => (mobile ? 'overlay-mobile' : 'overlay-desktop');
+		const styles = {};
+		if (mobile) {
+			Object.assign(styles, {
+				height: open ? '410px' : '0px',
+				width: '100%'
+			});
+		}
+		else {
+			Object.assign(styles, {
+				width: open ? '410px' : '0px',
+				height: '100%'
+			});
+		}
 
-		const styles = {
-			width: open ? '410px' : '0px'
+		const items = [
+			{ name: 'Share', description: 'Let\'s share' },
+			{ name: 'Print', description: '...print' },
+			{ name: 'Draw', description: '...draw and measure on the map' },
+			{ name: 'Routing', description: '...get a route' },
+			{ name: 'Tools', description: '...and do other fancy stuff!' },
+		];
+
+		const onItemClicked = (index) => {
+			const tabcontents = [...this.getElementsByClassName('tabcontent')];
+			tabcontents.forEach((tabcontent, i) => (i === index) ? tabcontent.style.display = 'block' : tabcontent.style.display = 'none');
+			const tablinks = [...this.getElementsByClassName('tablink')];
+			tablinks.forEach((tablink, i) => (i === index) ? tablink.classList.add('tablink-active') : tablink.classList.remove('tablink-active'));
 		};
 
+
 		return html`
-			<div class="sidePanel overlay" style=${styleMap(styles)} >
+			<div class="sidePanel overlay ${getOverlayClass()}" style=${styleMap(styles)} >
+				<div class="tab-bar">
+					${items.map((item, index) => html`<button class="tablink" @click=${() => onItemClicked(index)}>${item.name}</button>`)}
+				</div>	
 				<a @click="${closeSidePanel}" title="Close menue"><span class="icon close"></a>
 
 				<!-- Overlay content -->
 				<div class="overlay-content">
-					#content
+					${items.map(item => html`
+						<div class="tabcontent">
+							<!-- <h1>${item.name}</h1>-->
+							<p>${item.description}</p>
+						</div>
+					`)}
 				</div>
 			</div>
 		`;
--- test/components/menue/sidePanel/SidePanel.test.js
+++ test/components/menue/sidePanel/SidePanel.test.js
@@ -2,13 +2,13 @@
 
 import { SidePanel } from '../../../../src/components/menue/sidePanel/SidePanel';
 import sidePanelReducer from '../../../../src/components/menue/sidePanel/store/sidePanel.reducer';
-
 import { TestUtils } from '../../../test-utils';
+import { $injector } from '../../../../src/injection';
+
 window.customElements.define(SidePanel.tag, SidePanel);
 
 
 describe('SidePanelElement', () => {
-	let element;
 
 	beforeAll(() => {
 		window.classUnderTest = SidePanel.name;
@@ -18,8 +18,9 @@ describe('SidePanelElement', () => {
 		window.classUnderTest = undefined;
 	});
 
+	const setup = async (config) => {
 
-	beforeEach(async () => {
+		const { mobile } = config;
 
 		const state = {
 			sidePanel: {
@@ -27,21 +28,58 @@ describe('SidePanelElement', () => {
 			}
 		};
 		TestUtils.setupStoreAndDi(state, { sidePanel: sidePanelReducer });
-		element = await TestUtils.render(SidePanel.tag);
-	});
-
+		$injector.registerSingleton('EnvironmentService', {
+			mobile: mobile
+		});
+		return TestUtils.render(SidePanel.tag);
+	};
 
 	describe('when initialized', () => {
-		it('adds a div which holds the sidepanel content and a close icon', () => {
-			expect(element.querySelector('.sidePanel').style.width).toBe('410px');
+		it('adds a div which holds the sidepanel content and a close icon for desktop layout', async() => {
+
+			const element = await setup({ mobile: false });
+			
+			expect(element.querySelector('.sidePanel.overlay.overlay-desktop')).toBeTruthy();
+			expect(element.querySelector('.sidePanel.overlay.overlay-desktop').style.width).toBe('410px');
+			expect(element.querySelector('.sidePanel.overlay.overlay-desktop').style.height).toBe('100%');
 			expect(element.querySelector('.close')).toBeTruthy();
+			expect(element.getElementsByClassName('tabcontent').length).toBe(5);
+			expect(element.getElementsByClassName('tablink').length).toBe(5);
+
+			expect(element.getElementsByClassName('tabcontent')[0].style.display).toBe('block');
+			expect(element.getElementsByClassName('tablink')[0].classList.contains('tablink-active')).toBeTrue();
+		});
+
+		it('adds a div which holds the sidepanel content and a close icon for mobile layout', async() => {
+
+			const element = await setup({ mobile: true });
+			
+			expect(element.querySelector('.sidePanel.overlay.overlay-mobile')).toBeTruthy();
+			expect(element.querySelector('.sidePanel.overlay.overlay-mobile').style.width).toBe('100%');
+			expect(element.querySelector('.sidePanel.overlay.overlay-mobile').style.height).toBe('410px');
+			expect(element.querySelector('.close')).toBeTruthy();
+			expect(element.getElementsByClassName('tabcontent').length).toBe(5);
+			expect(element.getElementsByClassName('tablink').length).toBe(5);
+
+			expect(element.getElementsByClassName('tabcontent')[0].style.display).toBe('block');
+			expect(element.getElementsByClassName('tablink')[0].classList.contains('tablink-active')).toBeTrue();
 		});
 
 	});
-	describe('when close icon clicked', () => {
-		it('it closes the sidepanel', () => {
+
+	describe('when close button clicked', () => {
+		it('it closes the sidepanel (desktop)', async () => {
+			const element = await setup({ mobile: false });
 			element.querySelector('.close').click();
 			expect(element.querySelector('.sidePanel').style.width).toBe('0px');
+			expect(element.querySelector('.sidePanel').style.height).toBe('100%');
+		});
+
+		it('it closes the sidepanel (mobile)', async () => {
+			const element = await setup({ mobile: true });
+			element.querySelector('.close').click();
+			expect(element.querySelector('.sidePanel').style.width).toBe('100%');
+			expect(element.querySelector('.sidePanel').style.height).toBe('0px');
 		});
 	});
 });
