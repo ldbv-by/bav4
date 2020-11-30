bd7ae8f2d14e35f9bd1753efc452cf9aaed67f07
Thomas Aulinger <fam.aulinger@gmx.de>
2020-11-30 21:43:44 +0100

    add class which represents a single search result

---
+++ src/components/toolbox/search/autocomplete/service/SearchResult.js
@@ -0,0 +1,36 @@
+/**
+ * Represents a single search result.
+ * @class
+ * @author aul
+ */
+export default class SearchResult {
+
+	constructor(label, labelFormated, type, center, extent = []) {
+		this._label = label;
+		this._labelFormated = labelFormated;
+		this._type = type;
+		this._center = center;
+		this._extent = extent;
+	}
+
+	get label() {
+		return this._label;
+	}
+
+	get labelFormated() {
+		return this._labelFormated;
+	}
+
+	get type() {
+		return this._type;
+	}
+
+	get center() {
+		return this._center;
+	}
+
+	get extent() {
+		return this._extent;
+	}
+
+}
