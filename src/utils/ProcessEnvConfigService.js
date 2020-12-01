afbb28010025992edbf25bf73f5726b8eebf9b57
Thomas Aulinger <fam.aulinger@gmx.de>
2020-12-01 21:56:51 +0100

    env object could be missing

--- src/utils/ProcessEnvConfigService.js
+++ src/utils/ProcessEnvConfigService.js
@@ -8,7 +8,11 @@ export class ProcessEnvConfigService {
 
 	getValue(key) {
 		// eslint-disable-next-line no-undef
-		if (process.env[key]) {
+		if (!process.env) {
+			throw 'Env object not present, maybe .env-file is missing';
+		}
+		// eslint-disable-next-line no-undef
+		else if (process.env[key]) {
 			// eslint-disable-next-line no-undef
 			return process.env[key];
 		}
@@ -17,6 +21,10 @@ export class ProcessEnvConfigService {
 
 	hasKey(key) {
 		// eslint-disable-next-line no-undef
-		return Object.prototype.hasOwnProperty.call(process.env, key);
+		if (process.env) {
+			// eslint-disable-next-line no-undef
+			return Object.prototype.hasOwnProperty.call(process.env, key);
+		}
+		return false;
 	}
 }
