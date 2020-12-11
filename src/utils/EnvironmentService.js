5e704d5f1835d922abdccbf78303ef82ca669cc9
taulinger <fam.aulinger@gmx.de>
2020-12-11 19:14:42 +0100

    replace mobile property by touch and orientation

--- src/utils/EnvironmentService.js
+++ src/utils/EnvironmentService.js
@@ -1,10 +1,61 @@
 export class EnvironmentService {
 
-	constructor(_window) {
-		const urlParams = new URLSearchParams(_window.location.search);
-
-		this.mobile =  urlParams.get('mobile') === 'true';
-		this.touch = false;
+	constructor(_window = window) {
+		this._window = _window;
 	}
 
+	/**
+	 * @returns URLSearchParams
+	 */
+	getUrlParams() {
+		return new URLSearchParams(this._window.location.search);
+	}
+
+	isTouch() {
+		const navigator = this._window.navigator;
+		const window = this._window;
+		let hasTouchScreen = false;
+		if ('maxTouchPoints' in navigator) {
+			hasTouchScreen = navigator.maxTouchPoints > 0;
+		}
+		else {
+			var mQ = window.matchMedia && window.matchMedia('(pointer:coarse)');
+			if (mQ && mQ.media === '(pointer:coarse)') {
+				hasTouchScreen = !!mQ.matches;
+			}
+			else if ('orientation' in window) {
+				hasTouchScreen = true; // deprecated, but good fallback
+			}
+			else {
+				// Only as a last resort, fall back to user agent sniffing
+				var UA = navigator.userAgent;
+				hasTouchScreen = (
+					/\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+					/\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+				);
+			}
+		}
+		return hasTouchScreen;
+
+	}
+
+	/**
+	 * 
+	 * @see https://caniuse.com/screen-orientation
+	 */
+	getScreenOrientation() {
+		const orientation = (this._window.screen.orientation || {}).type || this._window.screen.mozOrientation || this._window.screen.msOrientation;
+		if (!orientation) {
+			const widthHeightRatio = this._window.screen.width / this._window.screen.height;
+			return {
+				portrait: widthHeightRatio < 1,
+				landscape: widthHeightRatio >= 1,
+
+			};
+		}
+		return {
+			portrait: orientation.startsWith('portrait'),
+			landscape: orientation.startsWith('landscape'),
+		};
+	}
 }
