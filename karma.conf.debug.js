592bc35c9e4d236fab299a65ee5a7fa3da26b17a
thiloSchlemmer <thiloSchlemmer@gmail.com>
2020-12-18 10:44:49 +0100

    Add Karma-config to debug in tests

---
+++ karma.conf.debug.js
@@ -0,0 +1,33 @@
+const webpackConfig = require('./webpack.test.config.js');
+
+module.exports = function (config) {
+	config.set({
+		frameworks: ['jasmine', 'iframes'],
+		// list of files / patterns to load in the browser
+		files: [
+			{ pattern: 'test/**/*.test.js', watched: false },
+		],
+		// preprocess matching files before serving them to the browser
+		preprocessors: {
+			'test/**/*.test.js': ['webpack', 'iframes'],
+		},
+		reporters: ['progress', 'coverage-istanbul'],
+		// port: 9876,
+		// colors: true,
+		// logLevel: config.LOG_INFO,
+		// autoWatch: true,
+		browsers: ['ChromeDebugging'],
+		customLaunchers: {
+			ChromeDebugging: {
+				base: 'ChromeHeadless',
+				flags: ['--remote-debugging-port=9222']
+			}
+		},
+		concurrency: Infinity,
+		webpack: webpackConfig,
+		coverageIstanbulReporter: {
+			dir: 'coverage/%browser%',
+			reports: ['text-summary', 'html']
+		},
+	});
+};
