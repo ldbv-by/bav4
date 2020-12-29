b89f53dc97125a2526fafffffdd498ee384a52fe
Thomas Aulinger <fam.aulinger@gmx.de>
2020-12-29 17:27:46 +0100

    add "comma-spacing" and "array-bracket-spacing" rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
 		'object-curly-spacing': ['error', 'always'],
 		'brace-style': ['error', 'stroustrup'],
 		'lines-between-class-members': ['error', 'always'],
+		'comma-spacing': ['error', { 'before': false, 'after': true }],
+		"array-bracket-spacing": [2, "never"],
 		'curly': ['error', 'all'],
 		'space-before-blocks': ['error', 'always'],
 		'import/no-default-export': ['error'],
