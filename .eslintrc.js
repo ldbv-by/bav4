c56973f92a52aebf37b0792a65eb7ba5f2c91097
Thomas Aulinger <fam.aulinger@gmx.de>
2020-12-29 17:45:44 +0100

    add "no-console" rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,9 +20,10 @@ module.exports = {
 		'brace-style': ['error', 'stroustrup'],
 		'lines-between-class-members': ['error', 'always'],
 		'comma-spacing': ['error', { 'before': false, 'after': true }],
-		"array-bracket-spacing": [2, "never"],
+		"array-bracket-spacing": ['error', "never"],
 		'curly': ['error', 'all'],
 		'space-before-blocks': ['error', 'always'],
+		'no-console': ['error', { allow: ["warn", "error"] }],
 		'import/no-default-export': ['error'],
 	}
 };
