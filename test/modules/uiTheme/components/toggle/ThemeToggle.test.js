9ef54b542693bf73adf9515f085b65c6497bb3b1
Thomas Aulinger <fam.aulinger@gmx.de>
2021-01-13 11:02:34 +0100

    use the toggle component

--- test/modules/uiTheme/components/toggle/ThemeToggle.test.js
+++ test/modules/uiTheme/components/toggle/ThemeToggle.test.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-undef */
 import { ThemeToggle } from '../../../../../src/modules/uiTheme/components/toggle/ThemeToggle';
+import { Toggle } from '../../../../../src/modules/commons/components/toggle/Toggle';
 import { TestUtils } from '../../../../test-utils';
 import { uiThemeReducer } from '../../../../../src/modules/uiTheme/store/uiTheme.reducer';
 import { $injector } from '../../../../../src/injection';
 
 window.customElements.define(ThemeToggle.tag, ThemeToggle);
+window.customElements.define(Toggle.tag, Toggle);
 
 let store;
 
@@ -30,22 +32,22 @@ describe('ThemeToggle', () => {
 		it('renders the view', async () => {
 			const element = await setup();
 
-			expect(element.shadowRoot.querySelector('.switch').title).toBe('uiTheme_toggle_tooltip_dark');
-			expect(element.shadowRoot.querySelector('input').checked).toBeTrue();
+			expect(element.shadowRoot.querySelector('ba-toggle').title).toBe('uiTheme_toggle_tooltip_dark');
+			expect(element.shadowRoot.querySelector('ba-toggle').getAttribute('checked')).toBe('true');
 			expect(element.shadowRoot.querySelector('.icon.adjust')).toBeTruthy();
-			expect(element.shadowRoot.querySelector('.slider.round')).toBeTruthy();
+			// expect(element.shadowRoot.querySelector('.slider.round')).toBeTruthy();
 		});
 	});
 
 	describe('when toggle clicked', () => {
 		it('changes the theme', async () => {
-
 			const element = await setup();
-			element.shadowRoot.querySelector('.switch').click();
+			
+			element.shadowRoot.querySelector('ba-toggle').shadowRoot.querySelector('input').click();
 
 			expect(store.getState().uiTheme.theme).toBe('light');
-			expect(element.shadowRoot.querySelector('input').checked).toBeFalse();
-			expect(element.shadowRoot.querySelector('.switch').title).toBe('uiTheme_toggle_tooltip_light');
+			expect(element.shadowRoot.querySelector('ba-toggle').getAttribute('checked')).toBe('false');
+			expect(element.shadowRoot.querySelector('ba-toggle').title).toBe('uiTheme_toggle_tooltip_light');
 		});
 	});
 });
