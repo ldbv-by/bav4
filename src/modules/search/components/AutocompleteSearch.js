4451f1084f9fa5995e545a45e80c7c3f358af14e
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-01-17 11:52:25 +0100

    update: disassemble autocomplete method
    moving the callbacks for input-events  to createview and progress in
    litifying
    
    change the lifecycle-hook to omAfterRender to enable lazyloading

--- src/modules/search/components/AutocompleteSearch.js
+++ src/modules/search/components/AutocompleteSearch.js
@@ -24,34 +24,92 @@ export class AutocompleteSearch extends BaElement {
 		this._currentFocus=-1;
 	}
 
+	/**
+	 * @private
+	 */
 	_updateCandidates(candidates) {
 		this._candidates = candidates;
 		this.render();
 	}
 
+	/**
+	 * @private
+	 */
 	_clearCandidates() {
 		this._currentFocus=-1;
 		this._updateCandidates([]);
 	}
 
+	/**
+	 * @private
+	 */
 	_setSearchValue(value) {
 		const input = this._root.querySelector('#autoComplete');
 		input.value = value;
 	}
 
-	/**
-	 * Ported from https://www.w3schools.com/howto/howto_js_autocomplete.asp
-	 * @param {*} input
-	 * @param {*} arr 
+	/** 
+	 * @override
 	 */
-	_autocomplete(input) {
-		/*the autocomplete function takes two arguments,
-		the text field element and an array of possible autocompleted values:*/
-		/*execute a function when someone writes in the text field:*/
-		//input.addEventListener('input', debounced(200, requestData));
-		/*execute a function presses a key on the keyboard:*/
-		input.addEventListener('keydown', (e) => {
-			// var x = document.getElementById(this.id + 'autocomplete-list');
+	onAfterRender(firsttime) {
+		if (firsttime) {
+			document.addEventListener('click', () => {
+				this._clearCandidates();
+			});
+		}
+	}
+
+	/**
+	 * @override
+	 */
+	createView() {
+		let inputText = this._onSelect.label ? this._onSelect.label : '';
+		const requestData = (e) => {
+			let val = e.target.value;
+			if (!val) {
+				this._clearCandidates();
+				return false;
+			}
+
+			this._searchService.getData(val)
+				.then((data) => {
+					if (data) {
+						this._updateCandidates(data);
+					}
+				});
+		};
+
+		const addActive = (x) => {
+			/*a function to classify an item as "active":*/
+			if (!x || x.length < 1) {
+				return false;
+			}
+			/*start by removing the "active" class on all items:*/
+			removeActive(x);
+			if (this._currentFocus >= x.length) {
+				this._currentFocus= 0;
+			}
+			if (this._currentFocus < 0) {
+				this._currentFocus= (x.length - 1);
+			}
+			/*add class "autocomplete-active":*/
+			x[this._currentFocus].classList.add('autocomplete-active');
+		};
+		const removeActive = (x) => {
+			/*a function to remove the "active" class from all autocomplete items:*/
+			for (var i = 0; i < x.length; i++) {
+				x[i].classList.remove('autocomplete-active');
+			}
+		}; 
+		
+		const onInput = (e) => debounced(200, requestData(e));
+		const onClick = (candidate) => {			
+			this._onSelect(candidate);
+			this._setSearchValue(candidate.label);
+			this._clearCandidates();
+		};
+
+		const onKeyDown = (e) => {
 			let x = this._root.querySelector('#autocomplete-list');
 			if (x) {
 				x = x.querySelectorAll('div');
@@ -80,75 +138,12 @@ export class AutocompleteSearch extends BaElement {
 					}
 				}
 			}
-		});
-		const addActive = (x) => {
-			/*a function to classify an item as "active":*/
-			if (!x || x.length < 1) {
-				return false;
-			}
-			/*start by removing the "active" class on all items:*/
-			removeActive(x);
-			if (this._currentFocus >= x.length) {
-				this._currentFocus= 0;
-			}
-			if (this._currentFocus < 0) {
-				this._currentFocus= (x.length - 1);
-			}
-			/*add class "autocomplete-active":*/
-			x[this._currentFocus].classList.add('autocomplete-active');
-		};
-		const removeActive = (x) => {
-			/*a function to remove the "active" class from all autocomplete items:*/
-			for (var i = 0; i < x.length; i++) {
-				x[i].classList.remove('autocomplete-active');
-			}
-		};
-
-		document.addEventListener('click', () => {
-			this._clearCandidates();
-		});
-	}
-
-
-	/**
-	 * @override
-	 */
-	onWindowLoad() {
-		this._autocomplete(this._root.querySelector('#autoComplete'));
-	}
-
-
-	/**
-	 * @override
-	 */
-	createView() {
-		let inputText = this._onSelect.label ? this._onSelect.label : '';
-		const requestData = (e) => {
-			let val = e.target.value;
-			if (!val) {
-				this._clearCandidates();
-				return false;
-			}
-
-			this._searchService.getData(val)
-				.then((data) => {
-					if (data) {
-						this._updateCandidates(data);
-					}
-				});
-		};
-		
-		const onInput = (e) => debounced(200, requestData(e));
-		const onClick = (candidate) => {			
-			this._onSelect(candidate);
-			this._setSearchValue(candidate.label);
-			this._clearCandidates();
 		};
 		
 		return html`
 		 <style>${css}</style>
 		 <div class="autocomplete">
-			<input id='autoComplete' .value=${inputText} @input=${onInput}/>
+			<input id='autoComplete' .value=${inputText} @input=${onInput} @keydown=${onKeyDown}/>
 			${this._candidates ? html`<div id='autocomplete-list' class='autocomplete-items'>${repeat(this._candidates, (candidate) => candidate.id, (candidate, index) => html`
 			<div index=${index} @click=${() => onClick(candidate)} >${unsafeHTML(candidate.labelFormated)}</div>
 		  `)}</div>`: html`` } 
