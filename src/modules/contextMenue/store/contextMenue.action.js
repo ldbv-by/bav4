f28420b9c867997340f105cd8c5460d060bf100a
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-01-22 07:55:09 +0100

    Add jsdoc for contextMenue actions

--- src/modules/contextMenue/store/contextMenue.action.js
+++ src/modules/contextMenue/store/contextMenue.action.js
@@ -11,8 +11,31 @@ const getStore = () => {
 };
 
 /**
+ * A pointer representing a location in (x,y) coordinate space, specified in integer precision for the display.
+ * @typedef {Object} Point
+ * @property {number} [x] The X coordinate of this Point.
+ * @property {number} [y] The Y coordinate of this Point.
+ */
+
+/**
+ * 
+ * @typedef {Object} Command
+ * @property {String} [label] The Label of this command.
+ * @property {String} [shortcut] The Keyboard-Shortcut (optional) of this Command.
+ * @property {function} [action1] The Callback to call, if the user select this command.
+ */
+
+/**
+ * Properties to display the contextmenu at a specific point with specific entries
+ * @typedef {Object} ContextMenuData
+ * @property {Point} [pointer] The Pointer, a location, where the contextmenu should be placed.
+ * @property {Command[]} [commands] The list of available commands, to show in the contextmenu.
+ */
+
+/**
  * Opens the contextMenue.
  * @function
+ * @param {ContextMenuData} contextMenuData the data to display the contextmenu at a specific point with specific entries
  */
 export const contextMenueOpen = (contextMenuData) => {
 	getStore().dispatch({
