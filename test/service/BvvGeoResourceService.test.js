b58fdd2b7be07dacf672de4162279a26a2fbfc82
Thomas Aulinger <fam.aulinger@gmx.de>
2021-01-22 18:10:50 +0100

    service for georesources (for now without backend connection)

---
+++ test/service/BvvGeoResourceService.test.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-undef */
+import { BvvGeoResourceService } from '../../src/services/BvvGeoResourceService';
+
+describe('BvvGeoResourceService', () => {
+
+	describe('all', () => {
+
+		it('provides all GeoResources', async () => {
+			const service = new BvvGeoResourceService();
+
+			const geoResources = await service.all();
+
+			expect(geoResources.length).toBe(3);
+		});
+	});
+
+	describe('byId', () => {
+
+		it('provides a GeoResource by id', async () => {
+			const service = new BvvGeoResourceService();
+
+			const geoResource = await service.byId('dop80');
+
+			expect(geoResource).toBeTruthy();
+		});
+
+		it('provides a rejected Promise when GeoResource is not found', (done) => {
+			const service = new BvvGeoResourceService();
+
+			service.byId('unknownId').then(() => {
+				done(new Error('Promise should not be resolved'));
+			}, (reason) => {
+				expect(reason).toBe('No GeoResource for unknownId found');
+				done();
+			});
+		});
+	});
+});
