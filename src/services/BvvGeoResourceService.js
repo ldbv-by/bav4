c99781a5dfa2a678fb7851d73612920169c94d01
Thomas Aulinger <fam.aulinger@gmx.de>
2021-01-29 16:23:09 +0100

    connect service to backend

--- src/services/BvvGeoResourceService.js
+++ src/services/BvvGeoResourceService.js
@@ -1,8 +1,8 @@
-
-import { AggregateGeoResource, VectorGeoResource, VectorSourceType, WmsGeoResource, WMTSGeoResource } from './domain/geoResources';
+import { AggregateGeoResource, VectorGeoResource, WmsGeoResource, WMTSGeoResource } from './domain/geoResources';
+import { $injector } from '../injection';
 
 /**
- * Provider for BVV {@link GeoResource}.
+ * Provider for BVV {@link GeoResource}s.
  * 
  * @class
  * @author aul
@@ -10,40 +10,92 @@ import { AggregateGeoResource, VectorGeoResource, VectorSourceType, WmsGeoResour
 export class BvvGeoResourceService {
 
 	constructor() {
+
+		const { HttpService: httpService, ConfigService: configService } = $injector.inject('HttpService', 'ConfigService');
+		this._httpService = httpService;
+		this._configService = configService;
+
 		this._georesources = null;
 	}
 
+	/**
+	 * Initializes this service, which means all available GeoResources are loaded and can be served in the future from the internal cache.
+	 * @public
+	 * @async
+	 * @returns {Promise<Array.<GeoResource>> | Promise.reject}
+	 */
 	async init() {
-		await this.all();
+		if (!this._georesources) {
+			try {
+				this._georesources = await this._load();
+				return Promise.resolve(this._georesources);
+			}
+			catch (e) {
+				return Promise.reject('GeoResourceService could not be initialized: ' + e.message);
+			}
+		}
+		return Promise.resolve(this._georesources);
+	}
+
+	async _load() {
+		const url = this._configService.getValue('BACKEND_URL') + 'georesources';
+
+		const result = await this._httpService.fetch(url, {
+			timeout: 2000,
+			mode: 'cors'
+		});
+
+		if (result.ok) {
+			const geoResources = [];
+			const georesourceDefinitions = await result.json();
+			georesourceDefinitions.forEach(definition => {
+				let geoResource = null;
+				switch (definition.type) {
+					case 'wms':
+						geoResource = new WmsGeoResource(definition.id, definition.label, definition.url, definition.layers, definition.format);
+						break;
+					case 'wmts':
+						geoResource = new WMTSGeoResource(definition.id, definition.label, definition.url);
+						break;
+					case 'vector':
+						geoResource = new VectorGeoResource(definition.id, definition.label, definition.url, Symbol.for(definition.sourceType));
+						break;
+					case 'aggregate':
+						geoResource = new AggregateGeoResource(definition.id, definition.label, definition.geoResourceIds);
+						break;
+
+				}
+				if(geoResource) {
+					geoResource.background = definition.background;
+					geoResource.opacity = definition.opacity;
+					geoResources.push(geoResource);
+				}
+				else{
+					console.warn('Could not create a GeoResource  for ' + definition.id);
+				}
+			});
+			return Promise.resolve(geoResources);
+		}
+		return Promise.reject(new Error('GeoResources could not be loaded'));
 	}
 
 	/**
 	 * Returns all available {@link GeoResource}.
 	 * @public
-	 * @async
 	 * @returns  {Array.<GeoResource>}
 	 */
-	async all() {
+	all() {
 		if (!this._georesources) {
-			const wms0 = new WmsGeoResource('bodendenkmal', 'Bodendenkmal', 'https://geoservices.bayern.de/wms/v1/ogc_denkmal.cgi', 'bodendenkmalO', 'image/png');
-			const wms1 = new WmsGeoResource('baudenkmal', 'Baudenkmal', 'https://geoservices.bayern.de/wms/v1/ogc_denkmal.cgi', 'bauensembleO,einzeldenkmalO', 'image/png');
-			const wms2 = new WmsGeoResource('dop80', 'DOP 80 Farbe', 'https://geoservices.bayern.de/wms/v2/ogc_dop80_oa.cgi?', 'by_dop80c', 'image/png');
-			const wmts0 = new WMTSGeoResource('atkis_sw', 'Webkarte s/w', 'https://intergeo{31-37}.bayernwolke.de/betty/g_atkisgray/{z}/{x}/{y}');
-			const vector0 = new VectorGeoResource('huetten', 'Hütten', 'http://www.geodaten.bayern.de/ba-data/Themen/kml/huetten.kml', VectorSourceType.KML);
-			const aggregate0 = new AggregateGeoResource('aggregate0', 'Aggregate', [wmts0, wms0]);
-
-			this._georesources = [wms0, wms1, wms2, wmts0, vector0, aggregate0];
-
+			throw new Error('GeoResourceService not yet initialized');
 		}
 		return this._georesources;
 	}
-
 
 	/**
 	 * Returns the corresponding  {@link GeoResource} for an id.
 	 * @public
 	 * @param {string} id Id of the desired {@link GeoResource}
-	 * @returns {Array.<GeoResource> | null}
+	 * @returns {GeoResource | null}
 	 */
 	byId(id) {
 		if (!this._georesources) {
@@ -51,5 +103,18 @@ export class BvvGeoResourceService {
 		}
 		const geoResource = this._georesources.find(georesource => georesource.id === id);
 		return geoResource || null;
+	}
+
+	/**
+	 * Adds a GeoResoure to the internal cache.
+	 * @param {GeoResource} georesource 
+	 * @returns {boolean} true, when succesfully added
+	 */
+	add(georesource) {
+		if (!this._georesources.find(_georesource => _georesource.id === georesource.id)) {
+			this._georesources.push(georesource);
+			return true;
+		}
+		return false;
 	}
 }
--- test/service/BvvGeoResourceService.test.js
+++ test/service/BvvGeoResourceService.test.js
@@ -1,59 +1,211 @@
 /* eslint-disable no-undef */
 import { BvvGeoResourceService } from '../../src/services/BvvGeoResourceService';
+import { WmsGeoResource, WMTSGeoResource } from '../../src/services/domain/geoResources';
+import { $injector } from '../../src/injection';
 
 describe('BvvGeoResourceService', () => {
+
+	let instanceUnderTest;
+
+	const configService = {
+		getValue: () => { }
+	};
+
+	const httpService = {
+		fetch: async () => { }
+	};
+
+	beforeAll(() => {
+		$injector
+			.registerSingleton('ConfigService', configService)
+			.registerSingleton('HttpService', httpService);
+	});
+
+	beforeEach(() => {
+		instanceUnderTest = new BvvGeoResourceService();
+	});
+	const wmsDefinition = { id: 'wmsId', label: 'wmsLabel', background: false, opacity: 0.5, url: 'wmsUrl', layers: 'wmsLayer', format: 'image/png', type: 'wms' };
+	const wmtsDefinition = { id: 'wmtsId', label: 'wmtsLabel', background: true, opacity: 1.0, url: 'wmtsUrl', type: 'wmts' };
+	const vectorDefinition = { id: 'wmtsId', label: 'vectorLabel', background: false, opacity: 1.0, url: 'vectorUrl', sourceType: 'kml', type: 'vector' };
+	const aggregateDefinition = { id: 'wmtsId', label: 'aggregateLabel', background: true, opacity: 1.0, geoResourceIds: ['wmtsId', 'wmsId'], type: 'aggregate' };
+
+	const wmtsGeoResource = new WMTSGeoResource('wmtsId', 'wmtsLabel', 'wmtsUrl');
+
+	const vadlidateGeoResourceProperties = (georesource, definition) => {
+		expect(georesource.id).toBe(definition.id);
+		expect(georesource.label).toBe(definition.label);
+		expect(georesource.background).toBe(definition.background);
+		expect(georesource.opacity).toBe(definition.opacity);
+		expect(Symbol.keyFor(georesource.getType())).toBe(definition.type);
+	};
 
 	describe('init', () => {
 
 		it('initializes the service', async () => {
-			const service = new BvvGeoResourceService();
+			
+			const backendUrl = 'https://backend.url';
+			const expectedArgs0 = backendUrl + 'georesources';
+			const expectedArgs1 = {
+				timeout: 2000,
+				mode: 'cors'
+			};
+			const configServiceSpy = spyOn(configService, 'getValue').withArgs('BACKEND_URL').and.returnValue(backendUrl);
+			const httpServiceSpy = spyOn(httpService, 'fetch').withArgs(expectedArgs0, expectedArgs1).and.returnValue(Promise.resolve(
+				new Response(
+					JSON.stringify([
+						wmsDefinition, wmtsDefinition, vectorDefinition, aggregateDefinition
+					])
+				)
+			));
 
-			expect(service._georesources).toBeNull();
+			expect(instanceUnderTest._georesources).toBeNull();
 
-			await service.init();
+			const georesources = await instanceUnderTest.init();
 
-			expect(service._georesources).not.toBeNull();
+			expect(configServiceSpy).toHaveBeenCalled();
+			expect(httpServiceSpy).toHaveBeenCalled();
+			expect(georesources.length).toBe(4);
+
+			const wmsGeoResource = georesources[0];
+			vadlidateGeoResourceProperties(wmsGeoResource, wmsDefinition);
+			expect(wmsGeoResource.url).toBe(wmsDefinition.url);
+			expect(wmsGeoResource.layers).toBe(wmsDefinition.layers);
+			expect(wmsGeoResource.format).toBe(wmsDefinition.format);
+
+			const wmtsGeoResource = georesources[1];
+			vadlidateGeoResourceProperties(wmtsGeoResource, wmtsDefinition);
+			expect(wmtsGeoResource.url).toBe(wmtsGeoResource.url);
+
+			const vectorGeoResource = georesources[2];
+			vadlidateGeoResourceProperties(vectorGeoResource, vectorDefinition);
+			expect(vectorGeoResource.url).toBe(vectorDefinition.url);
+			expect(Symbol.keyFor(vectorGeoResource.sourceType)).toBe(vectorDefinition.sourceType);
+
+			const aggregateGeoResource = georesources[3];
+			vadlidateGeoResourceProperties(aggregateGeoResource, aggregateDefinition);
+			expect(aggregateGeoResource.geoResourceIds).toEqual(aggregateDefinition.geoResourceIds);
+
+		});
+
+
+		it('just provides GeoResources when already initialized', async () => {
+
+			instanceUnderTest._georesources = [wmtsGeoResource];
+
+			const georesources = await instanceUnderTest.init();
+
+			expect(georesources.length).toBe(1);
+		});
+
+		it('logs a war statememt when GeoResource type cannot be resolved', async() => {
+
+			const warnSpy = spyOn(console, 'warn');
+			const backendUrl = 'https://backend.url';
+			spyOn(configService, 'getValue').and.returnValue(backendUrl);
+			spyOn(httpService, 'fetch').and.returnValue(Promise.resolve(
+				new Response(
+					JSON.stringify([
+						{ id: 'someId', type: 'somethingUnknown' }
+					])
+				)
+			));
+
+
+			await instanceUnderTest.init();
+
+			expect(warnSpy).toHaveBeenCalledWith('Could not create a GeoResource  for someId');
+		});
+
+		it('rejects when backend is not available', (done) => {
+
+			const backendUrl = 'https://backend.url';
+			const expectedArgs0 = backendUrl + 'georesources';
+			const expectedArgs1 = {
+				timeout: 2000,
+				mode: 'cors'
+			};
+			const configServiceSpy = spyOn(configService, 'getValue').withArgs('BACKEND_URL').and.returnValue(backendUrl);
+			const httpServiceSpy = spyOn(httpService, 'fetch').withArgs(expectedArgs0, expectedArgs1).and.returnValue(Promise.resolve(
+				new Response(null, { status: 404 })
+			));
+
+			expect(instanceUnderTest._georesources).toBeNull();
+
+			instanceUnderTest.init().then(() => {
+				done(new Error('Promise should not be resolved'));
+			}, (reason) => {
+				expect(instanceUnderTest._georesources).toBeNull();
+				expect(configServiceSpy).toHaveBeenCalled();
+				expect(httpServiceSpy).toHaveBeenCalled();
+				expect(reason).toBe('GeoResourceService could not be initialized: GeoResources could not be loaded');
+				done();
+			});
+
 		});
 	});
 
 	describe('all', () => {
 
-		it('provides all GeoResources', async () => {
-			const service = new BvvGeoResourceService();
+		it('provides all GeoResources', () => {
 
-			const geoResources = await service.all();
+			instanceUnderTest._georesources = [wmtsGeoResource];
 
-			expect(geoResources.length).toBe(6);
+			const geoResources = instanceUnderTest.all();
+
+			expect(geoResources.length).toBe(1);
+		});
+
+		it('throws an error when service hat not been initialized', () => {
+
+			expect(() => {
+				instanceUnderTest.all();
+			})
+				.toThrowError(/GeoResourceService not yet initialized/);
 		});
 	});
 
 	describe('byId', () => {
 
-		it('provides a GeoResource by id', async () => {
-			const service = new BvvGeoResourceService();
-			await service.init();
+		it('provides a GeoResource by id', () => {
 
-			const geoResource = service.byId('dop80');
+			instanceUnderTest._georesources = [wmtsGeoResource];
+
+
+			const geoResource = instanceUnderTest.byId('wmtsId');
 
 			expect(geoResource).toBeTruthy();
 		});
 
-		it('provides null if for an unknown id', async () => {
-			const service = new BvvGeoResourceService();
-			await service.init();
+		it('provides null if for an unknown id', () => {
 
-			const geoResource = service.byId('something');
+			instanceUnderTest._georesources = [wmtsGeoResource];
+
+			const geoResource = instanceUnderTest.byId('something');
 
 			expect(geoResource).toBeNull();
 		});
 
-		it('throws an error when service hat not been initialized', async () => {
-			const service = new BvvGeoResourceService();
+		it('throws an error when service hat not been initialized', () => {
 
 			expect(() => {
-				service.byId('unknownId');
+				instanceUnderTest.byId('unknownId');
 			})
 				.toThrowError(/GeoResourceService not yet initialized/);
+		});
+	});
+
+	describe('add', () => {
+
+		it('adds a GeoResource', async () => {
+
+			instanceUnderTest._georesources = [];
+			const geoResource = new WmsGeoResource('wms', 'Wms', 'https://some.url', 'someLayer', 'image/png');
+
+			let success = instanceUnderTest.add(geoResource);
+			expect(success).toBeTrue();
+
+			success = instanceUnderTest.add(geoResource);
+			expect(success).toBeFalse();
 		});
 	});
 });
