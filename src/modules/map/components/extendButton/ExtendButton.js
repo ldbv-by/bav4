9bcf093bf428e407df25894f134d153859bf63ba
Enes Bakir <bakir.enes@gmail.com>
2021-02-02 14:49:55 +0100

    dev: Extend Button: - initial commit

---
+++ src/modules/map/components/extendButton/ExtendButton.js
@@ -0,0 +1,52 @@
+import { html } from 'lit-html';
+import { BaElement } from '../../../BaElement';
+import css from './extendButton.css';
+import { $injector } from '../../../../injection';
+import arrowUpSvg from './assets/arrow-up.svg';
+
+/**
+ * Button that zooms map to extend
+ * @class 
+ * @author bakir_en  
+ */
+
+export class ExtendButton extends BaElement {
+
+	constructor() {
+		super();
+		const { TranslationService } = $injector.inject('TranslationService');
+		this._translationService = TranslationService;  
+	} 
+
+	/**
+     *@override 
+     */
+	createView() {
+        const translate = (key) => this._translationService.translate(key);
+        
+        const zoomExtend = () =>{
+            console.log('zoom to extend');
+        }; 
+
+		return html`
+            <style>${css}</style>
+            <div class="extend-button">
+                <ba-icon icon='${arrowUpSvg}' size=40 @click=${zoomExtend}></ba-icon>
+            </div>
+            
+        `;
+	} 
+
+	extractState(store) {
+		const { map: { zoom, position } } = store;
+		return { zoom, position };
+	}
+
+	onStateChanged() {
+		this.shadowRoot.getElementById('info-popup').closePopup();
+	}
+
+	static get tag() {
+		return 'ba-info-button';
+	} 
+} 
