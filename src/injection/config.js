980eb8d3873788d5e86ca200e63dddf34626672c
Thomas Aulinger <fam.aulinger@gmx.de>
2021-02-02 17:37:15 +0100

    remove deleted BvvSearchService

--- src/injection/config.js
+++ src/injection/config.js
@@ -2,7 +2,6 @@ import { $injector } from '.';
 import { StoreService } from '../services/StoreService';
 import { OlCoordinateService } from '../services/OlCoordinateService';
 import { EnvironmentService } from '../services/EnvironmentService';
-import { BvvSearchService } from '../modules/search/services/BvvSearchService';
 import { ProcessEnvConfigService } from '../services/ProcessEnvConfigService';
 import { HttpService } from '../services/HttpService';
 import { TranslationService } from '../services/TranslationService';
@@ -19,7 +18,6 @@ $injector
 	.register('CoordinateService', OlCoordinateService)
 	.registerSingleton('EnvironmentService', new EnvironmentService(window))
 	.registerSingleton('StoreService', new StoreService())
-	.register('SearchService', BvvSearchService)
 	.registerSingleton('GeoResourceService', new GeoResourceService(loadExampleGeoResources))
 	.registerSingleton('ShareService', new ShareService(navigator))
 	.register('UrlService', UrlService);
