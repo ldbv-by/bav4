6670e30e7daf027f8b8f64e62bd92e11f23e4f05
Thomas Aulinger <fam.aulinger@gmx.de>
2021-02-09 16:47:34 +0100

    refactor: one typedef file per type

--- src/services/domain/coordinateTypeDef.js
+++ src/services/domain/coordinateTypeDef.js
@@ -2,8 +2,3 @@
  * An array of numbers representing an xy coordinate. Example: `[16, 48]`.
  * @typedef {Array<number>} Coordinate
  */
-
-/**
- * An array of numbers representing an extent: `[minx, miny, maxx, maxy]`.
- * @typedef {Array<number>} Extent
- */
