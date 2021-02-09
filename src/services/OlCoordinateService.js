74d5cfeb6b4872598c1ce06e56baf9e96d164933
Thomas Aulinger <fam.aulinger@gmx.de>
2021-02-09 09:47:43 +0100

    add transformation methods for extents

--- src/services/OlCoordinateService.js
+++ src/services/OlCoordinateService.js
@@ -1,6 +1,16 @@
-import { fromLonLat, toLonLat } from 'ol/proj';
+import { fromLonLat, toLonLat, transformExtent } from 'ol/proj';
 import { createStringXY } from 'ol/coordinate';
 
+
+/**
+ * An array of numbers representing an xy coordinate. Example: `[16, 48]`.
+ * @typedef {Array<number>} Coordinate
+ */
+
+/**
+ * An array of numbers representing an extent: `[minx, miny, maxx, maxy]`.
+ * @typedef {Array<number>} Extent
+ */
 
 /**
  * Utilities for coordinates like transformation based on ol.
@@ -11,7 +21,8 @@ export class OlCoordinateService {
 
 	/**
 	 * Transforms a 3857 coordinate to longitude/latitude.
-	 * @param {*} coordinate3857 
+	 * @param {Coordinate} coordinate3857 
+	 * @public
 	 */
 	toLonLat(coordinate3857) {
 		return toLonLat(coordinate3857);
@@ -20,10 +31,29 @@ export class OlCoordinateService {
 
 	/**
 	 * Transforms a coordinate from longitude/latitude to 3857 coordinate
-	 * @param {*} coordinate4326 
+	 * @param {Coordinate} coordinate4326 
+	 * @public
 	 */
 	fromLonLat(coordinate4326) {
 		return fromLonLat(coordinate4326);
+	}
+
+	/**
+	 * Transforms an extent from 3857 to longitude/latitude
+	 * @param {Extent} extent3857 
+	 * @public
+	 */
+	toLonLatExtent(extent3857) {
+		return transformExtent(extent3857, 'EPSG:3857', 'EPSG:4326');
+	}
+
+	/**
+	 * Transforms an extent from longitude/latitude to 3857
+	 * @param {Extent} extent4326 
+	 * @public
+	 */
+	fromLonLatExtent(extent4326) {
+		return transformExtent(extent4326, 'EPSG:4326', 'EPSG:3857');
 	}
 
 	to25832() {
--- test/service/OlCoordinateService.test.js
+++ test/service/OlCoordinateService.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 import { OlCoordinateService } from '../../src/services/OlCoordinateService';
-import { fromLonLat, toLonLat } from 'ol/proj';
+import { fromLonLat, toLonLat, transformExtent } from 'ol/proj';
 
 describe('OlCoordinateService', () => {
 
@@ -11,7 +11,7 @@ describe('OlCoordinateService', () => {
 
 	describe('transforms coordinates', () => {
 
-		it('transforms from EPSG:4326 to EPSG:3857', () => {
+		it('from EPSG:4326 to EPSG:3857', () => {
 			const initialCooord4326 = [11.57245, 48.14021];
 			const coord3857 = fromLonLat(initialCooord4326);
 
@@ -21,7 +21,7 @@ describe('OlCoordinateService', () => {
 			expect(coord4326[1]).toBeCloseTo(initialCooord4326[1], 3);
 		});
 
-		it('transforms from EPSG:3857 to EPSG:4326', () => {
+		it('from EPSG:3857 to EPSG:4326', () => {
 			const initialCooord3857 = [1288239.2412306187, 6130212.561641981];
 			const coord4326 = toLonLat(initialCooord3857);
 
@@ -31,16 +31,43 @@ describe('OlCoordinateService', () => {
 			expect(coord3857[1]).toBeCloseTo(initialCooord3857[1], 3);
 		});
 
-		it('transforms from EPSG:3857 to EPSG:25832', () => {
+		it('from EPSG:3857 to EPSG:25832', () => {
 
 			expect(() => instanceUnderTest.to25832([1288239.2412306187, 6130212.561641981])).toThrowError(/Not yet implemented/);
 		});
 
-		it('transforms from EPSG:25832 to EPSG:3857', () => {
+		it('from EPSG:25832 to EPSG:3857', () => {
 
 			expect(() => instanceUnderTest.from25832([676696, 5367913])).toThrowError(/Not yet implemented/);
 		});
 
+	});
+
+	describe('transforms extents', () => {
+
+		it('from EPSG:4326 to EPSG:3857', () => {
+			const initialExtent4326 = [11.57245, 48.14021, 11.67245, 48.24021];
+			const extent3857 = transformExtent(initialExtent4326, 'EPSG:4326', 'EPSG:3857');
+
+			const extent4326 = instanceUnderTest.toLonLatExtent(extent3857);
+
+			expect(extent4326[0]).toBeCloseTo(initialExtent4326[0], 3);
+			expect(extent4326[1]).toBeCloseTo(initialExtent4326[1], 3);
+			expect(extent4326[2]).toBeCloseTo(initialExtent4326[2], 3);
+			expect(extent4326[3]).toBeCloseTo(initialExtent4326[3], 3);
+		});
+
+		it('transforms from EPSG:3857 to EPSG:4326', () => {
+			const initialExtent3857 =  [1288239.2412306187, 6130212.561641981, 1299371.190309946, 6146910.663709761];
+			const extent4326 = transformExtent(initialExtent3857, 'EPSG:3857', 'EPSG:4326');
+
+			const extent3857 = instanceUnderTest.fromLonLatExtent(extent4326);
+
+			expect(extent3857[0]).toBeCloseTo(initialExtent3857[0], 3);
+			expect(extent3857[1]).toBeCloseTo(initialExtent3857[1], 3);
+			expect(extent3857[2]).toBeCloseTo(initialExtent3857[2], 3);
+			expect(extent3857[3]).toBeCloseTo(initialExtent3857[3], 3);
+		});
 	});
 
 	describe('stringifies', () => {
