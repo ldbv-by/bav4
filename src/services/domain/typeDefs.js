ec42b05c79672b34931730a27b7c56bc16cffbb2
Thomas Aulinger <fam.aulinger@gmx.de>
2021-02-09 16:44:18 +0100

    add a typedef for coordinates/extent

---
+++ src/services/domain/typeDefs.js
@@ -0,0 +1,9 @@
+/**
+ * An array of numbers representing an xy coordinate. Example: `[16, 48]`.
+ * @typedef {Array<number>} Coordinate
+ */
+
+/**
+ * An array of numbers representing an extent: `[minx, miny, maxx, maxy]`.
+ * @typedef {Array<number>} Extent
+ */
