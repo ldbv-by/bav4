651d5e28f6f61e4d5f9bd7f50078d0c4267a484a
Thomas Aulinger <fam.aulinger@gmx.de>
2021-02-17 19:44:17 +0100

    display the content element

--- src/modules/map/components/contextMenue/MapContextMenue.js
+++ src/modules/map/components/contextMenue/MapContextMenue.js
@@ -2,6 +2,10 @@ import { html, nothing } from 'lit-html';
 import { styleMap } from 'lit-html/directives/style-map.js';
 import { BaElement } from '../../../BaElement';
 import css from './mapContextMenue.css';
+import { $injector } from '../../../../injection';
+import { close as closeContextMenue } from '../../store/mapContextMenue.action';
+
+import closeIcon from './assets/x-square.svg';
 
 /**
  * 
@@ -9,11 +13,20 @@ import css from './mapContextMenue.css';
  */
 export class MapContextMenue extends BaElement {
 
+	constructor() {
+		super();
+		const {
+			TranslationService: translastionService
+		} = $injector.inject('TranslationService');
 
-	_calculateParameter(eventCoordinate, element) {
+		this._translationService = translastionService;
+	}
+
+
+	_calculateParameter(coordinate, element) {
 		//alignment: location of the event coordinate relative to the menue
 		const values = { left: 0, top: 0, vAlignment: 'left', hAlignment: 'top' };
-		if (!element || !eventCoordinate) {
+		if (!element || !coordinate) {
 			return values;
 		}
 
@@ -25,21 +38,21 @@ export class MapContextMenue extends BaElement {
 		const windowWidth = window.innerWidth - (window.innerWidth * offsetBorderInPercent);
 		const windowHeight = window.innerHeight - (window.innerHeight * offsetBorderInPercent);
 
-		if ((windowWidth - eventCoordinate.x) < menuWidth) {
+		if ((windowWidth - coordinate[0]) < menuWidth) {
 			values.vAlignment = 'right';
-			values.left = eventCoordinate.x - menuWidth + 'px';
+			values.left = coordinate[0] - menuWidth + 'px';
 		}
 		else {
 			values.vAlignment = 'left';
-			values.left = eventCoordinate.x + 'px';
+			values.left = coordinate[0] + 'px';
 		}
-		if ((windowHeight - eventCoordinate.y) < menuHeight) {
+		if ((windowHeight - coordinate[1]) < menuHeight) {
 			values.hAlignment = 'bottom';
-			values.top = eventCoordinate.y - menuHeight + 'px';
+			values.top = coordinate[1] - menuHeight + 'px';
 		}
 		else {
 			values.hAlignment = 'top';
-			values.top = eventCoordinate.y + vOffset + 'px';
+			values.top = coordinate[1] + vOffset + 'px';
 		}
 		return values;
 	}
@@ -48,22 +61,33 @@ export class MapContextMenue extends BaElement {
 	 * @override
 	 */
 	createView() {
-		const { eventCoordinate } = this._state;
-		const parameters = this._calculateParameter(eventCoordinate, this._root.querySelector('.context-menue'));
+		const translate = (key) => this._translationService.translate(key);
+		const { coordinate, id } = this._state;
+
+		const parameters = this._calculateParameter(coordinate, this._root.querySelector('.context-menue'));
 		const style = { left: parameters.left, top: parameters.top };
 		const getClasses = () => {
 			const classes = [];
-			if (eventCoordinate) {
+			if (coordinate) {
 				classes.push('active');
 			}
 			classes.push(parameters.hAlignment + '-' + parameters.vAlignment);
 			return classes.join(' ');
 		};
+
+
+		//get content element
+		const content = coordinate ? document.getElementById(id) : nothing;
+
+		//extract content element from the dom and render it here 
+		//see: https://lit-html.polymer-project.org/guide/template-reference#supported-data-types-for-text-bindings -> Node
 		return html`
         <style>${css}</style>
-		<div  class='context-menue $ ${getClasses()}' style=${styleMap(style)} >
-			${eventCoordinate ? html`content renders here...<br>and here...<br>and here...` : nothing}
+		<div class='context-menue $ ${getClasses()}' style=${styleMap(style)}>
+			<div class='header'>${translate('map_context_menue_header')}<ba-icon class='close' icon='${closeIcon}' title=${translate('map_context_menue_close_button')} size=20 color='white'} @click=${closeContextMenue}></ba-icon></div>
+			${content}
         </div>`;
+
 	}
 
 	/**
@@ -71,10 +95,9 @@ export class MapContextMenue extends BaElement {
 	 * @param {Object} store 
 	 */
 	extractState(store) {
-		const { mapContextMenue: { eventCoordinate, data } } = store;
-		return { eventCoordinate, data };
+		const { mapContextMenue: { coordinate, id } } = store;
+		return { coordinate, id };
 	}
-
 
 	static get tag() {
 		return 'ba-map-context-menue';
