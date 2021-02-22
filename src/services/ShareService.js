73010f6802829a414f525f0a02298d1e2dc81cdd
Thomas Aulinger <fam.aulinger@gmx.de>
2021-02-22 17:44:34 +0100

    Take account of Clipboard API availabiltity

--- src/services/ShareService.js
+++ src/services/ShareService.js
@@ -1,9 +1,17 @@
 export class ShareService {
-	constructor(_navigator = window.navigator) {
-		this._navigator = _navigator;
+	constructor(_window = window) {
+		this._window = _window;
 	}
 
+	/**
+	 * 
+	 * @param {string} textToCopy 
+	 * @returns {Promise<undefined> | Promise.reject}
+	 */
 	copyToClipboard(textToCopy) {
-		return this._navigator.clipboard.writeText(textToCopy);
+		if (this._window.isSecureContext) {
+			return this._window.navigator.clipboard.writeText(textToCopy);
+		}
+		return Promise.reject(new Error('Clipboard API is not available'));
 	}
 } 
