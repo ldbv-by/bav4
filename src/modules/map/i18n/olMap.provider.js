e86ab690af7969d9f62c8790229f1e5290ed9a94
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-02-23 10:37:44 +0100

    fix olMap.provider-keys

--- src/modules/map/i18n/olMap.provider.js
+++ src/modules/map/i18n/olMap.provider.js
@@ -4,39 +4,39 @@ export const provide = (lang) => {
 		case 'en':
 			return {
 				//the first part of the snake_case key should be the name of the related module
-				map_zoom_in_button: 'Zoom in',
-				map_zoom_out_button: 'Zoom out',
-				map_info_button: 'Information',
-				map_info_button_help: 'Help',
-				map_info_button_contact: 'Contact',
-				map_info_button_about: 'About us',
-				map_zoom_extent_button: 'Zoom to full extent',
-				map_context_menu_content_icon: 'Copy to clipboard',
-				map_measure_start:'Click to start measurement',
-				map_measure_continue_line: 'Click to continue drawing the line (double-click to finish)',
-				map_measure_continue_polygon:'Click to continue drawing the polygon (double-click to finish)',
-				map_measure_snap_first_point:'Click to close the surface',
-				map_measure_snap_last_point:'Click to finish the line',
-				map_delete_last_point:'Press DEL to remove the last point drawn'
+				map_olMap_zoom_in_button: 'Zoom in',
+				map_olMap_zoom_out_button: 'Zoom out',
+				map_olMap_info_button: 'Information',
+				map_olMap_info_button_help: 'Help',
+				map_olMap_info_button_contact: 'Contact',
+				map_olMap_info_button_about: 'About us',
+				map_olMap_zoom_extent_button: 'Zoom to full extent',
+				map_olMap_contextMenu_content_icon: 'Copy to clipboard',
+				map_olMap_handler_measure_start:'Click to start measurement',
+				map_olMap_handler_measure_continue_line: 'Click to continue drawing the line (double-click to finish)',
+				map_olMap_handler_measure_continue_polygon:'Click to continue drawing the polygon (double-click to finish)',
+				map_olMap_handler_measure_snap_first_point:'Click to close the surface',
+				map_olMap_handler_measure_snap_last_point:'Click to finish the line',
+				map_olMap_handler_delete_last_point:'Press DEL to remove the last point drawn'
 			};
 
 		case 'de':
 			return {
 				//the first part of the snake_case key should be the name of the related module
-				map_zoom_in_button: 'Vergrößere Kartenausschnitt',
-				map_zoom_out_button: 'Verkleinere Kartenausschnitt',
-				map_info_button: 'Information',
-				map_info_button_help: 'Hilfe',
-				map_info_button_contact: 'Kontakt',
-				map_info_button_about: 'Impressum',
-				map_zoom_extent_button: 'Ganz Bayern anzeigen',
-				map_context_menu_content_icon: 'In die Zwischenablage kopieren',
-				map_measure_start:'Klicken, um die Messung zu beginnen',
-				map_measure_continue_line: 'Klicken, um die Messlinie zu zeichnen (Doppelklick zum Beenden)',			
-				map_measure_continue_polygon:'Klicken, um die Fläche zu zeichnen (Doppelklick zum Beenden)',
-				map_measure_snap_first_point:'Klicke, um die Fläche zu schliessen',
-				map_measure_snap_last_point:'Klicke, um die Messung abzuschliessen',
-				map_delete_last_point:'Letzter Punkt löschen: ENTF Taste'
+				map_olMap_zoom_in_button: 'Vergrößere Kartenausschnitt',
+				map_olMap_zoom_out_button: 'Verkleinere Kartenausschnitt',
+				map_olMap_info_button: 'Information',
+				map_olMap_info_button_help: 'Hilfe',
+				map_olMap_info_button_contact: 'Kontakt',
+				map_olMap_info_button_about: 'Impressum',
+				map_olMap_zoom_extent_button: 'Ganz Bayern anzeigen',
+				map_olMap_contextMenu_content_icon: 'In die Zwischenablage kopieren',
+				map_olMap_handler_measure_start:'Klicken, um die Messung zu beginnen',
+				map_olMap_handler_measure_continue_line: 'Klicken, um die Messlinie zu zeichnen (Doppelklick zum Beenden)',			
+				map_olMap_handler_measure_continue_polygon:'Klicken, um die Fläche zu zeichnen (Doppelklick zum Beenden)',
+				map_olMap_handler_measure_snap_first_point:'Klicke, um die Fläche zu schliessen',
+				map_olMap_handler_measure_snap_last_point:'Klicke, um die Messung abzuschliessen',
+				map_olMap_handler_delete_last_point:'Letzter Punkt löschen: ENTF Taste'
 			};
 
 		default:
--- test/modules/map/i18n/olMap.provider.test.js
+++ test/modules/map/i18n/olMap.provider.test.js
@@ -7,39 +7,39 @@ describe('i18n for map module', () => {
 
 		const map = provide('de');
 
-		expect(map.map_zoom_in_button).toBe('Vergrößere Kartenausschnitt');
-		expect(map.map_zoom_out_button).toBe('Verkleinere Kartenausschnitt');
-		expect(map.map_info_button).toBe('Information');
-		expect(map.map_info_button_help).toBe('Hilfe');
-		expect(map.map_info_button_contact).toBe('Kontakt');
-		expect(map.map_info_button_about).toBe('Impressum');
-		expect(map.map_zoom_extent_button).toBe('Ganz Bayern anzeigen');
-		expect(map.map_context_menu_content_icon).toBe('In die Zwischenablage kopieren');
-		expect(map.map_measure_start).toBe('Klicken, um die Messung zu beginnen');
-		expect(map.map_measure_continue_line).toBe('Klicken, um die Messlinie zu zeichnen (Doppelklick zum Beenden)');
-		expect(map.map_measure_continue_polygon).toBe('Klicken, um die Fläche zu zeichnen (Doppelklick zum Beenden)');
-		expect(map.map_measure_snap_first_point).toBe('Klicke, um die Fläche zu schliessen');
-		expect(map.map_measure_snap_last_point).toBe('Klicke, um die Messung abzuschliessen');
-		expect(map.map_delete_last_point).toBe('Letzter Punkt löschen: ENTF Taste');
+		expect(map.map_olMap_zoom_in_button).toBe('Vergrößere Kartenausschnitt');
+		expect(map.map_olMap_zoom_out_button).toBe('Verkleinere Kartenausschnitt');
+		expect(map.map_olMap_info_button).toBe('Information');
+		expect(map.map_olMap_info_button_help).toBe('Hilfe');
+		expect(map.map_olMap_info_button_contact).toBe('Kontakt');
+		expect(map.map_olMap_info_button_about).toBe('Impressum');
+		expect(map.map_olMap_zoom_extent_button).toBe('Ganz Bayern anzeigen');
+		expect(map.map_olMap_contextMenu_content_icon).toBe('In die Zwischenablage kopieren');
+		expect(map.map_olMap_handler_measure_start).toBe('Klicken, um die Messung zu beginnen');
+		expect(map.map_olMap_handler_measure_continue_line).toBe('Klicken, um die Messlinie zu zeichnen (Doppelklick zum Beenden)');
+		expect(map.map_olMap_handler_measure_continue_polygon).toBe('Klicken, um die Fläche zu zeichnen (Doppelklick zum Beenden)');
+		expect(map.map_olMap_handler_measure_snap_first_point).toBe('Klicke, um die Fläche zu schliessen');
+		expect(map.map_olMap_handler_measure_snap_last_point).toBe('Klicke, um die Messung abzuschliessen');
+		expect(map.map_olMap_handler_delete_last_point).toBe('Letzter Punkt löschen: ENTF Taste');
 	});
 
 	it('provides translation for en', () => {
 
 		const map = provide('en');
 
-		expect(map.map_zoom_in_button).toBe('Zoom in');
-		expect(map.map_zoom_out_button).toBe('Zoom out');
-		expect(map.map_info_button).toBe('Information');
-		expect(map.map_info_button_help).toBe('Help');
-		expect(map.map_info_button_contact).toBe('Contact');
-		expect(map.map_info_button_about).toBe('About us');
-		expect(map.map_context_menu_content_icon).toBe('Copy to clipboard');		
-		expect(map.map_measure_start).toBe('Click to start measurement');
-		expect(map.map_measure_continue_line).toBe('Click to continue drawing the line (double-click to finish)');
-		expect(map.map_measure_continue_polygon).toBe('Click to continue drawing the polygon (double-click to finish)');
-		expect(map.map_measure_snap_first_point).toBe('Click to close the surface');
-		expect(map.map_measure_snap_last_point).toBe('Click to finish the line');
-		expect(map.map_delete_last_point).toBe('Press DEL to remove the last point drawn');
+		expect(map.map_olMap_zoom_in_button).toBe('Zoom in');
+		expect(map.map_olMap_zoom_out_button).toBe('Zoom out');
+		expect(map.map_olMap_info_button).toBe('Information');
+		expect(map.map_olMap_info_button_help).toBe('Help');
+		expect(map.map_olMap_info_button_contact).toBe('Contact');
+		expect(map.map_olMap_info_button_about).toBe('About us');
+		expect(map.map_olMap_contextMenu_content_icon).toBe('Copy to clipboard');		
+		expect(map.map_olMap_handler_measure_start).toBe('Click to start measurement');
+		expect(map.map_olMap_handler_measure_continue_line).toBe('Click to continue drawing the line (double-click to finish)');
+		expect(map.map_olMap_handler_measure_continue_polygon).toBe('Click to continue drawing the polygon (double-click to finish)');
+		expect(map.map_olMap_handler_measure_snap_first_point).toBe('Click to close the surface');
+		expect(map.map_olMap_handler_measure_snap_last_point).toBe('Click to finish the line');
+		expect(map.map_olMap_handler_delete_last_point).toBe('Press DEL to remove the last point drawn');
 	});
 
 	it('provides an empty map for a unknown lang', () => {
