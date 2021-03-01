47dd002f4b1740c09c9f7428c037fbf38380d2e9
Thomas Aulinger <fam.aulinger@gmx.de>
2021-03-01 11:47:13 +0100

    register OlGeolocationHandler on OlMap

--- src/modules/map/services/index.js
+++ src/modules/map/services/index.js
@@ -1,8 +1,10 @@
 import { OlContextMenuEventHandler } from '../components/olMap/handler/contextMenu/OlContextMenuEventHandler';
 import { OlMeasurementHandler } from '../components/olMap/handler/measure/OlMeasurementHandler';
+import { OlGeolocationHandler } from '../components/olMap/handler/geolocation/OlGeolocationHandler';
 
 export const mapModule = ($injector) => {
 	$injector
 		.register('OlContextMenueMapEventHandler', OlContextMenuEventHandler)
-		.register('OlMeasurementHandler', OlMeasurementHandler);
+		.register('OlMeasurementHandler', OlMeasurementHandler)
+		.register('OlGeolocationHandler', OlGeolocationHandler);
 };
