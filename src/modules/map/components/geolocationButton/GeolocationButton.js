99ea366181274d5081b500c4455103ea0191d776
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-03-04 11:13:00 +0100

    update internal button-structure and behavior
    update internal button-structure from usage of custom component
    'ba-icon' to standard html-components, to implement behavior
    caused by user and statestore-changes and to create a custon
    button-design similar to 'ba-extent-button'
    
    add tests for complete test-coverage

--- src/modules/map/components/geolocationButton/GeolocationButton.js
+++ src/modules/map/components/geolocationButton/GeolocationButton.js
@@ -1,8 +1,8 @@
 import { html } from 'lit-html';
 import { BaElement } from '../../../BaElement';
-import geolocationSvg from './assets/geo-alt-fill.svg';
 import { $injector } from '../../../../injection';
 import css from './geolocationButton.css';
+import { classMap } from 'lit-html/directives/class-map.js';
 import { activate, deactivate } from '../../store/geolocation.action';
 
 /**
@@ -25,16 +25,35 @@ export class GeolocationButton extends BaElement {
 	createView() {
 		const { active, denied } = this._state;
 		const translate = (key) => this._translationService.translate(key);
-
 		const onClick = () => {
 			if (active) {
 				deactivate();
 			}
-			activate();
+			else {
+				activate();
+			}			
 		};
-		return html`<style>${css}</style>
-		<ba-icon icon='${geolocationSvg}' size=40 disabled=${denied} title=${translate('map_geolocationButton_title_activate')} @click=${onClick}></ba-icon>
-        `;
+
+		let title = translate('map_geolocationButton_title_activate');
+		if (active) {
+			title = translate('map_geolocationButton_title_deactivate');
+		}
+		else if (denied) {
+			title = translate('map_geolocationButton_title_denied');
+		}
+
+		const classes = {
+			inactive:!active,
+			active: active,
+			denied: denied,
+		};
+		return html`
+		<style>${css}</style> 
+		<div class='geolocation'>
+			<button class='geolocation-button' @click=${onClick} title=${title} >
+			<i class="icon geolocation-icon ${classMap(classes)}"></i></button>
+		</div>
+		`;
 	}
 
 	extractState(store) {
