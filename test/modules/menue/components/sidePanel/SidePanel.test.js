0ff440ecf6964962aa30daf39d419a58ee16f7bb
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-03-04 15:15:26 +0100

    move testcase to folder
    move testcase to folder to reflect location of
    ClassUnderTest 'SidePanel.js'

--- test/modules/menue/components/sidePanel/SidePanel.test.js
+++ test/modules/menue/components/sidePanel/SidePanel.test.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
 
-import { SidePanel } from '../../../../src/modules/menue/components/sidePanel/SidePanel';
-import { sidePanelReducer } from '../../../../src/modules/menue/store/sidePanel.reducer';
-import { toggleSidePanel } from '../../../../src/modules/menue/store/sidePanel.action';
-import { TestUtils } from '../../../test-utils';
-import { $injector } from '../../../../src/injection';
+import { SidePanel } from '../../../../../src/modules/menue/components/sidePanel/SidePanel';
+import { sidePanelReducer } from '../../../../../src/modules/menue/store/sidePanel.reducer';
+import { toggleSidePanel } from '../../../../../src/modules/menue/store/sidePanel.action';
+import { TestUtils } from '../../../../test-utils';
+import { $injector } from '../../../../../src/injection';
 
 window.customElements.define(SidePanel.tag, SidePanel);
 
