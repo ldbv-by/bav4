32a21e660cb10b28cc282ed4b4ed1560873f1b77
Thomas Aulinger <fam.aulinger@gmx.de>
2021-03-05 08:42:47 +0100

    improve docs

--- src/modules/map/store/position.action.js
+++ src/modules/map/store/position.action.js
@@ -1,5 +1,5 @@
 /**
- * Action creators to change/update the properties concerning the resolution and center of a map.
+ * Action creators to change/update the properties concerning the zoom level and center of a map.
  * @module map/action
  */
 import { ZOOM_CHANGED, CENTER_CHANGED, ZOOM_CENTER_CHANGED, FIT_REQUESTED } from './position.reducer';
@@ -10,7 +10,7 @@ import { EventLike } from '../../../utils/storeUtils';
 /**
  * Request for fitting a map to a geographic extent
  * @typedef {Object} FitRequest
- * @property {Extent} extent geographic extent
+ * @property {Extent} extent geographic extent in map projection
  * @property {FitRequestOptions} options options for this FitRequest
  */
 
@@ -20,6 +20,12 @@ import { EventLike } from '../../../utils/storeUtils';
  * @property {maxZoom} maxZoom max zoom level that is set even if extent would result in a higher zoom level
  */
 
+/**
+* A combination of zoom and center.
+* @typedef {Object} ZoomCenter
+* @property {number} zoom zoom level
+* @property {Coordinate} coordinate coordinate in map projection
+*/
 
 const getStore = () => {
 	const { StoreService } = $injector.inject('StoreService');
@@ -28,6 +34,7 @@ const getStore = () => {
 
 /**
  * Changes zoom level and the position.
+ * @param {ZoomCenter} zoomCenter zoom and center
  * @function
  */
 export const changeZoomAndCenter = (zoomCenter) => {
@@ -39,6 +46,7 @@ export const changeZoomAndCenter = (zoomCenter) => {
 
 /**
  * Changes zoom level.
+ * @param {number} zoom zoom level
  * @function
  */
 export const changeZoom = (zoom) => {
@@ -80,6 +88,7 @@ export const decreaseZoom = () => {
 
 /**
  * Changes the center.
+ * @param {coordinate} center coordinate in map projection
  * @function
  */
 export const changeCenter = (center) => {
