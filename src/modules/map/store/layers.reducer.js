1f28d3ea1381ac930d3054c0bd075ebfcaf6eb55
Thomas Aulinger <fam.aulinger@gmx.de>
2021-03-09 17:14:23 +0100

    remove BACKGROUND_CHANGED action

--- src/modules/map/store/layers.reducer.js
+++ src/modules/map/store/layers.reducer.js
@@ -1,7 +1,6 @@
 export const LAYER_ADDED = 'layer/added';
 export const LAYER_REMOVED = 'layer/removed';
 export const LAYER_MODIFIED = 'layer/modified';
-export const BACKGROUND_CHANGED = 'background/changed';
 
 
 export const initialState = {
@@ -121,13 +120,6 @@ export const layersReducer = (state = initialState, action) => {
 		}
 		case LAYER_MODIFIED: {
 			return modifyLayer(state, payload);
-		}
-		case BACKGROUND_CHANGED: {
-
-			return {
-				...state,
-				background: payload
-			};
 		}
 	}
 
