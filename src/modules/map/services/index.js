df451fd4c55cd37255705741bac4a9b7b29d19c5
Thomas Aulinger <fam.aulinger@gmx.de>
2021-03-12 17:02:18 +0100

    register observer

--- src/modules/map/services/index.js
+++ src/modules/map/services/index.js
@@ -1,9 +1,13 @@
+import { LayersObserver } from '../store/LayersObserver';
+import { PositionObserver } from '../store/PositionObserver';
 import { OlContextMenuEventHandler } from '../components/olMap/handler/contextMenu/OlContextMenuEventHandler';
 import { OlMeasurementHandler } from '../components/olMap/handler/measure/OlMeasurementHandler';
 import { OlGeolocationHandler } from '../components/olMap/handler/geolocation/OlGeolocationHandler';
 
 export const mapModule = ($injector) => {
 	$injector
+		.registerSingleton('LayersObserver', new LayersObserver())
+		.registerSingleton('PositionObserver', new PositionObserver())
 		.register('OlContextMenueMapEventHandler', OlContextMenuEventHandler)
 		.register('OlMeasurementHandler', OlMeasurementHandler)
 		.register('OlGeolocationHandler', OlGeolocationHandler);
