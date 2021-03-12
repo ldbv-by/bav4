f8bcf8b5adaa21812fd66161064c35e06028d085
Thomas Aulinger <fam.aulinger@gmx.de>
2021-03-12 12:31:50 +0100

    add enum which holds all valid query parameter keys

---
+++ test/service/domain/queryParameters.test.js
@@ -0,0 +1,14 @@
+import { QueryParameters } from '../../../src/services/domain/queryParameters';
+
+describe('QueryParameters', () => {
+
+	it('provides an enum of all valid query parameters', () => {
+		expect(Object.keys(QueryParameters).length).toBe(5);
+		
+		expect(QueryParameters.CENTER).toBe('c');
+		expect(QueryParameters.ZOOM).toBe('z');
+		expect(QueryParameters.LAYER).toBe('l');
+		expect(QueryParameters.LAYER_VISIBILITY).toBe('l_v');
+		expect(QueryParameters.LAYER_OPACITY).toBe('l_o');
+	});
+});
