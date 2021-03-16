aeaa9e69457c39adf77eca5917ecf6339b10bcd6
Thomas Aulinger <fam.aulinger@gmx.de>
2021-03-16 10:05:01 +0100

    add a type check on all methods

--- src/services/UrlService.js
+++ src/services/UrlService.js
@@ -1,4 +1,6 @@
 import { $injector } from '../injection';
+import { isString } from '../utils/checks';
+import { shortenBvvUrls } from './provider/urlShorteningProvider';
 
 const defaultProxyTemplateProvider = (proxyUrl, url) => {
 	return `${proxyUrl}?url=${url}`;
@@ -11,11 +13,12 @@ const defaultProxyTemplateProvider = (proxyUrl, url) => {
  */
 export class UrlService {
 
-	constructor(templateProvider = defaultProxyTemplateProvider) {
+	constructor(urlShorteningProvider = shortenBvvUrls, templateProvider = defaultProxyTemplateProvider) {
 		const { HttpService: httpService, ConfigService: configService } = $injector.inject('HttpService', 'ConfigService');
 		this._httpService = httpService;
 		this._proxyUrl = configService.getValueAsPath('PROXY_URL');
 		this._templateProvider = templateProvider;
+		this._urlShorteningProvider = urlShorteningProvider;
 	}
 
 	/**
@@ -23,35 +26,57 @@ export class UrlService {
 	* @param {string} url 
 	*/
 	proxifyInstant(url) {
+		if (!isString(url)) {
+			throw new TypeError('Parameter \'url\' must be a string');
+		}
 		return this._templateProvider(this._proxyUrl, encodeURIComponent(url));
 	}
 
 	/**
 	* Proxifies a url when needed.
 	* @param {string} url
-	* @async
 	* @public
 	*/
 	async proxify(url) {
-		const corsEnabled = await this.isCorsEnabled();
+		if (!isString(url)) {
+			throw new TypeError('Parameter \'url\' must be a string');
+		}
+		const corsEnabled = await this.isCorsEnabled(url);
 		if (corsEnabled) {
 			return url;
 		}
 		return this._templateProvider(this._proxyUrl, encodeURIComponent(url));
 	}
 
+
 	/**
 	* Tests if the remote resource enables CORS by using a head request
 	* @param {string} url
-	* @async 
 	* @public
 	*/
 	async isCorsEnabled(url) {
+		if (!isString(url)) {
+			throw new TypeError('Parameter \'url\' must be a string');
+		}
+
 		const result = await this._httpService.fetch(url, {
 			timeout: 1500,
-			method: 'HEAD',
-			mode: 'cors'
+			method: 'HEAD'
 		});
 		return result.ok;
+
+	}
+
+	/**
+	 * Shortens an url.
+	 * @param {string} url 
+	 * @async 
+	 * @public
+	 */
+	async shorten(url) {
+		if (!isString(url)) {
+			throw new TypeError('Parameter \'url\' must be a string');
+		}
+		return this._urlShorteningProvider(url);
 	}
 }
