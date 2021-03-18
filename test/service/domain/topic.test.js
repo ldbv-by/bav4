fe7f6fa070f727c9bd055a211eba8a45aac5c446
Thomas Aulinger <fam.aulinger@gmx.de>
2021-03-18 15:36:29 +0100

    add a default value for 'defaultBackground'

--- test/service/domain/topic.test.js
+++ test/service/domain/topic.test.js
@@ -4,7 +4,7 @@ import { Topic } from '../../../src/services/domain/topic';
 describe('Topic', () => {
 
 	it('provides setter for properties', () => {
-		const topic = new Topic('id', 'label', 'description', 'bg0', ['bg0', 'bg1'], ['ac0', 'ac2'], ['sel0', 'sel2']);
+		const topic = new Topic('id', 'label', 'description', ['bg0', 'bg1'], 'bg0', ['ac0', 'ac2'], ['sel0', 'sel2']);
 
 		expect(topic.id).toBe('id');
 		expect(topic.label).toBe('label');
@@ -16,9 +16,10 @@ describe('Topic', () => {
 	});
 
 	it('provides default properties', () => {
-		const topic = new Topic('id', 'label', ['bg0', 'bg1']);
+		const topic = new Topic('id', 'label', 'description', ['bg0', 'bg1']);
 
-		expect(topic.activatedLayers).toEqual([]);
+		expect(topic.defaultBackground).toBe('bg0');
+		expect(topic.selectedLayers).toEqual([]);
 		expect(topic.selectedLayers).toEqual([]);
 	});
 });
