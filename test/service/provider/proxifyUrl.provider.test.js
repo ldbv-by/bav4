379701c7f5bf72ed21b04fc566c5e194abf81de1
Thomas Aulinger <fam.aulinger@gmx.de>
2021-03-19 11:21:48 +0100

    refactor proxify url mechanism

---
+++ test/service/provider/proxifyUrl.provider.test.js
@@ -0,0 +1,26 @@
+import { $injector } from '../../../src/injection';
+import { bvvProxifyUrlProvider } from '../../../src/services/provider/proxifyUrl.provider';
+
+describe('proxyUrlTemplate', () => {
+
+
+	describe('bvv', () => {
+
+		const configService = {
+			getValueAsPath: () => {
+				return 'https://proxy.url';
+			}
+		};
+		beforeAll(() => {
+			$injector
+				.registerSingleton('ConfigService', configService);
+		});
+
+
+		it('it returns BVV specific proxifiedUrl', () => {
+			const proxifiedUrl = bvvProxifyUrlProvider('https://some.one');
+
+			expect(proxifiedUrl).toBe('https://proxy.url?url=https%3A%2F%2Fsome.one');
+		});
+	});
+});
