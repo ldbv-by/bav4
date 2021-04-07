4522c97d75e8e0ab3b4a52356141094058a88f0c
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-04-07 09:06:58 +0200

    fix i18n issue in menu-module

--- src/modules/menu/i18n/index.js
+++ src/modules/menu/i18n/index.js
@@ -1,4 +1,4 @@
 import { provide as menuProvider } from './menu.provider';
 import { $injector } from '../../../injection';
 const { TranslationService: translationService } = $injector.inject('TranslationService');
-translationService.register('toolboxProvider', menuProvider);
+translationService.register('menuProvider', menuProvider);
