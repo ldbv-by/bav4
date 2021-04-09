b019b22f22603f27cbce3d874aab117740f35823
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-04-09 08:33:47 +0200

    Add style for measure in toolbar menu

--- src/modules/menu/i18n/menu.provider.js
+++ src/modules/menu/i18n/menu.provider.js
@@ -5,14 +5,16 @@ export const provide = (lang) => {
 			return {
 				//the first part of the snake_case key should be the name of the related module
 				menu_toolbox_draw_button: 'Draw',
-				menu_toolbox_share_button: 'Share'
+				menu_toolbox_share_button: 'Share',
+				menu_toolbox_measure_button: 'Measure'
 			};
 
 		case 'de':
 			return {
 				//the first part of the snake_case key should be the name of the related module
 				menu_toolbox_draw_button: 'Zeichnen',
-				menu_toolbox_share_button: 'Teilen'
+				menu_toolbox_share_button: 'Teilen',
+				menu_toolbox_measure_button: 'Messen'
 			};
 
 		default:
