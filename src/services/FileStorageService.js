fd2a4c8d99e8777d9035ad9a84115f0e1b8437c2
Thomas Aulinger <taulinger@gmx.de>
2021-04-14 17:50:33 +0200

    add a service for persisting and loading ASCII based geodata.

---
+++ src/services/FileStorageService.js
@@ -0,0 +1,113 @@
+import { $injector } from '../injection';
+
+
+/**
+ * Service for persisting and loading ASCII based geodata.
+ * @author taulinger
+ * @interface FileStorageService
+ */
+
+/**
+ * Loads the the content of a file.
+ * @function
+ * @async
+ * @name FileStorageService#get
+ * @param {String} fileId 
+ * @returns {Promise<string>} content
+ */
+
+/**
+ * Saves the content of a file.
+ * If no id defined --> create a new file
+ *     returns new adminId and new file url
+ * If id is an adminId --> update the file
+ *     returns the same adminId and the same file url
+ * if id is an fileId --> fork the file
+ *     returns new adminId and new file url
+ * @function
+ * @async
+ * @name FileStorageService#save
+ * @param {string|null|undefined} id ID of the file
+ * @param {string} content 
+ * @param {FileStorageServiceDataTypes} type 
+ * @returns {Promise<FileSaveResult>} content
+ */
+
+/**
+ * MetaData of a successfully saved file.
+ * @typedef {Object} FileSaveResult
+ * @property {string} adminId The adminId of the succesfully saved file
+ * @property {string} fileId The fileId of the succesfully saved file
+ */
+
+/**
+ * MetaData of a successfully retrieved file.
+ * @typedef {Object} FileLoadResult
+ * @property {string} data The data of the succesfully retrieved file
+ * @property {FileStorageServiceDataTypes} type The type of the succesfully retrieved file
+ * @property {number} srid The srid of the succesfully retrieved data
+ */
+
+/**
+ * @enum
+ */
+export const FileStorageServiceDataTypes = Object.freeze({
+	KML: 'application/vnd.google-earth.kml+xml'
+});
+
+/**
+ * BVV service for persisting and loading ASCII based geodata using a RESTful endpoint.
+ * @class
+ * @author taulinger
+ * @implements {FileStorageService}
+ */
+export class BvvFileStorageService {
+
+	constructor() {
+		const { HttpService: httpService, ConfigService: configService } = $injector.inject('HttpService', 'ConfigService');
+		this._fileStorageUrl = configService.getValueAsPath('BACKEND_URL') + 'files';
+		this._httpService = httpService;
+	}
+
+	_getKeyByValue(object, value) {
+		return Object.keys(object).find(key => object[key] === value);
+	}
+
+	async get(fileId) {
+		const url = `${this._fileStorageUrl}/${fileId}`;
+		const result = await this._httpService.get(url);
+		if (result.ok) {
+			const type = this._getKeyByValue(FileStorageServiceDataTypes, result.headers.get('Content-Type'));
+			if (type) {
+				const data = await result.text();
+				return {
+					data: data,
+					type: FileStorageServiceDataTypes[type],
+					srid: 4326
+				};
+
+			}
+			throw new Error('Content-Type ' + result.headers.get('Content-Type') + ' currently not supported');
+		}
+		throw new Error('File could not be loaded: ' + url);
+	}
+
+
+	async save(id, content, type) {
+		if (type === FileStorageServiceDataTypes.KML) {
+
+			const url = id ? `${this._fileStorageUrl}/${id}` : this._fileStorageUrl;
+			const result = await this._httpService.post(url, content, type);
+			if (result.ok) {
+				const data = await result.json();
+				return {
+					adminId: data.adminId,
+					fileId: data.fileId,
+				};
+			}
+
+			throw new Error('File could not be saved: ' + url);
+		}
+		throw new Error('Content-Type ' + type + ' currently not supported');
+	}
+}
