71826beaebbb9ecb38f074236cc283b4cf75a9f2
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-04-19 13:14:32 +0200

    update tests of measurement.reducer

--- test/modules/map/store/measurement.reducer.test.js
+++ test/modules/map/store/measurement.reducer.test.js
@@ -1,6 +1,7 @@
 import { measurementReducer } from '../../../../src/modules/map/store/measurement.reducer';
-import { activate, deactivate } from '../../../../src/modules/map/store/measurement.action';
+import { activate, deactivate, setStatistic, reset } from '../../../../src/modules/map/store/measurement.action';
 import { TestUtils } from '../../../test-utils.js';
+import { EventLike } from '../../../../src/utils/storeUtils';
 
 
 
@@ -15,6 +16,8 @@ describe('measurementReducer', () => {
 	it('initiales the store with default values', () => {
 		const store = setup();
 		expect(store.getState().measurement.active).toBeFalse();
+		expect(store.getState().measurement.statistic).toEqual({ length:0, area:0 });
+		expect(store.getState().measurement.reset).toBeNull();
 	});
 
 
@@ -28,5 +31,22 @@ describe('measurementReducer', () => {
 		deactivate();
 
 		expect(store.getState().measurement.active).toBeFalse();
+	});
+
+	it('updates the statistic property', () => {
+		const store = setup();
+		const statistic = { length:42, area:2 };
+		
+		setStatistic(statistic);
+
+		expect(store.getState().measurement.statistic).toEqual({ length:42, area:2 });
+	});
+
+	it('updates the reset property', () => {
+		const store = setup();		
+		
+		reset();
+
+		expect(store.getState().measurement.reset).toBeInstanceOf(EventLike);
 	});
 });
