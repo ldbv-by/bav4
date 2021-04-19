89180b2dd7d59a023e8a35408be05362445f114c
Thomas Aulinger <taulinger@gmx.de>
2021-04-19 15:29:49 +0200

    parse attribution definition from bvv backend

--- test/service/provider/geoResource.provider.test.js
+++ test/service/provider/geoResource.provider.test.js
@@ -1,7 +1,94 @@
 import { $injector } from '../../../src/injection';
-import { loadBvvGeoResources, loadExampleGeoResources } from '../../../src/services/provider/geoResource.provider';
+import { getBvvAttribution } from '../../../src/services/provider/attributionProvider';
+import { loadBvvGeoResources, loadExampleGeoResources, parseBvvAttributionDefinition } from '../../../src/services/provider/geoResource.provider';
 
 describe('GeoResource provider', () => {
+
+
+	describe('parseBvvAttributionDefinition', () => {
+
+		it('it returns null when basic attribution definition is missing', () => {
+
+			const result = parseBvvAttributionDefinition({});
+
+			expect(result).toBeNull();
+		});
+
+		it('it parses a basic attribution definition', () => {
+
+			const attribution = {
+				copyright: {
+					label: 'label',
+					url: 'url'
+				},
+				description: 'description'
+			};
+			const attributionDefinition = {
+				attribution: {
+					copyright: attribution.copyright.label,
+					href: attribution.copyright.url,
+					description: attribution.description,
+				}
+			};
+			const result = parseBvvAttributionDefinition(attributionDefinition);
+
+			expect(result).toEqual([attribution]);
+		});
+
+		it('it parses extended attribution definitions', () => {
+
+
+			const attributionDefinition = {
+				attribution: {
+					copyright: 'label',
+					href: 'url',
+					description: 'description',
+				},
+				extendedAttributions: [
+					{
+
+					},
+					{
+						copyright: 'label1',
+						href: 'url1',
+						description: 'description1',
+					},
+					{
+						description: 'description2',
+					}
+
+				]
+			};
+			const result = parseBvvAttributionDefinition(attributionDefinition);
+
+			//completely from basic attribution definition
+			expect(result[0]).toEqual({
+				copyright: {
+					label: 'label',
+					url: 'url'
+				},
+				description: 'description'
+			});
+			//completely from extended attribution definition
+			expect(result[1]).toEqual({
+				copyright: {
+					label: 'label1',
+					url: 'url1'
+				},
+				description: 'description1'
+			});
+			//partially from extended attribution definition
+			expect(result[2]).toEqual({
+				copyright: {
+					label: 'label',
+					url: 'url'
+				},
+				description: 'description2'
+			});
+		});
+	});
+
+
 	describe('Bvv GeoResource provider', () => {
 
 
@@ -19,11 +106,14 @@ describe('GeoResource provider', () => {
 				.registerSingleton('HttpService', httpService);
 		});
 
+		const basicAttribution = {
+			copyright: 'copyright'
+		};
 
-		const wmsDefinition = { id: 'wmsId', label: 'wmsLabel', background: false, opacity: 0.5, url: 'wmsUrl', layers: 'wmsLayer', format: 'image/png', type: 'wms' };
-		const wmtsDefinition = { id: 'wmtsId', label: 'wmtsLabel', background: true, opacity: 1.0, url: 'wmtsUrl', type: 'wmts' };
-		const vectorDefinition = { id: 'wmtsId', label: 'vectorLabel', background: false, opacity: 1.0, url: 'vectorUrl', sourceType: 'kml', type: 'vector' };
-		const aggregateDefinition = { id: 'wmtsId', label: 'aggregateLabel', background: true, opacity: 1.0, geoResourceIds: ['wmtsId', 'wmsId'], type: 'aggregate' };
+		const wmsDefinition = { id: 'wmsId', label: 'wmsLabel', background: false, opacity: 0.5, url: 'wmsUrl', layers: 'wmsLayer', format: 'image/png', type: 'wms', attribution: basicAttribution };
+		const wmtsDefinition = { id: 'wmtsId', label: 'wmtsLabel', background: true, opacity: 1.0, url: 'wmtsUrl', type: 'wmts', attribution: basicAttribution };
+		const vectorDefinition = { id: 'wmtsId', label: 'vectorLabel', background: false, opacity: 1.0, url: 'vectorUrl', sourceType: 'kml', type: 'vector', attribution: basicAttribution };
+		const aggregateDefinition = { id: 'wmtsId', label: 'aggregateLabel', background: true, opacity: 1.0, geoResourceIds: ['wmtsId', 'wmsId'], type: 'aggregate', attribution: basicAttribution };
 
 
 		const vadlidateGeoResourceProperties = (georesource, definition) => {
@@ -64,19 +154,27 @@ describe('GeoResource provider', () => {
 			expect(wmsGeoResource.url).toBe(wmsDefinition.url);
 			expect(wmsGeoResource.layers).toBe(wmsDefinition.layers);
 			expect(wmsGeoResource.format).toBe(wmsDefinition.format);
+			expect(wmsGeoResource._attributionProvider).toBe(getBvvAttribution);
+			expect(wmsGeoResource._attribution).not.toBeNull();
 
 			const wmtsGeoResource = georesources[1];
 			vadlidateGeoResourceProperties(wmtsGeoResource, wmtsDefinition);
 			expect(wmtsGeoResource.url).toBe(wmtsGeoResource.url);
+			expect(wmtsGeoResource._attributionProvider).toBe(getBvvAttribution);
+			expect(wmtsGeoResource._attribution).not.toBeNull();
 
 			const vectorGeoResource = georesources[2];
 			vadlidateGeoResourceProperties(vectorGeoResource, vectorDefinition);
 			expect(vectorGeoResource.url).toBe(vectorDefinition.url);
 			expect(Symbol.keyFor(vectorGeoResource.sourceType)).toBe(vectorDefinition.sourceType);
+			expect(vectorGeoResource._attributionProvider).toBe(getBvvAttribution);
+			expect(vectorGeoResource._attribution).not.toBeNull();
 
 			const aggregateGeoResource = georesources[3];
 			vadlidateGeoResourceProperties(aggregateGeoResource, aggregateDefinition);
 			expect(aggregateGeoResource.geoResourceIds).toEqual(aggregateDefinition.geoResourceIds);
+			expect(aggregateGeoResource._attributionProvider).toBe(getBvvAttribution);
+			expect(aggregateGeoResource._attribution).not.toBeNull();
 
 		});
 
@@ -126,7 +224,7 @@ describe('GeoResource provider', () => {
 	describe('Example GeoResource provider', () => {
 		it('loads GeoResources', async () => {
 			const georesources = await loadExampleGeoResources();
-            
+
 			expect(georesources.length).toBe(6);
 		});
 	});
