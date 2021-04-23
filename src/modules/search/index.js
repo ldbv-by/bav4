ec4bdbcc9dfebf5ce895a2ff6a2240501375c3d0
Thomas Aulinger <taulinger@gmx.de>
2021-04-23 09:18:14 +0200

    move autocomplete search to commons module

--- src/modules/search/index.js
+++ src/modules/search/index.js
@@ -1,7 +1,3 @@
-import { AutocompleteSearch } from './components/AutocompleteSearch';
-if (!window.customElements.get(AutocompleteSearch.tag)) {
-	window.customElements.define(AutocompleteSearch.tag, AutocompleteSearch);
-}
 import { SearchContentPanel } from './components/menu/SearchContentPanel';
 if (!window.customElements.get(SearchContentPanel.tag)) {
 	window.customElements.define(SearchContentPanel.tag, SearchContentPanel);
