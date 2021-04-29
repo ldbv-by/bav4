18c9c48aef050a7d3fd8654c79b3d071e03fcbe3
Thomas Aulinger <taulinger@gmx.de>
2021-04-29 11:36:18 +0200

    cleanup

--- src/modules/search/components/menu/types/resultPanelUtils.js
+++ src/modules/search/components/menu/types/resultPanelUtils.js
@@ -1,16 +1,12 @@
-
-export const requestData = async(term, provider, minQueryLength) => {
-	if (term.trim().length >= minQueryLength) {
+export const requestData = async (term, provider, minQueryLength) => {
+	if (term && term.trim().length >= minQueryLength) {
 		try {
 			const result = await provider(term);
 			return result;
 		}
 		catch (error) {
 			console.warn(error.message);
-			return [];
 		}
 	}
-	else {
-		return [];
-	}
+	return [];
 };
--- test/modules/search/components/menu/types/resultPanelUtils.test.js
+++ test/modules/search/components/menu/types/resultPanelUtils.test.js
@@ -25,6 +25,15 @@ describe('requestData', () => {
 		});
 	});
 
+	describe('query term length null or undefined', () => {
+		it('returns empty array', async () => {
+
+			const result = await requestData(null, 10);
+
+			expect(result.length).toBe(0);
+		});
+	});
+
 	describe('provider throws an error', () => {
 		it('returns empty array and logs a warn statement', async () => {
 			const provider = async () => {
