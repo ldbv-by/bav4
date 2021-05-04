489eee984b85bcc13675265f0a1cf3c8f366b96e
Thomas Aulinger <taulinger@gmx.de>
2021-05-04 15:09:05 +0200

    add (preview) layer handling

--- src/modules/search/components/menu/types/geoResource/GeoResourceResultItem.js
+++ src/modules/search/components/menu/types/geoResource/GeoResourceResultItem.js
@@ -1,6 +1,9 @@
 import { html, nothing } from 'lit-html';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 import { BaElement } from '../../../../../BaElement';
+import { addLayer, removeLayer } from '../../../../../map/store/layers.action';
+import { MainMenuTabIndex } from '../../../../../menu/components/mainMenu/MainMenu';
+import { setTabIndex } from '../../../../../menu/store/mainMenu.action';
 import itemCss from '../item.css';
 import css from './geoResourceResultItem.css';
 
@@ -26,14 +29,40 @@ export class GeoResourceResultItem extends BaElement {
 		this.render();
 	}
 
+	_tmpLayerId(id) {
+		return `tmp_${this.constructor.name}_${id}`;
+	}
 
 	createView() {
+	
+		const onClick = (result) => {
+			//remove the preview layer
+			removeLayer(this._tmpLayerId(result.id));
+			//add the "real" layer 
+			addLayer(result.id, { label: result.label });
+			//switch to "maps" tab in main menu
+			setTabIndex(MainMenuTabIndex.MAPS);
+		};
+		const onMouseEnter = (result) => {
+			//add a preview layer
+			addLayer(this._tmpLayerId(result.id), { label: result.label, geoResourceId:result.id, constraints: { hidden: true, alwaysTop: true } });
+		};
+		const onMouseLeave = (result) => {
+			//remove the preview layer
+			removeLayer(this._tmpLayerId(result.id));
+		};
+
 		if (this._georesourceSearchResult) {
 
 			return html`
 				<style>${itemCss}</style>
 				<style>${css}</style>
-                <li>${unsafeHTML(this._georesourceSearchResult.labelFormated)}</li>
+                <li 
+					@click=${() => onClick(this._georesourceSearchResult)} 
+					@mouseenter=${() => onMouseEnter(this._georesourceSearchResult)} 
+					@mouseleave=${() => onMouseLeave(this._georesourceSearchResult)}>
+						${unsafeHTML(this._georesourceSearchResult.labelFormated)}
+				</li>
             `;
 		}
 		return nothing;
--- test/modules/search/components/menu/types/geoResource/GeoResourceResultItem.test.js
+++ test/modules/search/components/menu/types/geoResource/GeoResourceResultItem.test.js
@@ -1,3 +1,5 @@
+import { addLayer } from '../../../../../../../src/modules/map/store/layers.action';
+import { layersReducer } from '../../../../../../../src/modules/map/store/layers.reducer';
 import { GeoResourceResultItem } from '../../../../../../../src/modules/search/components/menu/types/geoResource/GeoResourceResultItem';
 import { SearchResult, SearchResultTypes } from '../../../../../../../src/modules/search/services/domain/searchResult';
 import { TestUtils } from '../../../../../../test-utils.js';
@@ -7,25 +9,96 @@ window.customElements.define(GeoResourceResultItem.tag, GeoResourceResultItem);
 describe('GeoResourceResultItem', () => {
 
 
-	beforeEach(async () => {
-		TestUtils.setupStoreAndDi({});
+	let store;
+	const setup = (state = {}) => {
+
+		store = TestUtils.setupStoreAndDi(state, {
+			layers: layersReducer
+		});
+		return TestUtils.render(GeoResourceResultItem.tag);
+	};
+
+	describe('methods', () => {
+
+		it('generates an id for a temporary layer', async () => {
+			const element = await setup();
+
+			expect(element._tmpLayerId('foo')).toBe('tmp_GeoResourceResultItem_foo');
+		});
+
 	});
 
 	describe('when initialized', () => {
 
 		it('renders nothing when no data available', async () => {
+			const element = await setup();
 
-			const element = await TestUtils.render(GeoResourceResultItem.tag);
 			expect(element.shadowRoot.children.length).toBe(0);
 		});
 
 		it('renders the view', async () => {
 			const data = new SearchResult('id', 'label', 'labelFormated', SearchResultTypes.GEORESOURCE);
-			const element = await TestUtils.render(GeoResourceResultItem.tag);
+			const element = await setup();
 
 			element.data = data;
 
 			expect(element.shadowRoot.querySelector('li').innerText).toBe('labelFormated');
+		});
+	});
+
+	describe('preview layer', () => {
+
+		describe('on mouse enter', () => {
+
+			it('adds a preview layer', async () => {
+				const id = 'id';
+				const data = new SearchResult(id, 'label', 'labelFormated', SearchResultTypes.GEORESOURCE);
+				const element = await setup();
+				element.data = data;
+
+				const target = element.shadowRoot.querySelector('li');
+				target.dispatchEvent(new Event('mouseenter'));
+
+				expect(store.getState().layers.active.length).toBe(1);
+				expect(store.getState().layers.active[0].id).toBe(element._tmpLayerId(id));
+			});
+		});
+
+		describe('on mouse leave', () => {
+
+			it('removes the preview layer', async () => {
+				const id = 'id';
+				const data = new SearchResult(id, 'label', 'labelFormated', SearchResultTypes.GEORESOURCE);
+				const element = await setup();
+				element.data = data;
+				addLayer(element._tmpLayerId(id));
+
+				expect(store.getState().layers.active.length).toBe(1);
+
+				const target = element.shadowRoot.querySelector('li');
+				target.dispatchEvent(new Event('mouseleave'));
+
+				expect(store.getState().layers.active.length).toBe(0);
+			});
+		});
+
+		describe('on click', () => {
+
+			it('removes the preview layer and adds the real layer', async () => {
+				const id = 'id';
+				const data = new SearchResult(id, 'label', 'labelFormated', SearchResultTypes.GEORESOURCE);
+				const element = await setup();
+				element.data = data;
+				addLayer(element._tmpLayerId(id));
+
+				expect(store.getState().layers.active.length).toBe(1);
+
+				const target = element.shadowRoot.querySelector('li');
+				target.click();
+
+				expect(store.getState().layers.active.length).toBe(1);
+				expect(store.getState().layers.active[0].id).toBe(id);
+			});
 		});
 	});
 });
