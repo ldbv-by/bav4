ef3cb7f11efe667a002ca35113626ddf2a54eb2c
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-05-17 06:44:29 +0200

    add testing methods for ruler-style

--- src/modules/map/components/olMap/handler/measure/StyleUtils.js
+++ src/modules/map/components/olMap/handler/measure/StyleUtils.js
@@ -1,8 +1,9 @@
 
-import { getGeometryLength, canShowAzimuthCircle } from './GeometryUtils';
+import { getGeometryLength, canShowAzimuthCircle, createOffsetGeometry } from './GeometryUtils';
 import { getPartitionDelta } from './GeometryUtils';
 import { Fill, Stroke, Style, Circle as CircleStyle } from 'ol/style';
 import { Polygon, LineString, Circle, MultiPoint } from 'ol/geom';
+
 
 
 const ZPOLYGON = 10;
@@ -12,12 +13,90 @@ const RED_COLOR = [255, 0, 0];
 const WHITE_COLOR = [255, 255, 255];
 const BLACK_COLOR = [0, 0, 0];
 
+const createRulerStyles = (feature, resolution) => {
+	const geom = feature.getGeometry();
+	const calculationHints = { fromProjection: 'EPSG:3857', toProjection: 'EPSG:25832' };
+	const partitionLength = getPartitionDelta(geom, resolution, calculationHints) * getGeometryLength(geom);	
+	const partitionTickDistance = partitionLength / resolution;	
+
+
+	const bigWidth = 10;
+	const smallWidth = 5;
+
+	const calcLineOffsetInMeter = (widthInPixel) => widthInPixel / 2 * resolution;
+	
+	const bigTickStyle = new Style({
+		geometry: createOffsetGeometry(geom, calcLineOffsetInMeter(bigWidth) ),
+		stroke: new Stroke({
+			color: RED_COLOR.concat([1]),
+			lineCap:'butt',
+			// width:bigWidth,
+			// lineDash:[3, partitionTickDistance - 3],
+			// lineDashOffset:3
+			width:1,
+			
+		})
+	});
+	const smallTickStyle = new Style({
+		geometry: createOffsetGeometry(geom, calcLineOffsetInMeter(smallWidth) ),
+		stroke: new Stroke({
+			color: RED_COLOR.concat([1]),
+			lineCap:'butt',
+			width:smallWidth,
+			lineDash:[2, (partitionTickDistance / 5) - 2],
+			lineDashOffset:2
+		})
+	});
+
+	return [bigTickStyle, smallTickStyle];
+};
+
+/**
+ * Inspired by example from https://stackoverflow.com/questions/57421223/openlayers-3-offset-stroke-style
+ * @param {*} feature 
+ * @param {*} resolution 
+ * @returns 
+ */
+export const measureStyleFunction3 = (feature, resolution) => {
+
+	const stroke = new Stroke({
+		color:RED_COLOR.concat([1]),
+		width:3,
+	});
+	const zIndex = (feature.getGeometry() instanceof LineString) ?	ZLINE : ZPOLYGON;
+
+	const styles = [
+		new Style({
+			fill: new Fill({ 
+				color:RED_COLOR.concat([0.4]) 
+			}),
+			stroke:stroke,
+			zIndex:zIndex
+		},
+		new Style({
+			stroke:stroke,
+			geometry: feature => {
+				
+				if (canShowAzimuthCircle(feature.getGeometry())) {					
+					const coords = feature.getGeometry().getCoordinates();
+					const radius = getGeometryLength(feature.getGeometry());
+					const circle = new Circle(coords[0], radius);
+					return circle;
+				}
+			},
+			zIndex:0
+		}))];
+	const rulerStyles = createRulerStyles(feature, resolution);
+	
+	
+	return styles.concat(rulerStyles);
+};
+
 export const measureStyleFunction2 = (feature, resolution) => {
 	const projectionHints = { fromProjection: 'EPSG:3857', toProjection: 'EPSG:25832' };
 	const dashedStroke = new Stroke({
 		color:RED_COLOR.concat([1]),
-		width:1,
-		// lineDash:[8]
+		width:1
 	});
 	const zIndex = (feature.getGeometry() instanceof LineString) ?	ZLINE : ZPOLYGON;
 	let simplifiedGeometry = feature.getGeometry();
