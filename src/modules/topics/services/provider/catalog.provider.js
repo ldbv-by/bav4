68334e6d7baee75091ff3bcc625b1ce5a556149c
Thomas Aulinger <taulinger@gmx.de>
2021-05-18 11:33:05 +0200

    add provider for catalog definitions

---
+++ src/modules/topics/services/provider/catalog.provider.js
@@ -0,0 +1,51 @@
+import { $injector } from '../../../../injection';
+
+
+/**
+ * Uses the BVV endpoint to load catalog definitions.
+ * @function
+ * @returns {Promise<Array<Object>>}
+ */
+export const loadBvvCatalog = async (topicId) => {
+
+	const { HttpService: httpService, ConfigService: configService } = $injector.inject('HttpService', 'ConfigService');
+
+	const url = `${configService.getValueAsPath('BACKEND_URL')}catalog/${topicId}`;
+
+	const result = await httpService.get(url);
+
+	if (result.ok) {
+		return await result.json();
+	}
+	throw new Error(`Catalog for '${topicId}' could not be loaded`);
+};
+
+/**
+ * Loads an example catalog without a backend.
+ * @function
+ * @returns {Promise<Array<Object>>}
+ */
+export const loadExampleCatalog = async (topicId) => {
+
+	return [
+		{
+			label: `Suptopic1 ${topicId}`,
+			children: [
+				{
+					geoResourceId: 'atkis'
+				},
+				{
+					geoResourceId: 'atkis_sw'
+				},
+				{
+					label: `Suptopic2 ${topicId}`,
+					children: [{
+						geoResourceId: 'atkis_sw'
+					}]
+				}
+			]
+		}, {
+			geoResourceId: 'atkis_sw'
+		}
+	];
+};
