f905d5f6461c3ee0af26e65a2c1ae37a5b296e69
Thomas Aulinger <taulinger@gmx.de>
2021-05-21 17:54:40 +0200

    Prepend common CSS classes for all content panels

---
+++ src/modules/menu/components/mainMenu/content/AbstractContentPanel.js
@@ -0,0 +1,36 @@
+import { html } from 'lit-html';
+import { BaElement } from '../../../../BaElement';
+import baElementCss from './../../../../baElement.css';
+import contentPanelCss from './abstractContentPanel.css';
+
+/**
+ * Base class for all content panels of the main menu.
+ * Just prepends common CSS classes for all content panels.
+ * @class
+ * @author taulinger
+ * @abstract
+ */
+export class AbstractContentPanel extends BaElement {
+
+	constructor() {
+		super();
+		if (this.constructor === AbstractContentPanel) {
+			// Abstract class can not be constructed.
+			throw new TypeError('Can not construct abstract class.');
+		}
+	}
+
+	/**
+    * @override
+    */
+	defaultCss() {
+		return html`
+		<style>
+            ${baElementCss}
+		</style>
+		<style>
+		    ${contentPanelCss}
+		</style>
+		`;
+	}
+}
