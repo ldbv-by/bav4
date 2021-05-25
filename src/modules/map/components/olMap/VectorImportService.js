b04e529dc3bdecf06b7d9c58a8f669dfb8645857
Thomas Aulinger <taulinger@gmx.de>
2021-05-25 23:43:25 +0200

    add a service for importing vector data

---
+++ src/modules/map/components/olMap/VectorImportService.js
@@ -0,0 +1,116 @@
+import { VectorSourceType } from '../../../../services/domain/geoResources';
+import VectorSource from 'ol/source/Vector';
+import { $injector } from '../../../../injection';
+import { load as featureLoader } from './utils/feature.provider';
+import { KML, GPX, GeoJSON } from 'ol/format';
+
+
+
+const getUrlService = () => {
+
+	const { UrlService: urlService } = $injector.inject('UrlService');
+	return urlService;
+};
+
+export const iconUrlFunction = (url) => getUrlService().proxifyInstant(url);
+
+export const mapVectorSourceTypeToFormat = (sourceType) => {
+
+	switch (sourceType) {
+		case VectorSourceType.KML:
+			return new KML({ iconUrlFunction: iconUrlFunction });
+
+		case VectorSourceType.GPX:
+			return new GPX();
+
+		case VectorSourceType.GEOJSON:
+			return new GeoJSON();
+	}
+	throw new Error(sourceType + ' currently not supported');
+};
+
+
+/**
+ * Service that imports vector data from internal and external geoResources.
+ * @class
+ * @author taulinger
+ */
+export class VectorDataImportService {
+
+
+	// needed soon
+	// isMeasureFeature(olFeature) {
+	// 	const regex = /^measure/;
+	// 	return (olFeature && regex.test(olFeature.getId()));
+	// }
+
+	/**
+     * Ensures that feature specific stylings and overlays are set for this source
+     * @param {VectorSource} olVectorSource
+     * @param {Map} olMap
+     * @returns object containing the addListenerKey and clearListenerKey
+     */
+	// eslint-disable-next-line no-unused-vars
+	applyStyling(vectorSource, map) {
+		//Todo: here we will apply stylings and overlays
+		//probably we will need a map instance for the overlays
+		const addListenerKey = vectorSource.on('addfeature', () => {
+		});
+		const clearListenerKey = vectorSource.on('clear', () => {
+		});
+		return { addListenerKey, clearListenerKey };
+	}
+
+	/**
+     * Builds an ol VectorSource from an internal VectorGeoResource
+     * @param {VectorGeoResource} vectorGeoResource 
+     * @returns olVectorSource
+     */
+	vectorSourceFromInternalData(geoResource) {
+
+		const {
+			MapService: mapService
+		} = $injector.inject('MapService');
+
+		const destinationSrid = mapService.getSrid();
+		const vectorSource = new VectorSource();
+		this.applyStyling(vectorSource);
+
+		geoResource.getData().then(data => {
+			const format = mapVectorSourceTypeToFormat(geoResource.sourceType);
+			const features = format.readFeatures(data);
+
+			// If we know a better name for the geoResource now, we update the label
+			switch (geoResource.sourceType) {
+				case VectorSourceType.KML:
+					geoResource.label = format.readName(data) || geoResource.label;
+					break;
+			}
+			features.forEach(f => {
+				f.getGeometry().transform('EPSG:' + geoResource.srid, 'EPSG:' + destinationSrid);
+				f.set('srid', destinationSrid, true);
+			});
+			vectorSource.addFeatures(features);
+		}, reason => {
+			console.warn(reason);
+		});
+		return vectorSource;
+	}
+
+	/**
+     * 
+     * Builds an ol VectorSource from an external VectorGeoResource
+     * @param {VectorGeoResource} vectorGeoResource 
+     * @returns olVectorSource
+     */
+	vectorSourceFromExternalData(geoResource) {
+		const { UrlService: urlService } = $injector.inject('UrlService');
+		const source = new VectorSource({
+			url: urlService.proxifyInstant(geoResource.url),
+			loader: featureLoader,
+			format: mapVectorSourceTypeToFormat(geoResource.sourceType)
+		});
+		this.applyStyling(source);
+		return source;
+	}
+}
---
+++ test/modules/map/components/olMap/VectorImportService.test.js
@@ -0,0 +1,144 @@
+import { $injector } from '../../../../../src/injection';
+import { VectorGeoResource, VectorSourceType } from '../../../../../src/services/domain/geoResources';
+import { load } from '../../../../../src/modules/map/components/olMap/utils/feature.provider';
+import { iconUrlFunction, mapVectorSourceTypeToFormat, VectorDataImportService } from '../../../../../src/modules/map/components/olMap/VectorImportService';
+import VectorSource from 'ol/source/Vector';
+
+
+describe('VectorImportService', () => {
+
+	const urlService = {
+		proxifyInstant: () => { }
+	};
+
+	const mapService = {
+		getSrid: () => { }
+	};
+	let instanceUnderTest;
+
+	beforeAll(() => {
+		$injector
+			.registerSingleton('UrlService', urlService)
+			.registerSingleton('MapService', mapService);
+	});
+
+	describe('utils', () => {
+
+		describe('mapVectorSourceTypeToFormat', () => {
+
+			it('maps vectorSourceType to olFormats', () => {
+
+				expect(mapVectorSourceTypeToFormat(VectorSourceType.KML).constructor.name).toBe('KML');
+				expect(mapVectorSourceTypeToFormat(VectorSourceType.GPX).constructor.name).toBe('GPX');
+				expect(mapVectorSourceTypeToFormat(VectorSourceType.GEOJSON).constructor.name).toBe('GeoJSON');
+				expect(() => {
+					mapVectorSourceTypeToFormat('unknown');
+				})
+					.toThrowError(/unknown currently not supported/);
+			});
+		});
+
+
+		describe('iconUrlFunction', () => {
+
+			it('provides a function that proxifies an url', () => {
+				const iconUrl = 'https://some.url';
+				spyOn(urlService, 'proxifyInstant').withArgs(iconUrl).and.returnValue('https://proxy.url?url=' + iconUrl);
+
+				expect(iconUrlFunction(iconUrl)).toBe('https://proxy.url?url=' + iconUrl);
+			});
+		});
+
+	});
+
+
+	describe('service methods', () => {
+
+		beforeEach(() => {
+			instanceUnderTest = new VectorDataImportService();
+		});
+
+		describe('vectorSourceFromInternalData', () => {
+
+			it('builds an olVectorSource for an internal VectorGeoresource', (done) => {
+				const srid = 3857;
+				const kmlName = '';
+				const geoResourceLabel = 'geoResourceLabel';
+				spyOn(mapService, 'getSrid').and.returnValue(srid);
+				const sourceAsString = `<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/kml/2.2 https://developers.google.com/kml/schema/kml22gx.xsd"><Document><name>${kmlName}</name><Placemark id="line_1617976924317"><ExtendedData><Data name="type"><value>line</value></Data></ExtendedData><description></description><Style><LineStyle><color>ff0000ff</color><width>3</width></LineStyle><PolyStyle><color>660000ff</color></PolyStyle></Style><LineString><tessellate>1</tessellate><altitudeMode>clampToGround</altitudeMode><coordinates>10.713458946685412,49.70007647302964 11.714932179089468,48.34411758499924</coordinates></LineString></Placemark></Document></kml>`;
+				const vectorGeoresource = new VectorGeoResource('someId', geoResourceLabel, VectorSourceType.KML).setSource(sourceAsString, 4326);
+				const applyStylingSpy = spyOn(instanceUnderTest, 'applyStyling');
+
+				const olVectorSource = instanceUnderTest.vectorSourceFromInternalData(vectorGeoresource);
+
+				expect(olVectorSource.constructor.name).toBe('VectorSource');
+				expect(applyStylingSpy).toHaveBeenCalledWith(olVectorSource);
+				//features are loaded from a promise
+				setTimeout(() => {
+					expect(olVectorSource.getFeatures().length).toBe(1);
+					expect(olVectorSource.getFeatures()[0].get('srid')).toBe(srid);
+					done();
+				});
+			});
+
+			it('updates the label of an internal VectorGeoresource if possible', (done) => {
+				const srid = 3857;
+				const kmlName = 'kmlName';
+				const geoResourceLabel = 'geoResourceLabel';
+				spyOn(mapService, 'getSrid').and.returnValue(srid);
+				const sourceAsString = `<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/kml/2.2 https://developers.google.com/kml/schema/kml22gx.xsd"><Document><name>${kmlName}</name><Placemark id="line_1617976924317"><ExtendedData><Data name="type"><value>line</value></Data></ExtendedData><description></description><Style><LineStyle><color>ff0000ff</color><width>3</width></LineStyle><PolyStyle><color>660000ff</color></PolyStyle></Style><LineString><tessellate>1</tessellate><altitudeMode>clampToGround</altitudeMode><coordinates>10.713458946685412,49.70007647302964 11.714932179089468,48.34411758499924</coordinates></LineString></Placemark></Document></kml>`;
+				const vectorGeoresource = new VectorGeoResource('someId', geoResourceLabel, VectorSourceType.KML).setSource(sourceAsString, 4326);
+
+				instanceUnderTest.vectorSourceFromInternalData(vectorGeoresource);
+
+				setTimeout(() => {
+					expect(vectorGeoresource.label).toBe(kmlName);
+					done();
+				});
+			});
+
+			it('logs a warn statement when source can not be resolved', (done) => {
+				const warnSpy = spyOn(console, 'warn');
+				const vectorGeoresource = new VectorGeoResource('someId', 'Label', VectorSourceType.KML).setSource(Promise.reject('somethingGotWrong'), 4326);
+
+				instanceUnderTest.vectorSourceFromInternalData(vectorGeoresource);
+
+				//features are loaded from a promise
+				setTimeout(() => {
+					expect(warnSpy).toHaveBeenCalledWith('somethingGotWrong');
+					done();
+				});
+			});
+		});
+
+		describe('vectorSourceFromInternalData', () => {
+
+			it('builds an olVectorSource for an external VectorGeoresource', () => {
+				const url = 'https://some.url';
+				spyOn(urlService, 'proxifyInstant').withArgs(url).and.returnValue('https://proxy.url?' + url);
+				const vectorGeoresource = new VectorGeoResource('someId', 'Label', VectorSourceType.KML).setUrl(url);
+				const applyStylingSpy = spyOn(instanceUnderTest, 'applyStyling');
+
+				const olVectorSource = instanceUnderTest.vectorSourceFromExternalData(vectorGeoresource);
+
+				expect(olVectorSource.constructor.name).toBe('VectorSource');
+				expect(olVectorSource.getUrl()).toBe('https://proxy.url?' + url);
+				expect(olVectorSource.getFormat().constructor.name).toBe('KML');
+				expect(olVectorSource.loader_).toEqual(load);
+				expect(olVectorSource.getFormat().iconUrlFunction_).toEqual(iconUrlFunction);
+				expect(applyStylingSpy).toHaveBeenCalledWith(olVectorSource);
+			});
+		});
+
+		describe('applyStyling', () => {
+
+			it('adds two listeners', () => {
+
+				const { addListenerKey, clearListenerKey } = instanceUnderTest.applyStyling(new VectorSource());
+
+				expect(addListenerKey).toBeDefined();
+				expect(clearListenerKey).toBeDefined();
+			});
+		});
+	});
+});
