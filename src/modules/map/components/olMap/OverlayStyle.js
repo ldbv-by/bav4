cb0b5d9c7ef8a461ac1a0633bb4ae3f7c7b1aec1
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-06-07 14:27:11 +0200

    rename arguments

--- src/modules/map/components/olMap/OverlayStyle.js
+++ src/modules/map/components/olMap/OverlayStyle.js
@@ -34,31 +34,31 @@ export class OverlayStyle {
 
 	/**
 	 * @abstract
-	 * @param {ol.feature} feature 
-	 * @param {ol.map} map
+	 * @param {ol.feature} olFeature 
+	 * @param {ol.map} olMap
 	 */
-	add(/*eslint-disable no-unused-vars */feature, map) {
+	add(/*eslint-disable no-unused-vars */olFeature, olMap) {
 		// The child has not implemented this method.
 		throw new TypeError('Please implement and call abstract method #add from child or do not call super.add from child.');
 	}
 
 	/**
 	 * @override
-	 * @param {ol.feature} feature 
-	 * @param {ol.map} map
+	 * @param {ol.feature} olFeature 
+	 * @param {ol.map} olMap
 	 * @param {ol.geometry|null} geometry
 	 */
-	update(feature, map, geometry = null) {
+	update(olFeature, olMap, geometry = null) {
 		// The child has not implemented this method.
 		throw new TypeError('Please implement and call abstract method #update from child or do not call super.update from child.');
 	}
 
 	/**
-	 * @param {ol.feature} feature 
-	 * @param {ol.map} map
+	 * @param {ol.feature} olFeature 
+	 * @param {ol.map} olMap
 	 */
-	remove(feature, map) {
-		const featureOverlays = feature.get('overlays') || [];				
-		featureOverlays.forEach(o => map.removeOverlay(o));	
+	remove(olFeature, olMap) {
+		const featureOverlays = olFeature.get('overlays') || [];				
+		featureOverlays.forEach(o => olMap.removeOverlay(o));	
 	}
 }
