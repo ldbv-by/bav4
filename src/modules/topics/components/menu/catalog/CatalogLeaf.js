9efd8d9c4de2a86d87f0cafcae6dd0fe1aa0df4f
alsturm <alexander_sturm@gmx.net>
2021-06-10 16:00:20 +0200

    add: layer name, toggle layer

--- src/modules/topics/components/menu/catalog/CatalogLeaf.js
+++ src/modules/topics/components/menu/catalog/CatalogLeaf.js
@@ -1,5 +1,8 @@
 import { html, nothing } from 'lit-html';
 import { AbstractContentPanel } from '../../../../menu/components/mainMenu/content/AbstractContentPanel';
+import css from './catalogLeaf.css';
+import { $injector } from '../../../../../injection';
+import { addLayer, removeLayer } from '../../../../../store/layers/layers.action';
 
 
 /**
@@ -9,31 +12,76 @@ import { AbstractContentPanel } from '../../../../menu/components/mainMenu/conte
  */
 export class CatalogLeaf extends AbstractContentPanel {
 
+
+
 	set data(catalogPart) {
 		this._catalogPart = catalogPart;
 		this.render();
 	}
 
-	createView() {
+	createView(state) {
+
+		const { currentTopicId, activeLayers } = state;
+
+		const {
+			GeoResourceService: geoResourceService
+		} = $injector.inject('GeoResourceService');
+
+		const style = document.createElement( 'style' );
+		style.innerHTML = `.ba-list-item { --primary-color-theme: var(--topic-theme-${currentTopicId});	 }`;
+		this.shadowRoot.appendChild( style );
 
 		if (this._catalogPart) {
 			const { geoResourceId } = this._catalogPart;
+
+			let isChecked = false;
+			if (activeLayers.length > 0) {
+				activeLayers.forEach(l => {
+					if (l.geoResourceId === geoResourceId) {						
+						isChecked = true;						
+					}
+				});
+			}
+
+			const geoR = geoResourceService.byId(geoResourceId);
+			let label = 'no geoR';
+			if (geoR) {
+				label = geoR.label;
+			}
+
+			const onToggle = (event) => {
+				if (geoR) {
+					if (event.detail.checked) {
+						addLayer(geoR.id, { label: geoR.label });
+					}
+					else {
+						removeLayer(geoR.id);
+					}
+				}
+			};
+
 			return html`
-			<a href='#' tabindex='0' class="ba-list-item">
-					<span class="ba-list-item__pre ">
-						<span class="ba-list-item__icon-info"></span>
-					</span>
-					<span class="ba-list-item__text">${geoResourceId}</span>
+			<style>
+			${css}		
+			</style>
+			<span class="ba-list-item" >		
+					<ba-checkbox class="ba-list-item__text" @toggle=${onToggle}  checked=${isChecked} tabindex='0'  title="checkbox"><span>${label}</span></ba-checkbox>						
 					<button class="ba-icon-button ba-list-item__after verticla-center seperator">						
 						<span  class='icon-background'>
 						 </span>
 						<i class='icon icon-secondary info'></i>
 					</button>
-				</a>
+				</span>
         	`;
 		}
 		return nothing;
 	}
+
+	extractState(globalState) {
+		const { topics: { current: currentTopicId }, layers: { active: activeLayers } } = globalState;
+		return { currentTopicId, activeLayers };
+	}
+
 
 	static get tag() {
 		return 'ba-catalog-leaf';
--- test/modules/topics/components/menu/catalog/CatalogLeaf.test.js
+++ test/modules/topics/components/menu/catalog/CatalogLeaf.test.js
@@ -1,15 +1,43 @@
 import { CatalogLeaf } from '../../../../../../src/modules/topics/components/menu/catalog/CatalogLeaf';
 import {  loadExampleCatalog } from '../../../../../../src/modules/topics/services/provider/catalog.provider';
 import { TestUtils } from '../../../../../test-utils.js';
+import { $injector } from '../../../../../../src/injection';
+import { topicsReducer } from '../../../../../../src/store/topics/topics.reducer';
+import { layersReducer } from '../../../../../../src/store/layers/layers.reducer';
+import {  defaultLayerProperties } from '../../../../../../src/store/layers/layers.reducer';
+
+
 
 window.customElements.define(CatalogLeaf.tag, CatalogLeaf);
 
 describe('CatalogLeaf', () => {
 
+	const geoResourceServiceMock = {
+		async init() { },
+		all() { 
+			return [];
+		},
+		byId() {}
+	};
 
-	const setup = () => {
+	const topicsServiceMock = {
+		byId() { }
+	};
+	const layer = { ...defaultLayerProperties,  id:'atkis_sw', label:'atkis_sw' };
+	const setup = (topics = 'foo', layers = [layer]) => {
 
-		TestUtils.setupStoreAndDi();
+		const state = {
+			topics: { current: topics },
+			layers: {
+				active: layers
+			}
+		};
+
+		TestUtils.setupStoreAndDi(state, { topics: topicsReducer, layers: layersReducer });
+		
+		$injector
+			.registerSingleton('GeoResourceService', geoResourceServiceMock)
+			.registerSingleton('TopicsService', topicsServiceMock);
 
 		return TestUtils.render(CatalogLeaf.tag);
 	};
@@ -17,10 +45,10 @@ describe('CatalogLeaf', () => {
 	describe('when initialized', () => {
 
 		it('renders the nothing', async () => {
-
+			
 			const element = await setup();
-
-			expect(element.shadowRoot.children.length).toBe(0);
+			
+			expect(element.shadowRoot.children.length).toBe(0);			
 		});
 	});
 
@@ -34,10 +62,11 @@ describe('CatalogLeaf', () => {
 			//assign data
 			element.data = leaf;
 
-			expect(element.shadowRoot.querySelector('.ba-list-item__text').innerText).toBe(leaf.geoResourceId);						
+			expect(element.shadowRoot.querySelectorAll('.ba-icon-button')).toHaveSize(1);		
+			expect(element.shadowRoot.querySelectorAll('ba-checkbox')).toHaveSize(1);		
+			expect(element.shadowRoot.querySelector('.ba-list-item__text').innerText).toBe('no geoR');						
 			expect(element.shadowRoot.querySelector('.ba-icon-button')).toBeTruthy();
 			expect(element.shadowRoot.querySelector('.info')).toBeTruthy();
 		});
 	});
 });
-
