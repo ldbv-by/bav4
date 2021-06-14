ce2356e3d46490975299fb9db4e29e4296aee187
alsturm <alexander_sturm@gmx.net>
2021-06-14 17:25:04 +0200

    update: test

--- test/modules/topics/components/menu/catalog/CatalogLeaf.test.js
+++ test/modules/topics/components/menu/catalog/CatalogLeaf.test.js
@@ -91,7 +91,7 @@ describe('CatalogLeaf', () => {
 
 				//test correct rendering of topic dependent style definition
 				//we have four style -Elements included: baElement.css, contentPanel.css, catalogLeaf.css and dynamically created style element, that we check here
-				expect(element.shadowRoot.styleSheets.item(3).cssRules.item(0).style.getPropertyValue('--primary-color-theme').trim()).toBe('var(--topic-theme-foo)');
+				expect(element.shadowRoot.styleSheets.item(3).cssRules.item(0).style.getPropertyValue('--primary-color-theme').trim()).toBe('var(--topic-theme-foo, var(--secondary-color-theme))');
 			});
 
 			it('renders a checkbox unchecked', async () => {
