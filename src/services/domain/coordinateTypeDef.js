0d64beb4f8ef0adff7fbe9e56236fbaa6aadd409
Thomas Aulinger <taulinger@gmx.de>
2021-06-25 13:41:04 +0200

    precise docs

--- src/services/domain/coordinateTypeDef.js
+++ src/services/domain/coordinateTypeDef.js
@@ -1,4 +1,4 @@
 /**
- * An array of numbers representing an xy coordinate. Example: `[16, 48]`.
+ * An array of numbers representing an xy coordinate. Ordering is [easting, northing] or [lon, lat]. Example: `[16, 48]`.
  * @typedef {Array<number>} Coordinate
  */
