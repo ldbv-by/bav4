4863f366077a0e5b64c7de5c0ffccb63e2b9cd3f
Thomas Aulinger <taulinger@gmx.de>
2021-06-28 16:58:12 +0200

    highlight search results

--- src/modules/search/components/menu/types/location/LocationResultItem.js
+++ src/modules/search/components/menu/types/location/LocationResultItem.js
@@ -1,8 +1,12 @@
 import { html, nothing } from 'lit-html';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html';
+import { $injector } from '../../../../../../injection';
 import { BaElement } from '../../../../../BaElement';
 import itemCss from '../item.css';
 import css from './locationResultItem.css';
+import { close as closeMainMenu } from '../../../../../menu/store/mainMenu.action';
+import { setFit } from '../../../../../../store/position/position.action';
+import { removeTemporaryHighlightFeature, setHighlightFeature, setTemporaryHighlightFeature } from '../../../../../../store/highlight/highlight.action';
 
 
 
@@ -20,6 +24,30 @@ import css from './locationResultItem.css';
  */
 export class LocationResultItem extends BaElement {
 
+	constructor() {
+		super();
+
+		const { EnvironmentService } = $injector.inject('EnvironmentService');
+		this._environmentService = EnvironmentService;
+		this._portrait = false;
+	}
+
+	initialize() {
+
+		const _window = this._environmentService.getWindow();
+		//MediaQuery for 'orientation'
+		const mediaQuery = _window.matchMedia('(orientation: portrait)');
+		const handleOrientationChange = (e) => {
+			this._portrait = e.matches;
+		};
+		mediaQuery.addEventListener('change', handleOrientationChange);
+		//initial set of local state
+		handleOrientationChange(mediaQuery);
+	}
+
+	static get _maxZoomLevel() {
+		return 19;
+	}
 
 	set data(locationSearchResult) {
 		this._locationSearchResult = locationSearchResult;
@@ -27,13 +55,41 @@ export class LocationResultItem extends BaElement {
 	}
 
 
-	createView() {		
+	createView() {
+		/**
+		 * Uses mouseenter and mouseleave events for adding/removing a temporary highlight feature.
+		 * These events are not fired on touch devices, so there's no extra handling needed.
+		 */
+		const onMouseEnter = (result) => {
+			setTemporaryHighlightFeature({ data: [...result.center] });
+		};
+		const onMouseLeave = () => {
+			removeTemporaryHighlightFeature();
+		};
+		const onClick = (result) => {
+
+			const extent = result.extent ? [...result.extent] : [...result.center, ...result.center];
+			removeTemporaryHighlightFeature();
+			setFit(extent, { maxZoom: LocationResultItem._maxZoomLevel });
+			if (!result.extent) {
+				setHighlightFeature({ data: [...result.center] });
+			}
+
+			if (this._portrait) {
+				//close the main menu
+				closeMainMenu();
+			}
+		};
+
 		if (this._locationSearchResult) {
 
 			return html`
 				<style>${itemCss}</style>
 				<style>${css}</style>
-                <li class='ba-list-item' >
+                <li class="ba-list-item"
+					@click=${() => onClick(this._locationSearchResult)} 
+					@mouseenter=${() => onMouseEnter(this._locationSearchResult)} 
+					@mouseleave=${() => onMouseLeave(this._locationSearchResult)}>
 					<span class="ba-list-item__pre ">
 						<span class="ba-list-item__icon-info">
 						</span>
--- test/modules/search/components/menu/types/location/LocationResultItem.test.js
+++ test/modules/search/components/menu/types/location/LocationResultItem.test.js
@@ -1,31 +1,176 @@
+import { $injector } from '../../../../../../../src/injection';
+import { mainMenuReducer } from '../../../../../../../src/modules/menu/store/mainMenu.reducer';
 import { LocationResultItem } from '../../../../../../../src/modules/search/components/menu/types/location/LocationResultItem';
 import { SearchResult, SearchResultTypes } from '../../../../../../../src/modules/search/services/domain/searchResult';
+import { highlightReducer } from '../../../../../../../src/store/highlight/highlight.reducer';
+import { positionReducer } from '../../../../../../../src/store/position/position.reducer';
 import { TestUtils } from '../../../../../../test-utils.js';
 window.customElements.define(LocationResultItem.tag, LocationResultItem);
 
 
 describe('LocationResultItem', () => {
 
+	let store;
 
-	beforeEach(async () => {
-		TestUtils.setupStoreAndDi({});
+	const windowMock = {
+		matchMedia() { }
+	};
+
+	const setup = (portraitOrientation = false, state = {}) => {
+
+		spyOn(windowMock, 'matchMedia')
+			.withArgs('(orientation: portrait)').and.returnValue(TestUtils.newMediaQueryList(portraitOrientation));
+
+		store = TestUtils.setupStoreAndDi(state, {
+			highlight: highlightReducer,
+			position: positionReducer,
+			mainMenu: mainMenuReducer
+		});
+
+		$injector.registerSingleton('EnvironmentService', {
+			getWindow: () => windowMock
+		});
+
+		return TestUtils.render(LocationResultItem.tag);
+	};
+
+	describe('static properties', () => {
+
+		it('_maxZoomValue', async () => {
+
+			expect(LocationResultItem._maxZoomLevel).toBe(19);
+		});
 	});
 
 	describe('when initialized', () => {
 
 		it('renders nothing when no data available', async () => {
 
-			const element = await TestUtils.render(LocationResultItem.tag);
+			const element = await setup();
 			expect(element.shadowRoot.children.length).toBe(0);
 		});
 
 		it('renders the view', async () => {
 			const data = new SearchResult('id', 'label', 'labelFormated', SearchResultTypes.LOCATION);
-			const element = await TestUtils.render(LocationResultItem.tag);
+			const element = await setup();
 
 			element.data = data;
 
 			expect(element.shadowRoot.querySelector('li').innerText).toBe('labelFormated');
+		});
+	});
+
+	describe('events', () => {
+
+		describe('on mouse enter', () => {
+
+			it('sets a temporary highlight feature', async () => {
+				const coordinate = [21, 42];
+				const id = 'id';
+				const data = new SearchResult(id, 'label', 'labelFormated', SearchResultTypes.LOCATION, coordinate);
+				const element = await setup();
+				element.data = data;
+
+				const target = element.shadowRoot.querySelector('li');
+				target.dispatchEvent(new Event('mouseenter'));
+
+				expect(store.getState().highlight.temporaryFeature.data).toEqual(coordinate);
+			});
+		});
+
+		describe('on mouse leave', () => {
+
+			it('removes a temporary highlight feature', async () => {
+				const coordinate = [21, 42];
+				const id = 'id';
+				const data = new SearchResult(id, 'label', 'labelFormated', SearchResultTypes.LOCATION, coordinate);
+				const element = await setup(false, {
+					highlight: {
+						temporaryFeature: { data: coordinate }
+					}
+				});
+				element.data = data;
+
+
+				const target = element.shadowRoot.querySelector('li');
+				target.dispatchEvent(new Event('mouseleave'));
+
+				expect(store.getState().highlight.temporaryFeature).toBeNull();
+			});
+		});
+
+		describe('on click', () => {
+
+			const coordinate = [21, 42];
+			const extent = [0, 1, 2, 3];
+			const id = 'id';
+
+			const setupOnClickTests = async (portraitOrientation, extent = null) => {
+
+				const data = new SearchResult(id, 'label', 'labelFormated', SearchResultTypes.LOCATION, coordinate, extent);
+				const element = await setup(portraitOrientation, {
+					highlight: {
+						feature: null,
+						temporaryFeature: { data: coordinate }
+					},
+					mainMenu:{
+						open: true
+					}
+				});
+				element.data = data;
+				return element;
+			};
+
+			it('removes the temporary highlight feature and set the permanent highlight feature', async () => {
+				const element = await setupOnClickTests();
+
+				const target = element.shadowRoot.querySelector('li');
+				target.click();
+
+				expect(store.getState().highlight.temporaryFeature).toBeNull();
+				expect(store.getState().highlight.feature.data).toEqual(coordinate);
+			});
+
+			it('removes the temporary highlight feature and sets NO highlight feature when we have an extent', async () => {
+
+				
+				const element = await setupOnClickTests(false, extent);
+
+				const target = element.shadowRoot.querySelector('li');
+				target.click();
+
+				expect(store.getState().highlight.temporaryFeature).toBeNull();
+				expect(store.getState().highlight.feature).toBeNull();
+			});
+
+			it('fits the map to an extent for a coordinate', async () => {
+				const element = await setupOnClickTests();
+
+				const target = element.shadowRoot.querySelector('li');
+				target.click();
+
+				expect(store.getState().position.fitRequest.payload.extent).toEqual([...coordinate, ...coordinate]);
+				expect(store.getState().position.fitRequest.payload.options.maxZoom).toBe(LocationResultItem._maxZoomLevel);
+			});
+
+			it('fits the map to an extent for an extent', async () => {
+				const element = await setupOnClickTests(false, extent);
+
+				const target = element.shadowRoot.querySelector('li');
+				target.click();
+
+				expect(store.getState().position.fitRequest.payload.extent).toEqual(extent);
+				expect(store.getState().position.fitRequest.payload.options.maxZoom).toBe(LocationResultItem._maxZoomLevel);
+			});
+
+			it('closes the main menu in portrait orientation', async () => {
+				const element = await setupOnClickTests(true);
+
+				const target = element.shadowRoot.querySelector('li');
+				target.click();
+
+				expect(store.getState().mainMenu.open).toBeFalse();
+			});
 		});
 	});
 });
