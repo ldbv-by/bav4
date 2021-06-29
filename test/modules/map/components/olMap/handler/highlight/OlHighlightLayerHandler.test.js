09837aa8ca28144c41a7cddbf6201e4de56bc1e9
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-06-29 14:24:58 +0200

    add test

--- test/modules/map/components/olMap/handler/highlight/OlHighlightLayerHandler.test.js
+++ test/modules/map/components/olMap/handler/highlight/OlHighlightLayerHandler.test.js
@@ -8,47 +8,62 @@ import View from 'ol/View';
 import { OSM, TileDebug } from 'ol/source';
 import { OlHighlightLayerHandler } from '../../../../../../../src/modules/map/components/olMap/handler/highlight/OlHighlightLayerHandler';
 describe('OlHighlightLayerHandler', () => {
-    const initialCenter = fromLonLat([11.57245, 48.14021]);
-    const initialState = {
-        active: false,
-        feature: null,
-        temporaryFeature: null
-    };
+	const initialCenter = fromLonLat([11.57245, 48.14021]);
+	const initialState = {
+		active: false,
+		feature: null,
+		temporaryFeature: null
+	};
 
-    const setup = (state = initialState) => {
-        const highlightState = {
-            highlight: state,
-            pointer: { beingDragged: false }
-        };
-        TestUtils.setupStoreAndDi(highlightState, { highlight: highlightReducer });
-    };
+	const setup = (state = initialState) => {
+		const highlightState = {
+			highlight: state,
+			pointer: { beingDragged: false }
+		};
+		TestUtils.setupStoreAndDi(highlightState, { highlight: highlightReducer });
+	};
 
-    const setupMap = () => {
-        const container = document.createElement('div');
-        return new Map({
-            layers: [
-                new TileLayer({
-                    source: new OSM(),
-                }),
-                new TileLayer({
-                    source: new TileDebug(),
-                })],
-            target: container,
-            view: new View({
-                center: initialCenter,
-                zoom: 1,
-            }),
-        });
+	const setupMap = () => {
+		const container = document.createElement('div');
+		return new Map({
+			layers: [
+				new TileLayer({
+					source: new OSM(),
+				}),
+				new TileLayer({
+					source: new TileDebug(),
+				})],
+			target: container,
+			view: new View({
+				center: initialCenter,
+				zoom: 1,
+			}),
+		});
 
-    };
+	};
 
-    it('instantiates the handler', () => {
-        setup();
-        const handler = new OlHighlightLayerHandler();
+	it('instantiates the handler', () => {
+		setup();
+		const handler = new OlHighlightLayerHandler();
 
-        expect(handler).toBeTruthy();
-        expect(handler.id).toBe('highlight_layer');
-        expect(handler._storeService.getStore()).toBeDefined();
-        expect(handler._unregister).toBeDefined();
-    });
+		expect(handler).toBeTruthy();
+		expect(handler.id).toBe('highlight_layer');
+		expect(handler._storeService.getStore()).toBeDefined();
+		expect(handler._unregister).toBeDefined();
+	});
+
+	describe('when activate', () => {
+
+
+		it('registers observer', () => {
+			const map = setupMap();
+			setup();
+
+			const handler = new OlHighlightLayerHandler();
+			const actualLayer = handler.activate(map);
+
+			expect(actualLayer).toBeTruthy();
+			expect(handler._unregister).toBeDefined();
+		});
+	});
 });
