163b05d3ba3f84076095f05873dc7392270273f3
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-07-01 10:55:55 +0200

    update style and tests

--- src/modules/map/components/olMap/handler/highlight/StyleUtils.js
+++ src/modules/map/components/olMap/handler/highlight/StyleUtils.js
@@ -1,37 +1,10 @@
 
-import { Style, Stroke, Fill, Circle as CircleStyle, Icon } from 'ol/style';
+import { Style, Icon } from 'ol/style';
 import locationIcon from './assets/location.svg';
 import tempLocationIcon from './assets/temporaryLocation.svg';
 
 
 export const nullStyleFunction = () => [new Style({})];
-
-export const highlightCircleStyleFunction = () => [new Style({
-	image: new CircleStyle({
-		radius: 6,
-		fill: new Fill({
-			color: [9, 157, 218, 0.9],
-		}),
-		stroke: new Stroke({
-			color: [44, 90, 146, 1],
-			width: 2,
-		}),
-	}),
-})];
-
-export const highlightTemporaryCircleStyleFunction = () => [new Style({
-
-	image: new CircleStyle({
-		radius: 4,
-		fill: new Fill({
-			color: [44, 90, 146, 0.9],
-		}),
-		stroke: new Stroke({
-			color: [9, 157, 218, 1],
-			width: 3,
-		}),
-	}),
-})];
 
 export const highlightFeatureStyleFunction = () => [new Style({
 	image: new Icon({
--- test/modules/map/components/olMap/handler/highlight/StyleUtils.test.js
+++ test/modules/map/components/olMap/handler/highlight/StyleUtils.test.js
@@ -1,36 +1,10 @@
 
-import { highlightCircleStyleFunction, highlightTemporaryCircleStyleFunction, highlightFeatureStyleFunction, highlightTemporaryFeatureStyleFunction, nullStyleFunction } from '../../../../../../../src/modules/map/components/olMap/handler/highlight/StyleUtils';
+import { highlightFeatureStyleFunction, highlightTemporaryFeatureStyleFunction, nullStyleFunction } from '../../../../../../../src/modules/map/components/olMap/handler/highlight/StyleUtils';
 import { Point } from 'ol/geom';
 import { Feature } from 'ol';
 
 
 describe('highlightStyleFunction', () => {
-	it('should create a style for a point-feature', () => {
-		const geometry = new Point([0, 0]);
-		const feature = new Feature({ geometry: geometry });
-
-		const styles = highlightCircleStyleFunction(feature);
-
-		expect(styles).toBeTruthy();
-		expect(styles.length).toBe(1);
-
-		const highlightStyle = styles[0];
-		expect(highlightStyle.getImage()).toBeTruthy();
-	});
-
-	it('should create a style for a temporary point-feature', () => {
-		const geometry = new Point([0, 0]);
-		const feature = new Feature({ geometry: geometry });
-
-		const styles = highlightTemporaryCircleStyleFunction(feature);
-
-		expect(styles).toBeTruthy();
-		expect(styles.length).toBe(1);
-
-		const highlightStyle = styles[0];
-		expect(highlightStyle.getImage()).toBeTruthy();
-	});
-
 	it('should create a svg-icon style for a point-feature', () => {
 		const geometry = new Point([0, 0]);
 		const feature = new Feature({ geometry: geometry });
