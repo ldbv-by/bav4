e2abc8f902e63e9e163ddf88d934c1e3a82625f7
Thomas Aulinger <taulinger@gmx.de>
2021-07-01 10:06:52 +0200

    avoid test coverage calculation for ShowCase.js

--- src/modules/header/components/Header.js
+++ src/modules/header/components/Header.js
@@ -1,11 +1,10 @@
 import { html } from 'lit-html';
-import { BaElement, renderTagOf } from '../../BaElement';
+import { BaElement } from '../../BaElement';
 import { open as openMainMenu, setTabIndex } from '../../menu/store/mainMenu.action';
 import { openModal } from '../../modal/store/modal.action';
 import { $injector } from '../../../injection';
 import css from './header.css';
 import { MainMenuTabIndex } from '../../menu/components/mainMenu/MainMenu';
-import { ShowCase } from '../../utils/components/showCase/ShowCase';
 
 
 /**
@@ -75,7 +74,7 @@ export class Header extends BaElement {
 	createView(state) {
 
 		const showModalInfo = () => {
-			openModal('Showcase', html`${renderTagOf(ShowCase)}`);
+			openModal('Showcase', html`<ba-ba-showcase>`);
 		};
 
 		const getOrientationClass = () => {
