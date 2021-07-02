19906c6133b1a8d1818abf69a170ed120db20e4a
Thomas Aulinger <taulinger@gmx.de>
2021-07-02 11:46:17 +0200

    make null the default value for 'id'

--- test/modules/search/services/domain/searchResult.test.js
+++ test/modules/search/services/domain/searchResult.test.js
@@ -16,7 +16,6 @@ describe('SearchResult', () => {
 
 	it('test constructor and getters', () => {
 		const searchResult = new SearchResult('id', 'label', 'labelFormated', SearchResultTypes.LOCATION);
-
 		expect(searchResult.id).toBe('id');
 		expect(searchResult.label).toBe('label');
 		expect(searchResult.labelFormated).toBe('labelFormated');
@@ -26,6 +25,9 @@ describe('SearchResult', () => {
 
 		const searchResult2 = new SearchResult('id1', 'label1', 'labelFormated1',  SearchResultTypes.GEORESOURCE, [0, 0], [0, 0, 1, 1]);
 		expect(searchResult2.extent).toEqual([0, 0, 1, 1]);
+
+		const searchResult3 = new SearchResult(undefined, 'label1', 'labelFormated1',  SearchResultTypes.GEORESOURCE);
+		expect(searchResult3.id).toBeNull();
 	});
 });
 
