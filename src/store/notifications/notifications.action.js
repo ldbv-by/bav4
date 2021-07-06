902d53d7a2c9ed3699173fa43289b4256afe0340
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-07-06 08:25:54 +0200

    update tests

--- src/store/notifications/notifications.action.js
+++ src/store/notifications/notifications.action.js
@@ -13,8 +13,8 @@ const getStore = () => {
  * @param {string} message the notification message
  * @param {'info'|'warn'|'error'} level the notification level (@see {@link LevelTypes})
  * @param {Boolean} permanent whether the notification invalidates after 
-* a specific amount of time or stays relevant until the user decide to dismiss
-* the message
+ * a specific amount of time or stays relevant until the user decide to dismiss
+ * the message
  */
 export const emitNotification = (message, level, isPermanent) => {
 	getStore().dispatch({
