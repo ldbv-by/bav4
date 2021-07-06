966376532d37e1b736aebf187b233d7307a1ecb3
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-07-06 14:42:05 +0200

    update tests

--- test/modules/map/components/mapButtonsContainer/MapButtonsContainer.test.js
+++ test/modules/map/components/mapButtonsContainer/MapButtonsContainer.test.js
@@ -18,11 +18,12 @@ describe('MapButtonsContainer', () => {
 
 			const element = await setup();
 
-			expect(element.shadowRoot.querySelector('div').children).toHaveSize(4);
+			expect(element.shadowRoot.querySelector('div').children).toHaveSize(5);
 			expect(element.shadowRoot.querySelectorAll('ba-rotation-button')).toHaveSize(1);
 			expect(element.shadowRoot.querySelectorAll('ba-geolocation-button')).toHaveSize(1);
 			expect(element.shadowRoot.querySelectorAll('ba-zoom-buttons')).toHaveSize(1);
 			expect(element.shadowRoot.querySelectorAll('ba-extent-button')).toHaveSize(1);
+			expect(element.shadowRoot.querySelectorAll('ba-info-button')).toHaveSize(1);
 		});
 	});
 });
