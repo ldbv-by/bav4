7257f1fc3d1c98f1ffc18f8d7a42e2feaa1bfb45
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-07-06 14:15:26 +0200

    update to optional param, typo  as suggested

--- src/store/notifications/notifications.action.js
+++ src/store/notifications/notifications.action.js
@@ -12,11 +12,11 @@ const getStore = () => {
  * emits a new notification to the system
  * @param {string} message the notification message
  * @param {'info'|'warn'|'error'} level the notification level (@see {@link LevelTypes})
- * @param {Boolean} permanent whether the notification invalidates after 
- * a specific amount of time or stays relevant until the user decide to dismiss
+ * @param {Boolean} [isPermanent=false] whether the notification invalidates after 
+ * a specific amount of time or stays relevant until the user decides to dismiss
  * the message
  */
-export const emitNotification = (message, level, isPermanent) => {
+export const emitNotification = (message, level, isPermanent = false) => {
 	getStore().dispatch({
 		type: NOTIFICATION_ADDED,
 		payload: new EventLike({ message:message, level:level, permanent:isPermanent })
--- src/store/notifications/notifications.reducer.js
+++ src/store/notifications/notifications.reducer.js
@@ -17,7 +17,7 @@ export const LevelTypes = Object.freeze({
 * @property {string} message the notification message
 * @property {'info'|'warn'|'error'} level the notification level (@see {@link LevelTypes})
 * @property {Boolean} permanent whether the notification invalidates after 
-* a specific amount of time or stays relevant until the user decide to dismiss
+* a specific amount of time or stays relevant until the user decides to dismiss
 * the message
  */
 
