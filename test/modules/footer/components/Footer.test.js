dc3efbe5ec562f66ecd063efb29c78904f63fb85
Thomas Aulinger <taulinger@gmx.de>
2021-07-07 14:13:27 +0200

    use global media state instead of local evaluation

--- test/modules/footer/components/Footer.test.js
+++ test/modules/footer/components/Footer.test.js
@@ -4,6 +4,7 @@ import { Footer } from '../../../../src/modules/footer/components/Footer';
 import { TestUtils } from '../../../test-utils.js';
 import { $injector } from '../../../../src/injection';
 import { mainMenuReducer } from '../../../../src/modules/menu/store/mainMenu.reducer';
+import { createNoInitialStateMediaReducer } from '../../../../src/store/media/media.reducer';
 
 window.customElements.define(Footer.tag, Footer);
 
@@ -14,16 +15,20 @@ describe('Footer', () => {
 		matchMedia() { }
 	};
 
-	const setup = (config = {}) => {
+	const setup = (state = {}, config = {}) => {
 		const { embed = false } = config;
 
-		const state = {
+		const stateOfStore = {
 			mainMenu: {
 				open: true
-			}
+			},
+			media: {
+				portrait: false
+			},
+			...state,
 		};
 
-		TestUtils.setupStoreAndDi(state,  { mainMenu: mainMenuReducer });
+		TestUtils.setupStoreAndDi(stateOfStore, { mainMenu: mainMenuReducer, media: createNoInitialStateMediaReducer() });
 		$injector.registerSingleton('EnvironmentService', {
 			isEmbedded: () => embed,
 			getWindow: () => windowMock
@@ -33,62 +38,57 @@ describe('Footer', () => {
 	};
 
 	describe('responsive layout ', () => {
+
 		it('layouts with open main menu for landscape mode', async () => {
+			const state = {
+				media: {
+					portrait: false
+				}
+			};
+			const element = await setup(state);
 
-			const matchMediaSpy = spyOn(windowMock, 'matchMedia')
-				.withArgs('(orientation: portrait)').and.returnValue(TestUtils.newMediaQueryList(false));
-
-			const element = await setup();
-
-			expect(element.shadowRoot.querySelector('.footer.is-open')).toBeTruthy();
-			expect(element.shadowRoot.querySelector('.content')).toBeTruthy();
+			expect(element.shadowRoot.querySelectorAll('.footer.is-open')).toHaveSize(1);
+			expect(element.shadowRoot.querySelectorAll('.content')).toHaveSize(1);
 			expect(window.getComputedStyle(element.shadowRoot.querySelector('.content')).display).toBe('block');
-			expect(element.shadowRoot.querySelector('ba-map-info')).toBeTruthy();
-			expect(matchMediaSpy).toHaveBeenCalledTimes(1);
+			expect(element.shadowRoot.querySelectorAll('ba-map-info')).toHaveSize(1);
 		});
 
 		it('layouts with open main menu for portrait mode', async () => {
+			const state = {
+				media: {
+					portrait: true
+				}
+			};
 
-			const matchMediaSpy = spyOn(windowMock, 'matchMedia')
-				.withArgs('(orientation: portrait)').and.returnValue(TestUtils.newMediaQueryList(true));
+			const element = await setup(state);
 
-			const element = await setup();
-
-			expect(element.shadowRoot.querySelector('.footer.is-open')).toBeFalsy();
-			expect(element.shadowRoot.querySelector('.content')).toBeTruthy();
+			expect(element.shadowRoot.querySelectorAll('.footer.is-open')).toHaveSize(0);
+			expect(element.shadowRoot.querySelectorAll('.content')).toHaveSize(1);
 			expect(window.getComputedStyle(element.shadowRoot.querySelector('.content')).display).toBe('none');
-			expect(element.shadowRoot.querySelector('ba-map-info')).toBeTruthy();
-			expect(matchMediaSpy).toHaveBeenCalledTimes(1);
+			expect(element.shadowRoot.querySelectorAll('ba-map-info')).toHaveSize(1);
 		});
 	});
 
 
 	describe('when initialized', () => {
 
-		beforeEach(function () {
-			spyOn(windowMock, 'matchMedia')
-				.withArgs('(orientation: portrait)').and.returnValue(TestUtils.newMediaQueryList(true));
-		});
-
 		it('removes a preload css class', async () => {
 			const element = await setup();
-			expect(element.shadowRoot.querySelector('.preload')).toBeFalsy();
+			
+			expect(element.shadowRoot.querySelectorAll('.preload')).toHaveSize(0);
 		});
 
 		it('adds footer elements and css classes for landscape mode', async () => {
+			const element = await setup({}, { portrait: false });
 
-			const element = await setup({ portrait: false });
-			
-			expect(element.shadowRoot.querySelector('.footer')).toBeTruthy();
-			expect(element.shadowRoot.querySelector('.content')).toBeTruthy();
-			expect(element.shadowRoot.querySelector('ba-map-info')).toBeTruthy();
-			expect(element.shadowRoot.querySelector('ba-attribution-info')).toBeTruthy();
+			expect(element.shadowRoot.querySelectorAll('.footer')).toHaveSize(1);
+			expect(element.shadowRoot.querySelectorAll('.content')).toHaveSize(1);
+			expect(element.shadowRoot.querySelectorAll('ba-map-info')).toHaveSize(1);
+			expect(element.shadowRoot.querySelectorAll('ba-attribution-info')).toHaveSize(1);
 		});
 
-
 		it('renders nothing when embedded', async () => {
-
-			const element = await setup({ embed: true });
+			const element = await setup({}, { embed: true });
 
 			expect(element.shadowRoot.children.length).toBe(0);
 		});
