18ce2362ea7f5aa1187400ec1df4b4ccabb9a6a4
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-07-14 13:22:17 +0200

    update to jasmine.clock as suggested

--- test/modules/notifications/components/NotificationItem.test.js
+++ test/modules/notifications/components/NotificationItem.test.js
@@ -7,77 +7,80 @@ window.customElements.define(NotificationItem.tag, NotificationItem);
 
 describe('NotificationItem', () => {
 	const notificationTemplate = {
-		message:null,
-		level:LevelTypes.INFO,
-		permanent:false,
-		id:1234,
-		index:0,
-		autocloseTime:0
+		message: null,
+		level: LevelTypes.INFO,
+		permanent: false,
+		id: 1234,
+		index: 0,
+		autocloseTime: 0
 	};
 
 	describe('when notification item is rendered', () => {
+
+		beforeEach(function () {
+			jasmine.clock().install();
+		});
+
+		afterEach(function () {
+			jasmine.clock().uninstall();
+		});
+
 		const setup = async (notification) => {
 			TestUtils.setupStoreAndDi({}, { notifications: notificationReducer });
-			$injector.registerSingleton('TranslationService', {	 translate: (key) => key });
-			const  element = await TestUtils.render(NotificationItem.tag);
-			element.content =  notification ;
+			$injector.registerSingleton('TranslationService', { translate: (key) => key });
+			const element = await TestUtils.render(NotificationItem.tag);
+			element.content = notification;
 			return element;
 		};
 
 		it('displays the notification message', async () => {
-			const element = await setup({ ...notificationTemplate, message:'FooBar' });
+			const element = await setup({ ...notificationTemplate, message: 'FooBar' });
 			const contentElement = element.shadowRoot.querySelector('.notification_content');
 
 			expect(contentElement.innerText).toContain('FooBar');
 		});
 
-		it('starts hiding with autoclose after 1 sec.', async(done) => {
+		it('starts hiding with autoclose after 1 sec.', async () => {
 			const autocloseTime = 1000;
-			const laterThenAutoCloseTime = autocloseTime + 100;            
-			const notification = { ...notificationTemplate, message:'FooBar', autocloseTime:autocloseTime };
+			const laterThenAutoCloseTime = autocloseTime + 100;
+			const notification = { ...notificationTemplate, message: 'FooBar', autocloseTime: autocloseTime };
 
 			const element = await setup(notification);
 			const hideSpy = spyOn(element, '_hide').and.callThrough();
 
-			setTimeout(() => {
-				expect(hideSpy).toHaveBeenCalled();
-				done();
-			}, laterThenAutoCloseTime);
-			
+			jasmine.clock().tick(laterThenAutoCloseTime);
+
+			expect(hideSpy).toHaveBeenCalled();
 		});
 
-		it('starts hiding with click on Close-Button', async() => {
+		it('starts hiding with click on Close-Button', async () => {
 			const autocloseTime = 1000;
-			const notification = { ...notificationTemplate, message:'FooBar', autocloseTime:autocloseTime };
+			const notification = { ...notificationTemplate, message: 'FooBar', autocloseTime: autocloseTime };
 
 			const element = await setup(notification);
 			const hideSpy = spyOn(element, '_hide').and.callThrough();
 			const closeElement = element.shadowRoot.querySelector('.notification_close');
 			closeElement.click();
 
-			expect(hideSpy).toHaveBeenCalled();			
+			expect(hideSpy).toHaveBeenCalled();
 		});
 
-		it('closes the notification item with call of onClose', async(done) => {
+		it('closes the notification item with call of onClose', async () => {
 			const autocloseTime = 1000;
-			const laterThenAutoCloseTime = autocloseTime + 100;            
-			const notification = { ...notificationTemplate, message:'FooBar', autocloseTime:autocloseTime };            
+			const laterThenAutoCloseTime = autocloseTime + 100;
+			const notification = { ...notificationTemplate, message: 'FooBar', autocloseTime: autocloseTime };
 
 			const element = await setup(notification);
 			element.onClose = jasmine.createSpy();
-			const hideSpy = spyOn(element, '_hide').and.callThrough();			
+			const hideSpy = spyOn(element, '_hide').and.callThrough();
 			const notificationElement = element.shadowRoot.querySelector('.notification_item');
 
-			setTimeout(() => {
-				notificationElement.dispatchEvent(new Event('transitionend'));
-				expect(hideSpy).toHaveBeenCalled();
+			jasmine.clock().tick(laterThenAutoCloseTime);
+			notificationElement.dispatchEvent(new Event('transitionend'));
+			expect(hideSpy).toHaveBeenCalled();
 
-				setTimeout(() => {
-					expect(element.onClose).toHaveBeenCalledWith(notification);
-					done();    
-				});
-				
-			}, laterThenAutoCloseTime);			
+
+			expect(element.onClose).toHaveBeenCalledWith(notification);
 		});
 	});
 });
