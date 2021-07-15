031b867fd8fe630b39935d83dfa40178c38b9b4c
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-07-15 15:15:27 +0200

    add interactions to draw

--- src/modules/map/components/olMap/handler/draw/OlDrawHandler.js
+++ src/modules/map/components/olMap/handler/draw/OlDrawHandler.js
@@ -1,8 +1,13 @@
-import { DRAW_LAYER_ID } from '../../../../store/DrawPlugin';
+import { DRAW_LAYER_ID, DRAW_TOOL_ID } from '../../../../store/DrawPlugin';
 import { OlLayerHandler } from '../OlLayerHandler';
 import { Vector as VectorSource } from 'ol/source';
 import { Vector as VectorLayer } from 'ol/layer';
 import { $injector } from '../../../../../../injection';
+import { DragPan, Draw, Modify, Select, Snap } from 'ol/interaction';
+import { modifyStyleFunction, createSketchStyleFunction, createSelectStyleFunction } from '../../olStyleUtils';
+import { StyleTypes } from '../../services/StyleService';
+import { noModifierKeys, singleClick } from 'ol/events/condition';
+import MapBrowserEventType from 'ol/MapBrowserEventType';
 
 /**
  * Handler for draw-interaction with the map
@@ -24,6 +29,8 @@ export class OlDrawHandler extends OlLayerHandler {
 		this._styleService = StyleService;
 
 		this._vectorLayer = null;
+		this._draw = false;
+		this._activeSketch = null;
 	}
 
 	/**
@@ -31,8 +38,6 @@ export class OlDrawHandler extends OlLayerHandler {
 	 * @override
 	 */
 	onActivate(olMap) {
-		this._map = olMap;
-
 		const createLayer = () => {
 			const source = new VectorSource({ wrapX: false });
 			const layer = new VectorLayer({
@@ -40,9 +45,29 @@ export class OlDrawHandler extends OlLayerHandler {
 			});
 			return layer;
 		};
+		
+		if (this._draw === false) {
+			this._map = olMap;
+			this._vectorLayer = createLayer();
 
-		this._vectorLayer = createLayer();
+			const source = this._vectorLayer.getSource();
+			this._select = this._createSelect();
+			this._select.setActive(false);
+			this._modify = this._createModify();
+			this._modify.setActive(false);
+			this._draw = this._createDraw(source, 'Polygon');
+			this._snap = new Snap({ source: source, pixelTolerance: this._getSnapTolerancePerDevice() });
+			this._dragPan = new DragPan();
+			this._dragPan.setActive(false);
 
+
+			olMap.addInteraction(this._select);
+			olMap.addInteraction(this._modify);
+			olMap.addInteraction(this._draw);
+			olMap.addInteraction(this._snap);
+			olMap.addInteraction(this._dragPan);
+		}
+		
 		return this._vectorLayer;
 	}
 
@@ -55,7 +80,88 @@ export class OlDrawHandler extends OlLayerHandler {
 	onDeactivate(olMap) {
 		//use the map to unregister event listener, interactions, etc
 		//olLayer currently undefined, will be fixed later		
+		olMap.removeInteraction(this._draw);
+		olMap.removeInteraction(this._modify);
+		olMap.removeInteraction(this._snap);
+		olMap.removeInteraction(this._select);
+		olMap.removeInteraction(this._dragPan);
 
 		this._map = null;
+	}
+
+	_createDraw(source, drawType) {
+		const draw = new Draw({
+			source: source,
+			type: drawType,
+			minPoints: 2,
+			snapTolerance: this._getSnapTolerancePerDevice(),
+			style: createSketchStyleFunction(this._styleService.getStyleFunction(StyleTypes.MEASURE))
+		});
+
+
+		draw.on('drawstart', event => {
+			this._activeSketch = event.feature;
+			this._pointCount = 1;
+			this._isSnapOnLastPoint = false;
+	
+			this._activeSketch.setId(DRAW_TOOL_ID + '_' + new Date().getTime());
+			this._overlayService.add(this._activeSketch, this._map, StyleTypes.DRAW);
+		});
+
+		draw.on('drawabort', event => this._overlayService.remove(event.feature, this._map));
+		draw.on('drawend', event => this._activateModify(event.feature));
+		return draw;
+	}
+
+
+	_createSelect() {
+		const layerFilter = (itemLayer) => {
+			itemLayer === this._vectorLayer;
+		};
+		const featureFilter = (itemFeature, itemLayer) => {
+			if (layerFilter(itemLayer)) {
+				return itemFeature;
+			}
+		};
+		const options = {
+			layers: layerFilter,
+			filter: featureFilter,
+			style: createSelectStyleFunction(this._styleService.getStyleFunction(StyleTypes.MEASURE))
+		};
+		const select = new Select(options);
+		select.getFeatures().on('change:length', this._updateStatistics);
+
+		return select;
+	}
+
+	_createModify() {
+		const options = {
+			features: this._select.getFeatures(),
+			style: modifyStyleFunction,
+			deleteCondition: event => {
+				const isDeletable = (noModifierKeys(event) && singleClick(event));
+				return isDeletable;
+			}
+		};
+
+		const modify = new Modify(options);
+		modify.on('modifystart', (event) => {
+			if (event.mapBrowserEvent.type !== MapBrowserEventType.SINGLECLICK) {
+				this._mapContainer.classList.add('grabbing');
+			}
+		});
+		modify.on('modifyend', event => {
+			if (event.mapBrowserEvent.type === MapBrowserEventType.POINTERUP || event.mapBrowserEvent.type === MapBrowserEventType.CLICK) {
+				this._mapContainer.classList.remove('grabbing');
+			}
+		});
+		return modify;
+	}
+
+	_getSnapTolerancePerDevice() {
+		if (this._environmentService.isTouch()) {
+			return 12;
+		}
+		return 4;
 	}
 }
