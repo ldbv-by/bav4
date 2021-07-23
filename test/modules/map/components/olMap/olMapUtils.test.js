659b1a890e2728261c4d8d975c114a8601a83be2
Thomas Aulinger <taulinger@gmx.de>
2021-07-23 12:23:31 +0200

    improve test coverage

--- test/modules/map/components/olMap/olMapUtils.test.js
+++ test/modules/map/components/olMap/olMapUtils.test.js
@@ -33,6 +33,31 @@ describe('olMapUtils', () => {
 
 			expect(myLayer.get('id')).toBe('someId');
 		});
+
+		it('retrieves an olLayer from a handler', () => {
+			const mockHandler = {
+				activate() { }
+			};
+			const map = new Map();
+			const olLayer = new BaseLayer({});
+			spyOn(mockHandler, 'activate').withArgs(map).and.returnValue(olLayer);
+
+			const myLayer = toOlLayerFromHandler('someId', mockHandler, map);
+
+			expect(myLayer.get('id')).toBe('someId');
+		});
+
+		it('it passes return values from a handler', () => {
+			const mockHandler = {
+				activate() { }
+			};
+			const map = new Map();
+			spyOn(mockHandler, 'activate').withArgs(map).and.returnValue(null);
+
+			const myLayer = toOlLayerFromHandler('someId', mockHandler, map);
+
+			expect(myLayer).toBeNull();
+		});
 	});
 
 	describe('registerLongPressListener', () => {
@@ -123,6 +148,20 @@ describe('olMapUtils', () => {
 			simulateMouseEvent(map, MapBrowserEventType.POINTERUP);
 
 			expect(spy).not.toHaveBeenCalled();
+		});
+
+		it('does nothing on pointer move WITHOUT dragging)', () => {
+			const defaultDelay = 300;
+			const spy = jasmine.createSpy();
+			const map = new Map();
+			registerLongPressListener(map, spy);
+
+			simulateMouseEvent(map, MapBrowserEventType.POINTERDOWN);
+			simulateMouseEvent(map, MapBrowserEventType.POINTERMOVE, 0, 0, false);
+			jasmine.clock().tick(defaultDelay + 100);
+			simulateMouseEvent(map, MapBrowserEventType.POINTERUP);
+
+			expect(spy).toHaveBeenCalled();
 		});
 	});
 });
