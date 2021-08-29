6c9ab46fd331280f6ca6e21c0deb35ae3874f71d
Thomas Aulinger <taulinger@gmx.de>
2021-08-29 21:55:48 +0200

    fix broken event handler bindings

--- src/modules/modal/store/modal.action.js
+++ src/modules/modal/store/modal.action.js
@@ -2,9 +2,8 @@
  * Action creators to change/update the properties of modal state.
  * @module modal/action
  */
-import { MODAL_CONTENT_ID, MODAL_CHANGED } from './modal.reducer';
+import { MODAL_CHANGED } from './modal.reducer';
 import { $injector } from '../../../injection';
-import { append } from '../../BaElement';
 
 const getStore = () => {
 	const { StoreService } = $injector.inject('StoreService');
@@ -20,17 +19,16 @@ const getStore = () => {
 /**
  * Opens the modal.
  * @param {string} title The title of the modal
- * @param {string} content  The content of the modal. Could either be a a plain string, a template string or a lit-html TemplateResult.
+ * @param {string|TemplateResult} content  The content of the modal. Could either be a a plain string or a lit-html TemplateResult.
  * @param {ModalOptions} options
  */
 export const openModal = (title, content, options = {}) => {
-
-	append(content, MODAL_CONTENT_ID);
 
 	getStore().dispatch({
 		type: MODAL_CHANGED,
 		payload: {
 			title: title,
+			content: content,
 			options: options
 		}
 	});
