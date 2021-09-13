b284fc99223be3d431dd5058dfda2b361ab1f2a1
schle_th <thilo.schlemmer@ldbv.bayern.de>
2021-09-13 14:30:00 +0200

    make lint happy

--- test/modules/map/components/olMap/BaOverlay.test.js
+++ test/modules/map/components/olMap/BaOverlay.test.js
@@ -7,78 +7,78 @@ window.customElements.define(BaOverlay.tag, BaOverlay);
 
 describe('BaOverlay', () => {
 
-    beforeEach(async () => {
-        TestUtils.setupStoreAndDi({});
-        // eslint-disable-next-line no-unused-vars
-    });
+	beforeEach(async () => {
+		TestUtils.setupStoreAndDi({});
+		// eslint-disable-next-line no-unused-vars
+	});
 
-    const setup = async (properties = {}) => {
-        const element = await TestUtils.render(BaOverlay.tag);
+	const setup = async (properties = {}) => {
+		const element = await TestUtils.render(BaOverlay.tag);
 
-        // transform test-geometries from assumed default geodetic projection to default map-projection
-        if (properties.geometry) {
-            properties.geometry.transform('EPSG:25832', 'EPSG:3857');
-        }
-        for (const property in properties) {
-            element[property] = properties[property];
-        }
-        return element;
-    };
+		// transform test-geometries from assumed default geodetic projection to default map-projection
+		if (properties.geometry) {
+			properties.geometry.transform('EPSG:25832', 'EPSG:3857');
+		}
+		for (const property in properties) {
+			element[property] = properties[property];
+		}
+		return element;
+	};
 
 
-    describe('when initialized with type property', () => {
-        it('renders the text view', async () => {
-            const element = await setup();
+	describe('when initialized with type property', () => {
+		it('renders the text view', async () => {
+			const element = await setup();
 
-            expect(element.type).toBe(BaOverlayTypes.TEXT);
-            expect(element.static).toBeFalse();
-            expect(element.value).toBe('');
-        });
+			expect(element.type).toBe(BaOverlayTypes.TEXT);
+			expect(element.static).toBeFalse();
+			expect(element.value).toBe('');
+		});
 
-        it('renders the help view', async () => {
-            const properties = { type: BaOverlayTypes.HELP, value: 'foo' };
-            const element = await setup(properties);
-            const div = element.shadowRoot.querySelector('div');
+		it('renders the help view', async () => {
+			const properties = { type: BaOverlayTypes.HELP, value: 'foo' };
+			const element = await setup(properties);
+			const div = element.shadowRoot.querySelector('div');
 
-            expect(div.classList.contains('help')).toBeTrue();
-            expect(div.classList.contains('floating')).toBeFalse();
-            expect(div.classList.contains('static')).toBeFalse();
-            expect(element.type).toBe(BaOverlayTypes.HELP);
-            expect(element.static).toBeFalse();
-            expect(element.value).toBe('foo');
-        });
-    });
+			expect(div.classList.contains('help')).toBeTrue();
+			expect(div.classList.contains('floating')).toBeFalse();
+			expect(div.classList.contains('static')).toBeFalse();
+			expect(element.type).toBe(BaOverlayTypes.HELP);
+			expect(element.static).toBeFalse();
+			expect(element.value).toBe('foo');
+		});
+	});
 
-    describe('when type changed', () => {
-        it('renders the changed view', async () => {
+	describe('when type changed', () => {
+		it('renders the changed view', async () => {
 
-            const element = await setup();
-            const div = element.shadowRoot.querySelector('div');
+			const element = await setup();
+			const div = element.shadowRoot.querySelector('div');
 
-            expect(element.type).toBe(BaOverlayTypes.TEXT);
-            expect(element.static).toBeFalse();
-            expect(element.value).toBe('');
+			expect(element.type).toBe(BaOverlayTypes.TEXT);
+			expect(element.static).toBeFalse();
+			expect(element.value).toBe('');
 
-            element.type = BaOverlayTypes.HELP;
+			element.type = BaOverlayTypes.HELP;
 
-            expect(div.classList.contains('help')).toBeTrue();
-            expect(div.classList.contains('floating')).toBeFalse();
-            expect(element.type).toBe(BaOverlayTypes.HELP);
-            expect(element.static).toBeFalse();
-        });
-    });
+			expect(div.classList.contains('help')).toBeTrue();
+			expect(div.classList.contains('floating')).toBeFalse();
+			expect(element.type).toBe(BaOverlayTypes.HELP);
+			expect(element.static).toBeFalse();
+		});
+	});
 
-    describe('when geometry changed', () => {
-        it('updates the position', async () => {
-            const geometry = new Point(0, 0);
-            const element = await setup();
-            const positionSpy = spyOn(element, '_updatePosition').and.callThrough();
+	describe('when geometry changed', () => {
+		it('updates the position', async () => {
+			const geometry = new Point(0, 0);
+			const element = await setup();
+			const positionSpy = spyOn(element, '_updatePosition').and.callThrough();
 
-            element.geometry = geometry;
+			element.geometry = geometry;
 
-            expect(positionSpy).toHaveBeenCalled();
-            expect(element.geometry).toBeTruthy();
-            expect(element.position).toBeTruthy();
-        });
-    });
+			expect(positionSpy).toHaveBeenCalled();
+			expect(element.geometry).toBeTruthy();
+			expect(element.position).toBeTruthy();
+		});
+	});
 });
