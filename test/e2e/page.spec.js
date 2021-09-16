98faca7f912077e880fbadf83222d512c911abf7
Thomas Aulinger <taulinger@gmx.de>
2021-09-16 09:33:13 +0200

    rename tests

--- test/e2e/page.spec.js
+++ test/e2e/page.spec.js
@@ -12,16 +12,16 @@ test.describe('page', () => {
 
 	test.describe('when loaded', () => {
 
-		test('should have a lang attribute', async ({ page }) => {
+		test('should contain a lang attribute', async ({ page }) => {
 			expect(await page.$('html[lang=\'en\']')).toBeTruthy();
 		});
 
-		test('should have a title tag', async ({ page }) => {
+		test('should contain a title tag', async ({ page }) => {
 			const title = await page.title();
 			expect(title).toBe('BAv4 (#nomigration)');
 		});
 
-		test('should have a viewport meta tag', async ({ page }) => {
+		test('should contain a viewport meta tag', async ({ page }) => {
 			const content = await page.getAttribute('head > meta[name=\'viewport\']', 'content');
 
 			expect(content).toContain('width=device-width');
@@ -30,49 +30,49 @@ test.describe('page', () => {
 			expect(content).toContain('user-scalable=0');
 		});
 
-		test('should have a charset meta tag', async ({ page }) => {
+		test('should contain a charset meta tag', async ({ page }) => {
 			expect(await page.$('head > meta[charset=\'utf-8\']')).toBeTruthy();
 		});
 
-		test('should have a <ba-header> component', async ({ page }) => {
+		test('should contain a <ba-header> component', async ({ page }) => {
 			expect(await page.$$('ba-header')).toHaveLength(1);
 		});
 
-		test('should have a <ba-main-menu> component', async ({ page }) => {
+		test('should contain a <ba-main-menu> component', async ({ page }) => {
 			expect(await page.$$('ba-main-menu')).toHaveLength(1);
 		});
-		test('should have a <ba-ol-map> component', async ({ page }) => {
+		test('should contain a <ba-ol-map> component', async ({ page }) => {
 			expect(await page.$$('ba-ol-map')).toHaveLength(1);
 		});
 
-		test('should have a <ba-map-button-container> component', async ({ page }) => {
+		test('should contain a <ba-map-button-container> component', async ({ page }) => {
 			expect(await page.$$('ba-map-button-container')).toHaveLength(1);
 		});
 
-		test('should have a <ba-tool-bar> component', async ({ page }) => {
+		test('should contain a <ba-tool-bar> component', async ({ page }) => {
 			expect(await page.$$('ba-tool-bar')).toHaveLength(1);
 		});
 
-		test('should have a <ba-footer> component', async ({ page }) => {
+		test('should contain a <ba-footer> component', async ({ page }) => {
 			expect(await page.$$('ba-footer')).toHaveLength(1);
 		});
 
-		test('should have a <ba-tool-container> component', async ({ page }) => {
+		test('should contain a <ba-tool-container> component', async ({ page }) => {
 			expect(await page.$$('ba-tool-container')).toHaveLength(1);
 		});
 
-		test('should have a <ba-context-menue> component', async ({ page }) => {
+		test('should contain a <ba-context-menue> component', async ({ page }) => {
 			expect(await page.$$('ba-context-menue')).toHaveLength(1);
 		});
-		test('should have a <ba-nonembedded-hint> component', async ({ page }) => {
+		test('should contain a <ba-nonembedded-hint> component', async ({ page }) => {
 			expect(await page.$$('ba-nonembedded-hint')).toHaveLength(1);
 		});
 
-		test('should have a <ba-theme-provider> component', async ({ page }) => {
+		test('should contain a <ba-theme-provider> component', async ({ page }) => {
 			expect(await page.$$('ba-theme-provider')).toHaveLength(1);
 		});
 
-		test('should have a <ba-modal> component', async ({ page }) => {
+		test('should contain a <ba-modal> component', async ({ page }) => {
 			expect(await page.$$('ba-modal')).toHaveLength(1);
 		});
 	});
