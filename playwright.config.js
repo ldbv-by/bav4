530e4ea508f77c0ba004040bacc23d113ec62c42
Thomas Aulinger <taulinger@gmx.de>
2021-09-16 09:18:01 +0200

    apply lint rules

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,4 @@
 module.exports = {
 	testDir: 'test/e2e',
-	reporter: [['list'], ['junit', { outputFile: 'reports/e2e/results.xml' }]],
+	reporter: [['dot'], ['junit', { outputFile: 'reports/e2e/results.xml' }]]
 };
