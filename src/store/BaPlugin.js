a7124c0fe1e381bd87595277a58ffbeab81bfa14
Thomas Aulinger <taulinger@gmx.de>
2021-10-11 13:49:42 +0200

    update docs

--- src/store/BaPlugin.js
+++ src/store/BaPlugin.js
@@ -1,9 +1,9 @@
 /**
 * Abstract base class for all BaPlugins.
-* Beside {@link BaElement},  BaPlugins are a second important place for structuring code and logic.
-* As they are connected to the central redux store, they are responsible for setting initial state or reacting
-* on state changes during the runtime of the app. In contrast to BaElement, they often act on a higher abstraction level,
-* managing global state being consumed by different BaElements implementations afterwards.
+* `BaPlugins` implementations are a second important place for structuring code and logic.
+* In contrast to components, they often act on a higher abstraction level,
+* managing global state being consumed by different components afterward.
+* For example, they could be responsible for setting an initial state or reacting to global state changes during the runtime of the app.
 * <br>
 * StorePlugins must implement {@link BaPlugin#register}, which is called by the global {@link StoreService}
 * after all dependency injection is done.
