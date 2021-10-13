7dff3b6e8dd9d67248ae319415e61afb4f491139
Thomas Aulinger <taulinger@gmx.de>
2021-10-13 17:50:43 +0200

    add a ContentPanel to display FeatureInfo items

---
+++ src/modules/featureInfo/components/FeatureInfoPanel.js
@@ -0,0 +1,61 @@
+import { html } from 'lit-html';
+import { $injector } from '../../../injection';
+import { clear } from '../../../store/featureInfo/featureInfo.action';
+import { AbstractMvuContentPanel } from '../../menu/components/mainMenu/content/AbstractMvuContentPanel';
+import css from './featureInfoPanel.css';
+
+const Update_FeatureInfo_Data = 'update_featureInfo_data';
+
+/**
+ * @class
+ * @author taulinger
+ */
+export class FeatureInfoPanel extends AbstractMvuContentPanel {
+
+	constructor() {
+		super({
+			featureInfoData: []
+		});
+
+		const { TranslationService } = $injector.inject('TranslationService');
+		this._translationService = TranslationService;
+
+		this.observe(store => store.featureInfo.current, current => this.signal(Update_FeatureInfo_Data, [...current]));
+	}
+
+	/**
+	 * @override
+	 */
+	update(type, data, model) {
+		switch (type) {
+			case Update_FeatureInfo_Data:
+				return { ...model, featureInfoData: [...data] };
+		}
+	}
+
+	/**
+	 *@override
+	 */
+	createView(model) {
+
+		const { featureInfoData } = model;
+		const translate = (key) => this._translationService.translate(key);
+
+		return html`
+        <style>${css}</style>
+		<div>
+			<ba-icon .title=${translate('featureInfo_close_button')} .type=${'primary'} @click=${clear}></ba-icon>
+			<div class="container">
+			${featureInfoData.map((item) => html`
+			<li class="item"><h3>${item.title}</h3>
+			${item.content}
+			</li>`)}
+			<div>
+		</div>
+        `;
+	}
+
+	static get tag() {
+		return 'ba-feature-info-panel';
+	}
+}
---
+++ test/modules/featureInfo/components/FeatureInfoPanel.test.js
@@ -0,0 +1,139 @@
+/* eslint-disable no-undef */
+import { TestUtils } from '../../../test-utils.js';
+import { $injector } from '../../../../src/injection';
+import { FeatureInfoPanel } from '../../../../src/modules/featureInfo/components/FeatureInfoPanel';
+import { featureInfoReducer } from '../../../../src/store/featureInfo/featureInfo.reducer';
+import { AbstractMvuContentPanel } from '../../../../src/modules/menu/components/mainMenu/content/AbstractMvuContentPanel.js';
+import { add } from '../../../../src/store/featureInfo/featureInfo.action.js';
+
+window.customElements.define(FeatureInfoPanel.tag, FeatureInfoPanel);
+
+
+
+describe('FeatureInfoPanel', () => {
+
+	let store;
+
+	const setup = (state) => {
+
+		store = TestUtils.setupStoreAndDi(state, { featureInfo: featureInfoReducer });
+		$injector
+			.registerSingleton('TranslationService', { translate: (key) => key });
+		return TestUtils.render(FeatureInfoPanel.tag);
+	};
+
+	describe('class', () => {
+
+		it('inherits from AbstractContentPanel', async () => {
+
+			const element = await setup();
+
+			expect(element instanceof AbstractMvuContentPanel).toBeTrue();
+		});
+	});
+
+	describe('when initialized', () => {
+
+		describe('and no featureInfo items are available', () => {
+
+			it('renders a close icon-button, a container and no items', async () => {
+
+				const element = await setup();
+				const button = element.shadowRoot.querySelector('ba-icon');
+				const container = element.shadowRoot.querySelectorAll('.container');
+				const items = element.shadowRoot.querySelectorAll('.item');
+
+				expect(button.title).toBe('featureInfo_close_button');
+				expect(container).toHaveSize(1);
+				expect(items).toHaveSize(0);
+			});
+		});
+
+		describe('and featureInfo items are available', () => {
+
+			it('renders a close icon-button, a container and no items', async () => {
+
+				const element = await setup({
+					featureInfo: {
+						current: [{ title: 'title0', content: 'content0' }, { title: 'title1', content: 'content1' }]
+					}
+				});
+				const button = element.shadowRoot.querySelector('ba-icon');
+				const container = element.shadowRoot.querySelectorAll('.container');
+				const items = element.shadowRoot.querySelectorAll('.item');
+
+				expect(button.title).toBe('featureInfo_close_button');
+				expect(container).toHaveSize(1);
+				expect(items).toHaveSize(2);
+			});
+		});
+	});
+
+	describe('when initialized', () => {
+
+		describe('and no featureInfo items are available', () => {
+
+			it('renders a close icon-button, a container and no items', async () => {
+
+				const element = await setup();
+				const button = element.shadowRoot.querySelector('ba-icon');
+				const container = element.shadowRoot.querySelectorAll('.container');
+				const items = element.shadowRoot.querySelectorAll('.item');
+
+				expect(button.title).toBe('featureInfo_close_button');
+				expect(container).toHaveSize(1);
+				expect(items).toHaveSize(0);
+			});
+		});
+
+		describe('and featureInfo items are available', () => {
+
+			it('renders a close icon-button, a container and no items', async () => {
+
+				const element = await setup({
+					featureInfo: {
+						current: [{ title: 'title0', content: 'content0' }, { title: 'title1', content: 'content1' }]
+					}
+				});
+				const button = element.shadowRoot.querySelector('ba-icon');
+				const container = element.shadowRoot.querySelectorAll('.container');
+				const items = element.shadowRoot.querySelectorAll('.item');
+
+				expect(button.title).toBe('featureInfo_close_button');
+				expect(container).toHaveSize(1);
+				expect(items).toHaveSize(2);
+			});
+		});
+	});
+
+	describe('when featureInfo items are added', () => {
+
+		it('renders renders the items', async () => {
+
+			const element = await setup();
+
+			add({ title: 'title0', content: 'content0' });
+			add({ title: 'title1', content: 'content1' });
+
+			const items = element.shadowRoot.querySelectorAll('.item');
+			expect(items).toHaveSize(2);
+		});
+	});
+
+	describe('when clear button clicked', () => {
+
+		it('clear the featureInfo in store', async () => {
+
+			const element = await setup({
+				featureInfo: {
+					current: [{ title: 'title0', content: 'content0' }, { title: 'title1', content: 'content1' }]
+				}
+			});
+			const iconButton = element.shadowRoot.querySelector('ba-icon');
+
+			iconButton.click();
+
+			expect(store.getState().featureInfo.current).toEqual([]);
+		});
+	});
+});
