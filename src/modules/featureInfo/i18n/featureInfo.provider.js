eea4b6161b1c794bf5c32cf0bb20b3133528e2e4
Thomas Aulinger <taulinger@gmx.de>
2021-10-13 17:11:26 +0200

    i18n for featureInfo module

---
+++ src/modules/featureInfo/i18n/featureInfo.provider.js
@@ -0,0 +1,19 @@
+export const provide = (lang) => {
+	switch (lang) {
+
+		case 'en':
+			return {
+				//the first part of the snake_case key should be the name of the related module
+				featureInfo_close_button: 'Close'
+			};
+
+		case 'de':
+			return {
+				//the first part of the snake_case key should be the name of the related module
+				featureInfo_close_button: 'Schließen'
+			};
+
+		default:
+			return {};
+	}
+};
