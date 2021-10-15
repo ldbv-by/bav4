777e6c10886387685dd17c30395f73e6fa7cf47f
Thomas Aulinger <taulinger@gmx.de>
2021-10-15 14:27:38 +0200

    improve docs

--- src/store/BaPlugin.js
+++ src/store/BaPlugin.js
@@ -1,7 +1,7 @@
 /**
 * Abstract base class for all BaPlugins.
 * `BaPlugins` implementations are a second important place for structuring code and logic.
-* In contrast to components, they often act on a higher abstraction level
+* In contrast to components, they often act a Controller on a higher abstraction level
 * managing global state being consumed by different components afterward.
 * For example, they could be responsible for setting an initial state or reacting to global state changes during the runtime of the app.
 * <br>
