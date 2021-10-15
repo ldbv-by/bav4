c59bdbad8443e879b2ebff965f39071e7cd57ccd
schle_th <thilo.schlemmer@ldbv.bayern.de>
2021-10-15 15:27:34 +0200

    make lint happy

--- src/modules/map/components/olMap/handler/OlSketchPropertyHandler.js
+++ src/modules/map/components/olMap/handler/OlSketchPropertyHandler.js
@@ -9,60 +9,60 @@ import { unByKey } from 'ol/Observable';
  */
 export class OlSketchPropertyHandler {
 
-    constructor(sketchFeature) {
-        if (!sketchFeature) {
-            throw new TypeError('sketchFeature must be defined');
-        }
+	constructor(sketchFeature) {
+		if (!sketchFeature) {
+			throw new TypeError('sketchFeature must be defined');
+		}
 
-        const onFeatureChange = (event) => {
-            this._monitorProperties(event.target);
-        };
-        this._sketchFeature = sketchFeature;
+		const onFeatureChange = (event) => {
+			this._monitorProperties(event.target);
+		};
+		this._sketchFeature = sketchFeature;
 
-        this._listener = this._sketchFeature.on('change', onFeatureChange);
-        this._pointCount = 1;
-        this._isSnapOnLastPoint = false;
-        this._isFinishOnFirstPoint = false;
-    }
+		this._listener = this._sketchFeature.on('change', onFeatureChange);
+		this._pointCount = 1;
+		this._isSnapOnLastPoint = false;
+		this._isFinishOnFirstPoint = false;
+	}
 
-    _getLineCoordinates(geometry) {
-        return (geometry instanceof Polygon) ? geometry.getCoordinates()[0].slice(0, -1) : geometry.getCoordinates();
-    }
+	_getLineCoordinates(geometry) {
+		return (geometry instanceof Polygon) ? geometry.getCoordinates()[0].slice(0, -1) : geometry.getCoordinates();
+	}
 
-    _monitorProperties(feature) {
+	_monitorProperties(feature) {
 
-        const lineCoordinates = this._getLineCoordinates(feature.getGeometry());
+		const lineCoordinates = this._getLineCoordinates(feature.getGeometry());
 
-        if (this._pointCount !== lineCoordinates.length) {
-            // a point is added or removed
-            this._pointCount = lineCoordinates.length;
-        }
-        else if (lineCoordinates.length > 1) {
-            const firstPoint = lineCoordinates[0];
-            const lastPoint = lineCoordinates[lineCoordinates.length - 1];
-            const lastPoint2 = lineCoordinates[lineCoordinates.length - 2];
+		if (this._pointCount !== lineCoordinates.length) {
+			// a point is added or removed
+			this._pointCount = lineCoordinates.length;
+		}
+		else if (lineCoordinates.length > 1) {
+			const firstPoint = lineCoordinates[0];
+			const lastPoint = lineCoordinates[lineCoordinates.length - 1];
+			const lastPoint2 = lineCoordinates[lineCoordinates.length - 2];
 
-            const isSnapOnFirstPoint = (lastPoint[0] === firstPoint[0] && lastPoint[1] === firstPoint[1]);
-            this._isFinishOnFirstPoint = (!this._isSnapOnLastPoint && isSnapOnFirstPoint);
+			const isSnapOnFirstPoint = (lastPoint[0] === firstPoint[0] && lastPoint[1] === firstPoint[1]);
+			this._isFinishOnFirstPoint = (!this._isSnapOnLastPoint && isSnapOnFirstPoint);
 
-            this._isSnapOnLastPoint = (lastPoint[0] === lastPoint2[0] && lastPoint[1] === lastPoint2[1]);
-        }
-    }
+			this._isSnapOnLastPoint = (lastPoint[0] === lastPoint2[0] && lastPoint[1] === lastPoint2[1]);
+		}
+	}
 
-    release() {
-        unByKey(this._listener);
-    }
+	release() {
+		unByKey(this._listener);
+	}
 
-    get isSnapOnLastPoint() {
-        return this._isSnapOnLastPoint;
-    }
+	get isSnapOnLastPoint() {
+		return this._isSnapOnLastPoint;
+	}
 
-    get isFinishOnFirstPoint() {
-        return this._isFinishOnFirstPoint;
-    }
+	get isFinishOnFirstPoint() {
+		return this._isFinishOnFirstPoint;
+	}
 
-    get pointCount() {
-        return this._pointCount;
-    }
+	get pointCount() {
+		return this._pointCount;
+	}
 
 }
--- test/modules/map/components/olMap/handler/OlSketchPropertyHandler.test.js
+++ test/modules/map/components/olMap/handler/OlSketchPropertyHandler.test.js
@@ -4,93 +4,93 @@ import { OlSketchPropertyHandler } from '../../../../../../src/modules/map/compo
 
 describe('OlSketchPropertyHandler', () => {
 
-    describe('when initialized', () => {
-        it('registers a listener for feature change', () => {
-            const featureMock = { on: () => { }, getGeometry: () => new Point([0, 0]) };
-            const listenerSpy = spyOn(featureMock, 'on');
-            const classUnderTest = new OlSketchPropertyHandler(featureMock);
+	describe('when initialized', () => {
+		it('registers a listener for feature change', () => {
+			const featureMock = { on: () => { }, getGeometry: () => new Point([0, 0]) };
+			const listenerSpy = spyOn(featureMock, 'on');
+			const classUnderTest = new OlSketchPropertyHandler(featureMock);
 
-            expect(classUnderTest).toBeTruthy();
-            expect(listenerSpy).toHaveBeenCalledWith('change', jasmine.any(Function));
-        });
+			expect(classUnderTest).toBeTruthy();
+			expect(listenerSpy).toHaveBeenCalledWith('change', jasmine.any(Function));
+		});
 
-        it('throws a TypeError , when feature is falsy', () => {
+		it('throws a TypeError , when feature is falsy', () => {
 
-            expect(() => new OlSketchPropertyHandler(null)).toThrowError(TypeError, 'sketchFeature must be defined');
+			expect(() => new OlSketchPropertyHandler(null)).toThrowError(TypeError, 'sketchFeature must be defined');
 
-        });
-
-
-
-        it('have default properties', () => {
-            const featureMock = {
-                on: () => { },
-                getGeometry: () => new Point([0, 0])
-            };
-
-            const classUnderTest = new OlSketchPropertyHandler(featureMock);
-
-            expect(classUnderTest).toBeTruthy();
-            expect(classUnderTest.pointCount).toBe(1);
-            expect(classUnderTest.isFinishOnFirstPoint).toBe(false);
-            expect(classUnderTest.isSnapOnLastPoint).toBe(false);
-        });
-
-        it('monitors feature changes', () => {
-            const feature = new Feature(new Point([0, 0]));
-            const classUnderTest = new OlSketchPropertyHandler(feature);
-
-            expect(classUnderTest.pointCount).toBe(1);
-
-            feature.setGeometry(new LineString([[0, 0], [1, 1]]));
-            feature.dispatchEvent('change');
-
-            expect(classUnderTest.pointCount).toBe(2);
-        });
-
-        it('detects finishOnFirstPoint for polyline', () => {
-
-            const feature = new Feature(new LineString([[0, 0], [1, 1], [2, 2]]));
-            const classUnderTest = new OlSketchPropertyHandler(feature);
+		});
 
 
 
-            feature.setGeometry(new LineString([[0, 0], [1, 1], [2, 2], [0, 0]]));
-            feature.dispatchEvent('change');
+		it('have default properties', () => {
+			const featureMock = {
+				on: () => { },
+				getGeometry: () => new Point([0, 0])
+			};
+
+			const classUnderTest = new OlSketchPropertyHandler(featureMock);
+
+			expect(classUnderTest).toBeTruthy();
+			expect(classUnderTest.pointCount).toBe(1);
+			expect(classUnderTest.isFinishOnFirstPoint).toBe(false);
+			expect(classUnderTest.isSnapOnLastPoint).toBe(false);
+		});
+
+		it('monitors feature changes', () => {
+			const feature = new Feature(new Point([0, 0]));
+			const classUnderTest = new OlSketchPropertyHandler(feature);
+
+			expect(classUnderTest.pointCount).toBe(1);
+
+			feature.setGeometry(new LineString([[0, 0], [1, 1]]));
+			feature.dispatchEvent('change');
+
+			expect(classUnderTest.pointCount).toBe(2);
+		});
+
+		it('detects finishOnFirstPoint for polyline', () => {
+
+			const feature = new Feature(new LineString([[0, 0], [1, 1], [2, 2]]));
+			const classUnderTest = new OlSketchPropertyHandler(feature);
 
 
-            expect(classUnderTest.isFinishOnFirstPoint).toBe(true);
-        });
 
-        it('detects finishOnFirstPoint for polygon', () => {
+			feature.setGeometry(new LineString([[0, 0], [1, 1], [2, 2], [0, 0]]));
+			feature.dispatchEvent('change');
 
-            const feature = new Feature(new Polygon([[[0, 0], [1, 1], [2, 2], [2, 2]]]));
-            const classUnderTest = new OlSketchPropertyHandler(feature);
 
-            feature.setGeometry(new Polygon([[[0, 0], [1, 1], [2, 2], [0, 0], [0, 0]]]));
-            feature.dispatchEvent('change');
+			expect(classUnderTest.isFinishOnFirstPoint).toBe(true);
+		});
 
-            expect(classUnderTest.isFinishOnFirstPoint).toBe(true);
-        });
+		it('detects finishOnFirstPoint for polygon', () => {
 
-        it('detects isSnapOnLastPoint for polygon', () => {
+			const feature = new Feature(new Polygon([[[0, 0], [1, 1], [2, 2], [2, 2]]]));
+			const classUnderTest = new OlSketchPropertyHandler(feature);
 
-            const feature = new Feature(new Polygon([[[0, 0], [1, 1], [2, 2], [2, 2]]]));
-            const classUnderTest = new OlSketchPropertyHandler(feature);
+			feature.setGeometry(new Polygon([[[0, 0], [1, 1], [2, 2], [0, 0], [0, 0]]]));
+			feature.dispatchEvent('change');
 
-            feature.setGeometry(new Polygon([[[0, 0], [1, 1], [2, 2], [2, 2], [0, 0]]]));
-            feature.dispatchEvent('change');
+			expect(classUnderTest.isFinishOnFirstPoint).toBe(true);
+		});
 
-            expect(classUnderTest.isSnapOnLastPoint).toBe(true);
-        });
+		it('detects isSnapOnLastPoint for polygon', () => {
 
-        it('deregisters listener on release', () => {
-            const feature = new Feature(new Point([0, 0]));
-            const classUnderTest = new OlSketchPropertyHandler(feature);
-            const empty = {};
-            classUnderTest.release();
+			const feature = new Feature(new Polygon([[[0, 0], [1, 1], [2, 2], [2, 2]]]));
+			const classUnderTest = new OlSketchPropertyHandler(feature);
 
-            expect(classUnderTest._listener).toEqual(empty);
-        });
-    });
+			feature.setGeometry(new Polygon([[[0, 0], [1, 1], [2, 2], [2, 2], [0, 0]]]));
+			feature.dispatchEvent('change');
+
+			expect(classUnderTest.isSnapOnLastPoint).toBe(true);
+		});
+
+		it('deregisters listener on release', () => {
+			const feature = new Feature(new Point([0, 0]));
+			const classUnderTest = new OlSketchPropertyHandler(feature);
+			const empty = {};
+			classUnderTest.release();
+
+			expect(classUnderTest._listener).toEqual(empty);
+		});
+	});
 });
