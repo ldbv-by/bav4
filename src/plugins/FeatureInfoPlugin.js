8fc5850b6e8547758894908328f324bb4213229f
Thomas Aulinger <taulinger@gmx.de>
2021-10-19 18:27:55 +0200

    cleanup

--- src/plugins/FeatureInfoPlugin.js
+++ src/plugins/FeatureInfoPlugin.js
@@ -22,11 +22,8 @@ export class FeatureInfoPlugin extends BaPlugin {
 			const { payload: { coordinate } } = evt;
 			clear();
 			updateCoordinate(coordinate);
-			//we simulate a FeatureInfo item here. Later we will call the FeatureInfoService.
+			//Fixme: we simulate a FeatureInfo item here. Later we will call the FeatureInfoService.
 			add({ title: 'title', content: '<p><b>Gasthaus Schloss Blumenthal</b></p>Wirtshaus, bewirtschaftete Alm<br/>Blumenthal 1 <br/>86551 Aichach a d Paar<br/><br/><b>Weitere Informationen finden Sie unter:</b><br/><a href="https://www.schloss-blumenthal.de/gasthaus-biergarten/#!/gh_main" target="_blank">Gasthaus Schloss Blumenthal</a>"' });
-			//Todo: check if we are in portrait mode. If true we first show a notification
-			// setTabIndex(TabIndex.FEATUREINFO);
-			// open();
 		};
 
 		const onFeatureInfoChanged = current => {
@@ -37,7 +34,7 @@ export class FeatureInfoPlugin extends BaPlugin {
 				setTabIndex(previousTabIndex);
 			}
 			else {
-				//Todo: check if we are in portrait mode. If true we first show a notification
+				//Todo: check if we are in portrait mode. If true, we first display a notification
 				setTabIndex(TabIndex.FEATUREINFO);
 				open();
 			}
