169c29b7c83aec51ca15db17c67a8665b37916bf
Thomas Aulinger <taulinger@gmx.de>
2021-10-25 12:41:16 +0200

    adjust tests

--- test/plugins/FeatureInfoPlugin.test.js
+++ test/plugins/FeatureInfoPlugin.test.js
@@ -2,7 +2,7 @@ import { TestUtils } from '../test-utils.js';
 import { featureInfoReducer } from '../../src/store/featureInfo/featureInfo.reducer';
 import { setClick } from '../../src/store/pointer/pointer.action';
 import { setTabIndex, TabIndex } from '../../src/store/mainMenu/mainMenu.action';
-import { clearFeatureInfoItems } from '../../src/store/featureInfo/featureInfo.action.js';
+import { addFeatureInfoItems, clearFeatureInfoItems } from '../../src/store/featureInfo/featureInfo.action.js';
 import { FeatureInfoPlugin } from '../../src/plugins/FeatureInfoPlugin.js';
 import { createNoInitialStateMainMenuReducer } from '../../src/store/mainMenu/mainMenu.reducer.js';
 import { pointerReducer } from '../../src/store/pointer/pointer.reducer.js';
@@ -30,31 +30,31 @@ describe('FeatureInfoPlugin', () => {
 
 	describe('when pointer click property changes', () => {
 
-		it('clears all previous existing featureInfo items', async () => {
-			const store = setup({
-				featureInfo: {
-					current: [{ title: 'foo0', content: 'bar0' }, { title: 'foo1', content: 'bar1' }]
-				}
-			});
+		it('clears all previous existing featureInfo items and updates the coordinate property', async () => {
+			const coordinate = [11, 22];
+			const store = setup();
 			const instanceUnderTest = new FeatureInfoPlugin();
 			await instanceUnderTest.register(store);
+			addFeatureInfoItems({ title: 'title', content: 'content' });
 
-			setClick({ coordinate: [11, 22], screenCoordinate: [33, 44] });
+			setClick({ coordinate: coordinate, screenCoordinate: [33, 44] });
 
-			expect(store.getState().featureInfo.current.length).toBe(1);
+			expect(store.getState().featureInfo.current).toHaveSize(0);
+			expect(store.getState().featureInfo.coordinate.payload).toBe(coordinate);
 		});
+	});
+
+	describe('when featureInfo current property changes', () => {
 
 		describe('and mainMenu is initially open', () => {
 
-			it('adds a FeatureInfo item and changes the mainMenu state', async () => {
+			it('the mainMenu state', async () => {
 				const store = setup();
 				const instanceUnderTest = new FeatureInfoPlugin();
 				await instanceUnderTest.register(store);
+				addFeatureInfoItems({ title: 'title', content: 'content' });
 
-				setClick({ coordinate: [11, 22], screenCoordinate: [33, 44] });
-
-				expect(store.getState().featureInfo.current.length).toBe(1);
-				expect(store.getState().featureInfo.coordinate.payload).toEqual([11, 22]);
+				expect(store.getState().featureInfo.current).toHaveSize(1);
 				expect(store.getState().mainMenu.tabIndex).toBe(TabIndex.FEATUREINFO);
 				expect(store.getState().mainMenu.open).toBeTrue();
 
@@ -67,7 +67,7 @@ describe('FeatureInfoPlugin', () => {
 
 		describe('and mainMenu initially closed', () => {
 
-			it('adds a FeatureInfo item and changes the mainMenu state', async () => {
+			it('changes the mainMenu state', async () => {
 				const store = setup({
 					mainMenu: {
 						tabIndex: 2,
@@ -76,11 +76,10 @@ describe('FeatureInfoPlugin', () => {
 				});
 				const instanceUnderTest = new FeatureInfoPlugin();
 				await instanceUnderTest.register(store);
+				addFeatureInfoItems({ title: 'title', content: 'content' });
 
-				setClick({ coordinate: [11, 22], screenCoordinate: [33, 44] });
 
-				expect(store.getState().featureInfo.current.length).toBe(1);
-				expect(store.getState().featureInfo.coordinate.payload).toEqual([11, 22]);
+				expect(store.getState().featureInfo.current).toHaveSize(1);
 				expect(store.getState().mainMenu.tabIndex).toBe(TabIndex.FEATUREINFO);
 				expect(store.getState().mainMenu.open).toBeTrue();
 
@@ -95,23 +94,34 @@ describe('FeatureInfoPlugin', () => {
 
 	describe('when tabIndex changes', () => {
 
-		it('adds a FeatureInfo item and changes the mainMenu state', async () => {
+		it('clears all previous existing featureInfo items (also initially)', async () => {
 			const store = setup({
 				mainMenu: {
 					tabIndex: 2,
 					open: false
+				},
+				featureInfo: {
+					current: [{ title: 'foo0', content: 'bar0' }, { title: 'foo1', content: 'bar1' }]
 				}
 			});
 			const instanceUnderTest = new FeatureInfoPlugin();
 			await instanceUnderTest.register(store);
 
-			setClick({ coordinate: [11, 22], screenCoordinate: [33, 44] });
 
-			expect(store.getState().featureInfo.current.length).toBe(1);
+			//should be cleared also initially
+			expect(store.getState().featureInfo.current).toHaveSize(0);
+
+			addFeatureInfoItems({ title: 'title', content: 'content' });
 
 			setTabIndex(TabIndex.MAPS);
 
-			expect(store.getState().featureInfo.current.length).toBe(0);
+			expect(store.getState().featureInfo.current).toHaveSize(0);
+
+			addFeatureInfoItems({ title: 'title', content: 'content' });
+
+			setTabIndex(TabIndex.FEATUREINFO);
+
+			expect(store.getState().featureInfo.current).toHaveSize(1);
 		});
 	});
 });
