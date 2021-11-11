3f73260dbfdc59b26b6fe9b15313bf1086ee85e2
thevoiceofthruth <hollywood20205@gmx.de>
2021-11-11 15:18:31 +0100

    remove call of signal method

--- test/modules/layerInfo/components/LayerInfoPanel.test.js
+++ test/modules/layerInfo/components/LayerInfoPanel.test.js
@@ -23,7 +23,7 @@ describe('LayerInfoPanel', () => {
 			expect(element.shadowRoot.children.length).toBe(0);
 		});
 
-		it('should show a layerInfo on the panel', async () => {
+		it('should show a layerInfo on the panel', async (done) => {
 
 			const layerInfo = new LayerInfoResult('<b>content</b>');
 			spyOn(layerInfoServiceMock, 'byId').withArgs('914c9263-5312-453e-b3eb-5104db1bf788').and.returnValue(layerInfo);
@@ -31,21 +31,23 @@ describe('LayerInfoPanel', () => {
 			const element = await TestUtils.render(LayerInfoPanel.tag);
 
 			element.geoResourceId = '914c9263-5312-453e-b3eb-5104db1bf788';
-			element.signal('UPDATE_LAYERINFO', layerInfo);
-			const divs = element.shadowRoot.querySelectorAll('div');
 
-			expect(divs.length).toBe(2);
-			expect(divs[1].innerText).toBe('content');
+			setTimeout(() => {
+				const divs = element.shadowRoot.querySelectorAll('div');
+
+				expect(divs.length).toBe(2);
+				expect(divs[1].innerText).toBe('content');
+				done();
+			});
 		});
 
-		it('should log a warn statement when Altitude Service is not available', async (done) => {
+		it('should log a warn statement when LayerInfoService is not available', async (done) => {
 
 			spyOn(layerInfoServiceMock, 'byId').withArgs('914c9263-5312-453e-b3eb-5104db1bf788').and.returnValue(Promise.reject(new Error('LayerInfo Error')));
 			const warnSpy = spyOn(console, 'warn');
 
 			const element = await TestUtils.render(LayerInfoPanel.tag);
 			element.geoResourceId = '914c9263-5312-453e-b3eb-5104db1bf788';
-			element.signal('UPDATE_LAYERINFO', null);
 
 			setTimeout(() => {
 				expect(warnSpy).toHaveBeenCalledWith('LayerInfo Error');
