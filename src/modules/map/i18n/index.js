859cee5d5b50809a6fdf557d034a0aaeff4ff698
Thomas Aulinger <taulinger@gmx.de>
2021-11-16 13:24:25 +0100

    remove obsolete i18n provider

--- src/modules/map/i18n/index.js
+++ src/modules/map/i18n/index.js
@@ -3,7 +3,6 @@ import { provide as infoButtonProvide } from './infoButton.provider';
 import { provide as zoomButtonsProvide } from './zoomButtons.provider';
 import { provide as geolocationButtonProvide } from './geolocationButton.provider';
 import { provide as contextMenuProvider } from './contextMenu.provider';
-import { provide as storeProvider } from './store.provider';
 import { provide as attributionInfoProvider } from './attributionInfo.provider';
 import { provide as rotationButtonProvider } from './rotationButton.provider';
 import { $injector } from '../../../injection';
@@ -13,7 +12,6 @@ translationService.register('infoButtonProvider', infoButtonProvide);
 translationService.register('zoomButtonsProvider', zoomButtonsProvide);
 translationService.register('geolocationButtonProvider', geolocationButtonProvide);
 translationService.register('contextMenuProvider', contextMenuProvider);
-translationService.register('storeProvider', storeProvider);
 translationService.register('attributionInfoProvider', attributionInfoProvider);
 translationService.register('rotationButtonProvider', rotationButtonProvider);
 
