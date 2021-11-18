a67f51f8d1e6d814910cf6437c4f7fa1d32a7cf0
thiloSchlemmer <thiloSchlemmer@gmail.com>
2021-11-18 11:57:48 +0100

    update doc

--- src/services/IconService.js
+++ src/services/IconService.js
@@ -5,6 +5,9 @@ const SVG_MARKER_NAME = 'marker';
 const SVG_MARKER_CONTENT = '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="rgb(255,255,255)" class="bi bi-geo-alt-fill" viewBox="0 0 16 16"><!-- MIT License --><path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/></svg>';
 /**
  * Service for managing icons
+ *
+ * This service provides a default-icon as IconResult with iconResult.name==='marker' and a list of icons based on svg-graphics as IconResult.
+ * This list always contains the default-icon or a 'marker'-Icon provided by the icons.provider as first element.
  * @author thiloSchlemmer
  */
 export class IconService {
