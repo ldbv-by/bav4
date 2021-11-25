d09d283c999dfa97a662456af08ab3a5fcc0bd5e
Thomas Aulinger <taulinger@gmx.de>
2021-11-25 08:55:28 +0100

    check query resolvement in case of error

--- test/plugins/FeatureInfoPlugin.test.js
+++ test/plugins/FeatureInfoPlugin.test.js
@@ -223,11 +223,13 @@ describe('FeatureInfoPlugin', () => {
 
 				setClick({ coordinate: coordinate, screenCoordinate: [33, 44] });
 
+				expect(store.getState().featureInfo.querying).toBeTrue();
 				setTimeout(() => {
 					expect(store.getState().featureInfo.current).toHaveSize(0);
 					expect(store.getState().notifications.latest.payload.content).toBe(`${layerLabel0}: featureInfoPlugin_featureInfoService_exception`);
 					expect(store.getState().notifications.latest.payload.level).toBe(LevelTypes.WARN);
 					expect(warnSpy).toHaveBeenCalledWith(errorMessage);
+					expect(store.getState().featureInfo.querying).toBeFalse();
 					done();
 				});
 			});
