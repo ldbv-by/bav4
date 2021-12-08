3b267a69da29c3a55309d68efab38e782b20e560
Thomas Aulinger <taulinger@gmx.de>
2021-12-08 18:11:39 +0100

    add povider for loading cadastrial parcel search results

--- test/modules/search/services/provider/searchResult.provider.test.js
+++ test/modules/search/services/provider/searchResult.provider.test.js
@@ -1,6 +1,6 @@
 import { $injector } from '../../../../../src/injection';
 import { SearchResultTypes } from '../../../../../src/modules/search/services/domain/searchResult';
-import { loadBvvGeoResourceSearchResults, loadBvvLocationSearchResults } from '../../../../../src/modules/search/services/provider/searchResult.provider';
+import { loadBvvCadastralParcelSearchResults, loadBvvGeoResourceSearchResults, loadBvvLocationSearchResults } from '../../../../../src/modules/search/services/provider/searchResult.provider';
 
 describe('SearchResult provider', () => {
 
@@ -19,7 +19,7 @@ describe('SearchResult provider', () => {
 			.registerSingleton('HttpService', httpService);
 	});
 
-	describe('Bvv SearchResult provider for locations GeoResources', () => {
+	describe('Bvv SearchResult provider for GeoResources', () => {
 
 		const mockResponse = [
 			{ 'id': '6f5a389c-4ef3-4b5a-9916-475fd5c5962b', 'attrs': { 'detail': 'Kartierung der bekannten Bodendenkmäler nach Art.1 Abs.4 u. Art.2 BayDSchG (Denkmalliste). Die Zahl der tatsächlich vorhandenen Bodendenkmäler kann höher sein. Die Denkmaleigenschaft hängt nicht von der Kartierung und der Eintragung in die Bayerische Denkmalliste ab. Auch Objekte, die nicht in der Bayerischen Denkmalliste verzeichnet sind, können Denkmäler sein, wenn sie die Kriterien nach Art.1 BayDSchG erfüllen. Bei allen Vorhaben ist eine frühzeitige Beteiligung des Bayerischen Landesamtes für Denkmalpflege nach Art.7 BayDSchG notwendig.', 'layer': '6f5a389c-4ef3-4b5a-9916-475fd5c5962b', 'label': '<b>Bodendenkmal</b>' } },
@@ -136,6 +136,86 @@ describe('SearchResult provider', () => {
 				expect(configServiceSpy).toHaveBeenCalled();
 				expect(httpServiceSpy).toHaveBeenCalled();
 				expect(error.message).toBe('SearchResults for locations could not be retrieved');
+			}
+		});
+	});
+
+	describe('Bvv SearchResult provider for cadastrial parcels', () => {
+
+		const mockResponse = [
+			{ id: 'id0', attrs: { coordinate: [10.270116669125855, 48.44638557638974], label: '<b>foo</b>, bar' } },
+			{ id: 'id1', attrs: { coordinate: [10.257489331997931, 48.436180253047496], label: '<b>some</b> <b>other</b>, result' } }
+		];
+
+		it('loads SearchResults for cadastial parcels', async () => {
+
+			const term = 'term';
+			const backendUrl = 'https://backend.url';
+			const expectedArgs0 = `${backendUrl}/search/type/cp/searchText/${term}`;
+			const configServiceSpy = spyOn(configService, 'getValueAsPath').withArgs('BACKEND_URL').and.returnValue(`${backendUrl}/`);
+			const httpServiceSpy = spyOn(httpService, 'get').withArgs(expectedArgs0).and.returnValue(Promise.resolve(
+				new Response(
+					JSON.stringify(
+						mockResponse
+					)
+				)
+			));
+
+			const searchResults = await loadBvvCadastralParcelSearchResults(term);
+
+			expect(configServiceSpy).toHaveBeenCalled();
+			expect(httpServiceSpy).toHaveBeenCalled();
+			expect(searchResults.length).toBe(2);
+
+			const searchResult0 = searchResults[0];
+
+			expect(searchResult0.id).toBe('id0');
+			expect(searchResult0.label).toBe('foo, bar');
+			expect(searchResult0.labelFormated).toBe('<b>foo</b>, bar');
+			expect(searchResult0.type).toBe(SearchResultTypes.CADASTRAL_PARCEL);
+			expect(searchResult0.center).toEqual([10.270116669125855, 48.44638557638974]);
+			expect(searchResult0.extent).toBeNull();
+		});
+
+		it('returns an empty array when nothing is found', async () => {
+
+			const term = 'term';
+			const backendUrl = 'https://backend.url';
+			const expectedArgs0 = `${backendUrl}/search/type/cp/searchText/${term}`;
+			const configServiceSpy = spyOn(configService, 'getValueAsPath').withArgs('BACKEND_URL').and.returnValue(`${backendUrl}/`);
+			const httpServiceSpy = spyOn(httpService, 'get').withArgs(expectedArgs0).and.returnValue(Promise.resolve(
+				new Response(
+					JSON.stringify(
+						[]
+					)
+				)
+			));
+
+			const searchResults = await loadBvvCadastralParcelSearchResults(term);
+
+			expect(configServiceSpy).toHaveBeenCalled();
+			expect(httpServiceSpy).toHaveBeenCalled();
+			expect(searchResults.length).toBe(0);
+		});
+
+		it('rejects when backend request cannot be fulfilled', async () => {
+
+			const term = 'term';
+			const backendUrl = 'https://backend.url';
+			const expectedArgs0 = `${backendUrl}/search/type/cp/searchText/${term}`;
+			const configServiceSpy = spyOn(configService, 'getValueAsPath').withArgs('BACKEND_URL').and.returnValue(`${backendUrl}/`);
+			const httpServiceSpy = spyOn(httpService, 'get').withArgs(expectedArgs0).and.returnValue(Promise.resolve(
+				new Response(null, { status: 404 })
+			));
+
+			try {
+				await loadBvvCadastralParcelSearchResults(term);
+				throw new Error('Promise should not be resolved');
+			}
+			catch (error) {
+				expect(configServiceSpy).toHaveBeenCalled();
+				expect(httpServiceSpy).toHaveBeenCalled();
+				expect(error.message).toBe('SearchResults for cadastrial parcels could not be retrieved');
 			}
 		});
 	});
