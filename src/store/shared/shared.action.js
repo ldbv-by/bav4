c48cbf2e781a7c39bb908b0a08ca4d731eea8c55
Schlemmer, Thilo (LDBV) <thilo.schlemmer@ldbv.bayern.de>
2021-12-09 10:49:37 +0100

    add shared-slice to store

---
+++ src/store/shared/shared.action.js
@@ -0,0 +1,37 @@
+/**
+ * Action creators for commonly shared data
+ * @module shared/action
+ */
+import { $injector } from '../../injection';
+import { FILE_SAVE_RESULT_CHANGED, TERMS_OF_USE_ACKNOWLEDGED_CHANGED } from './shared.reducer';
+
+const getStore = () => {
+	const { StoreService } = $injector.inject('StoreService');
+	return StoreService.getStore();
+};
+
+
+/**
+ * Set the acknowledgement of the user for the Terms of Use.
+ * @param {boolean} isAcknowledged
+ * @function
+ */
+export const termsOfUseAcknowledged = () => {
+
+	getStore().dispatch({
+		type: TERMS_OF_USE_ACKNOWLEDGED_CHANGED,
+		payload: true
+	});
+};
+
+/**
+ * Set the {@link FileSaveResult}
+ * @function
+ * @param {FileSaveResult} fileSaveResult the fileSaveResult of the stored data
+ */
+export const setFileSaveResult = (fileSaveResult) => {
+	getStore().dispatch({
+		type: FILE_SAVE_RESULT_CHANGED,
+		payload: fileSaveResult
+	});
+};
---
+++ src/store/shared/shared.reducer.js
@@ -0,0 +1,41 @@
+export const TERMS_OF_USE_ACKNOWLEDGED_CHANGED = 'shared/termsOfUsAcknowledged';
+export const FILE_SAVE_RESULT_CHANGED = 'shared/fileSaveResult';
+
+
+
+export const initialState = {
+	/**
+     * @type {boolean}
+     */
+	termsOfUseAcknowlegded: false,
+	/**
+     * @type {FileSaveResult}
+     */
+	fileSaveResult: null
+};
+
+export const sharedReducer = (state = initialState, action) => {
+
+	const { type, payload } = action;
+	switch (type) {
+		case TERMS_OF_USE_ACKNOWLEDGED_CHANGED: {
+
+			return {
+				...state,
+				termsOfUseAcknowlegded: payload
+
+			};
+		}
+		case FILE_SAVE_RESULT_CHANGED: {
+
+			return {
+				...state,
+				fileSaveResult: payload
+
+			};
+		}
+	}
+
+	return state;
+};
+
