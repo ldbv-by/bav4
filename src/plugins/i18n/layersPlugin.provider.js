99aebd1261fe473a764b9b508e5d86769a76e2b7
Thomas Aulinger <taulinger@gmx.de>
2022-01-27 17:58:09 +0100

    add additional translation for pending layers

--- src/plugins/i18n/layersPlugin.provider.js
+++ src/plugins/i18n/layersPlugin.provider.js
@@ -5,13 +5,15 @@ export const provide = (lang) => {
 		case 'en':
 			return {
 				//the first part of the snake_case key should be the name of the related plugin
-				layersPlugin_store_layer_default_layer_name: 'Data'
+				layersPlugin_store_layer_default_layer_name_vector: 'Data',
+				layersPlugin_store_layer_default_layer_name_future: 'Loading...'
 			};
 
 		case 'de':
 			return {
 				//the first part of the snake_case key should be the name of the related plugin
-				layersPlugin_store_layer_default_layer_name: 'Daten'
+				layersPlugin_store_layer_default_layer_name_vector: 'Daten',
+				layersPlugin_store_layer_default_layer_name_future: 'Wird geladen...'
 			};
 
 		default:
