0986119f8b420cc0fb37f046a03576abd7ae84e9
Schlemmer, Thilo (LDBV) <thilo.schlemmer@ldbv.bayern.de>
2022-02-08 13:20:07 +0100

    add simple drag&drop-behavior

--- src/modules/dndImport/components/DndImportPanel.js
+++ src/modules/dndImport/components/DndImportPanel.js
@@ -4,6 +4,8 @@ import { MvuElement } from '../../MvuElement';
 import css from './dndImportPanel.css';
 
 const Update_Drag_Active = 'update_drag_active';
+const DragAndDropTypesMimeTypeFiles = 'Files';
+const DragAndDropTypesMimeTypeText = 'text/plain';
 
 /**
  * @class
@@ -20,8 +22,8 @@ export class DndImportPanel extends MvuElement {
 	}
 
 	/**
- * @override
- */
+    * @override
+    */
 	update(type, data, model) {
 		switch (type) {
 			case Update_Drag_Active:
@@ -30,14 +32,71 @@ export class DndImportPanel extends MvuElement {
 	}
 
 	/**
- *@override
- */
+    *@override
+    */
 	createView(model) {
-
+		const translate = (key) => this._translationService.translate(key);
 		const getActiveClass = () => {
 			return model.isDragActive ? 'is-active' : 'is-hidden';
 		};
+
+		const stopRedirectAndDefaultHandler = (e) => {
+			if (e.stopPropagation) {
+				e.stopPropagation();
+			}
+			e.preventDefault();
+		};
+
+		const onDragEnter = (e) => {
+			stopRedirectAndDefaultHandler(e);
+			const types = e.dataTransfer.types || [];
+			const isImport = (types) => types.some(t => /(files|text\/plain)/i.test(t));
+			if (isImport(types)) {
+				this.signal(Update_Drag_Active, true);
+			}
+
+		};
+		const onDragOver = (e) => {
+			stopRedirectAndDefaultHandler(e);
+		};
+		const onDragLeave = (e) => {
+			stopRedirectAndDefaultHandler(e);
+			this.signal(Update_Drag_Active, false);
+		};
+		const onDrop = (e) => {
+			const types = e.dataTransfer.types || [];
+			types.forEach(type => {
+				switch (type) {
+					case DragAndDropTypesMimeTypeFiles:
+						this._importFile(e.dataTransfer);
+						break;
+					case DragAndDropTypesMimeTypeText:
+						this._importText(e.dataTransfer);
+						break;
+					default:
+						console.warn('No valid data in drop-object');
+				}
+
+			});
+
+			this.signal(Update_Drag_Active, false);
+		};
+
 		return html`<style>${css}</style>
-		<div class='dropzone ${getActiveClass()}' ></div>`;
+		<div class='dropzone ${getActiveClass()}' @dragenter=${onDragEnter} @dragover=${onDragOver} @dragleave=${onDragLeave} @drop=${onDrop}>${translate('dndImport_import_textcontent')}</div>`;
+	}
+
+	_importFile(dataTransfer) {
+		const files = dataTransfer.files;
+		if (files && 0 < files.length) {
+			Array.from(files).forEach(f => {
+				console.log('importing File', f);
+			});
+		}
+	}
+
+	_importText(dataTransfer) {
+		const textData = dataTransfer.getData(DragAndDropTypesMimeTypeText);
+		console.log('importing Text-Content:', textData);
 	}
 }
