cd84b8fc230f64a37585e895ac1e844fd58bb781
Thomas Aulinger <taulinger@gmx.de>
2022-02-09 12:31:20 +0100

    use injected fields

--- src/services/ImportService.js
+++ src/services/ImportService.js
@@ -13,7 +13,7 @@ export class ImportService {
 
 	constructor(vectorSourceTypeProvider = detectVectorSourceType) {
 		const { HttpService: httpService, GeoResourceService: geoResourceService, UrlService: urlService, TranslationService: translationService }
-            = $injector.inject('HttpService', 'GeoResourceService', 'UrlService', 'TranslationService');
+			= $injector.inject('HttpService', 'GeoResourceService', 'UrlService', 'TranslationService');
 		this._httpService = httpService;
 		this._geoResourceService = geoResourceService;
 		this._urlService = urlService;
@@ -22,9 +22,9 @@ export class ImportService {
 	}
 
 	/**
-    * Returns default vector data import options.
-    * @returns VectorDataImportOptions
-    */
+	* Returns default vector data import options.
+	* @returns VectorDataImportOptions
+	*/
 	_newDefaultImportVectorDataOptions() {
 		return {
 			id: createUniqueId().toString(),
@@ -35,28 +35,25 @@ export class ImportService {
 	}
 
 	/**
-    * Imports vector data from an URL and returns a {@link GeoResourceFuture}.
-    * The GeoResourceFuture is registered on the {@link GeoResourceService}.
-    * @param {string} url
-    * @param {VectorDataImportOptions} [options]
-    * @returns VectorGeoresouce
-    */
+	* Imports vector data from an URL and returns a {@link GeoResourceFuture}.
+	* The GeoResourceFuture is registered on the {@link GeoResourceService}.
+	* @param {string} url
+	* @param {VectorDataImportOptions} [options]
+	* @returns VectorGeoresouce
+	*/
 	importVectorDataFromUrl(url, options = {}) {
 		const { id, label, sourceType, detectVectorSourceTypeFunction } = { ...this._newDefaultImportVectorDataOptions(), ...options };
-		const { HttpService: httpService, GeoResourceService: geoResourceService, UrlService: urlService, TranslationService: translationService }
-            = $injector.inject('HttpService', 'GeoResourceService', 'UrlService', 'TranslationService');
 
 		const loader = async id => {
 
-			const proxyfiedUrl = urlService.proxifyInstant(url);
-
-			const result = await httpService.get(proxyfiedUrl);
+			const proxyfiedUrl = this._urlService.proxifyInstant(url);
+			const result = await this._httpService.get(proxyfiedUrl);
 
 			if (result.ok) {
 				const data = await result.text();
 				const resultingSourceType = sourceType ?? detectVectorSourceTypeFunction(data, result.headers.get('Content-Type'));
 				if (resultingSourceType) {
-					const vgr = observable(new VectorGeoResource(id, label ?? translationService.translate('layersPlugin_store_layer_default_layer_name_vector'), resultingSourceType),
+					const vgr = observable(new VectorGeoResource(id, label ?? this._translationService.translate('layersPlugin_store_layer_default_layer_name_vector'), resultingSourceType),
 						(prop, value) => {
 							if (prop === '_label') {
 								modifyLayer(id, { label: value });
@@ -70,32 +67,30 @@ export class ImportService {
 			throw new Error(`GeoResource for '${url}' could not be loaded: Http-Status ${result.status}`);
 		};
 
-		const geoResource = new GeoResourceFuture(id, loader, label ?? translationService.translate('layersPlugin_store_layer_default_layer_name_future'));
-		geoResourceService.addOrReplace(geoResource);
+		const geoResource = new GeoResourceFuture(id, loader, label ?? this._translationService.translate('layersPlugin_store_layer_default_layer_name_future'));
+		this._geoResourceService.addOrReplace(geoResource);
 		return geoResource;
 	}
 
 	/**
-     * Creates a {@link VectorGeoresouce} containing the given data.
-     * The VectorGeoresouce is registered on the {@link GeoResourceService}.
-     * @param {string} data
-     * @param {VectorDataImportOptions} [options]
-     * @returns VectorGeoresouce or `null` when no VectorGeoresouce could be created
-     */
+	 * Creates a {@link VectorGeoresouce} containing the given data.
+	 * The VectorGeoresouce is registered on the {@link GeoResourceService}.
+	 * @param {string} data
+	 * @param {VectorDataImportOptions} [options]
+	 * @returns VectorGeoresouce or `null` when no VectorGeoresouce could be created
+	 */
 	importVectorData(data, options) {
 		const { id, label, sourceType, detectVectorSourceTypeFunction } = { ...this._newDefaultImportVectorDataOptions(), ...options };
-		const { GeoResourceService: geoResourceService, TranslationService: translationService }
-            = $injector.inject('GeoResourceService', 'TranslationService');
 
 		const resultingSourceType = sourceType ?? detectVectorSourceTypeFunction(data);
 		if (resultingSourceType) {
-			const vgr = observable(new VectorGeoResource(id, label ?? translationService.translate('layersPlugin_store_layer_default_layer_name_vector'), resultingSourceType), (prop, value) => {
+			const vgr = observable(new VectorGeoResource(id, label ?? this._translationService.translate('layersPlugin_store_layer_default_layer_name_vector'), resultingSourceType), (prop, value) => {
 				if (prop === '_label') {
 					modifyLayer(id, { label: value });
 				}
 			});
 			vgr.setSource(data, 4326 /**valid for kml, gpx an geoJson**/);
-			geoResourceService.addOrReplace(vgr);
+			this._geoResourceService.addOrReplace(vgr);
 			return vgr;
 		}
 		console.warn(`SourceType for '${id}' could not be detected`);
