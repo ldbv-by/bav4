be6a26336b119df49321c9d591e6384ff170ac13
thiloSchlemmer <thiloSchlemmer@gmail.com>
2022-02-10 16:40:35 +0100

    update test

--- test/modules/dndImport/i18n/dndImport.provider.test.js
+++ test/modules/dndImport/i18n/dndImport.provider.test.js
@@ -10,6 +10,7 @@ describe('i18n for dndImport module', () => {
 		expect(map.dndImport_import_textcontent).toBe('Text oder URL hierhin ziehen');
 		expect(map.dndImport_import_filecontent).toBe('KML/GPX/GeoJSON Datei hierhin ziehen');
 		expect(map.dndImport_import_unknown).toBe('Die Import-Daten sind unbekannt');
+		expect(map.dndImport_import_unsupported).toBe('Der Dateityp wird nicht unterstützt');
 		expect(map.dndImport_import_file_error).toBe('Die Datei kann nicht gelesen werden');
 		expect(map.dndImport_import_no_file_found).toBe('Die Datei ist leer');
 	});
@@ -21,12 +22,13 @@ describe('i18n for dndImport module', () => {
 		expect(map.dndImport_import_textcontent).toBe('Drop Text-Data or URL here');
 		expect(map.dndImport_import_filecontent).toBe('Drop KML/GPX/GeoJSON file here');
 		expect(map.dndImport_import_unknown).toBe('Cannot read import-data');
+		expect(map.dndImport_import_unsupported).toBe('The file-type is not supported');
 		expect(map.dndImport_import_file_error).toBe('File is not readable');
 		expect(map.dndImport_import_no_file_found).toBe('File not found');
 	});
 
 	it('have the expected amount of translations', () => {
-		const expectedSize = 5;
+		const expectedSize = 6;
 		const deMap = provide('de');
 		const enMap = provide('en');
 
