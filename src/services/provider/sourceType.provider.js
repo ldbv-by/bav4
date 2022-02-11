6adff2c7925fd2eea6b51e88c76cfecab99b8a5a
Thomas Aulinger <taulinger@gmx.de>
2022-02-11 15:27:09 +0100

    add a source type provider

---
+++ src/services/provider/sourceType.provider.js
@@ -0,0 +1,80 @@
+
+
+import { $injector } from '../../injection';
+import { isString } from '../../utils/checks';
+import { MediaType } from '../HttpService';
+import { SourceType, SourceTypeName } from '../SourceTypeService';
+
+/**
+ * A function that tries to detect the source type for a url
+ *
+ * @typedef {function(string) : (Promise<SourceType>)} urlSourceTypeProvider
+ */
+/**
+ * A function that tries to detect the source type for a url
+ *
+ * @typedef {function(string, MediaType) : (SourceType|null)} dataSourceTypeProvider
+ */
+
+
+
+/**
+ * Uses a BVV endpoint to detect the source type.
+ * @function
+ * @param {string} url
+ * @returns {SourceType|null}
+ */
+export const bvvUrlSourceTypeProvider = async (url) => {
+
+	const { HttpService: httpService, ConfigService: configService } = $injector.inject('HttpService', 'ConfigService');
+	const endpointUrl = configService.getValueAsPath('BACKEND_URL') + 'sourceType';
+	const result = await httpService.get(`${endpointUrl}?url=${url}`);
+
+	switch (result.status) {
+		case 200: {
+			const { name, version } = await result.json();
+			return new SourceType(name, version);
+		}
+		case 204: {
+			return null;
+		}
+	}
+	throw new Error('SourceType could not be retrieved');
+};
+
+/**
+ * Default source type provider for data.
+ * Currently only character data are supported.
+ * @function
+ * @param {string} data
+ * @param {string} [mediaType]
+ * @returns VectorSourceType or `null`
+ */
+export const defaultDataSourceTypeProvider = (data, mediaType = null) => {
+	if (isString(data)) {
+		switch (mediaType) {
+			case MediaType.KML:
+				return { name: SourceTypeName.KML };
+			case MediaType.GPX:
+				return { name: SourceTypeName.GPX };
+			case MediaType.GeoJSON:
+				return { name: SourceTypeName.GeoJSON };
+		}
+		// alternatively, we check the content in a naive manner
+		if (data.includes('<kml') && data.includes('</kml>')) {
+			return { name: SourceTypeName.KML };
+		}
+		if (data.includes('<gpx') && data.includes('</gpx>')) {
+			return { name: SourceTypeName.GPX };
+		}
+		try {
+			if (JSON.parse(data).type) {
+				return { name: SourceTypeName.GeoJSON };
+			}
+		}
+		catch {
+			return null;
+		}
+	}
+	return null;
+};
