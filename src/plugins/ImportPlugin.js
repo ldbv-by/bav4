c92362a54d60468b148cca23e0725b6847957cca
thiloSchlemmer <thiloSchlemmer@gmail.com>
2022-02-15 13:25:45 +0100

    update used services, add tests (in progress)

--- src/plugins/ImportPlugin.js
+++ src/plugins/ImportPlugin.js
@@ -2,6 +2,7 @@ import { $injector } from '../injection';
 import { addLayer } from '../store/layers/layers.action';
 import { emitNotification, LevelTypes } from '../store/notifications/notifications.action';
 import { observe } from '../utils/storeUtils';
+import { provide as provider } from './i18n/importPlugin.provider';
 import { BaPlugin } from './BaPlugin';
 
 /**
@@ -9,28 +10,42 @@ import { BaPlugin } from './BaPlugin';
  * @author thiloSchlemmer
  */
 export class ImportPlugin extends BaPlugin {
+	constructor() {
+		super();
+		const { ImportService: importService, SourceTypeService: sourceTypeService, TranslationService: translationService } = $injector.inject('ImportService', 'SourceTypeService', 'TranslationService');
+		this._importService = importService;
+		this._sourceTypeService = sourceTypeService;
+		this._translationService = translationService;
+		translationService.register('importPluginProvider', provider);
+	}
 
 	/**
 	 * @override
 	 * @param {Store} store
 	 */
 	async register(store) {
-		const { ImportService: importService, SourceTypeService: sourceTypeService } = $injector.inject('ImportService', 'SourceTypeService');
-
 
 		const onChange = (latestImport) => {
 			try {
 				const importByUrl = (url) => {
-					const sourceType = sourceTypeService.forURL(url);
-					return importService.importVectorDataFromUrl(url, { sourceType: sourceType });
+					const sourceType = this._sourceTypeService.forURL(url);
+					const vectorGeoResource = this._importService.importVectorDataFromUrl(url, { sourceType: sourceType });
+					vectorGeoResource.onReject(() => emitNotification(`${this._translationService.translate('importPlugin_url_failed')}:${url}`, LevelTypes.ERROR));
+					return vectorGeoResource;
 				};
 				const importByData = (data, mimeType) => {
-					const sourceType = sourceTypeService.forData(data, mimeType);
-					importService.importVectorData(latestImport.data, { sourceType: sourceType });
+					const sourceType = this._sourceTypeService.forData(data, mimeType);
+					const vectorGeoResource = this._importService.importVectorData(latestImport.data, { sourceType: sourceType });
+					if (vectorGeoResource) {
+						return vectorGeoResource;
+					}
+					emitNotification(this._translationService.translate('importPlugin_data_failed'), LevelTypes.ERROR);
 				};
 
-				const vgr = latestImport.url ? importByUrl(latestImport.url) : importByData(latestImport.data, latestImport.mimeType);
-				addLayer(vgr.id, { label: vgr.label });
+				const vectorGeoResource = latestImport.url ? importByUrl(latestImport.url) : importByData(latestImport.data, latestImport.mimeType);
+
+				const { id, label } = vectorGeoResource;
+				addLayer(id, { label: label });
 			}
 			catch (error) {
 				emitNotification(error, LevelTypes.ERROR);
