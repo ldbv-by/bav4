5a2175093ce4cac46e304a989618e590ce6c5b16
thiloSchlemmer <thiloSchlemmer@gmail.com>
2022-02-17 07:32:44 +0100

    update test

--- test/service/StoreService.test.js
+++ test/service/StoreService.test.js
@@ -90,7 +90,7 @@ describe('StoreService', () => {
 			expect(store).toBeDefined();
 
 			const reducerKeys = Object.keys(store.getState());
-			expect(reducerKeys.length).toBe(20);
+			expect(reducerKeys.length).toBe(21);
 			expect(reducerKeys.includes('map')).toBeTrue();
 			expect(reducerKeys.includes('pointer')).toBeTrue();
 			expect(reducerKeys.includes('position')).toBeTrue();
