e28560bd783c19ae21d547f0cec14cd0c6965036
Thomas Aulinger <taulinger@gmx.de>
2022-02-23 11:06:20 +0100

    methods return a SourceTypeResult instance

--- src/services/SourceTypeService.js
+++ src/services/SourceTypeService.js
@@ -1,5 +1,5 @@
 import { isHttpUrl } from '../utils/checks';
-import { SourceTypeMaxFileSize } from './domain/sourceType';
+import { SourceTypeMaxFileSize, SourceTypeResult, SourceTypeResultStatus } from './domain/sourceType';
 import { bvvUrlSourceTypeProvider, defaultDataSourceTypeProvider, defaultMediaSourceTypeProvider } from './provider/sourceType.provider';
 
 
@@ -13,11 +13,11 @@ import { bvvUrlSourceTypeProvider, defaultDataSourceTypeProvider, defaultMediaSo
 export class SourceTypeService {
 
 	/**
-     *
-     * @param {urlSourceTypeProvider} [urlSourceTypeProvider=bvvUrlSourceTypeProvider]
-     * @param {dataSourceTypeProvider} [dataSourceTypeProvider=defaultDataSourceTypeProvider]
+	 *
+	 * @param {urlSourceTypeProvider} [urlSourceTypeProvider=bvvUrlSourceTypeProvider]
+	 * @param {dataSourceTypeProvider} [dataSourceTypeProvider=defaultDataSourceTypeProvider]
 	 * @param {mediaSourceTypeProvider} [mediaSourceTypeProvider=defaultMediaSourceTypeProvider]
-     */
+	 */
 	constructor(urlSourceTypeProvider = bvvUrlSourceTypeProvider, dataSourceTypeProvider = defaultDataSourceTypeProvider, mediaSourceTypeProvider = defaultMediaSourceTypeProvider) {
 		this._urlSourceTypeProvider = urlSourceTypeProvider;
 		this._dataSourceTypeProvider = dataSourceTypeProvider;
@@ -25,43 +25,41 @@ export class SourceTypeService {
 	}
 
 	/**
-     *
-     * @param {string} url
-     * @returns {SourceType|null} sourceType or `null` when no source type was detected
-     */
+	 *
+	 * @param {string} url
+	 * @returns {SourceTypeResult} the result of this request
+	 * @throws
+	 */
 	async forUrl(url) {
 		if (!isHttpUrl(url)) {
 			throw new TypeError('Parameter <url> must represent an Http URL');
 		}
-		try {
-			return await this._urlSourceTypeProvider(url);
-		}
-		catch (e) {
-			throw new Error(`Could not detect a SourceType: ${e}`);
-		}
+		return await this._urlSourceTypeProvider(url);
 	}
 
 	/**
-     *
-     * @param {string} data
-     * @param {string} [mediaType]
-     * @returns {SourceType|null} sourceType or `null` when no source type was detected
-     */
+	 *
+	 * @param {string} data
+	 * @param {string} [mediaType]
+	 * @returns {SourceTypeResult} the result of this request
+	 */
 	forData(data, mediaType = null) {
-		const sourceType = mediaType ? this._mediaSourceTypeProvider(mediaType) : null;
-		return sourceType ? sourceType : this._dataSourceTypeProvider(data);
+		const result = mediaType ? this._mediaSourceTypeProvider(mediaType) : null;
+		return result ?? this._dataSourceTypeProvider(data);
 	}
 
 	/**
 	 *
 	 * @param {Blob} blob the blob
-	 * @returns {SourceType|null} sourceType or `null` when no source type was detected or 'null'
-	 * when blob is too large
+	 * @returns {SourceTypeResult} the result of this request
 	 */
 	forBlob(blob) {
-		if (blob instanceof Blob && blob.size <= SourceTypeMaxFileSize) {
-			return this._mediaSourceTypeProvider(blob.type);
+		if (!(blob instanceof Blob)) {
+			throw new TypeError('Parameter <blob> must be an instance of Blob');
 		}
-		return null;
+		if (blob.size >= SourceTypeMaxFileSize) {
+			return new SourceTypeResult(SourceTypeResultStatus.MAX_SIZE_EXCEEDED);
+		}
+		return this._mediaSourceTypeProvider(blob.type);
 	}
 }
