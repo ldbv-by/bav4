503ed4be38f0dc8bb9a81127902bdf32b316a897
alsturm <alexander_sturm@gmx.net>
2022-03-07 14:22:01 +0100

    rename i18n keysa

--- src/modules/menu/components/mainMenu/content/misc/BvvMiscContentPanel.js
+++ src/modules/menu/components/mainMenu/content/misc/BvvMiscContentPanel.js
@@ -30,18 +30,18 @@ export class BvvMiscContentPanel extends AbstractMvuContentPanel {
         <div class="ba-list">	
 			<div class="ba-list-item  ba-list-item__header">
 				<span class="ba-list-item__text ">
-					<span class="ba-list-item__primary-text">${translate('misc_content_panel_settings')}</span>
+					<span class="ba-list-item__primary-text">${translate('menu_misc_content_panel_settings')}</span>
 				</span>
 			</div>		
 			<div  class="ba-list-item divider">
-				<span class="ba-list-item__text vertical-center">${translate('misc_content_panel_dark_mode')}</span>
+				<span class="ba-list-item__text vertical-center">${translate('menu_misc_content_panel_dark_mode')}</span>
 				<span class="ba-list-item__after">
 					<ba-theme-toggle></ba-theme-toggle>
 				</span>
 			</div>
 			<div class="ba-list-item  ba-list-item__header">
 				<span class="ba-list-item__text ">
-					<span class="ba-list-item__primary-text">${translate('misc_content_panel_information')}</span>
+					<span class="ba-list-item__primary-text">${translate('menu_misc_content_panel_information')}</span>
 				</span>
 			</div>		
 			<a class="ba-list-item" href='https://www.ldbv.bayern.de/hilfe.html' target='_blank'>
@@ -49,46 +49,46 @@ export class BvvMiscContentPanel extends AbstractMvuContentPanel {
 					<span class="ba-list-item__icon icon help">				
 					</span>
 				</span>
-				<span class="ba-list-item__text vertical-center">${translate('misc_content_panel_help')}</span>
+				<span class="ba-list-item__text vertical-center">${translate('menu_misc_content_panel_help')}</span>
 			</a>  
 			<a class="ba-list-item"  href='https://www.ldbv.bayern.de/service/kontakt.html' target='_blank'>
 				<span class="ba-list-item__pre">
 					<span class="ba-list-item__icon icon contact">
 					</span>
 				</span>
-				<span class="ba-list-item__text vertical-center">${translate('misc_content_panel_Contact')}</span>
+				<span class="ba-list-item__text vertical-center">${translate('menu_misc_content_panel_Contact')}</span>
 			</a>  
 			<a class="ba-list-item"  href='https://geoportal.bayern.de/geoportalbayern/seiten/nutzungsbedingungen.html' target='_blank'>
 				<span class="ba-list-item__pre">
 					<span class="ba-list-item__icon icon checklist">					
 					</span>
 				</span>
-				<span class="ba-list-item__text vertical-center">${translate('misc_content_panel_terms_of_use')}</span>
+				<span class="ba-list-item__text vertical-center">${translate('menu_misc_content_panel_terms_of_use')}</span>
 			</a>  
 			<a class="ba-list-item"  href='https://geoportal.bayern.de/geoportalbayern/seiten/datenschutz.html' target='_blank'>
 				<span class="ba-list-item__pre">
 					<span class="ba-list-item__icon icon lock">				
 					</span>
 				</span>
-				<span class="ba-list-item__text vertical-center">${translate('misc_content_panel_privacy_policy')}</span>
+				<span class="ba-list-item__text vertical-center">${translate('menu_misc_content_panel_privacy_policy')}</span>
 			</a>  
 			<a class="ba-list-item "  href='https://geoportal.bayern.de/geoportalbayern/seiten/impressum.html' target='_blank'>
 				<span class="ba-list-item__pre">
 					<span class="ba-list-item__icon icon imprint">				
 					</span>
 				</span>
-				<span class="ba-list-item__text vertical-center">${translate('misc_content_panel_imprint')}</span>
+				<span class="ba-list-item__text vertical-center">${translate('menu_misc_content_panel_imprint')}</span>
 			</a>  
 			<a class="ba-list-item divider"  href='https://github.com/ldbv-by/bav4-nomigration' target='_blank'>
 				<span class="ba-list-item__pre">
 					<span class="ba-list-item__icon icon git">
 					</span>
 				</span>
-				<span class="ba-list-item__text vertical-center">${translate('misc_content_panel_github')}</span>
+				<span class="ba-list-item__text vertical-center">${translate('menu_misc_content_panel_github')}</span>
 			</a>  
 			<div class="ba-list-item  ba-list-item__header">
 				<span class="ba-list-item__text ">
-					<span class="ba-list-item__primary-text">${translate('misc_content_panel_more_links')}</span>
+					<span class="ba-list-item__primary-text">${translate('menu_menu_misc_content_panel_more_links')}</span>
 				</span>
 			</div>	
 			<a class="ba-list-item" href='https://geodatenonline.bayern.de/geodatenonline' target='_blank'>
@@ -97,8 +97,8 @@ export class BvvMiscContentPanel extends AbstractMvuContentPanel {
 					</span>
 				</span>
 				<span class="ba-list-item__text ">
-					<span class="ba-list-item__primary-text">${translate('misc_content_panel_gdo_header')}</span>
-					<span class="ba-list-item__secondary-text">${translate('misc_content_panel_gdo_text')}</span>
+					<span class="ba-list-item__primary-text">${translate('menu_misc_content_panel_gdo_header')}</span>
+					<span class="ba-list-item__secondary-text">${translate('menu_misc_content_panel_gdo_text')}</span>
 				</span>
 			</a>             
 			<a class="ba-list-item" href='https://www.geoportal.bayern.de/geoportalbayern' target='_blank'>
@@ -107,8 +107,8 @@ export class BvvMiscContentPanel extends AbstractMvuContentPanel {
 					</span>
 				</span>
 				<span class="ba-list-item__text ">
-					<span class="ba-list-item__primary-text">${translate('misc_content_panel_gp_header')}</span>
-					<span class="ba-list-item__secondary-text">${translate('misc_content_panel_gp_text')}</span>
+					<span class="ba-list-item__primary-text">${translate('menu_misc_content_panel_gp_header')}</span>
+					<span class="ba-list-item__secondary-text">${translate('menu_misc_content_panel_gp_text')}</span>
 				</span>
 			</a>             		          
 			<a class="ba-list-item" href='https://www.energieatlas.bayern.de/' target='_blank'>
@@ -117,8 +117,8 @@ export class BvvMiscContentPanel extends AbstractMvuContentPanel {
 					</span>
 				</span>
 				<span class="ba-list-item__text ">
-					<span class="ba-list-item__primary-text">${translate('misc_content_panel_ea_header')}</span>
-					<span class="ba-list-item__secondary-text">${translate('misc_content_panel_ea_text')}</span>
+					<span class="ba-list-item__primary-text">${translate('menu_misc_content_panel_ea_header')}</span>
+					<span class="ba-list-item__secondary-text">${translate('menu_misc_content_panel_ea_text')}</span>
 				</span>
 			</a>             		          
 		</div>
--- test/modules/menu/components/mainMenu/content/more/BvvMiscContentPanel.test.js
+++ test/modules/menu/components/mainMenu/content/more/BvvMiscContentPanel.test.js
@@ -45,42 +45,42 @@ describe('MiscContentPanel', () => {
 
 			expect(links[0].href).toEqual('https://www.ldbv.bayern.de/hilfe.html');
 			expect(links[0].target).toEqual('_blank');
-			expect(links[0].querySelector('.ba-list-item__text').innerText).toEqual('misc_content_panel_help');
+			expect(links[0].querySelector('.ba-list-item__text').innerText).toEqual('menu_misc_content_panel_help');
 
 			expect(links[1].href).toEqual('https://www.ldbv.bayern.de/service/kontakt.html');
 			expect(links[1].target).toEqual('_blank');
-			expect(links[1].querySelector('.ba-list-item__text').innerText).toEqual('misc_content_panel_Contact');
+			expect(links[1].querySelector('.ba-list-item__text').innerText).toEqual('menu_misc_content_panel_Contact');
 
 			expect(links[2].href).toEqual('https://geoportal.bayern.de/geoportalbayern/seiten/nutzungsbedingungen.html');
 			expect(links[2].target).toEqual('_blank');
-			expect(links[2].querySelector('.ba-list-item__text').innerText).toEqual('misc_content_panel_terms_of_use');
+			expect(links[2].querySelector('.ba-list-item__text').innerText).toEqual('menu_misc_content_panel_terms_of_use');
 
 			expect(links[3].href).toEqual('https://geoportal.bayern.de/geoportalbayern/seiten/datenschutz.html');
 			expect(links[3].target).toEqual('_blank');
-			expect(links[3].querySelector('.ba-list-item__text').innerText).toEqual('misc_content_panel_privacy_policy');
+			expect(links[3].querySelector('.ba-list-item__text').innerText).toEqual('menu_misc_content_panel_privacy_policy');
 
 			expect(links[4].href).toEqual('https://geoportal.bayern.de/geoportalbayern/seiten/impressum.html');
 			expect(links[4].target).toEqual('_blank');
-			expect(links[4].querySelector('.ba-list-item__text').innerText).toEqual('misc_content_panel_imprint');
+			expect(links[4].querySelector('.ba-list-item__text').innerText).toEqual('menu_misc_content_panel_imprint');
 
 			expect(links[5].href).toEqual('https://github.com/ldbv-by/bav4-nomigration');
 			expect(links[5].target).toEqual('_blank');
-			expect(links[5].querySelector('.ba-list-item__text').innerText).toEqual('misc_content_panel_github');
+			expect(links[5].querySelector('.ba-list-item__text').innerText).toEqual('menu_misc_content_panel_github');
 
 			expect(links[6].href).toEqual('https://geodatenonline.bayern.de/geodatenonline');
 			expect(links[6].target).toEqual('_blank');
-			expect(links[6].querySelector('.ba-list-item__primary-text').innerText).toEqual('misc_content_panel_gdo_header');
-			expect(links[6].querySelector('.ba-list-item__secondary-text').innerText).toEqual('misc_content_panel_gdo_text');
+			expect(links[6].querySelector('.ba-list-item__primary-text').innerText).toEqual('menu_misc_content_panel_gdo_header');
+			expect(links[6].querySelector('.ba-list-item__secondary-text').innerText).toEqual('menu_misc_content_panel_gdo_text');
 
 			expect(links[7].href).toEqual('https://www.geoportal.bayern.de/geoportalbayern');
 			expect(links[7].target).toEqual('_blank');
-			expect(links[7].querySelector('.ba-list-item__primary-text').innerText).toEqual('misc_content_panel_gp_header');
-			expect(links[7].querySelector('.ba-list-item__secondary-text').innerText).toEqual('misc_content_panel_gp_text');
+			expect(links[7].querySelector('.ba-list-item__primary-text').innerText).toEqual('menu_misc_content_panel_gp_header');
+			expect(links[7].querySelector('.ba-list-item__secondary-text').innerText).toEqual('menu_misc_content_panel_gp_text');
 
 			expect(links[8].href).toEqual('https://www.energieatlas.bayern.de/');
 			expect(links[8].target).toEqual('_blank');
-			expect(links[8].querySelector('.ba-list-item__primary-text').innerText).toEqual('misc_content_panel_ea_header');
-			expect(links[8].querySelector('.ba-list-item__secondary-text').innerText).toEqual('misc_content_panel_ea_text');
+			expect(links[8].querySelector('.ba-list-item__primary-text').innerText).toEqual('menu_misc_content_panel_ea_header');
+			expect(links[8].querySelector('.ba-list-item__secondary-text').innerText).toEqual('menu_misc_content_panel_ea_text');
 		});
 	});
 });
