4c5e846380297c83aed2946ef46de0715f4233a5
Thomas Aulinger <taulinger@gmx.de>
2022-03-07 15:26:37 +0100

    use QueryParameter.ENABLE_TEST_IDS,  rename window property indicating test id generation

--- test/e2e/windowProperties.spec.js
+++ test/e2e/windowProperties.spec.js
@@ -1,17 +1,18 @@
 const { test, expect } = require('@playwright/test');
+const { QueryParameters } = require('../../src/services/domain/queryParameters');
 
 const BASE_URL = process.env.URL || 'http://localhost:8080';
 
 test.describe('global properties', () => {
 
-	test.describe('baGenerateTestIds property', () => {
+	test.describe('enableTestIds property', () => {
 
 		test.describe('when query parameter is not available', () => {
 			test('property should be `false`', async ({ page }) => {
 				await page.goto(`${BASE_URL}`);
 
 				const aHandle = await page.evaluateHandle(() => window);
-				const resultHandle = await page.evaluateHandle(window => window.baGenerateTestIds, aHandle);
+				const resultHandle = await page.evaluateHandle(window => window.enableTestIds, aHandle);
 
 				expect(await resultHandle.jsonValue()).toBe(false);
 
@@ -21,10 +22,10 @@ test.describe('global properties', () => {
 
 		test.describe('when query parameter has value of `true`', () => {
 			test('property should be `true`', async ({ page }) => {
-				await page.goto(`${BASE_URL}?generate_test_ids=true`);
+				await page.goto(`${BASE_URL}?${QueryParameters.ENABLE_TEST_IDS}=true`);
 
 				const aHandle = await page.evaluateHandle(() => window);
-				const resultHandle = await page.evaluateHandle(window => window.baGenerateTestIds, aHandle);
+				const resultHandle = await page.evaluateHandle(window => window.enableTestIds, aHandle);
 
 				expect(await resultHandle.jsonValue()).toBe(true);
 
@@ -34,10 +35,10 @@ test.describe('global properties', () => {
 
 		test.describe('when query parameter has value of something else', () => {
 			test('property should be `false`', async ({ page }) => {
-				await page.goto(`${BASE_URL}?generate_test_ids=foo`);
+				await page.goto(`${BASE_URL}?${QueryParameters.ENABLE_TEST_IDS}=foo`);
 
 				const aHandle = await page.evaluateHandle(() => window);
-				const resultHandle = await page.evaluateHandle(window => window.baGenerateTestIds, aHandle);
+				const resultHandle = await page.evaluateHandle(window => window.enableTestIds, aHandle);
 
 				expect(await resultHandle.jsonValue()).toBe(false);
 
