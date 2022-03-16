debdff8a96da4b20b99870802fcb3ab04988e538
Schlemmer, Thilo (LDBV) <thilo.schlemmer@ldbv.bayern.de>
2022-03-16 11:10:11 +0100

    fix import-behavior

--- src/modules/toolbox/components/importToolContent/ImportToolContent.js
+++ src/modules/toolbox/components/importToolContent/ImportToolContent.js
@@ -35,9 +35,10 @@ export class ImportToolContent extends AbstractToolContent {
 				setData(text, sourceType);
 			};
 			const handleFiles = (files) => {
-				Array.from(files).forEach(f => {
+				Array.from(files).forEach(async f => {
 					try {
-						const sourceTypeResult = this._sourceTypeService.forBlob(f);
+
+						const sourceTypeResult = await this._sourceTypeService.forBlob(f);
 						this._importOrNotify(sourceTypeResult, () => importData(f, sourceTypeResult.sourceType));
 					}
 					catch (error) {
