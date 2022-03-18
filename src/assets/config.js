71c1260ae9d465f0222f90c830b5fd106364474d
Thomas Aulinger <taulinger@gmx.de>
2022-03-18 12:14:16 +0100

    update doc

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,5 +1,5 @@
 /**
  * Placeholder file for delivering config properties.
- * This could be done here by appointing a global config object.
+ * This should be done here by appointing a global config object, e.g.
  * window.config = { FOO: 'bar'}
  */
