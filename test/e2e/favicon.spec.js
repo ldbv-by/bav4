eeeeb9f3d764d7732065fa843786da3bdab30b23
Thomas Aulinger <taulinger@gmx.de>
2022-03-23 11:55:29 +0100

    make test  suitable both for 'light' and 'webapp' mode

--- test/e2e/favicon.spec.js
+++ test/e2e/favicon.spec.js
@@ -6,7 +6,13 @@ test.describe('favicons', () => {
 
 	test('should provide a favicon', async ({ request }) => {
 
-		const response = await request.get(`${BASE_URL}/assets/favicon.svg`);
-		expect(response.ok()).toBeTruthy();
+		/**
+		 * favicons-webpack-plugin generates in 'light' mode a favicon.svg only,
+		 * whereas in 'webapp' mode the favicon.svg is missing
+		 */
+		const responseIco = await request.get(`${BASE_URL}/assets/favicon.ico`);
+		const responseSvg = await request.get(`${BASE_URL}/assets/favicon.svg`);
+
+		expect(responseIco.ok() || responseSvg.ok).toBeTruthy();
 	});
 });
