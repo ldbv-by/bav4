7f3433edf146025d0c534aea7d420414c137af95
Thomas Aulinger <taulinger@gmx.de>
2022-05-04 15:16:05 +0200

    register BaOverlay

--- src/modules/olMap/components/index.js
+++ src/modules/olMap/components/index.js
@@ -1,6 +1,9 @@
 import { OlMap } from './OlMap';
-
+import { BaOverlay } from './BaOverlay';
 if (!window.customElements.get(OlMap.tag)) {
 	window.customElements.define(OlMap.tag, OlMap);
+}
+if (!window.customElements.get(BaOverlay.tag)) {
+	window.customElements.define(BaOverlay.tag, BaOverlay);
 }
 
--- src/modules/olMap/index.js
+++ src/modules/olMap/index.js
@@ -1,6 +1,2 @@
 import './i18n';
 import './components';
-import { BaOverlay } from './BaOverlay';
-if (!window.customElements.get(BaOverlay.tag)) {
-	window.customElements.define(BaOverlay.tag, BaOverlay);
-}
