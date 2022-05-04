8e3f5d51ac0e31dbe24544d13e06967ee8028896
Thomas Aulinger <taulinger@gmx.de>
2022-05-04 10:13:52 +0200

    fix typo

--- test/modules/map/components/olMap/util/feature.provider.test.js
+++ test/modules/map/components/olMap/util/feature.provider.test.js
@@ -44,7 +44,7 @@ describe('feature.provider', () => {
 		expect(expectedFeatures[0].getGeometry().getCoordinates()[1]).toBeCloseTo(6386078, 0);
 	});
 
-	it('filters aus features without a geometry', async () => {
+	it('filters out features without a geometry', async () => {
 		const format = new KML();
 		// contains one valid and one invalid placemark
 		const kml = '<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.opengis.net/kml/2.2 https://developers.google.com/kml/schema/kml22gx.xsd"><Document><name>Invalid Placemarks</name><Placemark id="Line_valid"><description></description><LineString><tessellate>1</tessellate><altitudeMode>clampToGround</altitudeMode><coordinates>10.713458946685412,49.70007647302964 11.714932179089468,48.34411758499924</coordinates></LineString></Placemark><Placemark id="Point_invalid"><description>This is a invalid Point-Placemark</description><Point><coordinates>..</coordinates></Point></Placemark></Document></kml>';
