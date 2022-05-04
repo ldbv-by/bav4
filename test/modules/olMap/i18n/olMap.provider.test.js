65b45b742f659192b18e7c603360f3f3f982fc05
Thomas Aulinger <taulinger@gmx.de>
2022-05-04 14:50:52 +0200

    rename i18n  key

--- test/modules/olMap/i18n/olMap.provider.test.js
+++ test/modules/olMap/i18n/olMap.provider.test.js
@@ -21,7 +21,7 @@ describe('i18n for map module', () => {
 		expect(map.olMap_handler_delete_last_point).toBe('Letzter Punkt löschen: ENTF Taste');
 		expect(map.olMap_handler_measure_select).toBe('bestehende Messung auswählen oder neue Messung beginnen');
 		expect(map.olMap_handler_measure_layer_label).toBe('Messung');
-		expect(map.map_olMap_handler_draw_start).toBe('Klicken, um die Zeichnung zu beginnen');
+		expect(map.olMap_handler_draw_start).toBe('Klicken, um die Zeichnung zu beginnen');
 		expect(map.map_olMap_handler_draw_layer_label).toBe('Zeichnung');
 		expect(map.map_olMap_handler_draw_continue_line).toBe('Klicken, um die Linie zu zeichnen (Doppelklick zum Beenden)');
 		expect(map.map_olMap_handler_draw_select).toBe('bestehende Zeichnung auswählen oder neue Zeichnung beginnen');
@@ -49,7 +49,7 @@ describe('i18n for map module', () => {
 		expect(map.olMap_handler_delete_last_point).toBe('Press DEL to remove the last point drawn');
 		expect(map.olMap_handler_measure_select).toBe('Select existing or start new measurement');
 		expect(map.olMap_handler_measure_layer_label).toBe('Measurement');
-		expect(map.map_olMap_handler_draw_start).toBe('Click to start drawing');
+		expect(map.olMap_handler_draw_start).toBe('Click to start drawing');
 		expect(map.map_olMap_handler_draw_layer_label).toBe('Drawing');
 		expect(map.map_olMap_handler_draw_continue_line).toBe('Click to continue drawing the line (double-click to finish)');
 		expect(map.map_olMap_handler_draw_select).toBe('Select existing or start new drawing');
