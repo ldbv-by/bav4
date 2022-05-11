003e92d29a53417f79daee9c64520e96453c8e25
Schlemmer, Thilo (LDBV) <thilo.schlemmer@ldbv.bayern.de>
2022-05-11 13:46:35 +0200

    add test for domPurifySanitizeHtml.provider

---
+++ test/service/provider/domPurifySanitizeHtml.provider.test.js
@@ -0,0 +1,15 @@
+import { domPurifySanitizeHtml } from '../../../src/services/provider/domPurifySanitizeHtml.provider';
+
+describe('DOMPurify sanitize HTML provider', () => {
+
+	it('sanitize a HTML string', () => {
+		// examples from https://github.com/cure53/DOMPurify/blob/main/README.md
+		expect(domPurifySanitizeHtml('<img src=x onerror=alert(1)//>')).toBe('<img src="x">');
+		expect(domPurifySanitizeHtml('<svg><g/onload=alert(2)//<p>')).toBe('<svg><g></g></svg>');
+		expect(domPurifySanitizeHtml('<p>abc<iframe//src=jAva&Tab;script:alert(3)>def</p>')).toBe('<p>abc</p>');
+		expect(domPurifySanitizeHtml('<math><mi//xlink:href="data:x,<script>alert(4)</script>">')).toBe('<math><mi></mi></math>');
+		expect(domPurifySanitizeHtml('<TABLE><tr><td>HELLO</tr></TABL>')).toBe('<table><tbody><tr><td>HELLO</td></tr></tbody></table>');
+		expect(domPurifySanitizeHtml('<UL><li><A HREF=//google.com>click</UL>')).toBe('<ul><li><a href="//google.com">click</a></li></ul>');
+	});
+
+});
