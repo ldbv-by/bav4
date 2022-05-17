ace715ab92554f00167ae45a4da841e384bd1c7e
Thomas Aulinger <taulinger@gmx.de>
2022-05-17 09:10:37 +0200

    add baaCredentialVerify provider

--- src/services/provider/baa.provider.js
+++ src/services/provider/baa.provider.js
@@ -1,13 +1,18 @@
+import { MediaType } from '../HttpService';
+
+
 /**
  * A function that takes a url and returns a promise resolving with a {@link Credential} object.
  *
  * @typedef {function(string) : (Promise<Credential>)} baaCredentialProvider
  */
 
+import { $injector } from '../../injection';
+
 /**
- * Opens a UI mask where the user can enter credential information for a given url.
+ * Opens a UI mask where the user can enter username/password for a given url.
  * @param {string} url
- * @returns
+ * @returns {Credential} credential
  */
 // eslint-disable-next-line no-unused-vars
 export const baaCredentialFromUI = async (url) => {
@@ -17,7 +22,7 @@ export const baaCredentialFromUI = async (url) => {
 		 * Todo: here we will open our BaaCredentialsPanel. Depending on the result of its callback method,
 		 * we either resolve or reject our Promise.
 		 */
-		reject();
+		reject('not yet implemented');
 	});
 };
 
@@ -32,14 +37,27 @@ export const baaCredentialFromUI = async (url) => {
  * Uses an BVV endpoint to verify the given credential on a URL.
  * @param {string} url
  * @param {Credential} credential
- * @returns
+ * @returns the verified credential object
  */
-// eslint-disable-next-line no-unused-vars
 export const bvvBaaCredentialVerify = async (url, credential) => {
 
-	/**
-	 * Todo: implement me
-	 */
-	return Promise.reject();
+
+	const { HttpService: httpService, ConfigService: configService } = $injector.inject('HttpService', 'ConfigService');
+	const endpointUrl = configService.getValueAsPath('BACKEND_URL') + 'verifyCredential';
+	const requestPayload = {
+		...credential,
+		url: url
+	};
+	const { status } = await httpService.post(endpointUrl, JSON.stringify(requestPayload), MediaType.JSON, {
+		timeout: 2000
+	});
+
+	switch (status) {
+		case 200:
+			return Promise.resolve(credential);
+
+		default:
+			return Promise.reject(status);
+	}
 };
 
