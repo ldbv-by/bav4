8724caec70125a3478224a6b45c76b927a5488e4
thiloSchlemmer <thiloSchlemmer@gmail.com>
2022-05-24 07:13:13 +0200

    coerce sanitizeHtmlProvider by additional options
    coercing by additional blacklist of forbidden/ unsafe HTML-Tags and
    filtering of SVG- and mathML-Tags

--- src/services/provider/sanitizeHtml.provider.js
+++ src/services/provider/sanitizeHtml.provider.js
@@ -1,5 +1,37 @@
 import DOMPurify from 'dompurify';
+const Black_List = [
+	'style',
+	'iframe',
+	'embed',
+	'object',
+	'param',
+	'video',
+	'audio',
+	'track',
+	'form',
+	'fieldset',
+	'input',
+	'datalist',
+	'button',
+	'select',
+	'option',
+	'optgroup',
+	'textarea',
+	'output',
+	'keygen',
+	'script',
+	'noscript',
+	'canvas',
+	'template',
+	'dialog'
+];
 
-export const domPurifySanitizeHtml = (htmlString) => {
-	return DOMPurify.sanitize(htmlString);
+/**
+ * Sanitizes HTML content by removing unsafe HTML-, SVG-, mathML-Tags
+ * and scripts.
+ * @param {string} dirty the potentially dirty html content
+ * @returns {string} the cleaned html content
+ */
+export const domPurifySanitizeHtml = (dirty) => {
+	return DOMPurify.sanitize(dirty, { USE_PROFILES: { html: true }, FORBID_TAGS: Black_List });
 };
