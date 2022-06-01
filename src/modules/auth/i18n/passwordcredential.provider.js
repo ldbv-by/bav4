e009237631626338dfae8aba5bc3c0f143031707
Schlemmer, Thilo (LDBV) <thilo.schlemmer@ldbv.bayern.de>
2022-06-01 09:11:54 +0200

    update as suggested
    update i18n
    
    add test for default authenticate-callback; prevent
    toBeTruthy matcher; remove unused slice of state

--- src/modules/auth/i18n/passwordcredential.provider.js
+++ src/modules/auth/i18n/passwordcredential.provider.js
@@ -4,12 +4,12 @@ export const provide = (lang) => {
 		case 'en':
 			return {
 				//the first part of the snake_case key should be the name of the related module
-				auth_passwordCredentialPanel_title: 'authentication for:',
+				auth_passwordCredentialPanel_title: 'Authentication for:',
 				auth_passwordCredentialPanel_credential_username: 'Username',
 				auth_passwordCredentialPanel_credential_password: 'Password',
-				auth_passwordCredentialPanel_submit: 'connect',
+				auth_passwordCredentialPanel_submit: 'Submit',
 				auth_passwordCredentialPanel_credential_rejected: 'Authentication failed. Invalid username or password!',
-				auth_passwordCredentialPanel_authenticate: 'authenticate'
+				auth_passwordCredentialPanel_authenticate: 'Authenticating'
 			};
 
 		case 'de':
@@ -18,9 +18,9 @@ export const provide = (lang) => {
 				auth_passwordCredentialPanel_title: 'Authentifizierung für:',
 				auth_passwordCredentialPanel_credential_username: 'Name',
 				auth_passwordCredentialPanel_credential_password: 'Passwort',
-				auth_passwordCredentialPanel_submit: 'verbinden',
+				auth_passwordCredentialPanel_submit: 'Senden',
 				auth_passwordCredentialPanel_credential_rejected: 'Authentifizierung fehlgeschlagen. Name oder Passwort ungültig!',
-				auth_passwordCredentialPanel_authenticate: 'authentifiziere'
+				auth_passwordCredentialPanel_authenticate: 'Authentifiziere'
 			};
 
 		default:
