a38e368e700d3ecb3108adb01fa912a4c997d49f
thiloSchlemmer <thiloSchlemmer@gmail.com>
2022-07-05 12:39:19 +0200

    update showCase

--- src/modules/utils/components/showCase/ShowCase.js
+++ src/modules/utils/components/showCase/ShowCase.js
@@ -191,7 +191,7 @@ export class ShowCase extends BaElement {
 			emitFixedNotification(getContent(version));
 			version = nextVersion(version, 1, 3);
 		};
-		const menuitems = [{ label: 'item 1', action: () => emitNotification('Item 1', LevelTypes.INFO) }, { label: 'Item 2', action: () => emitNotification('Item 2', LevelTypes.INFO) }, { label: 'item 3', action: () => emitNotification('Item 3', LevelTypes.INFO) }, { label: 'item 4', disabled: true, action: () => emitNotification('Item 4', LevelTypes.INFO) }];
+		const menuitems = [{ label: 'Apple', action: () => emitNotification('Apple', LevelTypes.INFO) }, { label: 'Lemon', action: () => emitNotification('Lemon', LevelTypes.INFO) }, { label: 'Orange', action: () => emitNotification('Orange', LevelTypes.INFO) }, { label: 'Banana', disabled: true, action: () => emitNotification('Banana', LevelTypes.INFO) }];
 
 		return html`
 		<style>
