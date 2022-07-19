95c958ab67b3c11ea133b78d89d9cba39e39910f
thiloSchlemmer <thiloSchlemmer@gmail.com>
2022-07-19 10:13:54 +0200

    add/update tests

--- src/modules/toolbox/components/exportMfpToolContent/ExportMfpToolContent.js
+++ src/modules/toolbox/components/exportMfpToolContent/ExportMfpToolContent.js
@@ -1,11 +1,9 @@
 import { html } from 'lit-html';
-import css from './exportMfpToolContent.css';
 import { $injector } from '../../../../injection';
 import { AbstractToolContent } from '../toolContainer/AbstractToolContent';
 import { emitNotification, LevelTypes } from '../../../../store/notifications/notifications.action';
 import { setMapSize, setScale } from '../../../../store/mfp/mfp.action';
 
-const Update = 'update';
 const Update_Scale = 'update_scale';
 const Update_Map_Size = 'update_map_size';
 const Update_Capabilities = 'update_capabilities';
@@ -24,14 +22,11 @@ export class ExportMfpToolContent extends AbstractToolContent {
 	}
 
 	onInitialize() {
-		this.observe(state => state.mfp.scale, data => this.signal(Update_Scale, data), false);
-		this.observe(state => state.mfp.mapSize, data => this.signal(Update_Map_Size, data), false);
+		this._loadCapabilities();
 	}
 
 	update(type, data, model) {
 		switch (type) {
-			case Update:
-				return { ...model, mapSize: data.mapSize, scale: data.scale };
 			case Update_Scale:
 				return { ...model, scale: data };
 			case Update_Map_Size:
@@ -43,22 +38,9 @@ export class ExportMfpToolContent extends AbstractToolContent {
 
 	async _loadCapabilities() {
 		const capabilities = await this._mfpService.getCapabilities();
-		if (capabilities.length) {
+		if (capabilities.length > 0) {
 			this.signal(Update_Capabilities, capabilities);
 		}
-	}
-
-	_isMapSizeEqual(a, b) {
-		if (!a || !b) {
-			return false;
-		}
-		return a.width === b.width && a.height === b.height;
-	}
-
-	_getLayout(mapSize) {
-		const { capabilities } = this.getModel();
-		const layout = capabilities.find(capability => this._isMapSizeEqual(capability.mapSize, mapSize));
-		return layout ? layout : null;
 	}
 
 	createView(model) {
@@ -66,18 +48,14 @@ export class ExportMfpToolContent extends AbstractToolContent {
 		const translate = (key) => this._translationService.translate(key);
 		const capabilitiesAvailable = capabilities.length > 0;
 
-		if (!capabilitiesAvailable) {
-			this._loadCapabilities();
-		}
-
 		const mapSizes = capabilitiesAvailable ? capabilities.map(capability => {
 			return { name: capability.name, mapSize: capability.mapSize };
-		}) : [{ mapSize: null }];
+		}) : [];
 
-		const currentMapSize = mapSize ? mapSize : mapSizes[0].mapSize;
+		const currentMapSize = capabilitiesAvailable ? (mapSize ? mapSize : mapSizes[0].mapSize) : null;
 
-		const scales = capabilities.find(c => this._isMapSizeEqual(c.mapSize, currentMapSize))?.scales ;
-		const currentLayout = this._getLayout(currentMapSize);
+		const scales = capabilitiesAvailable ? capabilities.find(c => this._isMapSizeEqual(c.mapSize, currentMapSize))?.scales : [];
+		const currentLayout = capabilitiesAvailable ? capabilities.find(capability => this._isMapSizeEqual(capability.mapSize, mapSize)) : null;
 
 		const onClick = () => emitNotification(`Export to MapFishPrint with ${currentLayout} and ${scale}`, LevelTypes.INFO);
 
@@ -85,48 +63,59 @@ export class ExportMfpToolContent extends AbstractToolContent {
 			const layout = e.target.value;
 			const mapSize = capabilities.find(c => c.name === layout)?.mapSize;
 			setMapSize(mapSize);
+			this.signal(Update_Map_Size, mapSize);
 		};
 
 		const onChangeScale = (e) => {
-			const scale = e.target.value;
-			setScale(scale);
+			const parsedScale = parseInt(e.target.value);
+			setScale(parsedScale);
+			this.signal(Update_Scale, parsedScale);
 		};
 
 		const getScaleOptions = (scales, selectedScale) => {
 			if (scales && scales.length > 0) {
-				return scales.map((scale) => html`<option value=${scale} ?selected=${scale === selectedScale}>1:${scale} </option>)}`);
+				return scales.map((scale) => html`<option value=${scale} ?selected=${scale === selectedScale}>1:${scale}</option>)}`);
 			}
 			return html.nothing;
 		};
 
 		const getMapSizeOptions = (mapSizes, selectedMapSize) => {
 			if (mapSizes && mapSizes.length > 0) {
-				return mapSizes.map((m) => html`<option value=${m.mapSize} ?selected=${m.mapSize === selectedMapSize}>${m.name} </option>)}`);
+				return mapSizes.map((m) => html`<option value=${m.name} ?selected=${m.mapSize === selectedMapSize}>${m.name}</option>)}`);
 			}
 			return html.nothing;
 		};
 
-		return html`<style>${css}</style>
+		return html`
         <div class="ba-tool-container">
-        <div class="ba-tool-container__title">
-					${translate('toolbox_exportMfp_header')}
-            <div class='ba-tool-container__content'>
-            <div class="fieldset">
-					<select id='select_layout' @change=${onChangeMapSize}>
-						${getMapSizeOptions(mapSizes, mapSize)}
-					</select>
-					<label for="select_layout" class="control-label">${translate('toolbox_exportMfp_layout')}</label><i class="bar"></i>
+			<div class="ba-tool-container__title">
+						${translate('toolbox_exportMfp_header')}
 			</div>
-            <div class="fieldset">
-					<select id='select_scale'  @change=${onChangeScale}>
-                    ${getScaleOptions(scales, scale)}
-					</select>
-					<label for="select_scale" class="control-label">${translate('toolbox_exportMfp_scale')}</label><i class="bar"></i>
+			<div class='ba-tool-container__content'>
+				<div class="fieldset">
+						<select id='select_layout' @change=${onChangeMapSize}>
+							${getMapSizeOptions(mapSizes, mapSize)}
+						</select>
+						<label for="select_layout" class="control-label">${translate('toolbox_exportMfp_layout')}</label><i class="bar"></i>
+				</div>
+				<div class="fieldset">
+						<select id='select_scale' @change=${onChangeScale}>
+						${getScaleOptions(scales, scale)}
+						</select>
+						<label for="select_scale" class="control-label">${translate('toolbox_exportMfp_scale')}</label><i class="bar"></i>
+				</div>
 			</div>
-            <div class="ba-tool-container__actions footer"> 
-                <ba-button id='btn_submit' .label=${translate('toolbox_exportMfp_submit')} .type=${'primary'} @click=${onClick}></ba-button>
-            </div>
-        </div>`;
+			<div class="ba-tool-container__actions"> 
+				<ba-button id='btn_submit' class="tool-container__button" .label=${translate('toolbox_exportMfp_submit')} @click=${onClick}></ba-button>
+			</div>			
+		</div>`;
+	}
+
+	_isMapSizeEqual(a, b) {
+		if (!a || !b) {
+			return false;
+		}
+		return a.width === b.width && a.height === b.height;
 	}
 
 	static get tag() {
---
+++ test/modules/toolbox/components/exportMfpToolContent/ExportMfpToolContent.test.js
@@ -0,0 +1,175 @@
+import { $injector } from '../../../../../src/injection';
+import { ExportMfpToolContent } from '../../../../../src/modules/toolbox/components/exportMfpToolContent/ExportMfpToolContent';
+import { AbstractToolContent } from '../../../../../src/modules/toolbox/components/toolContainer/AbstractToolContent';
+import { createNoInitialStateMediaReducer } from '../../../../../src/store/media/media.reducer';
+import { mfpReducer } from '../../../../../src/store/mfp/mfp.reducer';
+import { TestUtils } from '../../../../test-utils';
+
+window.customElements.define(ExportMfpToolContent.tag, ExportMfpToolContent);
+
+
+describe('ExportMfpToolContent', () => {
+	let store;
+	const windowMock = {
+		matchMedia() { }
+	};
+
+
+	const mfpServiceMock = {
+		getCapabilities() {
+			return Promise.resolve([]);
+		}
+	};
+
+	const mfpDefaultState = {
+		active: false,
+		scale: null,
+		mapSize: null
+	};
+
+	const setup = async (mfpState = mfpDefaultState, config = {}) => {
+		const state = {
+			mfp: mfpState,
+			media: {
+				portrait: false
+			}
+		};
+
+		const { embed = false, isTouch = false } = config;
+
+		store = TestUtils.setupStoreAndDi(state, { mfp: mfpReducer, media: createNoInitialStateMediaReducer() });
+		$injector
+			.registerSingleton('EnvironmentService', {
+				isEmbedded: () => embed,
+				getWindow: () => windowMock,
+				isTouch: () => isTouch
+			})
+			.registerSingleton('TranslationService', { translate: (key) => key })
+			.registerSingleton('MfpService', mfpServiceMock);
+		return TestUtils.render(ExportMfpToolContent.tag);
+	};
+
+	describe('class', () => {
+
+		it('inherits from AbstractToolContent', async () => {
+
+			const element = await setup();
+
+			expect(element instanceof AbstractToolContent).toBeTrue();
+		});
+	});
+
+	describe('when instantiated', () => {
+
+		it('has a model with default values', async () => {
+			const element = await setup();
+			const model = element.getModel();
+			expect(model).toEqual({
+				mapSize: null,
+				scale: null,
+				capabilities: []
+			});
+		});
+	});
+
+	describe('when initialized', () => {
+		const scales = [42, 21, 1];
+		const dpis = [125, 200];
+		const capabilities = [{ name: 'foo', scales: scales, dpis: dpis, mapSize: { width: 42, height: 21 } }];
+		it('renders the view', async () => {
+			const element = await setup();
+
+			expect(element.shadowRoot.querySelector('#select_layout')).toBeTruthy();
+			expect(element.shadowRoot.querySelector('#select_scale')).toBeTruthy();
+			expect(element.shadowRoot.querySelector('#btn_submit').label).toBe('toolbox_exportMfp_submit');
+
+		});
+
+		it('requests once the capabilities from mfpService', async () => {
+
+			const capabilitiesSpy = spyOn(mfpServiceMock, 'getCapabilities').and.resolveTo(capabilities);
+			const element = await setup();
+
+			expect(capabilitiesSpy).toHaveBeenCalledTimes(1);
+			expect(element.getModel().capabilities).toEqual(capabilities);
+		});
+
+		it('creates select options from the capabilities', async () => {
+
+			spyOn(mfpServiceMock, 'getCapabilities').and.resolveTo(capabilities);
+			const element = await setup();
+
+			expect(element.shadowRoot.querySelectorAll('#select_layout option')).toHaveSize(1);
+			expect(element.shadowRoot.querySelectorAll('#select_scale option')).toHaveSize(3);
+		});
+
+		it('create empty select options, when capabilities are empty', async () => {
+			const element = await setup();
+
+			const layoutOptions = element.shadowRoot.querySelectorAll('#select_layout option');
+			const scaleOptions = element.shadowRoot.querySelectorAll('#select_scale option');
+
+			expect(layoutOptions).toHaveSize(0);
+			expect(scaleOptions).toHaveSize(0);
+		});
+
+		it('labels the layout options with a name from the capabilities', async () => {
+			spyOn(mfpServiceMock, 'getCapabilities').and.resolveTo(capabilities);
+			const element = await setup();
+
+			const layoutOption = element.shadowRoot.querySelector('#select_layout option');
+
+			expect(layoutOption.textContent).toBe('foo');
+		});
+
+		it('labels the scale options with a formatted scale from the capabilities', async () => {
+			spyOn(mfpServiceMock, 'getCapabilities').and.resolveTo(capabilities);
+			const element = await setup();
+
+			const layoutOption = element.shadowRoot.querySelector('#select_scale option');
+
+			expect(layoutOption.textContent).toMatch(/1:\d+/);
+		});
+
+		it('changes store, when a layout is selected', async () => {
+			spyOn(mfpServiceMock, 'getCapabilities').and.resolveTo(capabilities);
+			const element = await setup();
+
+			const layoutSelectElement = element.shadowRoot.querySelector('#select_layout');
+			const layoutOption = layoutSelectElement.item(0);
+			layoutOption.selected = true;
+			layoutSelectElement.dispatchEvent(new Event('change'));
+
+			expect(element.getModel().mapSize).toEqual({ width: 42, height: 21 });
+			expect(store.getState().mfp.mapSize).toEqual({ width: 42, height: 21 });
+		});
+
+		it('changes store, when a scale is selected', async () => {
+			spyOn(mfpServiceMock, 'getCapabilities').and.resolveTo(capabilities);
+			const element = await setup();
+
+			const scaleSelectElement = element.shadowRoot.querySelector('#select_scale');
+			const layoutOption = scaleSelectElement.item(0);
+			layoutOption.selected = true;
+			scaleSelectElement.dispatchEvent(new Event('change'));
+
+			expect(element.getModel().scale).toBe(42);
+			expect(store.getState().mfp.scale).toBe(42);
+		});
+
+	});
+
+	describe('_isMapSizeEqual', () => {
+		it('checks equality of MapSizes', () => {
+			const classUnderTest = new ExportMfpToolContent();
+
+			expect(classUnderTest._isMapSizeEqual(null, null)).toBeFalse();
+			expect(classUnderTest._isMapSizeEqual(undefined, undefined)).toBeFalse();
+			expect(classUnderTest._isMapSizeEqual({}, null)).toBeFalse();
+			expect(classUnderTest._isMapSizeEqual({}, {})).toBeTrue();
+			expect(classUnderTest._isMapSizeEqual({ width: 42, height: 21 }, {})).toBeFalse();
+			expect(classUnderTest._isMapSizeEqual({ width: 42, height: 21 }, { width: 42, height: 21 })).toBeTrue();
+			expect(classUnderTest._isMapSizeEqual({ width: 42, height: 21 }, { width: 21, height: 42 })).toBeFalse();
+		});
+	});
+});
