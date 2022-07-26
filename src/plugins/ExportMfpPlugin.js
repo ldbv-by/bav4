e447cf78ed84fcbf3365c5a6ee42ca812b69b1c3
Thomas Aulinger <taulinger@gmx.de>
2022-07-26 16:21:05 +0200

    handle jobSpec changes

--- src/plugins/ExportMfpPlugin.js
+++ src/plugins/ExportMfpPlugin.js
@@ -30,12 +30,12 @@ export class ExportMfpPlugin extends BaPlugin {
 	 * @param {Store} store
 	 */
 	async register(store) {
+		const { MfpService: mfpService, EnvironmentService: environmentService } = $injector.inject('MfpService', 'EnvironmentService');
 
 		const lazyInitialize = async () => {
 
 			if (!this._initialized) {
 				// let's set the initial mfp properties
-				const { MfpService: mfpService } = $injector.inject('MfpService');
 				const capabilities = await mfpService.getCapabilities();
 				const { id, scales, dpis } = capabilities[0];
 				setCurrent({ id: id, dpi: dpis[0], scale: scales[0] });
@@ -64,7 +64,18 @@ export class ExportMfpPlugin extends BaPlugin {
 			}
 		};
 
+		const onJobSpecChanged = async ({ payload: spec }) => {
+			if (spec) {
+				const url = await mfpService.createJob(spec);
+				environmentService.getWindow().location = url;
+			}
+			else {
+				mfpService.cancelJob();
+			}
+		};
+
 		observe(store, state => state.tools.current, onToolChanged);
 		observe(store, state => state.mfp.active, onChange);
+		observe(store, state => state.mfp.jobSpec, onJobSpecChanged);
 	}
 }
