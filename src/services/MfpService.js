d3040f13ccfc7a43c44784974a0a8c24c3ecb320
Thomas Aulinger <taulinger@gmx.de>
2022-09-15 13:52:21 +0200

    use BVV MfpCapabilities provider

--- src/services/MfpService.js
+++ src/services/MfpService.js
@@ -1,8 +1,10 @@
 import { sleep } from '../utils/sleep';
+import { loadBvvMfpCapabilities } from './provider/mfp.provider';
 /**
  *
  * @typedef {Object} MfpCapabilities
- * @property {string} name
+ * @property {string} id
+ * @property {string} urlId
  * @property {Array<number>} scales
  * @property {Array<number>} dpis
  * @property {MfpMapSize} mapSize
@@ -21,37 +23,29 @@ import { sleep } from '../utils/sleep';
  */
 export class MfpService {
 
-	constructor() {
+	constructor(mfpCapabilitiesProvider = loadBvvMfpCapabilities) {
 		this._abortController = null;
-	}
-
-	_getMockCapabilities() {
-		const scales = [2000000, 1000000, 500000, 200000, 100000, 50000, 25000, 10000, 5000, 2500, 1250, 1000, 500];
-		const dpis = [125, 200];
-
-		return [
-			{ id: 'a4_portrait', scales: scales, dpis: dpis, mapSize: { width: 539, height: 722 } },
-			{ id: 'a4_landscape', scales: scales, dpis: dpis, mapSize: { width: 785, height: 475 } },
-			{ id: 'a3_portrait', scales: scales, dpis: dpis, mapSize: { width: 786, height: 1041 } },
-			{ id: 'a3_landscape', scales: scales, dpis: dpis, mapSize: { width: 1132, height: 692 } }
-		];
+		this._mfpCapabilities = null;
+		this._mfpCapabilitiesProvider = mfpCapabilitiesProvider;
 	}
 
 	/**
 	 * @returns {Array<MfpCapabilities>} all available capbilities
 	 */
 	async getCapabilities() {
-		return this._getMockCapabilities();
+		if (!this._mfpCapabilities) {
+			this._mfpCapabilities = await this._mfpCapabilitiesProvider();
+		}
+		return this._mfpCapabilities;
 	}
 
 	/**
-	* Returns the corresponding  {@link MfpCapabilities} for an id.
-	* @public
+	* Returns the corresponding  {@link MfpCapabilities} for a specific id.
 	* @param {string} id Id of the desired {@link MfpCapabilities}
 	* @returns {MfpCapabilities|null}
 	*/
 	getCapabilitiesById(id) {
-		return this._getMockCapabilities().find(cp => cp.id === id) ?? null;
+		return this._mfpCapabilities?.find(cp => cp.id === id) ?? null;
 	}
 
 	/**
--- test/service/MfpService.test.js
+++ test/service/MfpService.test.js
@@ -1,39 +1,62 @@
 import { MfpService } from '../../src/services/MfpService';
+import { loadBvvMfpCapabilities } from '../../src/services/provider/mfp.provider';
 
 describe('MfpService', () => {
 
-	describe('init', () => {
+	const scales = [2000000, 1000000, 500000, 200000, 100000, 50000, 25000, 10000, 5000, 2500, 1250, 1000, 500];
+	const dpis = [125, 200];
+	const mockCapabilities = [
+		{ id: 'a4_portrait', urlId: 0, scales: scales, dpis: dpis, mapSize: { width: 539, height: 722 } },
+		{ id: 'a4_landscape', urlId: 0, scales: scales, dpis: dpis, mapSize: { width: 785, height: 475 } },
+		{ id: 'a3_portrait', urlId: 0, scales: scales, dpis: dpis, mapSize: { width: 786, height: 1041 } },
+		{ id: 'a3_landscape', urlId: 0, scales: scales, dpis: dpis, mapSize: { width: 1132, height: 692 } }
+	];
 
-		it('initializes the service', async () => {
+	const setup = (provider = loadBvvMfpCapabilities) => {
+		return new MfpService(provider);
+	};
+
+	describe('constructor', () => {
+
+		it('initializes the with default providers', async () => {
 			const instanceUnderTest = new MfpService();
 
 			expect(instanceUnderTest._abortController).toBeNull();
+			expect(instanceUnderTest._mfpCapabilitiesProvider).toEqual(loadBvvMfpCapabilities);
+		});
+
+		it('initializes the service with custom providers', async () => {
+			const customProvider = async () => { };
+			const instanceUnderTest = setup(customProvider);
+			expect(instanceUnderTest._mfpCapabilitiesProvider).toEqual(customProvider);
 		});
 	});
 
 	describe('getCapabilities', () => {
 
 		it('provides an array of MfpCapabilities', async () => {
+			const provider = jasmine.createSpy().and.resolveTo(mockCapabilities);
+			const instanceUnderTest = setup(provider);
 
-			const instanceUnderTest = new MfpService();
-
-			const result = await instanceUnderTest.getCapabilities();
-
-			expect(result).toHaveSize(4);
+			// first call served from provider
+			await expectAsync(instanceUnderTest.getCapabilities()).toBeResolvedTo(mockCapabilities);
+			// second from cache
+			await expectAsync(instanceUnderTest.getCapabilities()).toBeResolvedTo(mockCapabilities);
+			expect(provider).toHaveBeenCalledTimes(1);
 		});
 	});
 
 	describe('getCapabilitiesById', () => {
 
-		it('provides a MfpCapabilities object by its id', () => {
-
-			const instanceUnderTest = new MfpService();
+		it('provides a MfpCapabilities object by its id', async () => {
+			const instanceUnderTest = setup(async () => mockCapabilities);
+			// initially load capabilities
+			await instanceUnderTest.getCapabilities();
 
 			expect(instanceUnderTest.getCapabilitiesById('a4_landscape')).not.toBeNull();
 			expect(instanceUnderTest.getCapabilitiesById('foo')).toBeNull();
 		});
 	});
-
 
 	describe('createJob', () => {
 
