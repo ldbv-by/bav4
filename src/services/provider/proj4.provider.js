465ebc0037a1b83f4f009d2055495a9413c44dc9
Thomas Aulinger <taulinger@gmx.de>
2022-10-14 12:32:57 +0200

    proj4Provider returns registered SRIDs

--- src/services/provider/proj4.provider.js
+++ src/services/provider/proj4.provider.js
@@ -5,9 +5,9 @@ import proj4 from 'proj4';
 import { register } from 'ol/proj/proj4';
 
 /**
- * A function that registers proj4 definitions.
+ * A function that registers proj4 definitions and return an array of SRIDs
  *
- * @typedef {function(coordinate) : (Promise<number>)} proj4Provider
+ * @typedef {function(coordinate) : (array<number>)} proj4Provider
  */
 
 
@@ -19,4 +19,5 @@ export const loadBvvDefinitions = () => {
 	proj4.defs('EPSG:25832', '+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +axis=neu');
 	proj4.defs('EPSG:25833', '+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs +axis=neu');
 	register(proj4);
+	return [25832, 25833];
 };
