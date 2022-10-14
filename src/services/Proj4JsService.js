013e211f0cb567728e05861accffa953a2a8a53c
Thomas Aulinger <taulinger@gmx.de>
2022-10-14 14:29:24 +0200

    add a service for proj4js registration

---
+++ src/services/Proj4JsService.js
@@ -0,0 +1,25 @@
+import { loadBvvDefinitions } from './provider/proj4.provider';
+
+/**
+ * Registers all additional projections.
+ * @class
+ * @author taulinger
+ */
+export class Proj4JsService {
+
+	/**
+     *
+     * @param {proj4Provider} [proj4Provider=loadBvvDefinitions]
+     */
+	constructor(proj4Provider = loadBvvDefinitions) {
+		this._suppertedSrids = [4326, 3857, ...(proj4Provider())];
+	}
+
+	/**
+     * Returns a array of all supported projections as SRID. Contains at least 4326 and 3857.
+     * @returns supported projections
+     */
+	getProjections() {
+		return this._suppertedSrids;
+	}
+}
