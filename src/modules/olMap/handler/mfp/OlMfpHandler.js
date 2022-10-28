052a71b2457ffcf8f56a077652ba16218713e1b6
thiloSchlemmer <thiloSchlemmer@gmail.com>
2022-10-28 08:55:48 +0200

    update doc

--- src/modules/olMap/handler/mfp/OlMfpHandler.js
+++ src/modules/olMap/handler/mfp/OlMfpHandler.js
@@ -96,6 +96,10 @@ export class OlMfpHandler extends OlLayerHandler {
 	}
 
 	_register(store) {
+		// HINT: To observe the store for map changes (especially livecenter) results in a more intensivied call traffic with the store.
+		// A shortcut and allowed alternative is the direct binding to the ol map events.
+		// The current design is choosen prior to the alternative, due to the fact, that the call traffic have no substantial influence to
+		// the performance and time consumptions (< 1 ms), but makes it simpler to follow only one source of events.
 		return [
 			observe(store, state => state.mfp.current, (current) => this._updateMfpPage(current)),
 			observe(store, state => state.mfp.jobRequest, () => this._encodeMap()),
