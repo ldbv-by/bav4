695a6b6ef46ef281604bbaad54456af12cb1e65d
Norbert Klein <nklein@nkode.de>
2022-12-02 13:05:40 +0100

    fixed linting problems for AltitudeProfileAttributeType

--- src/modules/altitudeProfile/components/AnotherType.js
+++ src/modules/altitudeProfile/components/AnotherType.js
@@ -1,7 +1,7 @@
 import { AltitudeProfileAttributeType } from './AltitudeProfileAttributeType';
 
 export class AnotherType extends AltitudeProfileAttributeType {
-  constructor(name, lightColor, darkColor) {
-    super('anotherType', name, lightColor, darkColor);
-  }
+	constructor(name, lightColor, darkColor) {
+		super('anotherType', name, lightColor, darkColor);
+	}
 }
