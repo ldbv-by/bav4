77e3b0cd6efc419912804901749f83e8bf5bddcc
Norbert Klein <nklein@nkode.de>
2022-12-02 13:06:50 +0100

    fixed linting problems for AltitudeProfileUtils.js

--- src/modules/altitudeProfile/utils/AltitudeProfileUtils.js
+++ src/modules/altitudeProfile/utils/AltitudeProfileUtils.js
@@ -3,18 +3,18 @@ export const flatColor = '#66eeff';
 export const steepColor = '#ee4444';
 
 export const InclineType = {
-  Flat: 'Flat',
-  Steep: 'Steep',
+	Flat: 'Flat',
+	Steep: 'Steep'
 };
 
 export const startSteep = (gradientBg, xPoint) => {
-  gradientBg.addColorStop(xPoint, flatColor);
-  gradientBg.addColorStop(xPoint, steepColor);
-  return InclineType.Steep;
+	gradientBg.addColorStop(xPoint, flatColor);
+	gradientBg.addColorStop(xPoint, steepColor);
+	return InclineType.Steep;
 };
 
 export const startFlat = (gradientBg, xPoint) => {
-  gradientBg.addColorStop(xPoint, steepColor);
-  gradientBg.addColorStop(xPoint, flatColor);
-  return InclineType.Flat;
+	gradientBg.addColorStop(xPoint, steepColor);
+	gradientBg.addColorStop(xPoint, flatColor);
+	return InclineType.Flat;
 };
