47978769285f3b289354eeeecb5fda2a43bb93bb
Norbert Klein <nklein@nkode.de>
2022-12-02 13:09:25 +0100

    fixed linting problems for altitudeProfile.provider.js

--- src/modules/altitudeProfile/i18n/altitudeProfile.provider.js
+++ src/modules/altitudeProfile/i18n/altitudeProfile.provider.js
@@ -1,30 +1,30 @@
 export const provide = (lang) => {
-  switch (lang) {
-    case 'en':
-      return {
-        altitudeProfile_header: 'this is a header',
-        altitudeProfile_distance: 'Distance',
-        altitudeProfile_incline: 'Incline',
-        altitudeProfile_sumUp: 'SumUp',
-        altitudeProfile_sumDown: 'SumDown',
-        altitudeProfile_elevation: 'Elevation',
-        altitudeProfile_surface: 'surface',
-        altitudeProfile_unknown: 'unknown',
-      };
+	switch (lang) {
+		case 'en':
+			return {
+				altitudeProfile_header: 'this is a header',
+				altitudeProfile_distance: 'Distance',
+				altitudeProfile_incline: 'Incline',
+				altitudeProfile_sumUp: 'SumUp',
+				altitudeProfile_sumDown: 'SumDown',
+				altitudeProfile_elevation: 'Elevation',
+				altitudeProfile_surface: 'surface',
+				altitudeProfile_unknown: 'unknown'
+			};
 
-    case 'de':
-      return {
-        altitudeProfile_header: 'this is a header',
-        altitudeProfile_distance: 'Entfernung',
-        altitudeProfile_incline: 'Steigung',
-        altitudeProfile_sumUp: 'SumUp de',
-        altitudeProfile_sumDown: 'SumDown de',
-        altitudeProfile_elevation: 'Höhe',
-        altitudeProfile_surface: 'Untergrund',
-        altitudeProfile_unknown: 'unbekannt',
-      };
+		case 'de':
+			return {
+				altitudeProfile_header: 'this is a header',
+				altitudeProfile_distance: 'Entfernung',
+				altitudeProfile_incline: 'Steigung',
+				altitudeProfile_sumUp: 'SumUp de',
+				altitudeProfile_sumDown: 'SumDown de',
+				altitudeProfile_elevation: 'Höhe',
+				altitudeProfile_surface: 'Untergrund',
+				altitudeProfile_unknown: 'unbekannt'
+			};
 
-    default:
-      return {};
-  }
+		default:
+			return {};
+	}
 };
