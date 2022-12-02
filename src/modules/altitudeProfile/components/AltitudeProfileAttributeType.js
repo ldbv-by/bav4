ac4b1656229cf3549644595ad0269e7e5b05d486
Norbert Klein <nklein@nkode.de>
2022-12-02 11:58:44 +0100

    AltitudeProfileAttributeType

---
+++ src/modules/altitudeProfile/components/AltitudeProfileAttributeType.js
@@ -0,0 +1,40 @@
+import { getStore } from './AltitudeProfile';
+import { $injector } from '../../../injection';
+
+export class AltitudeProfileAttributeType {
+  constructor(attribute, name, lightColor, darkColor = null) {
+    this._attribute = attribute;
+    this._name = name;
+    this._lightColor = lightColor;
+    if (darkColor === null) {
+      this._darkColor = lightColor;
+    } else {
+      this._darkColor = darkColor;
+    }
+
+    const { TranslationService: translationService } = $injector.inject('TranslationService');
+    this._translationService = translationService;
+  }
+
+  get caption() {
+    const translate = (key) => this._translationService.translate(key);
+
+    const caption = translate('altitudeProfile_' + this._attribute);
+    return caption;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get color() {
+    const {
+      media: { darkSchema },
+    } = getStore().getState();
+
+    if (darkSchema) {
+      return this._darkColor;
+    }
+    return this._lightColor;
+  }
+}
---
+++ src/modules/altitudeProfile/components/SurfaceType.js
@@ -0,0 +1,7 @@
+import { AltitudeProfileAttributeType } from './AltitudeProfileAttributeType';
+
+export class SurfaceType extends AltitudeProfileAttributeType {
+  constructor(name, lightColor, darkColor) {
+    super('surface', name, lightColor, darkColor);
+  }
+}
