411fa04b90b971268aab38c57cd56b7237c94a4a
alsturm <alexander_sturm@gmx.net>
2022-12-05 16:34:03 +0100

    remove i18n

--- src/modules/chips/i18n/index.js
+++
@@ -1,7 +0,0 @@
-
-import { firstStepsProvide } from './chips.provider';
-import { $injector } from '../../../injection';
-const { TranslationService: translationService } = $injector.inject('TranslationService');
-translationService.register('chipsProvider', firstStepsProvide);
-
-
--- src/modules/chips/index.js
+++ src/modules/chips/index.js
@@ -1,4 +1,3 @@
-import './i18n';
 import { ChipsContainer } from './components/chipsContainer/ChipsContainer';
 if (!window.customElements.get(ChipsContainer.tag)) {
 	window.customElements.define(ChipsContainer.tag, ChipsContainer);
