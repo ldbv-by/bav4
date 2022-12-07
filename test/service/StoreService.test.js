1190f6fad6841f2f5e12973a9236929c18db322c
alsturm <alexander_sturm@gmx.net>
2022-12-07 12:46:51 +0100

    add chips to reducers test

--- test/service/StoreService.test.js
+++ test/service/StoreService.test.js
@@ -93,7 +93,7 @@ describe('StoreService', () => {
 			expect(store).toBeDefined();
 
 			const reducerKeys = Object.keys(store.getState());
-			expect(reducerKeys.length).toBe(24);
+			expect(reducerKeys.length).toBe(25);
 			expect(reducerKeys.includes('map')).toBeTrue();
 			expect(reducerKeys.includes('pointer')).toBeTrue();
 			expect(reducerKeys.includes('position')).toBeTrue();
@@ -118,6 +118,8 @@ describe('StoreService', () => {
 			expect(reducerKeys.includes('mfp')).toBeTrue();
 			expect(reducerKeys.includes('bottomSheet')).toBeTrue();
 			expect(reducerKeys.includes('altitudeProfile')).toBeTrue();
+			expect(reducerKeys.includes('altitudeProfile')).toBeTrue();
+			expect(reducerKeys.includes('chips')).toBeTrue();
 		});
 
 		it('registers all plugins', async () => {
