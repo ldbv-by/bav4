be0796481fe14d906aa560acb14e630ecd9518d0
Norbert Klein <nklein@nkode.de>
2022-12-12 16:49:29 +0100

    renamed AltitudeProfileUtils.js

--- src/modules/altitudeProfile/utils/altitudeProfileUtils.js
+++ src/modules/altitudeProfile/utils/altitudeProfileUtils.js
@@ -2,7 +2,7 @@ export const hereStartsSteep = 0.02;
 export const flatColor = '#66eeff';
 export const steepColor = '#ee4444';
 
-export const InclineType = {
+export const SlopeType = {
 	Flat: 'Flat',
 	Steep: 'Steep'
 };
@@ -10,11 +10,11 @@ export const InclineType = {
 export const startSteep = (gradientBg, xPoint) => {
 	gradientBg.addColorStop(xPoint, flatColor);
 	gradientBg.addColorStop(xPoint, steepColor);
-	return InclineType.Steep;
+	return SlopeType.Steep;
 };
 
 export const startFlat = (gradientBg, xPoint) => {
 	gradientBg.addColorStop(xPoint, steepColor);
 	gradientBg.addColorStop(xPoint, flatColor);
-	return InclineType.Flat;
+	return SlopeType.Flat;
 };
