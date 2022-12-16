30229cbfbfce2790dcd9b6dd3cbc48f6cc65b8c5
Schlemmer, Thilo (LDBV) <thilo.schlemmer@ldbv.bayern.de>
2022-12-16 10:49:13 +0100

    update i18n

--- test/modules/toolbox/i18n/toolbox.provider.test.js
+++ test/modules/toolbox/i18n/toolbox.provider.test.js
@@ -101,7 +101,7 @@ describe('i18n for menu module', () => {
 		expect(map.toolbox_exportMfp_id_a4_portrait).toBe('A4 Hochformat');
 		expect(map.toolbox_exportMfp_id_a3_landscape).toBe('A3 Querformat');
 		expect(map.toolbox_exportMfp_id_a3_portrait).toBe('A3 Hochformat');
-		expect(map.toolbox_exportMfp_options).toBe('Optionen');
+		expect(map.toolbox_exportMfp_options).toBe('Exportoptionen');
 		expect(map.toolbox_exportMfp_autorotation_title).toBe('Autorotation aktivieren/deaktivieren');
 		expect(map.toolbox_exportMfp_autorotation).toBe('Karte automatisch drehen');
 		expect(map.toolbox_exportMfp_show_grid_title).toBe('Ein Koordinatennetz im Export hinzufügen');
@@ -206,7 +206,7 @@ describe('i18n for menu module', () => {
 		expect(map.toolbox_exportMfp_id_a4_portrait).toBe('A4 portrait');
 		expect(map.toolbox_exportMfp_id_a3_landscape).toBe('A3 landscape');
 		expect(map.toolbox_exportMfp_id_a3_portrait).toBe('A3 portrait');
-		expect(map.toolbox_exportMfp_options).toBe('Options');
+		expect(map.toolbox_exportMfp_options).toBe('Exportoptions');
 		expect(map.toolbox_exportMfp_autorotation_title).toBe('Activate/deactivate autorotation');
 		expect(map.toolbox_exportMfp_autorotation).toBe('Automatically rotate map');
 		expect(map.toolbox_exportMfp_show_grid_title).toBe('Add coordinate grid in export');
