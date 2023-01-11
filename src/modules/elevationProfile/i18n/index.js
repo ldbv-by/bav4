a894b3e184138f58f8758ffb91ab41d3d4211cba
Thomas Aulinger <taulinger@gmx.de>
2023-01-11 17:08:30 +0100

    adjust to renaming

--- src/modules/elevationProfile/i18n/index.js
+++ src/modules/elevationProfile/i18n/index.js
@@ -1,4 +1,4 @@
 import { provide as elevationProfileProvider } from './elevationProfile.provider';
 import { $injector } from '../../../injection';
 const { TranslationService: translationService } = $injector.inject('TranslationService');
-translationService.register('altitudeProfileProvider', elevationProfileProvider);
+translationService.register('elevationProfileProvider', elevationProfileProvider);
