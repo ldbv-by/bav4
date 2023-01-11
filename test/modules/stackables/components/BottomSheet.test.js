9b3e045c9ca7ab9eb2ef8e4732119b5bc8753df1
alsturm <alexander_sturm@gmx.net>
2023-01-11 13:23:43 +0100

    test for changing orientation or toggle the menu

--- test/modules/stackables/components/BottomSheet.test.js
+++ test/modules/stackables/components/BottomSheet.test.js
@@ -1,10 +1,11 @@
 import { BottomSheet } from '../../../../src/modules/stackables/components/BottomSheet';
-
 import { TestUtils } from '../../../test-utils';
-
 import { html } from 'lit-html';
-
 import { TEST_ID_ATTRIBUTE_NAME } from '../../../../src/utils/markup';
+import { setIsPortrait } from '../../../../src/store/media/media.action';
+import { toggle } from '../../../../src/store/mainMenu/mainMenu.action';
+import { createNoInitialStateMainMenuReducer } from '../../../../src/store/mainMenu/mainMenu.reducer';
+import { createNoInitialStateMediaReducer } from '../../../../src/store/media/media.reducer';
 
 window.customElements.define(BottomSheet.tag, BottomSheet);
 
@@ -22,9 +23,9 @@ describe('BottomSheet', () => {
 			...state
 		};
 
-		TestUtils.setupStoreAndDi(initialState);
+		TestUtils.setupStoreAndDi(initialState, { mainMenu: createNoInitialStateMainMenuReducer(), media: createNoInitialStateMediaReducer() });
 
-		const element = await TestUtils.render(BottomSheet.tag);
+		const element = await TestUtils.renderAndLogLifecycle(BottomSheet.tag);
 		element.content = content;
 		return element;
 	};
@@ -83,7 +84,7 @@ describe('BottomSheet', () => {
 		});
 
 		it('layouts for landscape and closed Menu', async () => {
-			const element = await setup('FooBar', { mainMenu: { open: false }, media: { portrait: true } });
+			const element = await setup('FooBar', { mainMenu: { open: false }, media: { portrait: false } });
 			const contentElement = element.shadowRoot.querySelector('.bottom-sheet');
 
 			expect(contentElement.innerText).toContain('FooBar');
@@ -98,6 +99,50 @@ describe('BottomSheet', () => {
 			expect(contentElement.innerText).toContain('FooBar');
 			expect(element.shadowRoot.querySelectorAll(`[${TEST_ID_ATTRIBUTE_NAME}]`)).toHaveSize(1);
 			expect(element.shadowRoot.querySelectorAll('.bottom-sheet.is-open')).toHaveSize(0);
+		});
+
+		it('layouts for portrait and closed Menu', async () => {
+			const element = await setup('FooBar', { mainMenu: { open: false }, media: { portrait: true } });
+			const contentElement = element.shadowRoot.querySelector('.bottom-sheet');
+
+			expect(contentElement.innerText).toContain('FooBar');
+			expect(element.shadowRoot.querySelectorAll(`[${TEST_ID_ATTRIBUTE_NAME}]`)).toHaveSize(1);
+			expect(element.shadowRoot.querySelectorAll('.bottom-sheet.is-open')).toHaveSize(0);
+		});
+
+	});
+
+
+	describe('after initial rendering ', () => {
+
+		it('when orientation changes', async () => {
+			const element = await setup('FooBar', { mainMenu: { open: true }, media: { portrait: false, observeResponsiveParameter: true } });
+
+			const contentElement = element.shadowRoot.querySelector('.bottom-sheet');
+
+			expect(contentElement.innerText).toContain('FooBar');
+			expect(element.shadowRoot.querySelectorAll('.bottom-sheet.is-open')).toHaveSize(1);
+
+			setIsPortrait(true);
+			expect(element.shadowRoot.querySelectorAll('.bottom-sheet.is-open')).toHaveSize(0);
+
+			setIsPortrait(false);
+			expect(element.shadowRoot.querySelectorAll('.bottom-sheet.is-open')).toHaveSize(1);
+		});
+
+		it('when toggle menu', async () => {
+			const element = await setup('FooBar', { mainMenu: { open: true }, media: { portrait: false } });
+
+			const contentElement = element.shadowRoot.querySelector('.bottom-sheet');
+
+			expect(contentElement.innerText).toContain('FooBar');
+			expect(element.shadowRoot.querySelectorAll('.bottom-sheet.is-open')).toHaveSize(1);
+
+			toggle();
+			expect(element.shadowRoot.querySelectorAll('.bottom-sheet.is-open')).toHaveSize(0);
+
+			toggle();
+			expect(element.shadowRoot.querySelectorAll('.bottom-sheet.is-open')).toHaveSize(1);
 		});
 	});
 
