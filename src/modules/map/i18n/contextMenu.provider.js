516f928263589df75f3fb2aa885ee4825e4e512c
Thomas Aulinger <taulinger@gmx.de>
2023-01-11 17:15:08 +0100

    rename altitude > elevation

--- src/modules/map/i18n/contextMenu.provider.js
+++ src/modules/map/i18n/contextMenu.provider.js
@@ -6,7 +6,7 @@ export const provide = (lang) => {
 				//the first part of the snake_case key should be the name of the related module
 				map_contextMenu_header: 'Location',
 				map_contextMenu_close_button: 'Close',
-				map_contextMenuContent_altitude_label: 'Alt.',
+				map_contextMenuContent_elevation_label: 'Elev.',
 				map_contextMenuContent_community_label: 'Community',
 				map_contextMenuContent_district_label: 'District',
 				map_contextMenuContent_copy_icon: 'Copy to clipboard',
@@ -19,7 +19,7 @@ export const provide = (lang) => {
 				//the first part of the snake_case key should be the name of the related module
 				map_contextMenu_header: 'Position',
 				map_contextMenu_close_button: 'Schließen',
-				map_contextMenuContent_altitude_label: 'Höhe',
+				map_contextMenuContent_elevation_label: 'Höhe',
 				map_contextMenuContent_community_label: 'Gemeinde',
 				map_contextMenuContent_district_label: 'Gemarkung',
 				map_contextMenuContent_copy_icon: 'In die Zwischenablage kopieren',
