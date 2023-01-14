141df17bc5af9a1a9586394108f8eded9aa57be9
thiloSchlemmer <thiloSchlemmer@gmail.com>
2023-01-14 13:26:07 +0100

    remove deprecated style methods

--- src/modules/olMap/handler/mfp/styleUtils.js
+++ src/modules/olMap/handler/mfp/styleUtils.js
@@ -2,30 +2,7 @@ import { Style, Stroke, Fill, Text as TextStyle } from 'ol/style';
 import { DEVICE_PIXEL_RATIO } from 'ol/has';
 import { toContext } from 'ol/render';
 
-
-
 const fontSizePX = 70;
-export const mfpTextStyleFunction = (label, index = 0, globalOffset = 1) => {
-
-	return new Style({
-		text: new TextStyle({
-			text: label,
-			font: `normal ${fontSizePX}px sans-serif`,
-			stroke: new Stroke({
-				color: [0, 0, 0, 0.5],
-				width: 2
-			}),
-			fill: new Fill({
-				color: [80, 80, 80, 0.3]
-			}),
-			scale: 1,
-			offsetY: fontSizePX * index - (globalOffset / 2) * fontSizePX,
-			overflow: false,
-			placement: 'point',
-			baseline: 'hanging'
-		})
-	});
-};
 
 export const createThumbnailStyleFunction = (warnLabel, beingDraggedCallback) => {
 	const getCanvasContextRenderFunction = (state) => {
@@ -87,10 +64,9 @@ export const createThumbnailStyleFunction = (warnLabel, beingDraggedCallback) =>
 	return [renderStyle];
 };
 
-
 export const nullStyleFunction = () => [new Style({})];
 
-export const createSimpleMapMaskFunction = (map, getPixelCoordinatesCallback) => {
+export const createMapMaskFunction = (map, getPixelCoordinatesCallback) => {
 
 	const getMask = (map, pixelCoordinates) => {
 		const size = map.getSize();
@@ -130,120 +106,6 @@ export const createSimpleMapMaskFunction = (map, getPixelCoordinatesCallback) =>
 		const pixelMask = getMask(map, pixelCoordinates);
 		const ctx = event.context;
 		drawMask(ctx, pixelMask);
-
-		ctx.restore();
-	};
-	return renderMask;
-};
-
-
-export const createMapMaskFunction = (map, feature, useCacheCallback) => {
-	let cachedPixelCoordinates = null;
-	const getMask = (map, pixelCoordinates) => {
-		const size = map.getSize();
-		const width = size[0] * DEVICE_PIXEL_RATIO;
-		const height = size[1] * DEVICE_PIXEL_RATIO;
-
-		const outerPixelPolygon = [
-			[0, 0],
-			[width, 0],
-			[width, height],
-			[0, height],
-			[0, 0]];
-
-		return [outerPixelPolygon, pixelCoordinates];
-	};
-
-	const drawMask = (ctx, mask) => {
-		const outer = mask[0];
-		const inner = mask[1];
-		ctx.beginPath();
-
-		// outside -> clockwise
-		ctx.moveTo(outer[0][0], outer[0][1]);
-		outer.slice(1).forEach(c => ctx.lineTo(c[0], c[1]));
-		ctx.closePath();
-
-		// inside -> counter-clockwise
-		ctx.moveTo(inner[0][0], inner[0][1]);
-		[...inner].reverse().slice(1).forEach(c => ctx.lineTo(c[0], c[1]));
-		ctx.closePath();
-
-		ctx.fillStyle = 'rgba(0,0,0,0.4)';
-		ctx.fill();
-	};
-
-	const getPageRectangle = (pageCoordinates) => {
-		const xValues = pageCoordinates.map(c => c[0]);
-		const yValues = pageCoordinates.map(c => c[1]);
-		const width = Math.max(...xValues) - Math.min(...xValues);
-		const height = Math.max(...yValues) - Math.min(...yValues);
-
-		return { x: Math.min(...xValues), y: Math.min(...yValues), width: width, height: height };
-	};
-
-	const getCenter = (rectangle) => {
-		return [rectangle.x + rectangle.width / 2, rectangle.y + rectangle.height / 2];
-	};
-
-	const drawPassepartout = (ctx, pageCoordinates) => {
-		const passepartoutWidthFactor = 0.02;
-		const pageRectangle = getPageRectangle(pageCoordinates);
-		const center = getCenter(pageRectangle);
-		const centerRelative = pageCoordinates.map(c => [c[0] - center[0], c[1] - center[1]]);
-
-		ctx.strokeStyle = 'rgba(255,255,255,0.4)';
-		ctx.beginPath();
-
-		ctx.translate(center[0], center[1]);
-		ctx.scale(1 + passepartoutWidthFactor, 1 + passepartoutWidthFactor);
-		ctx.lineWidth = pageRectangle.width * passepartoutWidthFactor;
-		ctx.moveTo(centerRelative[0][0], centerRelative[0][1]);
-		centerRelative.slice(1).forEach(c => ctx.lineTo(c[0], c[1]));
-		ctx.closePath();
-
-		ctx.stroke();
-		ctx.setTransform(1, 0, 0, 1, 0, 0);
-	};
-
-	const drawInnerContour = (ctx, pageCoordinates) => {
-
-		ctx.strokeStyle = 'rgba(44, 90, 146, 1)';
-		ctx.lineWidth = 3;
-		ctx.beginPath();
-		ctx.moveTo(pageCoordinates[0], pageCoordinates[1]);
-		pageCoordinates.slice(1).forEach(c => ctx.lineTo(c[0], c[1]));
-		ctx.closePath();
-
-		ctx.stroke();
-		ctx.setTransform(1, 0, 0, 1, 0, 0);
-	};
-
-	const toPixelCoordinates = (geometry) => {
-		return geometry.getCoordinates()[0].map(c => map.getPixelFromCoordinate(c));
-	};
-	const getOrCreatePixelCoordinates = (feature) => {
-		const createNew = () => {
-			const pixelCoordinates = toPixelCoordinates(feature.getGeometry());
-			cachedPixelCoordinates = pixelCoordinates;
-			return pixelCoordinates;
-		};
-
-		const getCached = () => {
-			return cachedPixelCoordinates ? cachedPixelCoordinates : createNew();
-		};
-		const useCache = useCacheCallback();
-		return useCache ? getCached() : createNew();
-	};
-
-	const renderMask = (event) => {
-		const pixelCoordinates = getOrCreatePixelCoordinates(feature);
-		const pixelMask = getMask(map, pixelCoordinates);
-		const ctx = event.context;
-
-		drawMask(ctx, pixelMask);
-		drawPassepartout(ctx, pixelMask[1]);
-		drawInnerContour(ctx, pixelMask[1]);
 
 		ctx.restore();
 	};
--- test/modules/olMap/handler/mfp/styleUtils.test.js
+++ test/modules/olMap/handler/mfp/styleUtils.test.js
@@ -4,8 +4,8 @@ import { Geometry, Point, Polygon } from 'ol/geom';
 import { get as getProjection } from 'ol/proj';
 import RenderEvent from 'ol/render/Event';
 
-import { Stroke, Style, Text as TextStyle } from 'ol/style';
-import { createMapMaskFunction, mfpTextStyleFunction, nullStyleFunction, createThumbnailStyleFunction, createSimpleMapMaskFunction } from '../../../../../src/modules/olMap/handler/mfp/styleUtils';
+import { Stroke, Style } from 'ol/style';
+import { nullStyleFunction, createThumbnailStyleFunction, createMapMaskFunction } from '../../../../../src/modules/olMap/handler/mfp/styleUtils';
 
 describe('mfp style utility functions', () => {
 
@@ -13,37 +13,6 @@ describe('mfp style utility functions', () => {
 		const canvas = document.createElement('canvas');
 		return canvas.getContext('2d');
 	};
-
-	describe('mfpTextStyleFunction', () => {
-
-		it('should create a text-style ', () => {
-			const label = 'foo';
-			const textStyle = mfpTextStyleFunction(label).getText();
-
-			expect(textStyle).toEqual(jasmine.any(TextStyle));
-			expect(textStyle.getText()).toBe(label);
-			expect(textStyle.getFont()).toBe('normal 70px sans-serif');
-			expect(textStyle.getFill()).toBeTruthy();
-			expect(textStyle.getStroke()).toBeTruthy();
-			expect(textStyle.getScale()).toBe(1);
-			expect(textStyle.getOverflow()).toBeFalse();
-			expect(textStyle.getPlacement()).toBe('point');
-		});
-
-		it('should create a text-style with offset ', () => {
-			expect(mfpTextStyleFunction('foo', 0, 1).getText().getOffsetY()).toBe(-35);
-			expect(mfpTextStyleFunction('foo', 0).getText().getOffsetY()).toBe(-35);
-			expect(mfpTextStyleFunction('foo').getText().getOffsetY()).toBe(-35);
-
-			expect(mfpTextStyleFunction('foo', 0, 2).getText().getOffsetY()).toBe(-70);
-			expect(mfpTextStyleFunction('foo', 1, 2).getText().getOffsetY()).toBe(0);
-
-
-			expect(mfpTextStyleFunction('foo', 0, 3).getText().getOffsetY()).toBe(-105);
-			expect(mfpTextStyleFunction('foo', 1, 3).getText().getOffsetY()).toBe(-35);
-			expect(mfpTextStyleFunction('foo', 2, 3).getText().getOffsetY()).toBe(35);
-		});
-	});
 
 	describe('createThumbnailStyleFunction', () => {
 		const beingDraggedCallback = () => false;
@@ -163,83 +132,6 @@ describe('mfp style utility functions', () => {
 
 	describe('createMapMaskFunction', () => {
 
-		const createFeature = (name) => {
-			const geometry = new Polygon([[[5, 5], [6, 5], [6, 6], [5, 6], [5, 5]]]);
-			const feature = new Feature({ geometry: geometry });
-
-			feature.set('name', name);
-			feature.set('page_pixel_coordinates', [[5, 5], [6, 5], [6, 6], [5, 6], [5, 5]]);
-
-			return feature;
-		};
-
-		const createMapMock = () => {
-			return {
-				getSize: () => [10, 10], getCoordinateFromPixel: (p) => p, getPixelFromCoordinate: (c) => c
-			};
-		};
-
-		it('creates a function', () => {
-			const mapMock = {};
-			const feature = new Feature();
-			const renderFunction = createMapMaskFunction(mapMock, feature);
-
-			expect(renderFunction).toEqual(jasmine.any(Function));
-		});
-
-		const transform = [1, 0, 0, 1, 0, 0];
-		const projection = getProjection('EPSG:3857');
-		const viewState = {
-			projection: projection, resolution: 1, rotation: 0
-		};
-		const setupFrameState = (time) => {
-			return {
-				time: +time, coordinateToPixelTransform: transform, viewHints: [], viewState: viewState, extent: [0, 0, 10, 10]
-			};
-		};
-		const getPostRenderEvent = (time, context) => new RenderEvent('postrender', transform, setupFrameState(time), context);
-
-		it('draws a mask', () => {
-			const expectedFillColor = 'rgba(0,0,0,0.4)';
-			const feature = createFeature('');
-			const mapMock = createMapMock();
-			const context = get2dContext();
-
-			const fillStylePropertySpy = spyOnProperty(context, 'fillStyle', 'set').and.callThrough();
-			const moveToSpy = spyOn(context, 'moveTo').and.callThrough();
-
-			const renderFunction = createMapMaskFunction(mapMock, feature, () => false);
-			renderFunction(getPostRenderEvent(0, context));
-
-			expect(renderFunction).toEqual(jasmine.any(Function));
-			expect(fillStylePropertySpy).toHaveBeenCalledWith(expectedFillColor);
-
-			// for outer drawn polygon
-			expect(moveToSpy).toHaveBeenCalledWith(0, 0);
-			// for inner drawn polygon
-			expect(moveToSpy).toHaveBeenCalledWith(5, 5);
-		});
-
-		it('uses chached pixelCoordinates for the mask', () => {
-			const feature = createFeature('');
-			const mapMock = createMapMock();
-			const context = get2dContext();
-			const expectedRequests = 5; // for each coordinate in a boundary-polygon
-
-			const renderFunction = createMapMaskFunction(mapMock, feature, () => true);
-			const mapSpy = spyOn(mapMock, 'getPixelFromCoordinate').and.callThrough();
-
-			renderFunction(getPostRenderEvent(0, context)); // --> pixelCoordinates now cached
-
-			expect(mapSpy).toHaveBeenCalledTimes(expectedRequests);
-
-			renderFunction(getPostRenderEvent(0, context)); // should not request pixelCoordinates anymore
-			expect(mapSpy).toHaveBeenCalledTimes(expectedRequests);
-		});
-	});
-
-	describe('createSimpleMapMaskFunction', () => {
-
 		const pixelCoordinatesCallBack = () => [[5, 5], [6, 5], [6, 6], [5, 6], [5, 5]];
 
 		const createMapMock = () => {
@@ -250,7 +142,7 @@ describe('mfp style utility functions', () => {
 
 		it('creates a function', () => {
 			const mapMock = {};
-			const renderFunction = createSimpleMapMaskFunction(mapMock, pixelCoordinatesCallBack);
+			const renderFunction = createMapMaskFunction(mapMock, pixelCoordinatesCallBack);
 
 			expect(renderFunction).toEqual(jasmine.any(Function));
 		});
@@ -275,7 +167,7 @@ describe('mfp style utility functions', () => {
 			const fillStylePropertySpy = spyOnProperty(context, 'fillStyle', 'set').and.callThrough();
 			const moveToSpy = spyOn(context, 'moveTo').and.callThrough();
 
-			const renderFunction = createSimpleMapMaskFunction(mapMock, pixelCoordinatesCallBack);
+			const renderFunction = createMapMaskFunction(mapMock, pixelCoordinatesCallBack);
 			renderFunction(getPostRenderEvent(0, context));
 
 			expect(renderFunction).toEqual(jasmine.any(Function));
