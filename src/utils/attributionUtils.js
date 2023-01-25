1ee622dd18c780c5b2e25c87cda312865c903f2e
Thomas Aulinger <taulinger@gmx.de>
2023-01-25 16:42:44 +0100

    add attribution utils

---
+++ src/utils/attributionUtils.js
@@ -0,0 +1,27 @@
+/**
+ * Returns a unique array of Copyright object from an array of GeoResources.
+ * @param {GeoResource} geoResources
+ * @param {number} [zoomLevel=0] zoomLevel
+ * @returns unique array of Copyright objects
+ */
+export const getUniqueCopyrights = (geoResources = [], zoomLevel) => {
+
+	const availableCopyrights = geoResources
+		.map(g => g.getAttribution(zoomLevel))
+	//remove null/undefined
+		.filter(attr => !!attr)
+		.flat()
+		.reverse()
+		.map(attr => Array.isArray(attr.copyright) ? attr.copyright : [attr?.copyright]) // copyright property may be an array or null
+		.flat()
+	//remove null/undefined
+		.filter(copyr => !!copyr);
+
+	//make array unique by label
+	const uniqueCopyrights = availableCopyrights
+		.filter((copyr, index) => {
+			return availableCopyrights.findIndex(item => item.label === copyr.label) === index;
+		});
+
+	return uniqueCopyrights;
+};
---
+++ test/utils/attributionUtils.test.js
@@ -0,0 +1,39 @@
+import { XyzGeoResource } from '../../src/domain/geoResources';
+import { getMinimalAttribution } from '../../src/services/provider/attribution.provider';
+import { getUniqueCopyrights } from '../../src/utils/attributionUtils';
+
+describe('attributionUtils', () => {
+
+	describe('getUniqueCopyrightList', () => {
+
+		it('should return an unique array of copyright objects', () => {
+			const geoResources = [
+				new XyzGeoResource('geoResourceId0', '', '').setAttributionProvider((geoResourceId, zoomLevel) => getMinimalAttribution(`foo_${zoomLevel}`)),
+				//array of copyright
+				new XyzGeoResource('geoResourceId1', '', '').setAttributionProvider((geoResourceId, zoomLevel) => ({
+					copyright: [
+						{ label: `foo_${zoomLevel}` },
+						{ label: `bar_${zoomLevel}` }
+					]
+				})),
+				// array of attribution
+				new XyzGeoResource('geoResourceId2', '', '').setAttributionProvider((geoResourceId, zoomLevel) => [getMinimalAttribution(`foo_${zoomLevel}`), getMinimalAttribution(`foo_${zoomLevel}`)]),
+				// attribution is null
+				new XyzGeoResource('geoResourceId3', '', '').setAttributionProvider(() => null),
+				// copyright is null
+				new XyzGeoResource('geoResourceId4', '', '').setAttributionProvider(() => ({ copyright: null })),
+				new XyzGeoResource('geoResourceId5', '', '').setAttributionProvider((geoResourceId, zoomLevel) => getMinimalAttribution(`bar_${zoomLevel}`))
+			];
+
+			const copyrights = getUniqueCopyrights(geoResources, 5);
+
+			expect(copyrights).toHaveSize(2);
+			expect(copyrights[0].label).toBe('bar_5');
+			expect(copyrights[1].label).toBe('foo_5');
+		});
+
+		it('should return an empy array when no GeoResouces are provided', () => {
+			expect(getUniqueCopyrights()).toHaveSize(0);
+		});
+	});
+});
