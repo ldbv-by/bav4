985ce5b1d7168aa6c1e411200a2cff76bc0df7c3
thiloSchlemmer <thiloSchlemmer@gmail.com>
2023-02-07 07:54:02 +0100

    register ElevationProfileChip

--- src/modules/chips/index.js
+++ src/modules/chips/index.js
@@ -1,5 +1,9 @@
 import './i18n';
 import { ChipsContainer } from './components/chipsContainer/ChipsContainer';
+import { ElevationProfileChip } from './components/assistChips/ElevationProfileChip';
 if (!window.customElements.get(ChipsContainer.tag)) {
 	window.customElements.define(ChipsContainer.tag, ChipsContainer);
+}
+if (!window.customElements.get(ElevationProfileChip.tag)) {
+	window.customElements.define(ElevationProfileChip.tag, ElevationProfileChip);
 }
