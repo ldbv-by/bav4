5f7a7309541cdfe6d3052bf7b61bd520b013416e
thiloSchlemmer <thiloSchlemmer@gmail.com>
2023-02-20 11:33:16 +0100

    register component

---
+++ src/modules/iframe/components/iframeGenerator/index.js
@@ -0,0 +1,6 @@
+
+import { IFrameGenerator } from './IFrameGenerator';
+
+if (!window.customElements.get(IFrameGenerator.tag)) {
+	window.customElements.define(IFrameGenerator.tag, IFrameGenerator);
+}
--- src/modules/iframe/index.js
+++ src/modules/iframe/index.js
@@ -1,5 +1,6 @@
 import './i18n';
 import { NonEmbeddedHint } from './components/NonEmbeddedHint';
+import './components/iframeGenerator';
 if (!window.customElements.get(NonEmbeddedHint.tag)) {
 	window.customElements.define(NonEmbeddedHint.tag, NonEmbeddedHint);
 }
