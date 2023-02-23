47fe1cfff398bd7ac7ee28194abdd51bcbd63e40
Thomas Aulinger <taulinger@gmx.de>
2023-02-23 11:41:59 +0100

    adjust test

--- test/modules/utils/components/devInfo/DevInfo.test.js
+++ test/modules/utils/components/devInfo/DevInfo.test.js
@@ -55,7 +55,7 @@ describe('DevInfo', () => {
 
 			expect(store.getState().modal.data.title).toBe('Showcase');
 			//we expect a lit-html TemplateResult as content
-			expect(store.getState().modal.data.content.strings[0]).toBe('<ba-showcase>');
+			expect(store.getState().modal.data.content.strings[0]).toBe('<ba-showcase></ba-showcase>');
 		});
 	});
 });
