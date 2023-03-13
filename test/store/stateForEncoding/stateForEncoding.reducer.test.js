143c6b09b87356d16d1dc62c82ee8d73bab0aacf
Thomas Aulinger <taulinger@gmx.de>
2023-03-13 13:21:19 +0100

    add new slice-of-state 'stateForEncoding'

---
+++ test/store/stateForEncoding/stateForEncoding.reducer.test.js
@@ -0,0 +1,28 @@
+import { indicateChange } from '../../../src/store/stateForEncoding/stateForEncoding.action';
+import { stateForEncodingReducer } from '../../../src/store/stateForEncoding/stateForEncoding.reducer';
+import { EventLike } from '../../../src/utils/storeUtils';
+import { TestUtils } from '../../test-utils';
+
+describe('stateForEncodingReducer', () => {
+	const setup = (state) => {
+		return TestUtils.setupStoreAndDi(state, {
+			stateForEncoding: stateForEncodingReducer
+		});
+	};
+
+	it('initiales the store with default values', () => {
+		const store = setup();
+		expect(store.getState().stateForEncoding.changed.payload).toBeNull;
+		expect(store.getState().stateForEncoding.changed).toBeInstanceOf(EventLike);
+	});
+
+	it('updates the `changed` property', () => {
+		const store = setup();
+		const initialPayload = store.getState().stateForEncoding.changed;
+
+		indicateChange();
+
+		expect(store.getState().stateForEncoding.changed).not.toEqual(initialPayload);
+		expect(store.getState().stateForEncoding.changed).toBeInstanceOf(EventLike);
+	});
+});
