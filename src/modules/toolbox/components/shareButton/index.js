f715aa876674a5dc902a663ae7b458f9f8612480
Schlemmer, Thilo (LDBV) <thilo.schlemmer@ldbv.bayern.de>
2023-03-15 12:15:06 +0100

    fix index.js

--- src/modules/toolbox/components/shareButton/index.js
+++ src/modules/toolbox/components/shareButton/index.js
@@ -1,10 +1,5 @@
-import { ShareDialogContent } from '../../../share/components/ShareDialogContent';
 import { ShareButton } from './ShareButton';
 
 if (!window.customElements.get(ShareButton.tag)) {
 	window.customElements.define(ShareButton.tag, ShareButton);
-}
-
-if (!window.customElements.get(ShareDialogContent.tag)) {
-	window.customElements.define(ShareDialogContent.tag, ShareDialogContent);
 }
