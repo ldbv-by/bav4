3b80d6146843accef26240c8c96672ba44195770
alsturm <alexander_sturm@gmx.net>
2023-03-16 10:40:51 +0100

    update test

--- test/modules/iframe/i18n/iframe.provider.test.js
+++ test/modules/iframe/i18n/iframe.provider.test.js
@@ -32,7 +32,7 @@ describe('i18n for iframe module', () => {
 	});
 
 	it('have the expected amount of translations', () => {
-		const expectedSize = 9;
+		const expectedSize = 10;
 		const deMap = provide('de');
 		const enMap = provide('en');
 
