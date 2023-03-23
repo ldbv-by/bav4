6bcd523dc6d9c5f0af352bda07ebd8cb6b881839
Thomas Aulinger <taulinger@gmx.de>
2023-03-23 17:19:47 +0100

    rename subfolder

--- src/modules/iframe/index.js
+++ src/modules/iframe/index.js
@@ -1,8 +1,11 @@
 import './i18n';
-import './components/iframeGenerator';
+import { IframeGenerator } from './components/generator/IframeGenerator';
 import { NonEmbeddedHint } from './components/NonEmbeddedHint';
 import { ActivateMapButton } from './components/activateMapButton/ActivateMapButton';
 import { ViewLargerMapChip } from './components/viewLargerMapChip/ViewLargerMapChip';
+if (!window.customElements.get(IframeGenerator.tag)) {
+	window.customElements.define(IframeGenerator.tag, IframeGenerator);
+}
 if (!window.customElements.get(NonEmbeddedHint.tag)) {
 	window.customElements.define(NonEmbeddedHint.tag, NonEmbeddedHint);
 }
