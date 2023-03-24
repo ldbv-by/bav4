f175f047a84aad1e01c0039ef1a86fb9a4a1da31
Thomas Aulinger <taulinger@gmx.de>
2023-03-24 16:19:53 +0100

    add IframeContainerPlugin

---
+++ test/plugins/IframeContainerPlugin.test.js
@@ -0,0 +1,81 @@
+import { TestUtils } from '../test-utils.js';
+import { featureInfoReducer } from '../../src/store/featureInfo/featureInfo.reducer';
+import { abortOrReset, registerQuery, resolveQuery } from '../../src/store/featureInfo/featureInfo.action.js';
+import { iframeContainerReducer, initialState as iframeContainerInitialState } from '../../src/store/iframeContainer/iframeContainer.reducer.js';
+import { IframeContainerPlugin } from '../../src/plugins/IframeContainerPlugin.js';
+
+describe('IframeContainerPlugin', () => {
+	const setup = (state) => {
+		const initialState = {
+			iframeContainer: iframeContainerInitialState,
+			...state
+		};
+
+		const store = TestUtils.setupStoreAndDi(initialState, {
+			iframeContainer: iframeContainerReducer,
+			featureInfo: featureInfoReducer
+		});
+		return store;
+	};
+
+	describe('when featureInfo.querying property changes', () => {
+		it('does nothing when query is running', async () => {
+			const queryId = 'foo';
+			const store = setup({
+				featureInfo: {
+					queries: [],
+					querying: false,
+					current: [{ title: 'title', content: 'content' }]
+				}
+			});
+			const instanceUnderTest = new IframeContainerPlugin();
+			await instanceUnderTest.register(store);
+
+			registerQuery(queryId);
+
+			expect(store.getState().featureInfo.current).toHaveSize(1);
+			expect(store.getState().iframeContainer.active).toBeFalse();
+		});
+
+		describe('and we have FeatureInfo items', () => {
+			it('opens the FeatureInfo panel', async () => {
+				const queryId = 'foo';
+				const store = setup({
+					featureInfo: {
+						queries: [queryId],
+						querying: true,
+						current: [{ title: 'title', content: 'content' }]
+					}
+				});
+				const instanceUnderTest = new IframeContainerPlugin();
+				await instanceUnderTest.register(store);
+
+				resolveQuery(queryId);
+
+				expect(store.getState().featureInfo.current).toHaveSize(1);
+				expect(store.getState().iframeContainer.active).toBeTrue();
+			});
+
+			describe('and we have NO FeatureInfo items', () => {
+				describe('and MainMenu is initially closed', () => {
+					it('restores the previous panel and closes the menu', async () => {
+						const queryId = 'foo';
+						const store = setup({
+							featureInfo: {
+								queries: [queryId],
+								querying: true,
+								current: [{ title: 'title', content: 'content' }]
+							}
+						});
+						const instanceUnderTest = new IframeContainerPlugin();
+						await instanceUnderTest.register(store);
+
+						abortOrReset();
+
+						expect(store.getState().iframeContainer.active).toBeFalse();
+					});
+				});
+			});
+		});
+	});
+});
