9f7d87d1b58d4929d45c7e65b6c6dae7c28e1946
Norbert Klein <nklein@nkode.de>
2023-03-24 09:25:33 +0100

    feedback panel with some logging

--- src/modules/feedback/components/panel/FeedbackPanel.js
+++ src/modules/feedback/components/panel/FeedbackPanel.js
@@ -2,13 +2,14 @@ import { html } from 'lit-html';
 import { MvuElement } from '../../../MvuElement';
 import css from './feedbackPanel.css';
 
-const Update_Feedback = 'update_feedback';
+const Update_EMail = 'update_email';
+const Update_Topic = 'update_topic';
 
 export class FeedbackPanel extends MvuElement {
 	constructor() {
 		super({
 			age: 21,
-			oneText: 'one text',
+			topic: 'one text',
 			message: 'another text',
 			aSelection: ['one', 'two', 'three'],
 			selected: 'two',
@@ -36,20 +37,39 @@ export class FeedbackPanel extends MvuElement {
 
 	update(type, data, model) {
 		switch (type) {
-			case Update_Feedback:
-				return { ...model, feedback: data };
+			case Update_Topic:
+				return { ...model, topic: data };
+			case Update_EMail:
+				return { ...model, email: data };
 		}
 	}
 
 	createView(model) {
-		const { email, oneText, message, age } = model;
+		const { email, topic, message, age } = model;
 
-		const handleOneTextChange = () => {};
+		const handleTopicChange = (event) => {
+			const { value } = event.target;
+			this.signal(Update_Topic, value);
+		};
+
+		const handleEmailChange = (event) => {
+			const { value } = event.target;
+			this.signal(Update_EMail, value);
+		};
 
 		const _handleInputChange = (event) => {
 			const { name, value } = event.target;
-			this[name] = value;
+			console.log('🚀 ~ todo: handle ', name, value);
 		};
+
+		const handleSubmit = (event) => {
+			event.preventDefault();
+			const formdata = new FormData(event.target);
+			const data = Object.fromEntries(formdata.entries());
+			console.log('🚀 ~ FeedbackPanel ~ handleSubmit ~ data:', data);
+			// this.dispatchEvent(new CustomEvent('my-form-submit', { detail: data }));
+		};
+
 		return html`
 			<style>
 				${css}
@@ -57,21 +77,32 @@ export class FeedbackPanel extends MvuElement {
 
 			<h2 id="feedbackPanel">Feedback</h2>
 
-			<form>
-				<label for="name">Name:</label>
-				<input type="text" id="name" name="name" .value="${oneText}" @input="${handleOneTextChange}" required />
+			<div class="feedback-form-container">
+				<div class="feedback-form-left">
+					<form @submit="${handleSubmit}">
+						<label for="topic">Topic:</label>
+						<input type="text" id="topic" name="topic" .value="${topic}" @input="${handleTopicChange}" required />
 
-				<label for="email">Email:</label>
-				<input type="email" id="email" name="email" .value="${email}" @input="${_handleInputChange}" required />
+						<label for="email">Email:</label>
+						<input type="email" id="email" name="email" .value="${email}" @input="${handleEmailChange}" required />
 
-				<label for="message">Message:</label>
-				<textarea id="message" name="message" .value="${message}" @input="${_handleInputChange}" required></textarea>
+						<label for="message">Message:</label>
+						<textarea id="message" name="message" .value="${message}" @input="${_handleInputChange}" required></textarea>
 
-				<label for="age">Age:</label>
-				<input type="number" id="age" name="age" .value="${age}" @input="${_handleInputChange}" required />
+						<label for="age">Age:</label>
+						<input type="number" id="age" name="age" .value="${age}" @input="${_handleInputChange}" required />
 
-				<button type="submit">Submit</button>
-			</form>
+						<button type="submit">Submit</button>
+					</form>
+				</div>
+				<div class="feedback-form-right">
+					<p>Topic: ${topic}</p>
+					<p>Email: ${email}</p>
+					<p></p>
+					<p></p>
+					<p></p>
+				</div>
+			</div>
 		`;
 	}
 
