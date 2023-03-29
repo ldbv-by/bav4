237f7bc8232c219d85384ae52ef7cc5ea2bbe5d7
Thomas Aulinger <taulinger@gmx.de>
2023-03-29 18:45:03 +0200

    adjust after merge

--- test/injection/config.test.js
+++ test/injection/config.test.js
@@ -6,7 +6,7 @@ import { Injector } from '../../src/injection/core/injector.js';
 describe('injector configuration', () => {
 	it('registers the expected dependencies', () => {
 		expect($injector.isReady()).toBeTrue();
-		expect($injector.count()).toBe(62);
+		expect($injector.count()).toBe(63);
 
 		expect($injector.getScope('ProjectionService')).toBe(Injector.SCOPE_SINGLETON);
 		expect($injector.getScope('HttpService')).toBe(Injector.SCOPE_PERLOOKUP);
