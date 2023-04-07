b21e5bbd700a28133cf0b726c048e42080865e7d
Thomas Aulinger <taulinger@gmx.de>
2023-04-07 22:33:09 +0200

    also check for correct module name

--- checkOrApplyDoc.js
+++ checkOrApplyDoc.js
@@ -25,16 +25,17 @@ const findInDir = (dir, fileList = []) => {
 
 findInDir('./src').forEach((fp) => {
 	const moduleNameIdentifier = '/**\n * @module';
+	// path from src (excluded) + filename (without extension)
+	const moduleName = `${path.dirname(fp).split('src/')[1]}/${path.basename(fp).split('.js')[0]}`;
 	if (process.env.DOC_CHECK /** just check */) {
 		const content = fs.readFileSync(fp, 'utf8');
 		// check if we already have a @module name
 		if (!content.startsWith(moduleNameIdentifier)) {
 			throw new Error(`${fp} does not contain a JSDoc module name. Please run "npm run doc:apply"`);
+		} else if (!content.startsWith(`${moduleNameIdentifier} ${moduleName}`)) {
+			throw new Error(`${fp} does not contain the correct JSDoc module name. Please run "npm run doc:apply"`);
 		}
 	} else {
-		// path from src (excluded) + filename (without extension)
-		const moduleName = `${path.dirname(fp).split('src/')[1]}/${path.basename(fp).split('.js')[0]}`;
-
 		const content = fs.readFileSync(fp, 'utf8');
 		// check if we already have a @module name
 		if (content.startsWith(moduleNameIdentifier)) {
