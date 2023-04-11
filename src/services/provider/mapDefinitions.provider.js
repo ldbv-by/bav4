d06e6d31cf079511d6d443981ef16a84b0f8bcfc
Thomas Aulinger <taulinger@gmx.de>
2023-04-11 16:03:59 +0200

    add localProjectedSridDefinitionsForView

--- src/services/provider/mapDefinitions.provider.js
+++ src/services/provider/mapDefinitions.provider.js
@@ -7,38 +7,35 @@ import { $injector } from '../../injection';
  * Map related meta data
  * @typedef {Object} MapDefinitions
  * @property {Extent} defaultExtent default extent of the map
- * @property {Extent} localProjectedSridExtent the extent of the local projected system
- * @property {number} srid the internal SRID of the map
- * @property {number} defaultSridForView default SRID in which coordinates should be displayed within the UI
- * @property {function(Coordinate):(Array<SridDefinition>)} sridDefinitionsForView function which can take a coordinate and returns an array of SridDefinition
- * @property {number} localProjectedSrid the SRID of the local projected system.
  * @property {number} minZoomLevel the minimal zoom level the map should support
  * @property {number} maxZoomLevel the maximal zoom level the map should support
- */
-
-/**
- * Meta data for a srid
- * @typedef {Object} SridDefinition
- * @property {string} label label
- * @property {number} code srid
- * @property {number} digits decimal places for rounding
+ * @property {number} srid the internal SRID of the map
+ * @property {number} localProjectedSrid the SRID of the supported local projected system
+ * @property {Extent} localProjectedSridExtent the extent of the local supported projected system
+ * @property {function(Coordinate):(Array<SridDefinition>)} localProjectedSridDefinitionsForView function which can take a coordinate and returns an array of SridDefinitions
+ * @property {Array<SridDefinition>} globalSridDefinitionsForView array of global SridDefinitions
+ * @property {number} defaultSridForView default SRID in which coordinates should be displayed within the UI
  */
 
 /**
  * Provider for map releated meta data
  * @function
- * @returns {MapDefinitions} meta data
+ * @returns {MapDefinitions} BVV specific MapDefinitions
  */
 export const getBvvMapDefinitions = () => {
 	return {
 		defaultExtent: [995772.9694449581, 5982715.763684852, 1548341.2904285304, 6544564.28740462],
-		localProjectedSridExtent: [5, -80, 14, 80],
-		srid: 3857,
-		defaultSridForView: 25832,
-		sridDefinitionsForView: getBvvSridDefinitionsForView,
-		localProjectedSrid: 25832,
 		minZoomLevel: 0,
-		maxZoomLevel: 20
+		maxZoomLevel: 20,
+		srid: 3857,
+		localProjectedSrid: 25832,
+		localProjectedSridExtent: [5, -80, 14, 80],
+		localProjectedSridDefinitionsForView: getBvvSridDefinitionsForView,
+		globalSridDefinitionsForView: [
+			{ label: 'UTM', code: null /**global UTM */, digits: 0 },
+			{ label: 'WGS84', code: 4326, digits: 5 }
+		],
+		defaultSridForView: 25832
 	};
 };
 
--- test/service/provider/mapDefinitions.provider.test.js
+++ test/service/provider/mapDefinitions.provider.test.js
@@ -17,62 +17,67 @@ describe('MapDefinitions provider', () => {
 				localProjectedSridExtent,
 				srid,
 				defaultSridForView,
-				sridDefinitionsForView,
+				localProjectedSridDefinitionsForView,
 				localProjectedSrid,
 				minZoomLevel,
-				maxZoomLevel
+				maxZoomLevel,
+				globalSridDefinitionsForView
 			} = getBvvMapDefinitions();
 
 			expect(defaultExtent).toEqual([995772.9694449581, 5982715.763684852, 1548341.2904285304, 6544564.28740462]);
 			expect(localProjectedSridExtent).toEqual([5, -80, 14, 80]);
 			expect(srid).toBe(3857);
 			expect(defaultSridForView).toBe(25832);
-			expect(sridDefinitionsForView()).toEqual([
+			expect(localProjectedSridDefinitionsForView()).toEqual([
 				{ label: 'UTM', code: 25832, digits: 0 },
 				{ label: 'WGS84', code: 4326, digits: 5 }
 			]);
 			expect(localProjectedSrid).toEqual(25832);
 			expect(minZoomLevel).toBe(0);
 			expect(maxZoomLevel).toBe(20);
+			expect(globalSridDefinitionsForView).toEqual([
+				{ label: 'UTM', code: null, digits: 0 },
+				{ label: 'WGS84', code: 4326, digits: 5 }
+			]);
 		});
 
-		it('sridDefinitionsForView provides coordinate dependent definitions for a 25832 coordinate', () => {
+		it('localProjectedSridDefinitionsForView provides coordinate dependent definitions for a 25832 coordinate', () => {
 			const fakeCoord3857In32 = [42, 42];
 			spyOn(coordinateService, 'toLonLat').and.returnValue([11.18526, 48.64087]);
-			const { sridDefinitionsForView } = getBvvMapDefinitions();
+			const { localProjectedSridDefinitionsForView } = getBvvMapDefinitions();
 
-			expect(sridDefinitionsForView(fakeCoord3857In32)).toEqual([
+			expect(localProjectedSridDefinitionsForView(fakeCoord3857In32)).toEqual([
 				{ label: 'UTM', code: 25832, digits: 0 },
 				{ label: 'WGS84', code: 4326, digits: 5 }
 			]);
 		});
 
-		it('sridDefinitionsForView provides coordinate dependent definitions for a 25833 coordinate', () => {
+		it('localProjectedSridDefinitionsForView provides coordinate dependent definitions for a 25833 coordinate', () => {
 			const fakeCoord3857In33 = [42, 42];
 			spyOn(coordinateService, 'toLonLat').and.returnValue([12.18526, 48.64087]);
-			const { sridDefinitionsForView } = getBvvMapDefinitions();
+			const { localProjectedSridDefinitionsForView } = getBvvMapDefinitions();
 
-			expect(sridDefinitionsForView(fakeCoord3857In33)).toEqual([
+			expect(localProjectedSridDefinitionsForView(fakeCoord3857In33)).toEqual([
 				{ label: 'UTM', code: 25833, digits: 0 },
 				{ label: 'UTM', code: 25832, digits: 0 },
 				{ label: 'WGS84', code: 4326, digits: 5 }
 			]);
 		});
 
-		it('sridDefinitionsForView provides coordinate dependent definitions for a coordinate with a longitude value < 6°', () => {
+		it('localProjectedSridDefinitionsForView provides coordinate dependent definitions for a coordinate with a longitude value < 6°', () => {
 			const fakeCoord3857In33 = [42, 42];
 			spyOn(coordinateService, 'toLonLat').and.returnValue([5.18526, 48.64087]);
-			const { sridDefinitionsForView } = getBvvMapDefinitions();
+			const { localProjectedSridDefinitionsForView } = getBvvMapDefinitions();
 
-			expect(sridDefinitionsForView(fakeCoord3857In33)).toEqual([{ label: 'WGS84', code: 4326, digits: 5 }]);
+			expect(localProjectedSridDefinitionsForView(fakeCoord3857In33)).toEqual([{ label: 'WGS84', code: 4326, digits: 5 }]);
 		});
 
-		it('sridDefinitionsForView provides coordinate dependent definitions for a coordinate with a longitude value > 18°', () => {
+		it('localProjectedSridDefinitionsForView provides coordinate dependent definitions for a coordinate with a longitude value > 18°', () => {
 			const fakeCoord3857In33 = [42, 42];
 			spyOn(coordinateService, 'toLonLat').and.returnValue([18.18526, 48.64087]);
-			const { sridDefinitionsForView } = getBvvMapDefinitions();
+			const { localProjectedSridDefinitionsForView } = getBvvMapDefinitions();
 
-			expect(sridDefinitionsForView(fakeCoord3857In33)).toEqual([{ label: 'WGS84', code: 4326, digits: 5 }]);
+			expect(localProjectedSridDefinitionsForView(fakeCoord3857In33)).toEqual([{ label: 'WGS84', code: 4326, digits: 5 }]);
 		});
 	});
 });
