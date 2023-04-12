44f1591c98fc15cf8a5542f87c273cabcb312d02
Thomas Aulinger <taulinger@gmx.de>
2023-04-12 21:50:49 +0200

    add `type` property

--- src/domain/coordinateRepresentation.js
+++ src/domain/coordinateRepresentation.js
@@ -1,10 +1,11 @@
 /**
- * Defines how a coordinate should be represented within the UI.
+ * Defines the (target) representation for a coordinate.
  * @typedef {Object} CoordinateRepresentation
  * @property {string} label label
- * @property {number} [code] the SRID or `null`
+ * @property {number} code the SRID or `null`
  * @property {number} digits decimal places for rounding
  * @property {boolean} global suitable for global usage
+ * @property {string} type type of this CoordinateRepresentation. CoordinateRepresentation of the same type are interchangeable (e.g. global and local CoordinateRepresentation)
  */
 
 /**
@@ -12,7 +13,7 @@
  * @enum
  */
 export const CoordinateRepresentations = Object.freeze({
-	WGS84: { label: 'WGS84', code: 4326, digits: 5, global: true },
-	UTM: { label: 'UTM', code: null, digits: 0, global: true },
-	MGRS: { label: 'MGRS', code: null, digits: 0, global: true }
+	WGS84: { label: 'WGS84', code: 4326, digits: 5, global: true, type: 'wgs84' },
+	UTM: { label: 'UTM', code: null, digits: 0, global: true, type: 'utm' },
+	MGRS: { label: 'MGRS', code: null, digits: 0, global: true, type: 'mgrs' }
 });
