38168452cb51f236e29bbea763ded46c350c61ce
Thomas Aulinger <taulinger@gmx.de>
2023-04-13 18:18:48 +0200

    rework component

--- test/modules/footer/components/coordinateSelect/CoordinateSelect.test.js
+++ test/modules/footer/components/coordinateSelect/CoordinateSelect.test.js
@@ -3,35 +3,22 @@ import { $injector } from '../../../../../src/injection';
 import { TestUtils } from '../../../../test-utils.js';
 import { setPointerMove } from '../../../../../src/store/pointer/pointer.action';
 import { pointerReducer } from '../../../../../src/store/pointer/pointer.reducer';
+import { CoordinateRepresentations } from '../../../../../src/domain/coordinateRepresentation';
 
 window.customElements.define(CoordinateSelect.tag, CoordinateSelect);
 
 describe('CoordinateSelect', () => {
 	const coordinateServiceMock = {
-		stringify() {},
-		toLonLat() {},
-		transform() {}
+		stringify() {}
 	};
 
 	const mapServiceMock = {
-		getCoordinateRepresentations: () => {
-			return [
-				{ label: 'TEST', code: 99999 },
-				{ label: 'WGS84', code: 1111 }
-			];
-		},
-		getSrid: () => {
-			return 3857;
-		}
+		getCoordinateRepresentations: () => {}
 	};
 
-	const setup = (config) => {
-		const { touch = false } = config;
-
+	const setup = (config = { touch: false }) => {
 		const state = {
-			pointer: {
-				pointer: null
-			}
+			pointer: pointerReducer.initialState
 		};
 
 		TestUtils.setupStoreAndDi(state, { pointer: pointerReducer });
@@ -39,94 +26,90 @@ describe('CoordinateSelect', () => {
 		$injector.registerSingleton('TranslationService', { translate: (key) => key });
 		$injector.registerSingleton('CoordinateService', coordinateServiceMock);
 		$injector.registerSingleton('MapService', mapServiceMock);
-		$injector.registerSingleton('EnvironmentService', { isTouch: () => touch });
+		$injector.registerSingleton('EnvironmentService', { isTouch: () => config.touch });
 
-		return TestUtils.render(CoordinateSelect.tag);
+		return TestUtils.renderAndLogLifecycle(CoordinateSelect.tag);
 	};
+
+	describe('constructor', () => {
+		it('sets a default model', async () => {
+			spyOn(mapServiceMock, 'getCoordinateRepresentations').and.returnValue([CoordinateRepresentations.UTM, CoordinateRepresentations.WGS84]);
+			setup();
+			const element = new CoordinateSelect();
+
+			expect(element.getModel()).toEqual({
+				selectedCr: null,
+				pointerPosition: []
+			});
+		});
+	});
 
 	describe('when initialized', () => {
 		it('renders nothing when pointer position equals null', async () => {
-			const element = await setup({ touch: false });
+			spyOn(mapServiceMock, 'getCoordinateRepresentations').and.returnValue([CoordinateRepresentations.UTM, CoordinateRepresentations.WGS84]);
+			const element = await setup();
 
-			expect(element.shadowRoot.querySelector('.coordinate-label')).toBeFalsy();
+			expect(element.shadowRoot.querySelectorAll('.coordinate-label')).toHaveSize(0);
 		});
-		it('adds a div which shows coordinate select and coordinate display', async () => {
-			const element = await setup({ touch: false });
+		it('adds select element', async () => {
+			spyOn(mapServiceMock, 'getCoordinateRepresentations').and.returnValue([CoordinateRepresentations.UTM, CoordinateRepresentations.WGS84]);
+			const element = await setup();
 
-			setPointerMove({ coordinate: [12345, 67890], screenCoordinate: [] });
-
-			const container = element.shadowRoot.querySelector('.coordinate-container');
-
-			expect(container.childElementCount).toBe(2);
-			expect(container).toBeTruthy();
-
-			expect(element.shadowRoot.querySelector('select')).toBeTruthy();
-			expect(element.shadowRoot.querySelector('.coordinate-label')).toBeTruthy();
-			expect(element.shadowRoot.querySelectorAll('.select-coordinate-option')[0].value).toEqual('99999');
-			expect(element.shadowRoot.querySelectorAll('.select-coordinate-option')[1].value).toEqual('1111');
-			expect(element.shadowRoot.querySelector('select').title).toBe('footer_coordinate_select');
+			expect(element.shadowRoot.querySelectorAll('select')).toHaveSize(1);
+			expect(element.shadowRoot.querySelectorAll('select')[0].title).toBe('footer_coordinate_select');
+			expect(element.shadowRoot.querySelectorAll('.select-coordinate-option')[0].value).toBe(CoordinateRepresentations.UTM.label);
+			expect(element.shadowRoot.querySelectorAll('.select-coordinate-option')[1].value).toEqual(CoordinateRepresentations.WGS84.label);
+			expect(element.shadowRoot.querySelectorAll('.coordinate-label')).toHaveSize(0);
 		});
 	});
 
 	describe('on pointer move', () => {
-		it('updates the div which shows the current pointer position', async () => {
-			const element = await setup({ touch: false });
-
-			const transformMock = spyOn(coordinateServiceMock, 'transform').and.returnValue([21, 21]);
+		it('updates the coordinates', async () => {
+			spyOn(mapServiceMock, 'getCoordinateRepresentations').and.returnValue([CoordinateRepresentations.UTM, CoordinateRepresentations.WGS84]);
+			const element = await setup();
 			const stringifyMock = spyOn(coordinateServiceMock, 'stringify').and.returnValue('stringified coordinate');
-
 			const testCoordinate = [1211817.6233080907, 6168328.021915435];
 
-			// coordinates are shown after the pointer is moved, so initial there are no coordinates visible
 			setPointerMove({ coordinate: testCoordinate, screenCoordinate: [] });
 
-			expect(element.shadowRoot.querySelector('select').value).toEqual('99999');
-			expect(element.shadowRoot.querySelector('.select-coordinate-option').innerHTML.includes('TEST')).toBeTruthy();
+			expect(element.shadowRoot.querySelector('.coordinate-label').innerText).toBe('stringified coordinate');
+			expect(stringifyMock).toHaveBeenCalledOnceWith(testCoordinate, CoordinateRepresentations.UTM);
+		});
+		it('displays "-" when no CoordinateReference is available', async () => {
+			spyOn(mapServiceMock, 'getCoordinateRepresentations').and.callFake((coordinate) => {
+				return coordinate ? [] : [CoordinateRepresentations.UTM, CoordinateRepresentations.WGS84];
+			});
+			const element = await setup();
+			const testCoordinate = [1211817.6233080907, 6168328.021915435];
 
-			expect(element.shadowRoot.innerHTML.includes('stringified coordinate')).toBeTruthy();
+			setPointerMove({ coordinate: testCoordinate, screenCoordinate: [] });
 
-			expect(transformMock).toHaveBeenCalledOnceWith(testCoordinate, 3857, 99999);
-			expect(stringifyMock).toHaveBeenCalledOnceWith([21, 21], 99999);
+			expect(element.shadowRoot.querySelector('.coordinate-label').innerText).toContain('-');
 		});
 	});
 
 	describe('on selection change', () => {
-		it('updates the coordinate system', async () => {
-			const element = await setup({ touch: false });
-
-			const transformMock = spyOn(coordinateServiceMock, 'transform').and.returnValue([21, 21]);
-			const stringifyMock = spyOn(coordinateServiceMock, 'stringify').and.returnValue('stringified coordinate');
-			const toLonLatMock = spyOn(coordinateServiceMock, 'toLonLat').and.returnValue([42, 42]);
-
-			const testCoordinate = [23, 23];
+		it('updates the coordinate', async () => {
+			spyOn(mapServiceMock, 'getCoordinateRepresentations').and.returnValue([CoordinateRepresentations.UTM, CoordinateRepresentations.WGS84]);
+			spyOn(coordinateServiceMock, 'stringify').and.callFake((coordinate, cr) => `stringified coordinate for ${cr.label}`);
+			const element = await setup();
+			const testCoordinate = [1211817.6233080907, 6168328.021915435];
 			setPointerMove({ coordinate: testCoordinate, screenCoordinate: [] });
 
+			expect(element.shadowRoot.querySelector('.coordinate-label').innerText).toBe('stringified coordinate for UTM');
+
+			// change selection
 			const select = element.shadowRoot.querySelector('select');
-
-			// shows no coordinates (default)
-			select.value = '123';
+			select.value = CoordinateRepresentations.WGS84.label;
 			select.dispatchEvent(new Event('change'));
-			expect(element.shadowRoot.innerHTML.includes('stringified coordinate')).toBeFalsy();
 
-			// change to code '1111' - toLonLat method is called
-			select.value = '1111';
-			select.dispatchEvent(new Event('change'));
-			setPointerMove({ coordinate: testCoordinate, screenCoordinate: [] });
-			expect(element.shadowRoot.innerHTML.includes('stringified coordinate')).toBeTruthy();
-			expect(toLonLatMock).toHaveBeenCalledWith(testCoordinate);
-			expect(stringifyMock).toHaveBeenCalledWith([42, 42], 1111, { digits: 5 });
-
-			// change to code '99999' - transform method is called
-			select.value = '99999';
-			select.dispatchEvent(new Event('change'));
-			expect(element.shadowRoot.innerHTML.includes('stringified coordinate')).toBeTruthy();
-			expect(transformMock).toHaveBeenCalledWith(testCoordinate, 3857, 99999);
-			expect(stringifyMock).toHaveBeenCalledWith([21, 21], 99999);
+			expect(element.shadowRoot.querySelector('.coordinate-label').innerText).toBe('stringified coordinate for WGS84');
 		});
 	});
 
 	describe('on touch devices', () => {
 		it("doesn't show select and label", async () => {
+			spyOn(mapServiceMock, 'getCoordinateRepresentations').and.returnValue([CoordinateRepresentations.UTM, CoordinateRepresentations.WGS84]);
 			const element = await setup({ touch: true });
 
 			expect(element.shadowRoot.children.length).toBe(0);
