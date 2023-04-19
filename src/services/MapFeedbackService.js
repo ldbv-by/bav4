cc081a40336f3daa390d6f26ffbc9c22720c16dd
Thomas Aulinger <taulinger@gmx.de>
2023-04-19 19:10:51 +0200

    use BVV specific provider

--- src/services/MapFeedbackService.js
+++ src/services/MapFeedbackService.js
@@ -3,27 +3,53 @@
  */
 /**
  * @typedef {Object} MapFeedback
- * @property {string} state the encoded state of the map
- * @property {string} category the category of this feedback message
- * @property {string} description the category of this feedback message
- * @property {string} [email] the email address of the editor of this feedback message
- * @property {string} [fileId] the id of the referenced geometry file of this feedback message
+ * @property {string} state The encoded state of the map
+ * @property {string} category The category of this feedback message
+ * @property {string} description The actual message of this feedback message
+ * @property {string} fileId The id of the referenced geometry file of this feedback message
+ * @property {string} [email] The email address of the editor of this feedback message
+ */
+
+import { bvvMapFeedbackCategoriesProvider, bvvMapFeedbackStorageProvider } from './provider/mapFeedbackStorage.provider';
+
+/**
+ * A function that stores a MapFeedback
+ * @async
+ * @param {module:services/MapFeedbackService~MapFeedback} mapFeedback
+ * @typedef {Function} mapFeedbackStorageProvider
+ * @throws {@link Error} when storing was not succesfull
+ * @returns {Promise<Boolean>} `true` when storing was successful
+ */
+/**
+ * A function that returns a list of categories for a MapFeedback
+ * @async
+ * @typedef {Function} mapFeedbackCategoriesProvider
+ * @returns {Array<String>} available categories
  */
 
 /**
- * It's a stub!
  * @class
  */
 export class MapFeedbackService {
-	constructor() {
-		this._categories = ['Foo', 'Bar'];
+	/**
+	 *
+	 * @param {module:services/MapFeedbackService~mapFeedbackStorageProvider} mapFeedbackStorageProvider
+	 * @param {module:services/MapFeedbackService~mapFeedbackCategoriesProvider} mapFeedbackCategoriesProvider
+	 */
+	constructor(mapFeedbackStorageProvider = bvvMapFeedbackStorageProvider, mapFeedbackCategoriesProvider = bvvMapFeedbackCategoriesProvider) {
+		this._mapFeedbackStorageProvider = mapFeedbackStorageProvider;
+		this._mapFeedbackCategoriesProvider = mapFeedbackCategoriesProvider;
+		this._categories = null;
 	}
 	/**
 	 * Returns all possible categories for a MapFeedback.
-	 * @throws an {@link Error} when categories are not available.
-	 * @returns {Array<string>}
+	 * @throws `Error` when categories are not available.
+	 * @returns {Array<String>}
 	 */
 	async getCategories() {
+		if (!this._categories) {
+			this._categories = await this._mapFeedbackCategoriesProvider();
+		}
 		return [...this._categories];
 	}
 
@@ -31,11 +57,10 @@ export class MapFeedbackService {
 	 *
 	 * Saves a MapFeedback object.
 	 * @param {module:services/MapFeedbackService~MapFeedback} mapFeedback
-	 * @throws an {@link Error} when saving was not succesfull
-	 * @returns {boolean} `true` when storing was successful
+	 * @throws `Error` when storing was not succesfull
+	 * @returns {Boolean} `true` when storing was successful
 	 */
-	// eslint-disable-next-line no-unused-vars
 	async save(mapFeedback) {
-		return true;
+		return this._mapFeedbackStorageProvider(mapFeedback);
 	}
 }
