ace7e395a052322ae0295cd48144bb2cdaa48638
Norbert Klein <nklein@nkode.de>
2023-04-19 17:13:54 +0200

    npm run doc:apply

--- src/modules/feedback/components/mapFeedback/MapFeedbackPanel.js
+++ src/modules/feedback/components/mapFeedback/MapFeedbackPanel.js
@@ -1,3 +1,6 @@
+/**
+ * @module modules/feedback/components/mapFeedback/MapFeedbackPanel
+ */
 import { html } from 'lit-html';
 import { $injector } from '../../../../injection';
 import { MvuElement } from '../../../MvuElement';
