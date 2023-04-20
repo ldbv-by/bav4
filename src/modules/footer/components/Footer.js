7036c934b3d96dd1720c00530b79e4d467d23372
alsturm <alexander_sturm@gmx.net>
2023-04-20 15:59:16 +0200

    add private policy panel to footer

--- src/modules/footer/components/Footer.js
+++ src/modules/footer/components/Footer.js
@@ -79,7 +79,10 @@ export class Footer extends MvuElement {
 		};
 
 		const createChildrenView = () => {
-			return html` <ba-map-info></ba-map-info> `;
+			return html`
+				<ba-privacy-policy></ba-privacy-policy>
+				<ba-map-info></ba-map-info>
+			`;
 		};
 
 		return html`
