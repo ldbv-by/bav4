12101cb01a9edfddcb0947419dc011399c6c5c25
Thomas Aulinger <taulinger@gmx.de>
2023-04-25 19:35:47 +0200

    make prettier happy

--- test/utils/mgrs.test.js
+++ test/utils/mgrs.test.js
@@ -1,7 +1,7 @@
 import { LLtoUTM, forward } from '../../src/utils/mgrs';
 
 describe('mgrs', () => {
-	// no detailed test here, we just check if our needed functions are 
+	// no detailed tests here, we just check if our needed functions are available
 	it('exports all necessary functions', () => {
 		expect(typeof LLtoUTM).toBe('function');
 		expect(typeof forward).toBe('function');
