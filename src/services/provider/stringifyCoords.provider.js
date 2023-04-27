75912c2444ccfb135dfa64104639e17f959f52a3
Schlemmer, Thilo (LDBV) <thilo.schlemmer@ldbv.bayern.de>
2023-04-27 06:40:36 +0200

    fix typos

--- src/services/provider/stringifyCoords.provider.js
+++ src/services/provider/stringifyCoords.provider.js
@@ -5,7 +5,7 @@ import { LLtoUTM, forward } from '../../utils/mgrs';
 import { GlobalCoordinateRepresentations } from '../../domain/coordinateRepresentation';
 
 /**
- * A function that returns a string representaion of a coordinate.
+ * A function that returns a string representation of a coordinate.
  * @param {module:domain/coordinateTypeDef~Coordinate} coordinate
  * @param {module:domain/coordinateRepresentation~CoordinateRepresentation} coordinateRepresentation
  * @param {function} transformFn
@@ -14,7 +14,7 @@ import { GlobalCoordinateRepresentations } from '../../domain/coordinateRepresen
  */
 
 /**
- * Bvv specific immplementation of {@link module:services/provider/stringifyCoords_provider~stringifyCoordProvider}
+ * Bvv specific implementation of {@link module:services/provider/stringifyCoords_provider~stringifyCoordProvider}
  * @async
  * @implements {module:services/provider/stringifyCoords_provider~stringifyCoordProvider}
  */
