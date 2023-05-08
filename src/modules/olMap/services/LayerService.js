c5a8d011dfded43aad640b7137b01684786cf851
Thomas Aulinger <taulinger@gmx.de>
2023-05-08 19:46:48 +0200

    XyzGeoResource: handle also urls as array

--- src/modules/olMap/services/LayerService.js
+++ src/modules/olMap/services/LayerService.js
@@ -85,16 +85,20 @@ export class LayerService {
 
 			case GeoResourceTypes.XYZ: {
 				const xyzSource = () => {
+					const config = {
+						url: Array.isArray(geoResource.urls) ? undefined : geoResource.urls,
+						urls: Array.isArray(geoResource.urls) ? geoResource.urls : undefined
+					};
 					switch (geoResource.tileGridId) {
 						case 'adv_wmts':
 							return new XYZSource({
-								url: geoResource.url,
+								...config,
 								tileGrid: new AdvWmtsTileGrid(),
 								projection: new Projection({ code: 'EPSG:25832' }) // to make it testable we use a Projection instead of a ProjectionLike here
 							});
 						default:
 							return new XYZSource({
-								url: geoResource.url
+								...config
 							});
 					}
 				};
