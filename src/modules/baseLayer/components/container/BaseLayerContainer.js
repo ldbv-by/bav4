2abf0f5c34a4578f50ed2a2695d8c02c3fd85a6e
Thomas Aulinger <taulinger@gmx.de>
2023-05-25 18:03:27 +0200

    add BaseLayerContainer component

---
+++ src/modules/baseLayer/components/container/BaseLayerContainer.js
@@ -0,0 +1,56 @@
+import { html } from 'lit-html';
+import css from './baseLayerContainer.css';
+import { $injector } from '../../../../injection';
+import { MvuElement } from '../../../MvuElement';
+
+/**
+ * Manages multiple {@link BaseLayerSwitcher} instances
+ * @class
+ */
+export class BaseLayerContainer extends MvuElement {
+	constructor() {
+		super({
+			categories: {
+				raster: ['atkis', 'luftbild_labels', 'tk', 'historisch', 'atkis_sw'],
+				vector: [
+					'by_style_standard',
+					'by_style_grau',
+					'by_style_nacht',
+					'by_style_hoehenlinien',
+					'by_style_luftbild',
+					'by_style_wandern',
+					'by_style_radln'
+				]
+			}
+		});
+
+		const { TranslationService: translationService } = $injector.inject('TranslationService');
+		this._translationService = translationService;
+	}
+
+	createView(model) {
+		const { categories } = model;
+		const allBaseGeoResourceIds = Array.from(new Set(Object.values(categories).flat()));
+		const translate = (key) => this._translationService.translate(key);
+
+		return html`
+			<style>
+				${css}
+			</style>
+			<div class="title">${translate('baseLayer_container_header')}</div>
+			<div class="container">
+				${Object.entries(categories).map(
+					([key, value]) =>
+						html`<div class="title">${translate(`baseLayer_container_category_${key}`)}</div>
+							<div>
+								<ba-base-layer-switcher .configuration=${{ all: allBaseGeoResourceIds, specific: value }}></ba-base-layer-switcher>
+							</div>`
+				)}
+			</div>
+		`;
+	}
+
+	static get tag() {
+		return 'ba-base-layer-container';
+	}
+}
---
+++ test/modules/baseLayer/components/BaseLayerContainer.test.js
@@ -0,0 +1,88 @@
+import { $injector } from '../../../../src/injection';
+import { BaseLayerContainer } from '../../../../src/modules/baseLayer/components/container/BaseLayerContainer';
+import { BaseLayerSwitcher } from '../../../../src/modules/baseLayer/components/switcher/BaseLayerSwitcher';
+import { TestUtils } from '../../../test-utils';
+
+window.customElements.define(BaseLayerContainer.tag, BaseLayerContainer);
+
+describe('BaseLayerSwitcher', () => {
+	const setup = async (state = {}) => {
+		TestUtils.setupStoreAndDi(state);
+
+		$injector.registerSingleton('TranslationService', { translate: (key) => key });
+
+		return TestUtils.render(BaseLayerContainer.tag);
+	};
+
+	describe('when instantiated', () => {
+		it('has a model containing default values', async () => {
+			await setup();
+			const model = new BaseLayerContainer().getModel();
+
+			expect(model).toEqual({
+				categories: {
+					raster: ['atkis', 'luftbild_labels', 'tk', 'historisch', 'atkis_sw'],
+					vector: [
+						'by_style_standard',
+						'by_style_grau',
+						'by_style_nacht',
+						'by_style_hoehenlinien',
+						'by_style_luftbild',
+						'by_style_wandern',
+						'by_style_radln'
+					]
+				}
+			});
+		});
+	});
+
+	describe('when initialized ', () => {
+		it('renders two BaseLayerSwitcher instances', async () => {
+			const element = await setup();
+
+			expect(element.shadowRoot.querySelectorAll(BaseLayerSwitcher.tag)).toHaveSize(2);
+			expect(element.shadowRoot.querySelectorAll(BaseLayerSwitcher.tag)[0].configuration).toEqual({
+				specific: ['atkis', 'luftbild_labels', 'tk', 'historisch', 'atkis_sw'],
+				all: [
+					'atkis',
+					'luftbild_labels',
+					'tk',
+					'historisch',
+					'atkis_sw',
+					'by_style_standard',
+					'by_style_grau',
+					'by_style_nacht',
+					'by_style_hoehenlinien',
+					'by_style_luftbild',
+					'by_style_wandern',
+					'by_style_radln'
+				]
+			});
+			expect(element.shadowRoot.querySelectorAll(BaseLayerSwitcher.tag)[1].configuration).toEqual({
+				specific: [
+					'by_style_standard',
+					'by_style_grau',
+					'by_style_nacht',
+					'by_style_hoehenlinien',
+					'by_style_luftbild',
+					'by_style_wandern',
+					'by_style_radln'
+				],
+				all: [
+					'atkis',
+					'luftbild_labels',
+					'tk',
+					'historisch',
+					'atkis_sw',
+					'by_style_standard',
+					'by_style_grau',
+					'by_style_nacht',
+					'by_style_hoehenlinien',
+					'by_style_luftbild',
+					'by_style_wandern',
+					'by_style_radln'
+				]
+			});
+		});
+	});
+});
