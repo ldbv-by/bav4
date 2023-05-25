9f0aa67da67f19171907459719fd506dccc14bd5
Schlemmer, Thilo (LDBV) <thilo.schlemmer@ldbv.bayern.de>
2023-05-25 09:32:17 +0200

    update i18n

--- src/modules/export/i18n/export.provider.js
+++ src/modules/export/i18n/export.provider.js
@@ -6,6 +6,15 @@ export const provide = (lang) => {
 				export_item_label_kml: 'KML',
 				export_item_description_kml: 'usable in BayernAtlas or Google Earth and many GIS-Applications',
 				export_item_download_kml: '.kml',
+				export_item_label_gpx: 'GPX',
+				export_item_description_gpx: 'usable on GPS-Devices',
+				export_item_download_gpx: '.gpx',
+				export_item_label_geojson: 'GeoJSON',
+				export_item_description_geojson: 'usable in many GIS-Applications',
+				export_item_download_geojson: '.geojson',
+				export_item_label_ewkt: 'EWKT',
+				export_item_description_ewkt: 'usable in BayernAtlas and many GIS-Applications',
+				export_item_download_ewkt: '.ewkt',
 				export_item_srid_selection: 'Select SRID',
 				export_item_srid_selection_disabled: 'SRID is predefined'
 			};
@@ -16,6 +25,15 @@ export const provide = (lang) => {
 				export_item_label_kml: 'KML',
 				export_item_description_kml: 'verwendbar im BayernAtlas oder Google Earth und vielen GIS-Anwendungen',
 				export_item_download_kml: '.kml',
+				export_item_label_gpx: 'GPX',
+				export_item_description_gpx: 'verwendbar in GPS-Geräten',
+				export_item_download_gpx: '.gpx',
+				export_item_label_geojson: 'GeoJSON',
+				export_item_description_geojson: 'verwendbar in vielen GIS-Anwendungen',
+				export_item_download_geojson: '.geojson',
+				export_item_label_ewkt: 'EWKT',
+				export_item_description_ewkt: 'verwendbar im BayernAtlas und vielen GIS-Anwendungen',
+				export_item_download_ewkt: '.ewkt',
 				export_item_srid_selection: 'SRID auswählen',
 				export_item_srid_selection_disabled: 'SRID ist vordefiniert'
 			};
