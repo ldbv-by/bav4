d7eb8842d232fc73a514814ef3ce125b0e865c80
Schlemmer, Thilo (LDBV) <thilo.schlemmer@ldbv.bayern.de>
2023-06-12 11:20:10 +0200

    optimize geojson description

--- src/modules/export/i18n/export.provider.js
+++ src/modules/export/i18n/export.provider.js
@@ -4,13 +4,13 @@ export const provide = (lang) => {
 			return {
 				//the first part of the snake_case key should be the name of the related module
 				export_item_label_kml: 'KML',
-				export_item_description_kml: 'saves styles (symbols, color and width) and all geometry types',
+				export_item_description_kml: 'saves attributes, styles (symbols, color and width) and all geometry types',
 				export_item_download_kml: '.kml',
 				export_item_label_gpx: 'GPX',
 				export_item_description_gpx: 'saves points and linestrings (tracks). Polygons are converted to linestrings',
 				export_item_download_gpx: '.gpx',
 				export_item_label_geojson: 'GeoJSON',
-				export_item_description_geojson: 'saves styles (symbols, color and width) and all geometry types',
+				export_item_description_geojson: 'saves attributes and all geometry types, but no styles',
 				export_item_download_geojson: '.geojson',
 				export_item_label_ewkt: 'EWKT',
 				export_item_description_ewkt: 'saves all geometry types, no styles',
@@ -23,13 +23,13 @@ export const provide = (lang) => {
 			return {
 				//the first part of the snake_case key should be the name of the related module
 				export_item_label_kml: 'KML',
-				export_item_description_kml: 'übernimmt Stil (Symbol, Farbe und Strichstärke) und alle Geometrietypen',
+				export_item_description_kml: 'übernimmt Attribute, Stil (Symbol, Farbe und Strichstärke) und alle Geometrietypen',
 				export_item_download_kml: '.kml',
 				export_item_label_gpx: 'GPX',
 				export_item_description_gpx: 'übernimmt die Punkte und Linienzüge (Tracks). Polygone werden in Linienzüge umgewandelt',
 				export_item_download_gpx: '.gpx',
 				export_item_label_geojson: 'GeoJSON',
-				export_item_description_geojson: 'übernimmt Stil (Symbol, Farbe und Strichstärke) und alle Geometrietypen',
+				export_item_description_geojson: 'übernimmt Attribute und alle Geometrietypen, aber keine Stile',
 				export_item_download_geojson: '.geojson',
 				export_item_label_ewkt: 'EWKT',
 				export_item_description_ewkt: 'übernimmt alle Geometrietypen, keine Stile',
--- test/modules/export/i18n/export.provider.test.js
+++ test/modules/export/i18n/export.provider.test.js
@@ -5,13 +5,13 @@ describe('i18n for search module', () => {
 		const map = provide('de');
 
 		expect(map.export_item_label_kml).toBe('KML');
-		expect(map.export_item_description_kml).toBe('übernimmt Stil (Symbol, Farbe und Strichstärke) und alle Geometrietypen');
+		expect(map.export_item_description_kml).toBe('übernimmt Attribute, Stil (Symbol, Farbe und Strichstärke) und alle Geometrietypen');
 		expect(map.export_item_download_kml).toBe('.kml');
 		expect(map.export_item_label_gpx).toBe('GPX');
 		expect(map.export_item_description_gpx).toBe('übernimmt die Punkte und Linienzüge (Tracks). Polygone werden in Linienzüge umgewandelt');
 		expect(map.export_item_download_gpx).toBe('.gpx');
 		expect(map.export_item_label_geojson).toBe('GeoJSON');
-		expect(map.export_item_description_geojson).toBe('übernimmt Stil (Symbol, Farbe und Strichstärke) und alle Geometrietypen');
+		expect(map.export_item_description_geojson).toBe('übernimmt Attribute und alle Geometrietypen, aber keine Stile');
 		expect(map.export_item_download_geojson).toBe('.geojson');
 		expect(map.export_item_label_ewkt).toBe('EWKT');
 		expect(map.export_item_description_ewkt).toBe('übernimmt alle Geometrietypen, keine Stile');
@@ -24,13 +24,13 @@ describe('i18n for search module', () => {
 		const map = provide('en');
 
 		expect(map.export_item_label_kml).toBe('KML');
-		expect(map.export_item_description_kml).toBe('saves styles (symbols, color and width) and all geometry types');
+		expect(map.export_item_description_kml).toBe('saves attributes, styles (symbols, color and width) and all geometry types');
 		expect(map.export_item_download_kml).toBe('.kml');
 		expect(map.export_item_label_gpx).toBe('GPX');
 		expect(map.export_item_description_gpx).toBe('saves points and linestrings (tracks). Polygons are converted to linestrings');
 		expect(map.export_item_download_gpx).toBe('.gpx');
 		expect(map.export_item_label_geojson).toBe('GeoJSON');
-		expect(map.export_item_description_geojson).toBe('saves styles (symbols, color and width) and all geometry types');
+		expect(map.export_item_description_geojson).toBe('saves attributes and all geometry types, but no styles');
 		expect(map.export_item_download_geojson).toBe('.geojson');
 		expect(map.export_item_label_ewkt).toBe('EWKT');
 		expect(map.export_item_description_ewkt).toBe('saves all geometry types, no styles');
