80dc040459947105dd7b9a5e1b7260f5fd8c646b
Thomas Aulinger <taulinger@gmx.de>
2023-06-15 16:51:29 +0200

    use correct i18n key

--- test/modules/olMap/handler/featureInfo/OlFeatureInfoHandler.test.js
+++ test/modules/olMap/handler/featureInfo/OlFeatureInfoHandler.test.js
@@ -361,8 +361,8 @@ describe('OlFeatureInfoHandler', () => {
 			startRequest(matchingCoordinate);
 
 			expect(store.getState().featureInfo.current).toHaveSize(2);
-			expect(store.getState().featureInfo.current[0]).toEqual({ title: 'olMap_handler_featureInfo_not_available', content: '' });
-			expect(store.getState().featureInfo.current[1]).toEqual({ title: 'olMap_handler_featureInfo_not_available', content: '' });
+			expect(store.getState().featureInfo.current[0]).toEqual({ title: 'global_featureInfo_not_available', content: '' });
+			expect(store.getState().featureInfo.current[1]).toEqual({ title: 'global_featureInfo_not_available', content: '' });
 			expect(store.getState().highlight.features).toHaveSize(0);
 		});
 	});
