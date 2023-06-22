aa5f1af789391444629d4daa19439edfaa3d6603
Schlemmer, Thilo (LDBV) <thilo.schlemmer@ldbv.bayern.de>
2023-06-22 09:28:54 +0200

    rename i18n provider

--- src/modules/feedback/i18n/index.js
+++ src/modules/feedback/i18n/index.js
@@ -1,6 +1,6 @@
 import { provide as feedbackProvider } from './feedback.provider';
-import { provide as fiveButtonRatingProvider } from './starsRatingPanel.provider';
+import { provide as likertItemRatingProvider } from './likertItemRatingPanel.provider';
 import { $injector } from '../../../injection';
 const { TranslationService: translationService } = $injector.inject('TranslationService');
 translationService.register('feedbackProvider', feedbackProvider);
-translationService.register('fiveButtonRatingProvider', fiveButtonRatingProvider);
+translationService.register('likertItemRatingProvider', likertItemRatingProvider);
--- test/modules/feedback/i18n/likertItemRatingPanel.provider.test.js
+++ test/modules/feedback/i18n/likertItemRatingPanel.provider.test.js
@@ -1,4 +1,4 @@
-import { provide } from '../../../../src/modules/feedback/i18n/starsRatingPanel.provider';
+import { provide } from '../../../../src/modules/feedback/i18n/likertItemRatingPanel.provider';
 
 describe('i18n for starsRatingPanel', () => {
 	it('provides translation for de', () => {
