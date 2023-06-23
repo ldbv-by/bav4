c72fd4d5f7ebbdbceef10a1d16f3e96c44c8bd46
Schlemmer, Thilo (LDBV) <thilo.schlemmer@ldbv.bayern.de>
2023-06-23 15:08:33 +0200

    add test

--- src/modules/map/components/assistChips/index.js
+++ src/modules/map/components/assistChips/index.js
@@ -1,4 +1,8 @@
 import { SharePositionChip } from './SharePositionChip';
+import { MapFeedbackChip } from './MapFeedbackChip';
 if (!window.customElements.get(SharePositionChip.tag)) {
 	window.customElements.define(SharePositionChip.tag, SharePositionChip);
+}
+if (!window.customElements.get(MapFeedbackChip.tag)) {
+	window.customElements.define(MapFeedbackChip.tag, MapFeedbackChip);
 }
