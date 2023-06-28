d604a366d13fe5de971cbb96835168fb34824dd8
Norbert Klein <nklein@nkode.de>
2023-06-28 16:51:17 +0200

    added style-utils.js

---
+++ src/utils/style-utils.js
@@ -0,0 +1,5 @@
+const getStyle = (element, prop) => parseInt(window.getComputedStyle(element).getPropertyValue(prop));
+
+const getPseudoStyle = (element, prop) => parseInt(window.getComputedStyle(element, ':before').getPropertyValue(prop));
+
+export { getStyle, getPseudoStyle };
