efa817805de949e348c0692d04a3bfefcac463f1
Thomas Aulinger <taulinger@gmx.de>
2023-07-04 15:45:42 +0200

    update

--- src/chunks/elevation-profile.js
+++ src/chunks/elevation-profile.js
@@ -1,4 +1,6 @@
-//This file just defines all dependencies for the chunk "elevation-profile"
+/**
+ * This file just defines all dependencies for the chunk "elevation-profile"
+ */
 import { ElevationProfile } from '../modules/elevationProfile/components/panel/ElevationProfile';
 
 if (!window.customElements.get(ElevationProfile.tag)) {
