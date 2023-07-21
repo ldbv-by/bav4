03e2899af89662685aa3f58f41725d4edee67dd9
Thomas Aulinger <taulinger@gmx.de>
2023-07-21 13:33:49 +0200

    update

--- src/modules/elevationProfile/i18n/elevationProfile.provider.js
+++ src/modules/elevationProfile/i18n/elevationProfile.provider.js
@@ -7,12 +7,12 @@ export const provide = (lang) => {
 				elevationProfile_sumUp: 'Ascent',
 				elevationProfile_sumDown: 'Descent',
 				elevationProfile_alt: 'Elevation',
-				elevationProfile_surface: 'surface',
+				elevationProfile_surface: 'Surface',
 				elevationProfile_elevation_profile: 'Elevation Profile',
-				elevationProfile_verticalHeight: 'Vertical Height',
-				elevationProfile_highestPoint: 'Highest Point',
-				elevationProfile_lowestPoint: 'Lowest Point',
-				elevationProfile_linearDistance: 'Linear Distance',
+				elevationProfile_verticalHeight: 'Elevation difference start - end',
+				elevationProfile_highestPoint: 'Highest point',
+				elevationProfile_lowestPoint: 'Lowest point',
+				elevationProfile_linearDistance: 'Linear distance',
 				elevationProfile_could_not_load: 'Elevation Profile could not be loaded',
 				elevationProfile_unknown: 'unknown'
 			};
@@ -26,11 +26,11 @@ export const provide = (lang) => {
 				elevationProfile_alt: 'Höhe',
 				elevationProfile_surface: 'Untergrund',
 				elevationProfile_elevation_profile: 'Höhenprofil',
-				elevationProfile_verticalHeight: 'Höhe',
+				elevationProfile_verticalHeight: 'Höhendifferenz Start - Ende',
 				elevationProfile_highestPoint: 'Höchster Punkt',
 				elevationProfile_lowestPoint: 'Tiefster Punkt',
-				elevationProfile_linearDistance: 'Strecke',
-				elevationProfile_could_not_load: 'Höhenprofil konnte nicht geladen werden',
+				elevationProfile_linearDistance: 'Luftlinie',
+				elevationProfile_could_not_load: 'Das Höhenprofil konnte nicht geladen werden',
 				elevationProfile_unknown: 'unbekannt'
 			};
 
