91625e82cb4e8a46d2f3c4e876b5be4d56b09bce
Schlemmer, Thilo (LDBV) <thilo.schlemmer@ldbv.bayern.de>
2023-09-11 08:13:23 +0200

    update tests

--- test/service/provider/etaCalculator.provider.test.js
+++ test/service/provider/etaCalculator.provider.test.js
@@ -1,23 +1,52 @@
 import { bvvEtaCalculatorProvider } from '../../../src/services/provider/etaCalculator.provider';
 
+const milliSecondsToHour = (milliseconds) => {
+	// 1- Convert to seconds:
+	const seconds = milliseconds / 1000;
+	// 2- Extract hours:
+	const hours = parseInt(seconds / 3600); // 3,600 seconds in 1 hour
+	const remainingSeconds = seconds % 3600; // seconds remaining after extracting hours
+	// 3- Extract minutes:
+	const minutes = parseInt(remainingSeconds / 60); // 60 seconds in 1 minute
+	// 4- Keep only seconds not extracted to minutes:
+	// remainingSeconds = seconds % 60;
+	const size = 2;
+
+	let hoursFormatted = hours + '';
+	while (hoursFormatted.length < size) hoursFormatted = '0' + hoursFormatted;
+	let minutesFormatted = minutes + '';
+	while (minutesFormatted.length < size) minutesFormatted = '0' + minutesFormatted;
+	return hoursFormatted + ':' + minutesFormatted;
+};
+
 describe('ETACalculator provider', () => {
 	describe('Bvv ETACalculator provider', () => {
+		const upDownEvenStats = { distance: 34200, up: 300, down: 300 };
+		const moreUpThanDownStats = { distance: 34200, up: 800, down: 300 };
 		const provider = bvvEtaCalculatorProvider;
+
+		const calculateETA = (etaCalculator, stats) => {
+			return milliSecondsToHour(etaCalculator.getETAfor(stats.distance, stats.up, stats.down));
+		};
 		it('have defined ETACalculators ', () => {
 			expect(provider('hike').getETAfor).toBeDefined();
 			expect(provider('bike').getETAfor).toBeDefined();
 			expect(provider('mtb').getETAfor).toBeDefined();
 			expect(provider('roadbike').getETAfor).toBeDefined();
+
 			expect(provider('foo')).toBeNull();
+		});
 
-			const distance = 5000;
-			const elevationUp = 500;
-			const elevationDown = 100;
+		it('calculates correct ETA for defined vehicle', () => {
+			expect(calculateETA(provider('hike'), upDownEvenStats)).toBe('09:21');
+			expect(calculateETA(provider('bike'), upDownEvenStats)).toBe('02:46');
+			expect(calculateETA(provider('mtb'), upDownEvenStats)).toBe('02:05');
+			expect(calculateETA(provider('roadbike'), upDownEvenStats)).toBe('01:38');
 
-			expect(provider('hike').getETAfor(distance, elevationUp, elevationDown)).toBe(8970000);
-			expect(provider('bike').getETAfor(distance, elevationUp, elevationDown)).toBe(6601440);
-			expect(provider('mtb').getETAfor(distance, elevationUp, elevationDown)).toBe(4951440);
-			expect(provider('roadbike').getETAfor(distance, elevationUp, elevationDown)).toBeCloseTo(4834361.9, 1);
+			expect(calculateETA(provider('hike'), moreUpThanDownStats)).toBe('10:11');
+			expect(calculateETA(provider('bike'), moreUpThanDownStats)).toBe('03:48');
+			expect(calculateETA(provider('mtb'), moreUpThanDownStats)).toBe('02:51');
+			expect(calculateETA(provider('roadbike'), moreUpThanDownStats)).toBe('02:38');
 		});
 	});
 });
