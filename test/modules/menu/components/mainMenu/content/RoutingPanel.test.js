14f3a817ef07a6647192c7af8ecf0d5008458b8a
thiloSchlemmer <thiloSchlemmer@gmail.com>
2023-10-05 12:43:17 +0200

    add tests

--- test/modules/menu/components/mainMenu/content/RoutingPanel.test.js
+++ test/modules/menu/components/mainMenu/content/RoutingPanel.test.js
@@ -1,23 +1,29 @@
+import { TabIds } from '../../../../../../src/domain/mainMenu';
 import { $injector } from '../../../../../../src/injection';
 import { AbstractMvuContentPanel } from '../../../../../../src/modules/menu/components/mainMenu/content/AbstractMvuContentPanel';
 import { RoutingPanel } from '../../../../../../src/modules/menu/components/mainMenu/content/routing/RoutingPanel';
+import { createNoInitialStateMainMenuReducer } from '../../../../../../src/store/mainMenu/mainMenu.reducer';
 import { createNoInitialStateMediaReducer } from '../../../../../../src/store/media/media.reducer';
+import { isTemplateResult } from '../../../../../../src/utils/checks';
 import { TestUtils } from '../../../../../test-utils';
 
 window.customElements.define(RoutingPanel.tag, RoutingPanel);
 
 describe('RoutingPanel', () => {
+	let store;
 	const setup = (state) => {
 		const initialState = {
+			mainMenu: {
+				open: true,
+				tab: null
+			},
 			media: {
 				portrait: false
 			},
 			...state
 		};
 
-		TestUtils.setupStoreAndDi(initialState, {
-			media: createNoInitialStateMediaReducer()
-		});
+		store = TestUtils.setupStoreAndDi(initialState, { mainMenu: createNoInitialStateMainMenuReducer(), media: createNoInitialStateMediaReducer() });
 		$injector.registerSingleton('TranslationService', { translate: (key) => key });
 		return TestUtils.render(RoutingPanel.tag);
 	};
@@ -40,14 +46,37 @@ describe('RoutingPanel', () => {
 	});
 
 	describe('when initialized', () => {
-		it('renders a category bar', async () => {
+		it('renders the routing components', async () => {
 			const element = await setup();
 
 			const container = element.shadowRoot.querySelectorAll('.container');
-			const categoryBar = element.shadowRoot.querySelectorAll('ba-routing-category-bar');
 
 			expect(container).toHaveSize(1);
-			expect(categoryBar).toHaveSize(1);
+			expect(element.shadowRoot.querySelectorAll('ba-routing-waypoints')).toHaveSize(1);
+			expect(element.shadowRoot.querySelectorAll('ba-routing-category-bar')).toHaveSize(1);
+			expect(element.shadowRoot.querySelectorAll('ba-routing-info')).toHaveSize(1);
+			expect(element.shadowRoot.querySelectorAll('ba-routing-details')).toHaveSize(1);
+
+			expect(element.shadowRoot.querySelectorAll('.demo')).toHaveSize(1);
+		});
+	});
+
+	describe('when close icon is clicked', () => {
+		it('updates the store', async () => {
+			const element = await setup();
+
+			const closeIcon = element.shadowRoot.querySelector('ba-icon');
+
+			closeIcon.click();
+
+			expect(store.getState().mainMenu.tab).toBe(TabIds.MAPS);
+		});
+	});
+
+	describe('when demo content needed', () => {
+		it('renders demo content', async () => {
+			const element = await setup();
+			expect(isTemplateResult(element._getDemoContent())).toBeTrue();
 		});
 	});
 });
