989f7533432b5be5e48ee8cb5df59083e70f0cb2
Thomas Aulinger <taulinger@gmx.de>
2023-10-06 16:41:36 +0200

    add tooltip provider for OlRoutingHandler

---
+++ src/modules/olMap/handler/routing/tooltipMessage.provider.js
@@ -0,0 +1,35 @@
+/**
+ * @module modules/olMap/handler/routing/tooltipMessage_provider
+ */
+import { $injector } from '../../../../injection';
+import { ROUTING_CATEGORY, ROUTING_FEATURE_TYPE, RoutingFeatureTypes } from './OlRoutingHandler';
+
+export const provide = (interactionState) => {
+	const { TranslationService: translationService } = $injector.inject('TranslationService');
+	const translate = (key, params = []) => translationService.translate(key, params);
+
+	const { feature } = interactionState;
+	switch (feature.get(ROUTING_FEATURE_TYPE)) {
+		case RoutingFeatureTypes.ROUTE_ALTERNATIVE: {
+			const { label } = feature.get(ROUTING_CATEGORY);
+			return translate('olMap_handler_routing_choose_alternative_route', [label]);
+		}
+		case RoutingFeatureTypes.ROUTE_SEGMENT: {
+			// return 'Zum Ändern der Route ziehen';
+			return translate('olMap_handler_routing_modify_segment');
+		}
+		case RoutingFeatureTypes.START: {
+			// return 'Zum Ändern des Startpunktes ziehen';
+			return translate('olMap_handler_routing_modify_start');
+		}
+		case RoutingFeatureTypes.DESTINATION: {
+			// return 'Zum Ändern des Zielpunktes ziehen';
+			return translate('olMap_handler_routing_modify_destination');
+		}
+		case RoutingFeatureTypes.INTERMEDIATE: {
+			// return 'Zum Ändern des Zwischenpunktes ziehen';
+			return translate('olMap_handler_routing_modify_intermediate');
+		}
+	}
+	return '';
+};
