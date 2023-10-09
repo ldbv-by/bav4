3e29a5b990cf8de222f786e770fdd28b520b4019
Thomas Aulinger <taulinger@gmx.de>
2023-10-09 13:00:49 +0200

    extend routing category

--- src/services/RoutingService.js
+++ src/services/RoutingService.js
@@ -104,41 +104,66 @@ export const mockCategoriesProvider = async () => {
 	const hike = {
 		id: 'hike',
 		label: 'Wandern',
+		description: 'Wandern auf der gewöhnlich schnellsten Route',
+		color: 'gray',
+		borderColor: 'red',
 		subcategories: []
 	};
-	const bvv_hike = {
+	const bvv_bike = {
 		id: 'bvv-hike',
 		label: 'Wandern (Freizeitwege)',
+		description: 'Wandern möglichst auf offiziellen Wanderwegen',
+		color: 'red',
+		borderColor: 'gray',
 		subcategories: [hike]
 	};
-	const bayernnetz_bike = {
+	const bayernnetzBike = {
 		id: 'bayernnetz-bike',
-		label: 'Mountainbike (Bayernnetz)',
+		label: 'Fahrrad (Bayernnetz)',
+		description: 'Fahrradfahren möglichst auf Wegen des Bayernnetzes',
+		color: 'blue',
+		borderColor: 'gray',
 		subcategories: []
 	};
 	const bike = {
 		id: 'bike',
 		label: 'Fahrrad',
+		description: 'Fahrradfahren auf der gewöhnlich schnellsten Route',
+		color: 'gray',
+		borderColor: 'green',
+		zIndex: 1,
 		subcategories: []
 	};
-	const bvv_bike = {
+	const bvv_hike = {
 		id: 'bvv-bike',
 		label: 'Fahrrad (Freizeitwege)',
-		subcategories: [bike, bayernnetz_bike]
+		description: 'Fahrradfahren möglichst auf offiziellen Freizeitwegen',
+		color: 'green',
+		borderColor: 'gray',
+		subcategories: [bike, bayernnetzBike]
 	};
 	const mtb = {
 		id: 'mtb',
 		label: 'Mountainbike',
+		description: 'Mountainbiken auf der gewöhnlich schnellsten Route',
+		color: 'gray',
+		borderColor: 'SpringGreen',
 		subcategories: []
 	};
 	const bvv_mtb = {
 		id: 'bvv-mtb',
 		label: 'Mountainbike (Freizeitwege)',
+		description: 'Mountainbiken möglichst auf offiziellen Freizeitwegen',
+		color: 'SpringGreen',
+		borderColor: 'gray',
 		subcategories: [mtb]
 	};
 	const race = {
 		id: 'racingbike',
 		label: 'Rennrad',
+		description: 'Rennradfahren auf der gewöhnlich schnellsten Route',
+		color: 'gray',
+		borderColor: 'purple',
 		subcategories: []
 	};
 
