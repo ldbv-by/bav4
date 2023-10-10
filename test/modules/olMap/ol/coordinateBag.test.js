9f11b564baf9adb75a8e6bf8dec7ccb5b0fb2792
thiloSchlemmer <thiloSchlemmer@gmail.com>
2023-10-10 11:38:40 +0200

    update test

--- test/modules/olMap/ol/coordinateBag.test.js
+++ test/modules/olMap/ol/coordinateBag.test.js
@@ -2,11 +2,21 @@ import { CoordinateBag } from '../../../../src/modules/olMap/ol/geodesic/coordin
 
 describe('CoordinateBag', () => {
 	describe('constructor', () => {
-		it('initializes an instance with correct parameters', () => {
+		it('initializes an instance', () => {
 			const instance = new CoordinateBag();
 
 			expect(instance).toBeInstanceOf(CoordinateBag);
 			expect(instance.lastCoordinate).toBeNull();
+			expect(instance.lineStringIndex).toBe(0);
+			expect(instance.lineStrings).toEqual([[]]);
+
+			expect(instance.subSegments).toEqual([[]]);
+			expect(instance.subSegmentsExtents).toEqual([[]]);
+			expect(instance.segmentIndex).toBe(-1);
+			expect(instance.segmentIndices).toEqual([]);
+
+			expect(instance.polygons).toEqual({});
+			expect(instance.worldIndex).toBe(0);
 		});
 	});
 });
