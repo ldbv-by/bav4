88cb1c6e16bd4cc12896fecb7222820b38fea89c
thiloSchlemmer <thiloSchlemmer@gmail.com>
2023-10-10 13:20:20 +0200

    clean code

--- src/modules/olMap/ol/geodesic/coordinateBag.js
+++ src/modules/olMap/ol/geodesic/coordinateBag.js
@@ -23,20 +23,19 @@ const Dateline_Buffer = 40;
  *
  */
 export class CoordinateBag {
-	constructor() {
-		this.lastCoordinate = null;
+	#lastCoordinate = null;
+	#lineStringIndex = 0;
+	#lineStrings = [[]];
 
-		this.lineStringIndex = 0;
-		this.lineStrings = [[]];
+	#subsegments = [[]];
+	#subsegmentExtents = [[]];
+	#segmentIndex = -1;
+	#segmentIndices = [];
 
-		this.subsegments = [[]];
-		this.subsegmentExtents = [[]];
-		this.segmentIndex = -1;
-		this.segmentIndices = [];
+	#polygons = {};
+	#worldIndex = 0;
 
-		this.polygons = {};
-		this.worldIndex = 0;
-	}
+	constructor() {}
 
 	/**
 	 * adds a new coordinate to the bag
@@ -46,49 +45,49 @@ export class CoordinateBag {
 	 */
 	add(coordinate, newSegment = false) {
 		if (newSegment) {
-			this.segmentIndex++;
+			this.#segmentIndex++;
 		}
-		if (this.lastCoordinate && 180 - Math.abs(this.lastCoordinate[0]) < Dateline_Buffer) {
-			if (coordinate[0] < 0 && this.lastCoordinate[0] > 0) {
+		if (this.#lastCoordinate && 180 - Math.abs(this.#lastCoordinate[0]) < Dateline_Buffer) {
+			if (coordinate[0] < 0 && this.#lastCoordinate[0] > 0) {
 				this._push(coordinate, Right_World);
-				this.lineStrings[++this.lineStringIndex] = [];
-			} else if (coordinate[0] > 0 && this.lastCoordinate[0] < 0) {
+				this.#lineStrings[++this.#lineStringIndex] = [];
+			} else if (coordinate[0] > 0 && this.#lastCoordinate[0] < 0) {
 				this._push(coordinate, Left_World);
-				this.lineStrings[++this.lineStringIndex] = [];
+				this.#lineStrings[++this.#lineStringIndex] = [];
 			}
 		}
 		this._push(coordinate);
-		this.lastCoordinate = coordinate;
+		this.#lastCoordinate = coordinate;
 	}
 
 	_push(coordinate, offset = 0) {
 		const coord = [coordinate[0] + offset * 360, coordinate[1]];
-		const polygonId = 'polygon_' + this.worldIndex;
-		this.worldIndex += offset;
+		const polygonId = 'polygon_' + this.#worldIndex;
+		this.#worldIndex += offset;
 		//Push to lineString (border of the shape)
-		this.lineStrings[this.lineStringIndex].push(coord);
+		this.#lineStrings[this.#lineStringIndex].push(coord);
 		//Push to polygons (To color the area of the shape)
-		if (this.polygons[polygonId] == null) {
-			this.polygons[polygonId] = [];
+		if (this.#polygons[polygonId] == null) {
+			this.#polygons[polygonId] = [];
 		}
-		this.polygons[polygonId].push(coord);
+		this.#polygons[polygonId].push(coord);
 		/* Push to subsegments (Used to calculate distances form mouse cursor to shape and to
         calculate the extent of each segment and subsegment) */
-		if (this.segmentIndex >= 0 && this.lineStrings[this.lineStringIndex].length > 1) {
+		if (this.#segmentIndex >= 0 && this.#lineStrings[this.#lineStringIndex].length > 1) {
 			const lastCoord = [...this.lastCoordinate];
 			const subsegment = [proj4(Epsg_Wgs84, Epsg_WebMercartor, lastCoord), proj4(Epsg_Wgs84, Epsg_WebMercartor, coord)];
 			subsegment[1][0] += offset * Deg360_In_WebMercartor;
 			const subsegmentExtent = boundingExtent(subsegment);
-			if (!this.subsegments[this.segmentIndex]) {
-				if (this.segmentIndex > 0) {
-					this.subsegments[this.segmentIndex - 1].push(subsegment);
-					this.subsegmentExtents[this.segmentIndex - 1].push(subsegmentExtent);
+			if (!this.#subsegments[this.#segmentIndex]) {
+				if (this.#segmentIndex > 0) {
+					this.#subsegments[this.#segmentIndex - 1].push(subsegment);
+					this.#subsegmentExtents[this.#segmentIndex - 1].push(subsegmentExtent);
 				}
-				this.subsegments[this.segmentIndex] = [];
-				this.subsegmentExtents[this.segmentIndex] = [];
+				this.#subsegments[this.#segmentIndex] = [];
+				this.#subsegmentExtents[this.#segmentIndex] = [];
 			} else {
-				this.subsegments[this.segmentIndex].push(subsegment);
-				this.subsegmentExtents[this.segmentIndex].push(subsegmentExtent);
+				this.#subsegments[this.#segmentIndex].push(subsegment);
+				this.#subsegmentExtents[this.#segmentIndex].push(subsegmentExtent);
 			}
 		}
 	}
@@ -98,10 +97,10 @@ export class CoordinateBag {
 	 * @returns  {MultiLineString | null}
 	 */
 	createGeometry() {
-		if (this.lineStrings[this.lineStringIndex].length <= 1) {
-			this.lineStrings.pop();
+		if (this.#lineStrings[this.#lineStringIndex].length <= 1) {
+			this.#lineStrings.pop();
 		}
-		return new MultiLineString(this.lineStrings).transform(Epsg_Wgs84, Epsg_WebMercartor);
+		return new MultiLineString(this.#lineStrings).transform(Epsg_Wgs84, Epsg_WebMercartor);
 	}
 
 	/**
@@ -111,24 +110,24 @@ export class CoordinateBag {
 	createPolygon(geodesicGeometry) {
 		if (
 			!geodesicGeometry.isPolygon ||
-			(geodesicGeometry.isDrawing && this.lineStringIndex === 1) ||
-			(this.lineStringIndex > 1 && this.worldIndex !== 0) ||
-			this.lineStringIndex > 2
+			(geodesicGeometry.isDrawing && this.#lineStringIndex === 1) ||
+			(this.#lineStringIndex > 1 && this.#worldIndex !== 0) ||
+			this.#lineStringIndex > 2
 		) {
 			/* If polygon should not be filled OR
             the chance for the algorithm to not color the polygon correctly is too high.*/
 			return null;
 		}
 		return new MultiPolygon(
-			Object.values(this.polygons).map((coordinates) => {
+			Object.values(this.#polygons).map((coordinates) => {
 				const first = coordinates[0];
-				if (this.lineStringIndex === 1) {
+				if (this.#lineStringIndex === 1) {
 					/* The polygon goes through north or south pol. The coloring will be correct as
                     long as the shape is not too complex (no overlapping) */
 					if (coordinates.length <= 1) return [coordinates];
 					const last = coordinates[coordinates.length - 1];
 					// Drawing at 90deg breaks things, thats why we stop at 89
-					const lat = geographicMath.copysign(89, this.worldIndex * geodesicGeometry.totalArea);
+					const lat = geographicMath.copysign(89, this.#worldIndex * geodesicGeometry.area);
 					coordinates.push([last[0], lat], [first[0], lat]);
 				}
 				coordinates.push(first);
--- test/modules/olMap/ol/coordinateBag.test.js
+++ test/modules/olMap/ol/coordinateBag.test.js
@@ -6,17 +6,6 @@ describe('CoordinateBag', () => {
 			const instance = new CoordinateBag();
 
 			expect(instance).toBeInstanceOf(CoordinateBag);
-			expect(instance.lastCoordinate).toBeNull();
-			expect(instance.lineStringIndex).toBe(0);
-			expect(instance.lineStrings).toEqual([[]]);
-
-			expect(instance.subsegments).toEqual([[]]);
-			expect(instance.subsegments).toEqual([[]]);
-			expect(instance.segmentIndex).toBe(-1);
-			expect(instance.segmentIndices).toEqual([]);
-
-			expect(instance.polygons).toEqual({});
-			expect(instance.worldIndex).toBe(0);
 		});
 	});
 });
