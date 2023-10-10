f1dac6edbd81b6fd26714fa38b03e3784790307e
Thomas Aulinger <taulinger@gmx.de>
2023-10-10 14:25:09 +0200

    extend status codes

--- test/domain/routing.test.js
+++ test/domain/routing.test.js
@@ -2,11 +2,13 @@ import { RoutingStatusCodes } from '../../src/domain/routing';
 
 describe('RoutingStatusCodes', () => {
 	it('provides an enum of all valid status codes', () => {
-		expect(Object.keys(RoutingStatusCodes).length).toBe(4);
+		expect(Object.keys(RoutingStatusCodes).length).toBe(6);
 
 		expect(RoutingStatusCodes.Ok).toBe(200);
 		expect(RoutingStatusCodes.Http_Backend_400).toBe(400);
 		expect(RoutingStatusCodes.Http_Backend_500).toBe(500);
 		expect(RoutingStatusCodes.Start_Destination_Missing).toBe(900);
+		expect(RoutingStatusCodes.Destination_Missing).toBe(901);
+		expect(RoutingStatusCodes.Start_Missing).toBe(902);
 	});
 });
