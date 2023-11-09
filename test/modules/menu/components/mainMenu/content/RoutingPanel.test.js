b7e17a695bbc1330255b3a77b06fee5497b167dc
thiloSchlemmer <thilo.schlemmer@ldbv.bayern.de>
2023-11-09 16:00:30 +0100

    fix test

--- test/modules/menu/components/mainMenu/content/RoutingPanel.test.js
+++ test/modules/menu/components/mainMenu/content/RoutingPanel.test.js
@@ -13,7 +13,7 @@ window.customElements.define(RoutingPanel.tag, RoutingPanel);
 
 describe('RoutingPanel', () => {
 	let store;
-	const setup = (state, properties = {}) => {
+	const setup = (state) => {
 		const initialState = {
 			mainMenu: {
 				open: true,
@@ -31,7 +31,7 @@ describe('RoutingPanel', () => {
 			routing: routingReducer
 		});
 		$injector.registerSingleton('TranslationService', { translate: (key) => key });
-		return TestUtils.render(RoutingPanel.tag, properties);
+		return TestUtils.render(RoutingPanel.tag);
 	};
 
 	describe('class', () => {
@@ -59,28 +59,29 @@ describe('RoutingPanel', () => {
 			expect(element.shadowRoot.querySelectorAll('ba-routing-container')).toHaveSize(0);
 			expect(element.shadowRoot.querySelectorAll('.demo')).toHaveSize(1);
 		});
+		describe('when component is activated as AbstractMvuContentPanel', () => {
+			it('renders the routing components', async () => {
+				const element = await setup();
+				element.setActive(true);
+				const container = element.shadowRoot.querySelectorAll('.container');
 
-		it('renders the routing components', async () => {
-			const element = await setup({}, { active: true });
+				expect(container).toHaveSize(1);
 
-			const container = element.shadowRoot.querySelectorAll('.container');
+				// lazy loading component with the routing content
+				const lazyLoadElement = element.shadowRoot.querySelector('ba-lazy-load');
+				expect(lazyLoadElement).toBeTruthy();
+				expect(lazyLoadElement.content.strings[0].includes('<ba-routing-container')).toBeTrue();
+				expect(element.shadowRoot.querySelectorAll('ba-profile-chip')).toHaveSize(1);
 
-			expect(container).toHaveSize(1);
-
-			// lazy loading component with the routing content
-			const lazyLoadElement = element.shadowRoot.querySelector('ba-lazy-load');
-			expect(lazyLoadElement).toBeTruthy();
-			expect(lazyLoadElement.content.strings[0].includes('<ba-routing-container')).toBeTrue();
-			expect(element.shadowRoot.querySelectorAll('ba-profile-chip')).toHaveSize(1);
-
-			expect(element.shadowRoot.querySelectorAll('.demo')).toHaveSize(1);
+				expect(element.shadowRoot.querySelectorAll('.demo')).toHaveSize(1);
+			});
 		});
 	});
 
 	describe('when close icon is clicked', () => {
 		it('updates the store', async () => {
 			const warnSpy = spyOn(console, 'warn').and.callThrough();
-			const element = await setup({}, { active: true });
+			const element = await setup();
 
 			const closeIcon = element.shadowRoot.querySelector('ba-icon');
 
@@ -95,14 +96,14 @@ describe('RoutingPanel', () => {
 
 	describe('when demo content needed', () => {
 		it('renders demo content', async () => {
-			const element = await setup({}, { active: true });
+			const element = await setup({});
 			expect(isTemplateResult(element._getDemoContent())).toBeTrue();
 		});
 	});
 
 	describe('when demo buttons pressed', () => {
 		it('loads demo content', async () => {
-			const element = await setup({}, { active: true });
+			const element = await setup();
 
 			const button1 = element.shadowRoot.querySelector('#button1');
 			const button2 = element.shadowRoot.querySelector('#button2');
