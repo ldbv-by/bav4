08696abdf99e8058754bd1ec2cda668f8916805c
Thomas Aulinger <taulinger@gmx.de>
2023-11-20 18:23:43 +0100

    add deep clone util function

---
+++ src/utils/clone.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * Deep clones an object or an array by converting to a JSON string. Does not work for arrays or objects containing a function or Symbol properties.
+ * @param {Object|Array} value
+ * @returns deep clones object or array
+ * @throws SyntaxError when the value could not be stringified as JSON
+ */
+export const deepClone = (value) => {
+	return JSON.parse(JSON.stringify(value));
+};
