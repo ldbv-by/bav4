e267c3710c3f5803ff5bcd8059507cb44147d7c8
Thomas Aulinger <taulinger@gmx.de>
2023-11-28 17:45:39 +0100

    add additional coordinate types

--- src/domain/coordinateTypeDef.js
+++ src/domain/coordinateTypeDef.js
@@ -2,6 +2,16 @@
  * @module domain/coordinateTypeDef
  */
 /**
- * An array of numbers representing an xy coordinate. Ordering is [easting, northing] or [lon, lat]. Example: `[16, 48]`.
+ * An array of numbers representing an XY coordinate. Ordering is [easting, northing] or [lon, lat]. Example: `[16, 48]`.
  * @typedef {Array<number>} Coordinate
+ */
+
+/**
+ * An array of numbers or other types. The first two entries must be a number and represent an xy coordinate. Ordering is [easting, northing] or [lon, lat]. Example: `[16, 48]`.
+ * @typedef {Array<number|object>} CoordinateLike
+ */
+
+/**
+ * An array of numbers representing an XYZ coordinate. Ordering is [easting, northing, z] or [lon, lat, z]. Example: `[16, 48, 1000]`.
+ * @typedef {Array<number>} CoordinateXYZ
  */
