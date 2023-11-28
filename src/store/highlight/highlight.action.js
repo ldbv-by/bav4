12a375380bbbb0565fb6d5ac9851bff5389e296b
Thomas Aulinger <taulinger@gmx.de>
2023-11-28 11:27:39 +0100

    update jsdoc

--- src/store/highlight/highlight.action.js
+++ src/store/highlight/highlight.action.js
@@ -31,11 +31,29 @@ import { $injector } from '../../injection';
  * @enum {Number}
  */
 export const HighlightFeatureType = Object.freeze({
+	/**
+	 * Marker (location pin)
+	 */
 	MARKER: 0,
+	/**
+	 * Marker (location pin) expressing a temporary meaning
+	 */
 	MARKER_TMP: 1,
+	/**
+	 * Indicates that a query is running (e.g. a feature info request)
+	 */
 	QUERY_RUNNING: 2,
+	/**
+	 * Indicates that a query was successful
+	 */
 	QUERY_SUCCESS: 3,
+	/**
+	 * Highlights a coordinate or a feature
+	 */
 	DEFAULT: 4,
+	/**
+	 * Highlights a coordinate or a feature expressing a temporary meaning
+	 */
 	DEFAULT_TMP: 5
 });
 
