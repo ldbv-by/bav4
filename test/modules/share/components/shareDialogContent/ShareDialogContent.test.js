09e7f9a9aef94836e0bf5f33a565994c2262c275
Schlemmer, Thilo (LDBV) <thilo.schlemmer@ldbv.bayern.de>
2023-11-29 14:16:27 +0100

    replace toggle with switch in ShareDialogContent

--- test/modules/share/components/shareDialogContent/ShareDialogContent.test.js
+++ test/modules/share/components/shareDialogContent/ShareDialogContent.test.js
@@ -4,9 +4,10 @@ import { $injector } from '../../../../../src/injection';
 import { notificationReducer } from '../../../../../src/store/notifications/notifications.reducer';
 import { LevelTypes } from '../../../../../src/store/notifications/notifications.action';
 import { Toggle } from '../../../../../src/modules/commons/components/toggle/Toggle';
+import { Switch } from '../../../../../src/modules/commons/components/switch/Switch';
 
 window.customElements.define(ShareDialogContent.tag, ShareDialogContent);
-window.customElements.define(Toggle.tag, Toggle);
+window.customElements.define(Switch.tag, Switch);
 
 describe('ShareDialogContent', () => {
 	let store;
@@ -60,7 +61,7 @@ describe('ShareDialogContent', () => {
 			const element = await setup();
 			element.urls = shareUrls;
 
-			expect(element.shadowRoot.querySelectorAll('ba-toggle')).toHaveSize(1);
+			expect(element.shadowRoot.querySelectorAll('ba-switch')).toHaveSize(1);
 			expect(element.shadowRoot.querySelectorAll('input')).toHaveSize(1);
 			expect(element.shadowRoot.querySelectorAll('ba-icon')).toHaveSize(1);
 		});
@@ -86,7 +87,7 @@ describe('ShareDialogContent', () => {
 			const element = await setup({}, { share: true });
 			element.urls = shareUrls;
 
-			const toggleElement = element.shadowRoot.querySelector('ba-toggle');
+			const toggleElement = element.shadowRoot.querySelector('ba-switch');
 
 			expect(toggleElement.checked).toBe(false);
 		});
@@ -97,7 +98,7 @@ describe('ShareDialogContent', () => {
 			const element = await setup();
 			element.urls = shareUrls;
 
-			const toggleElement = element.shadowRoot.querySelector('ba-toggle');
+			const toggleElement = element.shadowRoot.querySelector('#toggle');
 
 			// toggle default value is not checked => false => url = fieldId
 			expect(element.shadowRoot.querySelector('.share_url').value).toBe(shareUrls.fileId);
@@ -131,7 +132,7 @@ describe('ShareDialogContent', () => {
 			const element = await setup();
 			element.urls = shareUrls;
 
-			const toggleElement = element.shadowRoot.querySelector('ba-toggle');
+			const toggleElement = element.shadowRoot.querySelector('ba-switch');
 			toggleElement.click();
 
 			const copyButton = element.shadowRoot.querySelector('.share_item .share_copy_icon');
@@ -226,7 +227,7 @@ describe('ShareDialogContent', () => {
 			const url = 'https://v.bayern.de/foobar';
 			element.urls = url;
 
-			expect(element.shadowRoot.querySelectorAll('ba-toggle')).toHaveSize(0);
+			expect(element.shadowRoot.querySelectorAll('ba-switch')).toHaveSize(0);
 			expect(element.shadowRoot.querySelectorAll('input')).toHaveSize(1);
 			expect(element.shadowRoot.querySelectorAll('input')[0].value).toBe(url);
 			expect(element.shadowRoot.querySelectorAll('ba-icon')).toHaveSize(1);
