953e373f2ae8cb00e1bc7f92f2399cba427e974d
Schlemmer, Thilo (LDBV) <2117317+thiloSchlemmer@users.noreply.github.com>
2024-01-10 15:58:48 +0100

    update IconResult, remove mocked routingIcons

--- src/services/IconService.js
+++ src/services/IconService.js
@@ -4,7 +4,6 @@
 import { loadBvvIcons } from './provider/icons.provider';
 import { getBvvIconColor } from './provider/iconColor.provider';
 import { getBvvIconUrlFactory } from './provider/iconUrl.provider';
-import { $injector } from '../injection/index';
 
 const Svg_Encoding_B64_Flag = 'data:image/svg+xml;base64,';
 const Svg_Marker_Name = 'marker';
@@ -69,7 +68,6 @@ export class IconService {
 	 *  @returns {Promise<Array<IconResult>>}
 	 */
 	async _load() {
-		const mockedRoutingIcons = loadRoutingIcons();
 		try {
 			const isMarkerIcon = (iconResult) => iconResult.id === Svg_Marker_Name;
 			const providerIcons = await this._iconProvider();
@@ -77,10 +75,10 @@ export class IconService {
 
 			this._icons =
 				indexOfMarkerIcon < 0
-					? [this._default, ...providerIcons, ...mockedRoutingIcons]
-					: [providerIcons[indexOfMarkerIcon], ...providerIcons.filter((i) => !isMarkerIcon(i)), ...mockedRoutingIcons];
+					? [this._default, ...providerIcons]
+					: [providerIcons[indexOfMarkerIcon], ...providerIcons.filter((i) => !isMarkerIcon(i))];
 		} catch (e) {
-			this._icons = [this._default, ...loadFallbackIcons(), ...mockedRoutingIcons];
+			this._icons = [this._default, ...loadFallbackIcons()];
 			console.warn('Icons could not be fetched from backend. Using fallback icons ...');
 		}
 		return this._icons;
@@ -141,9 +139,10 @@ export class IconResult {
 	 * @param {function(string):(boolean)} urlMatcher a function to check, when a url is matching as remote-location for this icon
 	 * @param {function(Array<number>): (string)} urlProvider a function, which provides a url as remote-location for this icon with a specified rgb-color as Array<number>
 	 */
-	constructor(id, svg, urlMatcher = null, urlProvider = null) {
+	constructor(id, svg, urlMatcher = null, urlProvider = null, isMonochrome = true) {
 		this._id = id;
 		this._svg = svg;
+		this._isMonochrome = isMonochrome;
 		this._base64 = this._toBase64();
 		this._urlMatcher = urlMatcher ? urlMatcher : () => false;
 		this._urlProvider = urlProvider ? urlProvider : () => null;
@@ -186,6 +185,10 @@ export class IconResult {
 	get anchor() {
 		return this._id === Svg_Marker_Name ? [0.5, 1] : [0.5, 0.5];
 	}
+
+	get isMonochrome() {
+		return this._isMonochrome;
+	}
 }
 
 /**
@@ -208,46 +211,6 @@ const loadFallbackIcons = () => {
 		new IconResult(
 			'square',
 			'<svg id="square" xmlns="http://www.w3.org/2000/svg" width="34.0" height="34.0" viewBox="0.0 0.0 48.0 48.0" fill="rgb(255,255,255)"><!-- SIL OFL 1.1 --><path d="M9.0,7.0L39.015625,7.0Q39.875,7.0,40.4375,7.5625Q41.0,8.125,41.0,8.984375L41.0,39.0Q41.0,39.875,40.4375,40.4375Q39.875,41.0,39.015625,41.0L9.0,41.0Q8.125,41.0,7.5625,40.4375Q7.0,39.875,7.0,39.0L7.0,8.984375Q7.0,8.125,7.5625,7.5625Q8.125,7.0,9.0,7.0Z" /></svg>'
-		)
-	];
-};
-
-/**
- * Temporary function to mock backend implementation supplying IconResults for routing icons
- *
- * The backend should provide a icon result object with {id:string,svg:string,staticIcon:string} for routing icons.
- * This objects will be transformed to IconResult like the result of this mock-method.
- */
-const loadRoutingIcons = () => {
-	const { ConfigService: configService } = $injector.inject('HttpService', 'ConfigService');
-	const url = configService.getValueAsPath('BACKEND_URL') + 'icons';
-	const matcher = (id) => {
-		return (idOrUrl) => idOrUrl === id || !!idOrUrl?.endsWith(`/${id}.png`);
-	};
-
-	const urlFactoryFunction = (url, id) => {
-		return () => {
-			return `${url}/${id}.png`;
-		};
-	};
-	return [
-		new IconResult(
-			'rt_mocked_start',
-			'<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="rgba(9, 157, 218, 1)" viewBox="0 0 16 16"><path fill="#fff" d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/><path d="M8.028 14.624s5.203-4.931 5.203-8.671c0-6.937-10.406-6.937-10.406 0 0 3.741 5.203 8.671 5.203 8.671z" style="fill:#369dc9"/><ellipse cx="8.026" cy="5.976" rx="4.06" ry="3.981" style="opacity:.15500004;fill:#000;stroke-width:1.09297371;paint-order:stroke fill markers"/><path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" style="fill:#fff" transform="translate(4.483 1.786)scale(.50331)"/></svg>',
-			matcher('rt_mocked_start'),
-			urlFactoryFunction(url, 'rt_start')
-		),
-		new IconResult(
-			'rt_mocked_intermediate',
-			'<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="30" height="30" viewBox="0 0 308.621 308.621"><circle cx="154.311" cy="154.311" r="142.325" style="opacity:1;fill:#369dc9;fill-opacity:1;fill-rule:nonzero;stroke:#fff;stroke-width:23.97122383;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1"/><circle cx="154.311" cy="154.311" r="92.116" style="opacity:1;fill:#fff;fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:22.86945724;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1"/></svg>',
-			matcher('rt_mocked_intermediate'),
-			urlFactoryFunction(url, 'rt_intermediate')
-		),
-		new IconResult(
-			'rt_mocked_destination',
-			'<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="rgba(9, 157, 218, 1)" viewBox="0 0 16 16"><path fill="#fff" d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/><path d="M8.028 14.624s5.203-4.931 5.203-8.671c0-6.937-10.406-6.937-10.406 0 0 3.741 5.203 8.671 5.203 8.671z" style="fill:#369dc9"/><ellipse cx="8.026" cy="5.976" rx="4.06" ry="3.981" style="opacity:.15500004;fill:#000;stroke-width:1.09297371;paint-order:stroke fill markers"/><path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001" style="fill:#fff" transform="translate(5.228 3.1)scale(.35398)"/></svg>',
-			matcher('rt_mocked_destination'),
-			urlFactoryFunction(url, 'rt_destination')
 		)
 	];
 };
