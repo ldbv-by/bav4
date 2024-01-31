a8068ef72f81b9103bbe0b414b40c490a4b75509
alsturm <alexander_sturm@gmx.net>
2024-01-31 10:47:50 +0100

    add test

---
+++ test/plugins/NavigationRailPlugin.test.js
@@ -0,0 +1,85 @@
+import { NavigationRailPlugin } from '../../src/plugins/NavigationRailPlugin.js';
+import { TestUtils } from '../test-utils.js';
+import { navigationRailReducer } from '../../src/store/navigationRail/navigationRail.reducer';
+import { createNoInitialStateMainMenuReducer } from '../../src/store/mainMenu/mainMenu.reducer';
+import { createNoInitialStateMediaReducer } from '../../src/store/media/media.reducer';
+import { setTab } from '../../src/store/mainMenu/mainMenu.action';
+import { TabIds } from '../../src/domain/mainMenu';
+
+describe('NavigationRailPlugin', () => {
+	const setup = (state) => {
+		const initialState = {
+			navigationRail: {
+				open: false,
+				visitedTabIdsSet: new Set([])
+			},
+			media: {
+				portrait: false,
+				minWidth: false
+			},
+			mainMenu: {
+				open: true,
+				tab: null
+			},
+			...state
+		};
+
+		const store = TestUtils.setupStoreAndDi(initialState, {
+			navigationRail: navigationRailReducer,
+			mainMenu: createNoInitialStateMainMenuReducer(),
+			media: createNoInitialStateMediaReducer()
+		});
+		return store;
+	};
+
+	describe('when tabId changes', () => {
+		it('open FeatureInfoTab in landscape orientation', async () => {
+			const store = setup();
+			const instanceUnderTest = new NavigationRailPlugin();
+			await instanceUnderTest.register(store);
+			expect(store.getState().navigationRail.open).toBeFalse();
+
+			setTab(TabIds.FEATUREINFO);
+
+			expect(store.getState().navigationRail.open).toBeTrue();
+		});
+
+		it('open FeatureInfoTab in portrait orientation', async () => {
+			const state = {
+				media: {
+					portrait: true,
+					minWidth: true
+				}
+			};
+
+			const store = setup(state);
+			const instanceUnderTest = new NavigationRailPlugin();
+			await instanceUnderTest.register(store);
+
+			expect(store.getState().navigationRail.open).toBeFalse();
+
+			setTab(TabIds.FEATUREINFO);
+
+			expect(store.getState().navigationRail.open).toBeFalse();
+		});
+
+		it('open SEARCHTab in landscape orientation', async () => {
+			const state = {
+				media: {
+					portrait: true,
+					minWidth: true
+				}
+			};
+
+			const store = setup(state);
+			const instanceUnderTest = new NavigationRailPlugin();
+			await instanceUnderTest.register(store);
+
+			expect(store.getState().navigationRail.open).toBeFalse();
+
+			setTab(TabIds.SEARCH);
+
+			expect(store.getState().navigationRail.open).toBeFalse();
+		});
+	});
+});
