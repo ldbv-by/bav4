48ffab0c0d95aa326fc580a0a8b919b3cdd1f23c
Thomas Aulinger <taulinger@gmx.de>
2024-02-16 16:56:56 +0100

    update

--- test/i18n/global.provider.test.js
+++ test/i18n/global.provider.test.js
@@ -14,7 +14,7 @@ describe('global i18n', () => {
 		expect(map.global_import_url_failed).toBe('URL-Import failed');
 		expect(map.global_import_data_failed).toBe('Importing data failed');
 		expect(map.global_import_unsupported_sourceType).toBe('Source type could not be detected or is not supported');
-		expect(map.global_import_authenticationModal_title).toBe('Authentication required');
+		expect(map.global_import_authenticationModal_title).toBe('Authentication');
 		expect(map.global_locally_imported_dataset_copyright_label).toBe('Dataset and/or style provided by third party');
 		expect(map.global_share_unsupported_geoResource_warning).toBe("The following layers won't be shared:");
 		expect(map.global_privacy_policy_url).toBe('https://geoportal.bayern.de/geoportalbayern/seiten/datenschutz.html');
@@ -40,7 +40,7 @@ describe('global i18n', () => {
 		expect(map.global_import_url_failed).toBe('URL-Import schlug fehl');
 		expect(map.global_import_data_failed).toBe('Import der Daten schlug fehl');
 		expect(map.global_import_unsupported_sourceType).toBe('Daten-Typ konnte nicht erkannt werden oder wird nicht unterstützt');
-		expect(map.global_import_authenticationModal_title).toBe('Anmeldung erforderlich');
+		expect(map.global_import_authenticationModal_title).toBe('Anmeldung');
 		expect(map.global_locally_imported_dataset_copyright_label).toBe('Mit Darstellung durch den Anwender');
 		expect(map.global_share_unsupported_geoResource_warning).toBe('Folgende Ebenen werden nicht geteilt:');
 		expect(map.global_privacy_policy_url).toBe('https://geoportal.bayern.de/geoportalbayern/seiten/datenschutz.html');
