a14833ae248b90a074df1ef22426188759cd20bd
Schlemmer, Thilo (LDBV) <2117317+thiloSchlemmer@users.noreply.github.com>
2024-02-19 12:15:37 +0100

    typo

--- src/store/modal/modal.action.js
+++ src/store/modal/modal.action.js
@@ -18,7 +18,7 @@ const getStore = () => {
 
 /**
  * Opens the modal.
- * @param {string|TemplateResult} title The title of the modal. Could either be a a plain string or a lit-html `TemplateResult`.
+ * @param {string|TemplateResult} title The title of the modal. Could either be a plain string or a lit-html `TemplateResult`.
  * @param {string|TemplateResult} content  The content of the modal. Could either be a a plain string or a lit-html `TemplateResult`.
  * @param {module:store/modal/modal_action~ModalOptions} options Options for the modal
  * @function
