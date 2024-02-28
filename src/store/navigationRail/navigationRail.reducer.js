f08c28f0e7a5ddbb8e2334fbb482d47581a1e01f
alsturm <alexander_sturm@gmx.net>
2024-02-28 16:09:22 +0100

    add initialState

--- src/store/navigationRail/navigationRail.reducer.js
+++ src/store/navigationRail/navigationRail.reducer.js
@@ -1,5 +1,6 @@
 export const OPEN_CLOSED_CHANGED = 'navigationRail/open';
 export const ADD_TAB_ID = 'navigationRail/visitedTabIds';
+export const MEDIA_QUERY = '(orientation: landscape) and (max-width: 80em)';
 
 export const initialState = {
 	open: false,
@@ -23,4 +24,33 @@ export const navigationRailReducer = (state = initialState, action) => {
 		}
 	}
 	return state;
+};
+
+/**
+ * Provides a media reducer which has no initial state
+ * @param {object} initialState
+ * @returns media reducer
+ */
+export const createNoInitialStateNavigationRailReducer = () => {
+	return (state = null, action) => navigationRailReducer(state, action);
+};
+
+/**
+ * Provides a media reducer which initial state is beeing obtained from the window object.
+ * @param {Window} _window
+ * @returns media reducer
+ */
+export const createNavigationRailReducer = (_window = window) => {
+	const initialState = {
+		/**
+		 * @property {boolean}
+		 */
+		open: _window.matchMedia(MEDIA_QUERY).matches ? true : false,
+		/**
+		 * @property {number}
+		 */
+		visitedTabIds: []
+	};
+
+	return (state = initialState, action) => navigationRailReducer(state, action);
 };
--- test/store/navigationRail/navigationRail.reducer.test.js
+++ test/store/navigationRail/navigationRail.reducer.test.js
@@ -1,24 +1,58 @@
-import { navigationRailReducer } from '../../../src/store/navigationRail/navigationRail.reducer.js';
+import {
+	createNoInitialStateNavigationRailReducer,
+	createNavigationRailReducer,
+	initialState
+} from '../../../src/store/navigationRail/navigationRail.reducer.js';
 import { open, close, toggle, addTabId } from '../../../src/store/navigationRail/navigationRail.action';
 import { TestUtils } from '../../test-utils.js';
 import { TabIds } from '../../../src/domain/mainMenu';
 
 describe('navigationRailReducer', () => {
-	const setup = (state) => {
+	const windowMock = {
+		matchMedia() {}
+	};
+
+	const setup = (navigationRailReducer, state) => {
 		return TestUtils.setupStoreAndDi(state, {
 			navigationRail: navigationRailReducer
 		});
 	};
 
 	it('initializes the store with default values', () => {
-		const store = setup();
+		const store = setup(createNoInitialStateNavigationRailReducer(), { navigationRail: initialState });
 		expect(store.getState().navigationRail.open).toBeFalse();
 		expect(store.getState().navigationRail.visitedTabIds).toEqual([]);
 	});
 
+	describe('createNavigationRailReducer', () => {
+		describe('returns a reducer function', () => {
+			it("initiales the store by media query for ORIENTATION 'landscape' and 'max-width: 80em'", () => {
+				spyOn(windowMock, 'matchMedia').withArgs('(orientation: landscape) and (max-width: 80em)').and.returnValue(TestUtils.newMediaQueryList(true));
+				const store = setup(createNavigationRailReducer(windowMock));
+
+				expect(store.getState().navigationRail.open).toBeTrue();
+			});
+
+			it("initiales the store by media query for ORIENTATION 'landscape' and 'max-width: 80em'", () => {
+				spyOn(windowMock, 'matchMedia')
+					.withArgs('(orientation: landscape) and (max-width: 80em)')
+					.and.returnValue(TestUtils.newMediaQueryList(false));
+				const store = setup(createNavigationRailReducer(windowMock));
+
+				expect(store.getState().navigationRail.open).toBeFalse();
+			});
+
+			it('uses the real window as default argument', () => {
+				const store = setup(createNavigationRailReducer());
+
+				expect(store.getState().navigationRail.open).toMatch(/true|false/);
+			});
+		});
+	});
+
 	describe("changes the 'open' property", () => {
 		it('sets true', () => {
-			const store = setup();
+			const store = setup(createNoInitialStateNavigationRailReducer());
 
 			open();
 
@@ -26,7 +60,7 @@ describe('navigationRailReducer', () => {
 		});
 
 		it('sets false', () => {
-			const store = setup({ navigationRail: { open: true } });
+			const store = setup(createNoInitialStateNavigationRailReducer(), { navigationRail: { open: true } });
 
 			expect(store.getState().navigationRail.open).toBeTrue();
 
@@ -36,7 +70,7 @@ describe('navigationRailReducer', () => {
 		});
 
 		it('toggles current value', () => {
-			const store = setup({ navigationRail: { open: true } });
+			const store = setup(createNoInitialStateNavigationRailReducer(), { navigationRail: { open: true } });
 
 			expect(store.getState().navigationRail.open).toBeTrue();
 
@@ -52,7 +86,7 @@ describe('navigationRailReducer', () => {
 
 	describe("changes the 'visitedTabIds' property", () => {
 		it('sets a TabId', () => {
-			const store = setup();
+			const store = setup(createNoInitialStateNavigationRailReducer(), { navigationRail: initialState });
 
 			addTabId(TabIds.ROUTING);
 
@@ -60,7 +94,7 @@ describe('navigationRailReducer', () => {
 		});
 
 		it('sets redundant TabIds', () => {
-			const store = setup();
+			const store = setup(createNoInitialStateNavigationRailReducer(), { navigationRail: initialState });
 
 			addTabId(TabIds.FEATUREINFO);
 			addTabId(TabIds.ROUTING);
