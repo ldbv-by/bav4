687fcabaf71b3d37ca8190fa2febfba9a6716f23
Thomas Aulinger <taulinger@gmx.de>
2024-03-12 15:50:25 +0100

    adjust message

--- test/i18n/global.provider.test.js
+++ test/i18n/global.provider.test.js
@@ -22,8 +22,8 @@ describe('global i18n', () => {
 		expect(map.global_marker_symbol_label).toBe('Marker');
 		expect(map.global_featureInfo_not_available).toBe('FeatureInfo is not available');
 		expect(map.global_routingService_init_exception).toBe('Routing currently not available');
-		expect(map.global_geoResource_not_available(['id'])).toBe('Failed to add a layer for the GeoResource with ID "id"');
-		expect(map.global_geoResource_not_available(['id', 'Reason...'])).toBe('Failed to add a layer for the GeoResource with ID "id" (Reason...)');
+		expect(map.global_geoResource_not_available(['id'])).toBe('Failed to add a layer for the GeoResource "id"');
+		expect(map.global_geoResource_not_available(['id', 'Reason...'])).toBe('Failed to add a layer for the GeoResource "id" (Reason...)');
 		expect(map.global_geoResource_unauthorized).toBe('401 - Unauthorized');
 		expect(map.global_geoResource_forbidden).toBe('403 - Forbidden');
 	});
@@ -49,9 +49,9 @@ describe('global i18n', () => {
 		expect(map.global_marker_symbol_label).toBe('Markierung');
 		expect(map.global_featureInfo_not_available).toBe('FeatureInfo ist nicht verfügbar');
 		expect(map.global_routingService_init_exception).toBe('Die Routing-Funktion steht derzeit leider nicht zur Verfügung');
-		expect(map.global_geoResource_not_available(['id'])).toBe('Es konnte keine Ebene für die GeoRessource mit der ID "id" geladen werden');
+		expect(map.global_geoResource_not_available(['id'])).toBe('Es konnte keine Ebene für die GeoRessource "id" geladen werden');
 		expect(map.global_geoResource_not_available(['id', 'Grund...'])).toBe(
-			'Es konnte keine Ebene für die GeoRessource mit der ID "id" geladen werden (Grund...)'
+			'Es konnte keine Ebene für die GeoRessource "id" geladen werden (Grund...)'
 		);
 		expect(map.global_geoResource_unauthorized).toBe('401 - Fehlende Berechtigung');
 		expect(map.global_geoResource_forbidden).toBe('403 - Zugriff nicht erlaubt');
