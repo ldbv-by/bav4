fd77ea67dd407808d723ec201bc4c7ba3ba2e771
Thomas Aulinger <taulinger@gmx.de>
2024-03-14 17:09:16 +0100

    throttle generic notification

--- src/plugins/GlobalErrorPlugin.js
+++ src/plugins/GlobalErrorPlugin.js
@@ -5,27 +5,38 @@ import { BaPlugin } from './BaPlugin';
 import { UnavailableGeoResourceError } from '../domain/errors';
 import { LevelTypes, emitNotification } from '../store/notifications/notifications.action';
 import { $injector } from '../injection/index';
+import { throttled } from '../utils/timer';
 
 /**
- * This plugin catches and handles all errors of type {@link BaRuntimeError}.
+ * This plugin catches exceptions of type `Error` and {@link BaRuntimeError} and displays a notification.
  *
  * @class
  * @author taulinger
  */
 export class GlobalErrorPlugin extends BaPlugin {
-	/**
-	 * @override
-	 */
-	async register() {
+	#translationService;
+	#geoResourceService;
+	#errorListener;
+	#unhandledrejectionListener;
+
+	constructor() {
+		super();
 		const { TranslationService: translationService, GeoResourceService: geoResourceService } = $injector.inject(
 			'TranslationService',
 			'GeoResourceService'
 		);
-		const translate = (key, params = []) => translationService.translate(key, params);
+		this.#translationService = translationService;
+		this.#geoResourceService = geoResourceService;
+	}
+	/**
+	 * @override
+	 */
+	async register() {
+		const translate = (key, params = []) => this.#translationService.translate(key, params);
 
 		const handleError = (error) => {
 			if (error instanceof UnavailableGeoResourceError) {
-				const geoResourceName = geoResourceService.byId(error.geoResourceId)?.label ?? error.geoResourceId;
+				const geoResourceName = this.#geoResourceService.byId(error.geoResourceId)?.label ?? error.geoResourceId;
 				switch (error.httpStatus) {
 					case 401:
 						emitNotification(
@@ -44,16 +55,37 @@ export class GlobalErrorPlugin extends BaPlugin {
 						break;
 				}
 			} else {
-				emitNotification(`${translate('global_generic_exception')}`, LevelTypes.ERROR);
+				this._emitThrottledGenericNotification();
 			}
 		};
 
-		window.addEventListener('error', (event) => {
+		this.#errorListener = (event) => {
 			handleError(event.error);
-		});
+		};
 
-		window.addEventListener('unhandledrejection', (event) => {
+		this.#unhandledrejectionListener = (event) => {
 			handleError(event.reason);
-		});
+		};
+		window.addEventListener('error', this.#errorListener);
+		window.addEventListener('unhandledrejection', this.#unhandledrejectionListener);
+	}
+
+	_emitThrottledGenericNotification() {
+		const translate = (key, params = []) => this.#translationService.translate(key, params);
+		emitNotificationThrottled(() => emitNotification(`${translate('global_generic_exception')}`, LevelTypes.ERROR));
+	}
+
+	/**
+	 * Mainly for test purposes.
+	 */
+	_unregisterListeners() {
+		window.removeEventListener('error', this.#errorListener);
+		window.removeEventListener('unhandledrejection', this.#unhandledrejectionListener);
+	}
+
+	static get THROTTLE_NOTIFICATION_DELAY_MS() {
+		return 3000;
 	}
 }
+
+const emitNotificationThrottled = throttled(GlobalErrorPlugin.THROTTLE_NOTIFICATION_DELAY_MS, (action) => action());
