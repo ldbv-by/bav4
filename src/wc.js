3f370acbdb9803bc866899d73abcf246810fa970
Thomas Aulinger <taulinger@gmx.de>
2024-04-08 14:59:32 +0200

    trigger WcEvents.LOAD event

--- src/wc.js
+++ src/wc.js
@@ -1,4 +1,5 @@
 // register required modules (same as for iframe)
+import { WcEvents } from './domain/wcEvents';
 import './embed';
 
 import { PublicComponent } from './modules/public/components/PublicComponent';
@@ -6,3 +7,4 @@ import { PublicComponent } from './modules/public/components/PublicComponent';
 if (!customElements.get(PublicComponent.tag)) {
 	window.customElements.define(PublicComponent.tag, PublicComponent);
 }
+window.dispatchEvent(new CustomEvent(WcEvents.LOAD));
