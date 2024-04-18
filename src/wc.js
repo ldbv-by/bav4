30cbbf2988f5e64877e0b022cd01df7f08de5fb9
Thomas Aulinger <taulinger@gmx.de>
2024-04-18 14:36:18 +0200

    load also the config.js

--- src/wc.js
+++ src/wc.js
@@ -1,10 +1,17 @@
-// register required modules (same as for iframe)
 import { WcEvents } from './domain/wcEvents';
+// register required modules (same as for iframe)
 import './embed';
 
 import { PublicComponent } from './modules/public/components/PublicComponent';
 
-if (!customElements.get(PublicComponent.tag)) {
-	window.customElements.define(PublicComponent.tag, PublicComponent);
-}
-window.dispatchEvent(new CustomEvent(WcEvents.LOAD));
+// load the configuration script
+const scriptEl = document.createElement('script');
+scriptEl.setAttribute('src', '../config.js');
+document.body.appendChild(scriptEl);
+
+scriptEl.addEventListener('load', () => {
+	if (!customElements.get(PublicComponent.tag)) {
+		window.customElements.define(PublicComponent.tag, PublicComponent);
+	}
+	window.dispatchEvent(new CustomEvent(WcEvents.LOAD));
+});
