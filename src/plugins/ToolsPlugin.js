5acfc6ae8d9a00ad2bdb2c20400082a015966268
Thomas Aulinger <taulinger@gmx.de>
2024-05-06 13:40:05 +0200

    handle WC attribute changes

--- src/plugins/ToolsPlugin.js
+++ src/plugins/ToolsPlugin.js
@@ -6,10 +6,13 @@ import { setCurrentTool } from '../store/tools/tools.action';
 import { QueryParameters } from '../domain/queryParameters';
 import { $injector } from '../injection';
 import { EmbedTools, Tools } from '../domain/tools';
+import { observe } from '../utils/storeUtils';
 
 /**
  * This plugin checks for the presence of the query parameter `TOOL_ID` (see {@link QueryParameters})
  * and activates the corresponding tool.
+ *
+ * If the app is embedded a a web component, it observes the `TOOL_ID` attribute.
  *
  * @class
  * @author taulinger
@@ -18,7 +21,7 @@ export class ToolsPlugin extends BaPlugin {
 	/**
 	 * @override
 	 */
-	async register() {
+	async register(store) {
 		const { EnvironmentService: environmentService } = $injector.inject('EnvironmentService');
 
 		// check if we have a query parameter defining the tab id
@@ -28,6 +31,22 @@ export class ToolsPlugin extends BaPlugin {
 			/**in embed mode we check the list of allowed tools*/ (!environmentService.isEmbedded() || EmbedTools.includes(toolId))
 		) {
 			setCurrentTool(toolId);
+		}
+
+		if (environmentService.isEmbeddedAsWC()) {
+			// handle WC attribute changes
+			observe(
+				store,
+				(state) => state.wcAttribute.changed,
+				() => {
+					const toolId = environmentService.getQueryParams().get(QueryParameters.TOOL_ID) ?? null;
+					if (!toolId) {
+						setCurrentTool(null);
+					} else if (EmbedTools.includes(toolId)) {
+						setCurrentTool(toolId);
+					}
+				}
+			);
 		}
 	}
 }
