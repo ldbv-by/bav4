acde4c44f57148b4823562f5db2a2e8054f33588
alsturm <alexander_sturm@gmx.net>
2024-05-27 11:36:45 +0200

    fix undo state

--- src/modules/iframe/components/tools/DrawTool.js
+++ src/modules/iframe/components/tools/DrawTool.js
@@ -228,7 +228,7 @@ export class DrawTool extends MvuElement {
 		const activeTool = this._getActiveTool(model);
 		const activeToolName = activeTool ? activeTool.name : 'noTool';
 		const removeAllowed = ['draw', 'modify'].includes(mode);
-		const unfinishedLine = mode === 'draw' && activeToolName === 'line' && validGeometry;
+		const unfinishedLine = mode === 'draw' && (activeToolName === 'polygon' || activeToolName === 'line') && validGeometry;
 
 		const getActiveIconTypes = () => {
 			const iconTypes = {
