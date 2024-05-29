f9fc64094f27ff1659a8019714ac50adfe2c7647
alsturm <alexander_sturm@gmx.net>
2024-05-29 11:14:27 +0200

    fix test

--- test/modules/search/i18n/search.provider.test.js
+++ test/modules/search/i18n/search.provider.test.js
@@ -28,7 +28,7 @@ describe('i18n for search module', () => {
 	});
 
 	it('have the expected amount of translations', () => {
-		const expectedSize = 5;
+		const expectedSize = 8;
 		const deMap = provide('de');
 		const enMap = provide('en');
 
