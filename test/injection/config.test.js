ef36b2d6505363bea1e8bed005f828c93338aa7b
Thomas Aulinger <taulinger@gmx.de>
2024-07-15 10:40:39 +0200

    register FileStoragePlugin

--- test/injection/config.test.js
+++ test/injection/config.test.js
@@ -6,7 +6,7 @@ import { Injector } from '../../src/injection/core/injector.js';
 describe('injector configuration', () => {
 	it('registers the expected dependencies', () => {
 		expect($injector.isReady()).toBeTrue();
-		expect($injector.count()).toBe(78);
+		expect($injector.count()).toBe(79);
 
 		expect($injector.getScope('ProjectionService')).toBe(Injector.SCOPE_SINGLETON);
 		expect($injector.getScope('ConfigService')).toBe(Injector.SCOPE_SINGLETON);
@@ -65,6 +65,7 @@ describe('injector configuration', () => {
 		expect($injector.getScope('IframeStatePlugin')).toBe(Injector.SCOPE_SINGLETON);
 		expect($injector.getScope('IframeContainerPlugin')).toBe(Injector.SCOPE_SINGLETON);
 		expect($injector.getScope('SharePlugin')).toBe(Injector.SCOPE_SINGLETON);
+		expect($injector.getScope('FileStoragePlugin')).toBe(Injector.SCOPE_SINGLETON);
 		expect($injector.getScope('ToolsPlugin')).toBe(Injector.SCOPE_SINGLETON);
 		expect($injector.getScope('BeforeUnloadPlugin')).toBe(Injector.SCOPE_SINGLETON);
 		expect($injector.getScope('IframeGeometryIdPlugin')).toBe(Injector.SCOPE_SINGLETON);
