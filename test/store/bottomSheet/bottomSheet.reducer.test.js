fc24264237985c079cff69f851afaa8634220ae0
Schlemmer, Thilo (LDBV) <2117317+thiloSchlemmer@users.noreply.github.com>
2024-07-31 11:01:25 +0200

    update tests

--- test/store/bottomSheet/bottomSheet.reducer.test.js
+++ test/store/bottomSheet/bottomSheet.reducer.test.js
@@ -15,7 +15,7 @@ describe('bottomSheetReducer', () => {
 		expect(store.getState().bottomSheet.active).toBeFalse();
 	});
 
-	it('updates the stores properties', () => {
+	it('updates the main bottom sheet properties', () => {
 		const store = setup();
 
 		openBottomSheet('content');
@@ -26,6 +26,49 @@ describe('bottomSheetReducer', () => {
 		closeBottomSheet();
 
 		expect(store.getState().bottomSheet.data).toEqual(jasmine.arrayWithExactContents([{ id: 'main', content: null }]));
+		expect(store.getState().bottomSheet.active).toBeFalse();
+	});
+
+	it('updates the other bottom sheet properties', () => {
+		const store = setup();
+
+		openBottomSheet('content');
+
+		expect(store.getState().bottomSheet.data).toEqual(jasmine.arrayWithExactContents([{ id: 'main', content: 'content' }]));
+		expect(store.getState().bottomSheet.active).toBeTrue();
+
+		openBottomSheet('content', 'some');
+
+		expect(store.getState().bottomSheet.active).toBeTrue();
+		expect(store.getState().bottomSheet.data).toEqual(
+			jasmine.arrayWithExactContents([
+				{ id: 'some', content: 'content' },
+				{ id: 'main', content: 'content' }
+			])
+		);
+
+		openBottomSheet('contentUpdate');
+		openBottomSheet('contentUpdate', 'some');
+
+		closeBottomSheet();
+		closeBottomSheet('some');
+
+		expect(store.getState().bottomSheet.data).toEqual(
+			jasmine.arrayWithExactContents([
+				{ id: 'some', content: null },
+				{ id: 'main', content: null }
+			])
+		);
+		expect(store.getState().bottomSheet.active).toBeFalse();
+
+		closeBottomSheet('unknown');
+
+		expect(store.getState().bottomSheet.data).toEqual(
+			jasmine.arrayWithExactContents([
+				{ id: 'some', content: null },
+				{ id: 'main', content: null }
+			])
+		);
 		expect(store.getState().bottomSheet.active).toBeFalse();
 	});
 });
