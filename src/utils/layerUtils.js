bffa502b6a7079961eded7f99ee820703fa8debc
Thomas Aulinger <taulinger@gmx.de>
2024-09-17 14:55:05 +0200

    check if GeoResourceService is available

--- src/utils/layerUtils.js
+++ src/utils/layerUtils.js
@@ -1,13 +1,23 @@
+/**
+ * @module utils/layerUtils
+ */
 import { $injector } from '../injection/index';
 
 /**
- * Determines the resulting timestamp of a layer
+ * Determines the resulting timestamp of a layer.
+ * Requires a registered {@link GeoResourceService} for injection. If it is not available it returns the existing timestamp of the given `Layer`
+ * @function
  * @param {module:store/layers/layers_action~Layer} layer
  * @returns the timestamp or null
  */
 export const getTimestamp = (layer) => {
-	const { GeoResourceService: geoResourceService } = $injector.inject('GeoResourceService');
+	// first check if the GeoResourceService is available
+	if ($injector.getScope('GeoResourceService')) {
+		const { GeoResourceService: geoResourceService } = $injector.inject('GeoResourceService');
 
-	const geoResource = geoResourceService.byId(layer.geoResourceId);
-	return geoResource?.hasTimestamps() ? (layer.timestamp ?? geoResource.timestamps[0]) : null;
+		const geoResource = geoResourceService.byId(layer.geoResourceId);
+		return geoResource?.hasTimestamps() ? (layer.timestamp ?? geoResource.timestamps[0]) : null;
+	}
+
+	return layer.timestamp;
 };
