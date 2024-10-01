383a540c1e5ba5921e38a3c080bb2e05a48cb073
alsturm <alexander_sturm@gmx.net>
2024-10-01 14:07:22 +0200

    add media

--- test/modules/menu/components/mainMenu/content/maps/MapsContentPanel.test.js
+++ test/modules/menu/components/mainMenu/content/maps/MapsContentPanel.test.js
@@ -3,12 +3,24 @@ import { LayerManager } from '../../../../../../../src/modules/layerManager/comp
 import { AbstractMvuContentPanel } from '../../../../../../../src/modules/menu/components/mainMenu/content/AbstractMvuContentPanel';
 import { MapsContentPanel } from '../../../../../../../src/modules/menu/components/mainMenu/content/maps/MapsContentPanel';
 import { TestUtils } from '../../../../../../test-utils';
+import { $injector } from '../../../../../../../src/injection';
+import { createNoInitialStateMediaReducer } from '../../../../../../../src/store/media/media.reducer';
 
 window.customElements.define(MapsContentPanel.tag, MapsContentPanel);
 
 describe('MapsContentPanel', () => {
-	const setup = () => {
-		TestUtils.setupStoreAndDi();
+	const setup = (state = {}) => {
+		const initialState = {
+			media: {
+				portrait: true //because of safari test bug
+			},
+			...state
+		};
+
+		TestUtils.setupStoreAndDi(initialState, {
+			media: createNoInitialStateMediaReducer()
+		});
+		$injector.registerSingleton('EnvironmentService', {});
 		return TestUtils.render(MapsContentPanel.tag);
 	};
 
@@ -25,9 +37,38 @@ describe('MapsContentPanel', () => {
 			const element = await setup();
 
 			expect(element.shadowRoot.querySelectorAll(BaseLayerContainer.tag)).toHaveSize(1);
-			expect((element.shadowRoot.querySelector(BaseLayerContainer.tag).style.display = 'sticky'));
-			expect((element.shadowRoot.querySelector(BaseLayerContainer.tag).style.top = '2.5em'));
+			expect(window.getComputedStyle(element.shadowRoot.querySelector(BaseLayerContainer.tag)).position).toBe('sticky');
 			expect(element.shadowRoot.querySelectorAll(LayerManager.tag)).toHaveSize(1);
+		});
+	});
+
+	describe('responsive layout ', () => {
+		it('layouts for landscape', async () => {
+			const state = {
+				media: {
+					portrait: false
+				}
+			};
+
+			const element = await setup(state);
+
+			expect(element.shadowRoot.querySelectorAll('.is-landscape')).toHaveSize(1);
+			expect(element.shadowRoot.querySelectorAll('.is-portrait')).toHaveSize(0);
+			expect(window.getComputedStyle(element.shadowRoot.querySelector(BaseLayerContainer.tag)).top).toBe('0px');
+		});
+
+		it('layouts for portrait', async () => {
+			const state = {
+				media: {
+					portrait: true
+				}
+			};
+
+			const element = await setup(state);
+
+			expect(element.shadowRoot.querySelectorAll('.is-landscape')).toHaveSize(0);
+			expect(element.shadowRoot.querySelectorAll('.is-portrait')).toHaveSize(1);
+			expect(window.getComputedStyle(element.shadowRoot.querySelector(BaseLayerContainer.tag)).top).toBe('-40px');
 		});
 	});
 });
