4acabe6d8cd3f45d199e26b5a6e759ab78b50166
Schlemmer, Thilo (LDBV) <2117317+thiloSchlemmer@users.noreply.github.com>
2024-10-09 12:18:29 +0200

    update timeTravel i18n

--- src/modules/commons/i18n/valueSelect.provider.js
+++ src/modules/commons/i18n/valueSelect.provider.js
@@ -3,13 +3,13 @@ export const provide = (lang) => {
 		case 'en':
 			return {
 				//the first part of the snake_case key should be the name of the related module
-				valueSelect_icon_hint: 'Select value'
+				valueSelect_icon_hint: 'Click to select value'
 			};
 
 		case 'de':
 			return {
 				//the first part of the snake_case key should be the name of the related module
-				valueSelect_icon_hint: 'Auswählen'
+				valueSelect_icon_hint: 'Klicken zum auswählen'
 			};
 
 		default:
