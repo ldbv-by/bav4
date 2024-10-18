9ed169c3345cf61455191e213cf59cb2e5fbea81
Schlemmer, Thilo (LDBV) <2117317+thiloSchlemmer@users.noreply.github.com>
2024-10-18 11:16:19 +0200

    update doc

--- src/services/SecurityService.js
+++ src/services/SecurityService.js
@@ -3,11 +3,19 @@
  */
 import { domPurifySanitizeHtml } from './provider/sanitizeHtml.provider';
 
+/**
+ * @class
+ */
 export class SecurityService {
 	constructor(sanitizeHtmlProvider = domPurifySanitizeHtml) {
 		this._sanitizeHtmlProvider = sanitizeHtmlProvider;
 	}
 
+	/**
+	 * Returns the given html string as sanitized string.
+	 * @param {string} html The html content to be sanitized.
+	 * @returns {string} The sanitized html content.
+	 */
 	sanitizeHtml(html) {
 		return this._sanitizeHtmlProvider(html);
 	}
