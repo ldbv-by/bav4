0341987adab5ea9b0e71f6e307616c49b12dc9cd
alsturm <alexander_sturm@gmx.net>
2024-10-31 14:18:02 +0100

    lint

--- test/modules/layerManager/components/LayerItem.test.js
+++ test/modules/layerManager/components/LayerItem.test.js
@@ -25,7 +25,7 @@ import cloneSvg from '../../../../src/modules/layerManager/components/assets/clo
 import zoomToExtentSvg from '../../../../src/modules/layerManager/components/assets/zoomToExtent.svg';
 import infoSvg from '../../../../src/modules/layerManager/components/assets/info.svg';
 import { createNoInitialStateMediaReducer } from '../../../../src/store/media/media.reducer';
-import { closeSlider, openSlider } from '../../../../src/store/timeTravel/timeTravel.action';
+import { closeSlider } from '../../../../src/store/timeTravel/timeTravel.action';
 
 window.customElements.define(LayerItem.tag, LayerItem);
 window.customElements.define(ValueSelect.tag, ValueSelect);
