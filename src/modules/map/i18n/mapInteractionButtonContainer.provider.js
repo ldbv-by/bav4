3a5287ea2ccfaa55ae45b148a01d02d1b8136b10
Thomas Aulinger <taulinger@gmx.de>
2024-11-11 16:58:39 +0100

    rename provider

--- src/modules/map/i18n/mapInteractionButtonContainer.provider.js
+++ src/modules/map/i18n/mapInteractionButtonContainer.provider.js
@@ -3,13 +3,13 @@ export const provide = (lang) => {
 		case 'en':
 			return {
 				//the first part of the snake_case key should be the name of the related module
-				active_state_buttons_stop_routing: 'Stop routing'
+				map_interaction_button_container: 'Complete routing'
 			};
 
 		case 'de':
 			return {
 				//the first part of the snake_case key should be the name of the related module
-				active_state_buttons_stop_routing: 'Routing beenden'
+				map_interaction_button_container: 'Routing abschließen'
 			};
 
 		default:
