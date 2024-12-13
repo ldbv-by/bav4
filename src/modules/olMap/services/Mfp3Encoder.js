2f36f939c6b62b21f30d6ddd9c53dec7cd2040a2
thiloSchlemmer <2117317+thiloSchlemmer@users.noreply.github.com>
2024-12-13 13:41:35 +0100

    Mfp3Encoder: handle highlight layer (#2772)

--- src/modules/olMap/services/Mfp3Encoder.js
+++ src/modules/olMap/services/Mfp3Encoder.js
@@ -21,6 +21,7 @@ import { BaOverlay, OVERLAY_STYLE_CLASS } from '../components/BaOverlay';
 import { findAllBySelector } from '../../../utils/markup';
 import { setQueryParams } from '../../../utils/urlUtils';
 import { QueryParameters } from '../../../domain/queryParameters';
+import { HIGHLIGHT_LAYER_ID } from '../../../plugins/HighlightPlugin';
 
 const UnitsRatio = 39.37; //inches per meter
 const PointsPerInch = 72; // PostScript points 1/72"
@@ -241,7 +242,10 @@ export class BvvMfp3Encoder {
 		 * - WMTS/XYZ layers are defined for a specific projection. If the application projection and the print projection differs, the layer must be replaced.
 		 */
 		const encodableLayer = this._getSubstitutionLayerOptional(layer);
-		const geoResource = this._geoResourceService.byId(encodableLayer.get('geoResourceId'));
+		const geoResource =
+			layer.get('id') === HIGHLIGHT_LAYER_ID
+				? { exportable: true, getType: () => GeoResourceTypes.VECTOR }
+				: this._geoResourceService.byId(encodableLayer.get('geoResourceId'));
 
 		if (!geoResource) {
 			encodingErrorCallback(`[${layer.get('id')}]`, MFP_ENCODING_ERROR_TYPE.MISSING_GEORESOURCE);
