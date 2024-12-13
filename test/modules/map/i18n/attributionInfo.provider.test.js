ab3eec0400fbbcda9262631956a84522cb8aff66
alsturm <alexander_sturm@gmx.net>
2024-12-13 14:57:50 +0100

    add aria-label

--- test/modules/map/i18n/attributionInfo.provider.test.js
+++ test/modules/map/i18n/attributionInfo.provider.test.js
@@ -7,6 +7,7 @@ describe('i18n for attribution info', () => {
 		expect(map.map_attributionInfo_label).toBe('Data');
 		expect(map.map_attributionInfo_collapse_title_open).toBe('Show all');
 		expect(map.map_attributionInfo_collapse_title_close).toBe('Close');
+		expect(map.map_attributionInfo_label_aria_label).toBe('Data source ');
 	});
 
 	it('provides translation for de', () => {
@@ -15,10 +16,11 @@ describe('i18n for attribution info', () => {
 		expect(map.map_attributionInfo_label).toBe('Daten');
 		expect(map.map_attributionInfo_collapse_title_open).toBe('Alle anzeigen');
 		expect(map.map_attributionInfo_collapse_title_close).toBe('Schließen');
+		expect(map.map_attributionInfo_label_aria_label).toBe('Datenquelle ');
 	});
 
 	it('contains the expected amount of entries', () => {
-		const expectedSize = 3;
+		const expectedSize = 4;
 		const deMap = provide('de');
 		const enMap = provide('en');
 
