432abb8960908e4b687a326bd0a82af952308b7e
Schlemmer, Thilo (LDBV) <2117317+thiloSchlemmer@users.noreply.github.com>
2025-01-08 13:15:49 +0100

    clean code

--- src/modules/olMap/services/Mfp3Encoder.js
+++ src/modules/olMap/services/Mfp3Encoder.js
@@ -21,7 +21,6 @@ import { BaOverlay, OVERLAY_STYLE_CLASS } from '../components/BaOverlay';
 import { findAllBySelector } from '../../../utils/markup';
 import { setQueryParams } from '../../../utils/urlUtils';
 import { QueryParameters } from '../../../domain/queryParameters';
-import { HIGHLIGHT_LAYER_ID } from '../../../plugins/HighlightPlugin';
 import { GEODESIC_FEATURE_PROPERTY } from '../ol/geodesic/geodesicGeometry';
 
 const UnitsRatio = 39.37; //inches per meter
