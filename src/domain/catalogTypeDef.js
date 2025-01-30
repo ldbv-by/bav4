86c1806b241b0916de6049c083ccb98f1ad1c69e
Thomas Aulinger <taulinger@gmx.de>
2025-01-30 18:42:17 +0100

    rename CatalogEntry to CatalogNode and add property "id"

--- src/domain/catalogTypeDef.js
+++ src/domain/catalogTypeDef.js
@@ -2,10 +2,11 @@
  * @module domain/catalogTypeDef
  */
 /**
- * @typedef {Object} CatalogEntry
- * @property {string} label The label of this CatalogEntry
+ * @typedef {Object} CatalogNode
+ * @property {string} label The label of this CatalogNode
+ * @property {string} id The id of this CatalogNode
  * @property {boolean} [open] `true` if this entry should be displayed opened
- * @property {Array<module:domain/catalogTypeDef~GeoResourceRef|module:domain/catalogTypeDef~CatalogEntry>} children The elements of this CatalogEntry
+ * @property {Array<module:domain/catalogTypeDef~GeoResourceRef|module:domain/catalogTypeDef~CatalogNode>} children The elements of this CatalogNode
  */
 
 /**
