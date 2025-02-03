47af5109b5988bdde0f24e32c194101c5c67aff3
Schlemmer, Thilo (LDBV) <2117317+thiloSchlemmer@users.noreply.github.com>
2025-02-03 13:04:08 +0100

    update/fix i18n

--- src/modules/info/i18n/coordinateInfo.provider.js
+++ src/modules/info/i18n/coordinateInfo.provider.js
@@ -5,7 +5,8 @@ export const provide = (lang) => {
 				//the first part of the snake_case key should be the name of the related module
 				info_coordinateInfo_copy_icon: 'Copy to clipboard',
 				info_coordinateInfo_clipboard_error: '"Copy to clipboard" is not available',
-				info_coordinateInfo_clipboard_success: 'was copied to clipboard'
+				info_coordinateInfo_clipboard_success: 'was copied to clipboard',
+				info_coordinateInfo_elevation_label: 'Elev. (m)'
 			};
 
 		case 'de':
@@ -13,7 +14,8 @@ export const provide = (lang) => {
 				//the first part of the snake_case key should be the name of the related module
 				info_coordinateInfo_copy_icon: 'In die Zwischenablage kopieren',
 				info_coordinateInfo_clipboard_error: '"In die Zwischenablage kopieren" steht nicht zur Verfügung',
-				info_coordinateInfo_clipboard_success: 'wurde in die Zwischenablage kopiert'
+				info_coordinateInfo_clipboard_success: 'wurde in die Zwischenablage kopiert',
+				info_coordinateInfo_elevation_label: 'Höhe (m)'
 			};
 
 		default:
--- test/modules/info/i18n/coordinateInfo.provider.test.js
+++ test/modules/info/i18n/coordinateInfo.provider.test.js
@@ -7,6 +7,7 @@ describe('i18n for coordinate info', () => {
 		expect(map.info_coordinateInfo_copy_icon).toBe('Copy to clipboard');
 		expect(map.info_coordinateInfo_clipboard_error).toBe('"Copy to clipboard" is not available');
 		expect(map.info_coordinateInfo_clipboard_success).toBe('was copied to clipboard');
+		expect(map.info_coordinateInfo_elevation_label).toBe('Elev. (m)');
 	});
 
 	it('provides translation for de', () => {
@@ -15,10 +16,11 @@ describe('i18n for coordinate info', () => {
 		expect(map.info_coordinateInfo_copy_icon).toBe('In die Zwischenablage kopieren');
 		expect(map.info_coordinateInfo_clipboard_error).toBe('"In die Zwischenablage kopieren" steht nicht zur Verfügung');
 		expect(map.info_coordinateInfo_clipboard_success).toBe('wurde in die Zwischenablage kopiert');
+		expect(map.info_coordinateInfo_elevation_label).toBe('Höhe (m)');
 	});
 
 	it('contains the expected amount of entries', () => {
-		const expectedSize = 3;
+		const expectedSize = 4;
 		const deMap = provide('de');
 		const enMap = provide('en');
 
