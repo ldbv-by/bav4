861a0ef15858850307db0cfb5a19bcb2a0c66256
Thomas Aulinger <49945713+taulinger@users.noreply.github.com>
2025-02-10 17:22:51 +0100

    fix: set default topic again (#2891)

--- src/plugins/TopicsPlugin.js
+++ src/plugins/TopicsPlugin.js
@@ -20,6 +20,8 @@ export class TopicsPlugin extends BaPlugin {
 			setCurrent(topicId);
 			// when we have a topic we want to display the corresponding catalog
 			setIndex(TopicsContentPanelIndex.CATALOG_0);
+		} else {
+			setCurrent(topicsService.default().id);
 		}
 	}
 
