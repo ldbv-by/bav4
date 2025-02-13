eafd11fd0e3c3ca0d61a6f510deb975080e73616
Schlemmer, Thilo (LDBV) <2117317+thiloSchlemmer@users.noreply.github.com>
2025-02-13 15:29:53 +0100

    fix i18n

--- src/modules/info/i18n/geometryInfo.provider.js
+++ src/modules/info/i18n/geometryInfo.provider.js
@@ -4,7 +4,7 @@ export const provide = (lang) => {
 			return {
 				//the first part of the snake_case key should be the name of the related module
 				info_geometryInfo_title_coordinate: 'Coordinate',
-				info_geometryInfo_title_azimuth: 'Azimuth-angle',
+				info_geometryInfo_title_azimuth: 'Azimuth',
 				info_geometryInfo_title_line_length: 'Distance',
 				info_geometryInfo_title_polygon_length: 'Perimeter',
 				info_geometryInfo_title_polygon_area: 'Area'
@@ -14,7 +14,7 @@ export const provide = (lang) => {
 			return {
 				//the first part of the snake_case key should be the name of the related module
 				info_geometryInfo_title_coordinate: 'Koordinate',
-				info_geometryInfo_title_azimuth: 'Azimuth-Winkel',
+				info_geometryInfo_title_azimuth: 'Azimut',
 				info_geometryInfo_title_line_length: 'Länge',
 				info_geometryInfo_title_polygon_length: 'Umfang',
 				info_geometryInfo_title_polygon_area: 'Fläche'
