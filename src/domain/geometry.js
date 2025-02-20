c5c3fc0551e1de293c066bef52993674f3c721b2
Thomas Aulinger <taulinger@gmx.de>
2025-02-20 14:59:55 +0100

    update doc

--- src/domain/geometry.js
+++ src/domain/geometry.js
@@ -2,7 +2,7 @@
  * @module domain/geometry
  */
 /**
- * A geometry. The actual geometry is encoded as the specified VectorSourceType or null if it is unknown (in that case a consumer should use the `SourceTypeService`  for determination).
+ * A geometry. The actual geometry is encoded as the specified VectorSourceType or `null` if it is unknown (in that case a consumer should use the `SourceTypeService`  for determination).
  */
 export class Geometry {
 	#data;
@@ -10,7 +10,7 @@ export class Geometry {
 	/**
 	 *
 	 * @param {String} data The data of this geometry
-	 * @param {VectorSourceType} [sourceType] The data type of this geometry or null if unknown
+	 * @param {VectorSourceType} [sourceType] The data type of this geometry or `null` if unknown
 	 */
 	constructor(data, sourceType = null) {
 		this.#data = data;
