522ec5e8339746b6d3856fae89032b6bf0614b64
Thomas Aulinger <taulinger@gmx.de>
2025-02-21 14:03:51 +0100

    sourceType param should be a "SourceType"

--- src/domain/geometry.js
+++ src/domain/geometry.js
@@ -1,8 +1,12 @@
 /**
  * @module domain/geometry
  */
+
+import { isString } from '../utils/checks';
+import { SupportedVectorSourceTypes } from './sourceType';
+
 /**
- * A geometry. The actual geometry is encoded as the specified VectorSourceType or `null` if it is unknown (in that case a consumer should use the `SourceTypeService`  for determination).
+ * A geometry. The actual geometry is encoded as the specified SourceType or `null` if it is unknown (in that case a consumer can use the `SourceTypeService`  for determination).
  */
 export class Geometry {
 	#data;
@@ -10,9 +14,15 @@ export class Geometry {
 	/**
 	 *
 	 * @param {String} data The data of this geometry
-	 * @param {VectorSourceType} [sourceType] The data type of this geometry or `null` if unknown
+	 * @param {SourceType} [sourceType] The source type of this geometry or `null` if unknown
 	 */
 	constructor(data, sourceType = null) {
+		if (sourceType && !SupportedVectorSourceTypes.includes(sourceType.name)) {
+			throw new Error(`Unsupported source type: ${sourceType.toString()}`);
+		}
+		if (!isString(data)) {
+			throw new Error('<data> must be a String');
+		}
 		this.#data = data;
 		this.#sourceType = sourceType;
 	}
--- test/domain/geometry.test.js
+++ test/domain/geometry.test.js
@@ -1,12 +1,17 @@
-import { VectorSourceType } from '../../src/domain/geoResources';
 import { Geometry } from '../../src/domain/geometry';
+import { SourceType, SourceTypeName } from '../../src/domain/sourceType';
 
 describe('Geometry', () => {
 	it('provides a constructor and getters for properties', () => {
-		const geometry = new Geometry('data', VectorSourceType.GEOJSON);
+		const geometry = new Geometry('data', new SourceType(SourceTypeName.GPX));
 
 		expect(geometry.data).toBe('data');
-		expect(geometry.sourceType).toBe(VectorSourceType.GEOJSON);
+		expect(geometry.sourceType).toEqual(new SourceType(SourceTypeName.GPX));
 		expect(new Geometry('data').sourceType).toBeNull();
+	});
+
+	it('check the constructors arguments', () => {
+		expect(() => new Geometry('data', 'Foo')).toThrowError('Unsupported source type: Foo');
+		expect(() => new Geometry(123, new SourceType(SourceTypeName.GPX))).toThrowError('<data> must be a String');
 	});
 });
