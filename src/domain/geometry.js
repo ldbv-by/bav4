a4f53f058132a8c1d6927f24720b7dc22b9d7cca
Thomas Aulinger <taulinger@gmx.de>
2025-02-27 16:46:01 +0100

    allow GeoJSON data to be an object

--- src/domain/geometry.js
+++ src/domain/geometry.js
@@ -3,7 +3,7 @@
  */
 
 import { isString } from '../utils/checks';
-import { SupportedVectorSourceTypes } from './sourceType';
+import { SourceTypeName, SupportedVectorSourceTypes } from './sourceType';
 
 /**
  * A geometry. The actual geometry is encoded as the specified SourceType or `null` if it is unknown (in that case a consumer can use the `SourceTypeService`  for determination).
@@ -13,17 +13,17 @@ export class Geometry {
 	#sourceType;
 	/**
 	 *
-	 * @param {String} data The data of this geometry
+	 * @param {String|object} data The data of this geometry (Note type `object' is only allowed in case of sourceType GeoJSON)
 	 * @param {SourceType} [sourceType] The source type of this geometry or `null` if unknown
 	 */
 	constructor(data, sourceType = null) {
 		if (sourceType && !SupportedVectorSourceTypes.includes(sourceType.name)) {
 			throw new Error(`Unsupported source type: ${sourceType.toString()}`);
 		}
-		if (!isString(data)) {
+		if (!isString(data) && sourceType.name !== SourceTypeName.GEOJSON) {
 			throw new Error('<data> must be a String');
 		}
-		this.#data = data;
+		this.#data = isString(data) ? data : JSON.stringify(data);
 		this.#sourceType = sourceType;
 	}
 
