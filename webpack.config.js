70b944d845558eaa345cdcef523cd614d9750912
Thomas Aulinger <taulinger@gmx.de>
2025-03-31 16:50:36 +0200

    Revert "only config.js should be excluded from hashing (#2993)"
    This reverts commit 1699abd7e002738cf5473c97e339a83c482fcf66.

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: (chunkData) => {
-			if (['config'].includes(chunkData.chunk.name)) {
+			if (['wc', 'config'].includes(chunkData.chunk.name)) {
 				return '[name].js';
 			}
 			return hashFilenames ? '[name].[contenthash].js' : '[name].js';
@@ -71,7 +71,7 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			filename: 'wc/wrapper/index.html',
 			template: 'src/wcWrapper.html',
-			chunks: []
+			chunks: ['wc']
 		}),
 		new CopyPlugin({
 			patterns: [{ from: path.resolve(__dirname, './src/assets/favicon'), to: path.join('assets') }]
