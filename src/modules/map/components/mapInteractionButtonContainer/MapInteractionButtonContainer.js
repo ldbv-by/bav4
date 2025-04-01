4543699c168d5da13016ee2f97d1498136540ca9
Alexander Sturm <76113751+alsturm@users.noreply.github.com>
2025-04-01 16:29:42 +0200

    Map interaction button container position (#2997)
    * add context position of the panel
    
    * fix style, update tests
    
    * fix lint
    
    * update test
    
    * fix lint
    
    * cleanup
    
    ---------
    
    Co-authored-by: Thomas Aulinger <taulinger@gmx.de>

--- src/modules/map/components/mapInteractionButtonContainer/MapInteractionButtonContainer.js
+++ src/modules/map/components/mapInteractionButtonContainer/MapInteractionButtonContainer.js
@@ -2,15 +2,21 @@
  * @module modules/map/components/mapInteractionButtonContainer/MapInteractionButtonContainer
  */
 import { html } from 'lit-html';
-import { $injector } from '../../../../injection';
-import { MvuElement } from '../../../MvuElement';
-import css from './mapInteractionButtonContainer.css';
-import { setCurrentTool } from '../../../../store/tools/tools.action';
 import { Tools } from '../../../../domain/tools';
+import { $injector } from '../../../../injection';
+import { setCurrentTool } from '../../../../store/tools/tools.action';
+import { findAllBySelector } from '../../../../utils/markup';
+import { MvuElement } from '../../../MvuElement';
 import closeSvg from './assets/close.svg';
+import css from './mapInteractionButtonContainer.css';
+import { classMap } from 'lit-html/directives/class-map.js';
+import { BottomSheet } from '../../../stackables/components/bottomSheet/BottomSheet';
 
 const Update_IsPortrait_HasMinWidth = 'update_isPortrait_hasMinWidth';
 const Update_ToolId = 'update_tooId';
+const Update_Bottom_Sheet = 'update_bottom_sheet';
+const Update_Main_Menu = 'update_main_menu';
+const Update_IsOpen_NavigationRail = 'update_isOpen_NavigationRail';
 
 /**
  * Container for map interaction buttons.
@@ -21,15 +27,19 @@ const Update_ToolId = 'update_tooId';
 
 export class MapInteractionButtonContainer extends MvuElement {
 	#translationService;
+	#environmentService;
 	constructor() {
 		super({
+			toolId: null,
+			isOpenMainMenu: false,
 			isPortrait: false,
-			toolId: null
+			isOpenNavigationRail: false
 		});
 
-		const { TranslationService } = $injector.inject('TranslationService');
+		const { TranslationService, EnvironmentService } = $injector.inject('TranslationService', 'EnvironmentService');
 
 		this.#translationService = TranslationService;
+		this.#environmentService = EnvironmentService;
 	}
 
 	update(type, data, model) {
@@ -38,6 +48,12 @@ export class MapInteractionButtonContainer extends MvuElement {
 				return { ...model, ...data };
 			case Update_ToolId:
 				return { ...model, toolId: data };
+			case Update_Bottom_Sheet:
+				return { ...model, active: data };
+			case Update_Main_Menu:
+				return { ...model, ...data };
+			case Update_IsOpen_NavigationRail:
+				return { ...model, ...data };
 		}
 	}
 
@@ -50,6 +66,31 @@ export class MapInteractionButtonContainer extends MvuElement {
 			(state) => state.tools.current,
 			(current) => this.signal(Update_ToolId, current)
 		);
+		this.observe(
+			(state) => state.bottomSheet.active,
+			(active) => this.signal(Update_Bottom_Sheet, active)
+		);
+		this.observe(
+			(state) => state.mainMenu,
+			(mainMenu) => this.signal(Update_Main_Menu, { isOpenMainMenu: mainMenu.open, tab: mainMenu.tab })
+		);
+		this.observe(
+			(state) => state.navigationRail,
+			(navigationRail) => this.signal(Update_IsOpen_NavigationRail, { isOpenNavigationRail: navigationRail.open })
+		);
+	}
+
+	/**
+	 * @override
+	 */
+	onAfterRender() {
+		setTimeout(() => {
+			const mapInteractionButtonContainer = this.shadowRoot.getElementById('mapInteractionButtonContainer');
+			const bottomSheet = findAllBySelector(this.#environmentService.getWindow().document, BottomSheet.tag);
+			bottomSheet[0]
+				? (mapInteractionButtonContainer.style.bottom = bottomSheet[0].offsetHeight + 10 + 'px')
+				: (mapInteractionButtonContainer.style.bottom = 'var(--map-interaction-container-bottom)');
+		});
 	}
 
 	/**
@@ -57,10 +98,13 @@ export class MapInteractionButtonContainer extends MvuElement {
 	 */
 	createView(model) {
 		const translate = (key) => this.#translationService.translate(key);
-		const { isPortrait, toolId } = model;
+		const { toolId, isOpenMainMenu, isOpenNavigationRail, isPortrait } = model;
 
-		const getOrientationClass = () => {
-			return isPortrait ? 'is-portrait' : 'is-landscape';
+		const classes = {
+			'is-open': isOpenMainMenu,
+			'is-open-navigationRail': isOpenNavigationRail,
+			'is-portrait': isPortrait,
+			'is-landscape': !isPortrait
 		};
 
 		const getShowRoutingClass = () => {
@@ -74,9 +118,9 @@ export class MapInteractionButtonContainer extends MvuElement {
 			<style>
 				${css}
 			</style>
-			<div class="map-interaction-button-container ${getOrientationClass()}">
+			<div id="mapInteractionButtonContainer" class="map-interaction-button-container ${classMap(classes)}">
 				<ba-button
-					class="${getShowRoutingClass()} routing"
+					class="${getShowRoutingClass()} routing ui-center"
 					.icon=${closeSvg}
 					.label=${translate('map_interaction_button_container_routing')}
 					.type=${'primary'}
--- test/modules/map/components/mapInteractionButtonContainer/MapInteractionButtonContainer.test.js
+++ test/modules/map/components/mapInteractionButtonContainer/MapInteractionButtonContainer.test.js
@@ -4,13 +4,16 @@ import { $injector } from '../../../../../src/injection/index.js';
 import { Tools } from '../../../../../src/domain/tools';
 import { toolsReducer } from '../../../../../src/store/tools/tools.reducer';
 import { createNoInitialStateMediaReducer } from '../../../../../src/store/media/media.reducer.js';
+import { bottomSheetReducer } from '../../../../../src/store/bottomSheet/bottomSheet.reducer';
+import { createNoInitialStateMainMenuReducer } from '../../../../../src/store/mainMenu/mainMenu.reducer';
+import { createNoInitialStateNavigationRailReducer } from '../../../../../src/store/navigationRail/navigationRail.reducer';
+import { html } from 'lit-html';
 
 window.customElements.define(MapInteractionButtonContainer.tag, MapInteractionButtonContainer);
 
 describe('MapInteractionButtonContainer', () => {
 	let store;
-	const setup = (state = {}, config = {}) => {
-		const { embed = false } = config;
+	const setup = (state = {}) => {
 		const initialState = {
 			media: {
 				portrait: false
@@ -18,16 +21,26 @@ describe('MapInteractionButtonContainer', () => {
 			tools: {
 				current: false
 			},
+			mainMenu: {
+				open: true,
+				tab: null
+			},
+			navigationRail: {
+				open: true
+			},
 			...state
 		};
 
 		store = TestUtils.setupStoreAndDi(initialState, {
 			tools: toolsReducer,
-			media: createNoInitialStateMediaReducer()
+			media: createNoInitialStateMediaReducer(),
+			bottomSheet: bottomSheetReducer,
+			mainMenu: createNoInitialStateMainMenuReducer(),
+			navigationRail: createNoInitialStateNavigationRailReducer()
 		});
 
 		$injector.registerSingleton('EnvironmentService', {
-			isEmbedded: () => embed
+			getWindow: () => window
 		});
 		$injector.registerSingleton('TranslationService', { translate: (key) => key });
 		return TestUtils.render(MapInteractionButtonContainer.tag);
@@ -39,8 +52,10 @@ describe('MapInteractionButtonContainer', () => {
 			const element = new MapInteractionButtonContainer();
 
 			expect(element.getModel()).toEqual({
+				toolId: null,
 				isPortrait: false,
-				toolId: null
+				isOpenMainMenu: false,
+				isOpenNavigationRail: false
 			});
 		});
 	});
@@ -51,8 +66,10 @@ describe('MapInteractionButtonContainer', () => {
 			const model = new MapInteractionButtonContainer().getModel();
 
 			expect(model).toEqual({
+				toolId: null,
 				isPortrait: false,
-				toolId: null
+				isOpenMainMenu: false,
+				isOpenNavigationRail: false
 			});
 		});
 	});
@@ -62,6 +79,7 @@ describe('MapInteractionButtonContainer', () => {
 			const element = await setup();
 
 			expect(element.shadowRoot.querySelector('.map-interaction-button-container').children).toHaveSize(2);
+			expect(element.shadowRoot.querySelectorAll('ba-button.routing.ui-center')).toHaveSize(1);
 			expect(element.shadowRoot.querySelectorAll('ba-button.routing.hide')).toHaveSize(1);
 			expect(element.shadowRoot.querySelectorAll('ba-button.layer-swipe.hide')).toHaveSize(1);
 		});
@@ -70,7 +88,7 @@ describe('MapInteractionButtonContainer', () => {
 			const element = await setup({ tools: { current: Tools.ROUTING } });
 			expect(element.shadowRoot.querySelector('.map-interaction-button-container').children).toHaveSize(2);
 			expect(element.shadowRoot.querySelectorAll('ba-button.layer-swipe.hide')).toHaveSize(1);
-			expect(element.shadowRoot.querySelectorAll('ba-button.routing')).toHaveSize(1);
+			expect(element.shadowRoot.querySelectorAll('ba-button.routing.ui-center')).toHaveSize(1);
 			expect(element.shadowRoot.querySelectorAll('ba-button.routing')[0].label).toBe('map_interaction_button_container_routing');
 			expect(element.shadowRoot.querySelectorAll('ba-button.routing')[0].title).toBe('');
 		});
@@ -111,6 +129,88 @@ describe('MapInteractionButtonContainer', () => {
 
 			expect(element.shadowRoot.querySelectorAll('.is-portrait')).toHaveSize(1);
 			expect(element.shadowRoot.querySelectorAll('.is-landscape')).toHaveSize(0);
+		});
+
+		it('layouts with open navigation rail', async () => {
+			const state = {
+				media: {
+					portrait: false
+				},
+				navigationRail: {
+					open: true
+				}
+			};
+
+			const element = await setup(state);
+			expect(element.shadowRoot.querySelectorAll('.is-open-navigationRail')).toHaveSize(1);
+		});
+
+		it('layouts with closed navigation rail', async () => {
+			const state = {
+				media: {
+					portrait: false
+				},
+				navigationRail: {
+					open: false
+				}
+			};
+			const element = await setup(state);
+			expect(element.shadowRoot.querySelectorAll('.is-open-navigationRail')).toHaveSize(0);
+		});
+
+		it('layouts with open main menu', async () => {
+			const state = {
+				media: {
+					portrait: false
+				},
+				mainMenu: {
+					open: true
+				}
+			};
+
+			const element = await setup(state);
+			expect(element.shadowRoot.querySelectorAll('.is-open')).toHaveSize(1);
+		});
+
+		it('layouts with closed main menu', async () => {
+			const state = {
+				media: {
+					portrait: false
+				},
+				mainMenu: {
+					open: false
+				}
+			};
+			const element = await setup(state);
+			expect(element.shadowRoot.querySelectorAll('.is-open')).toHaveSize(0);
+		});
+	});
+
+	describe('reacts to bottom sheet', () => {
+		it('sets bottom value to 110px', async () => {
+			const element = await setup({ tools: { current: Tools.ROUTING } });
+			const bottomSheetContainer = TestUtils.renderTemplateResult(
+				html`<ba-bottom-sheet style="display: block;height: 100px;"><div>TEST</div></ba-bottom-sheet>`
+			);
+			document.body.appendChild(bottomSheetContainer);
+			const container = element.shadowRoot.querySelector('#mapInteractionButtonContainer');
+			expect(element.shadowRoot.querySelectorAll('#mapInteractionButtonContainer')).toHaveSize(1);
+			expect(container.style.getPropertyValue('bottom')).toBe('');
+
+			await TestUtils.timeout();
+
+			expect(container.style.getPropertyValue('bottom')).toBe('110px');
+		});
+
+		it('sets bottom value to variable', async () => {
+			const element = await setup({ tools: { current: Tools.ROUTING } });
+			const container = element.shadowRoot.querySelector('#mapInteractionButtonContainer');
+			expect(element.shadowRoot.querySelectorAll('#mapInteractionButtonContainer')).toHaveSize(1);
+			expect(container.style.getPropertyValue('bottom')).toBe('');
+
+			await TestUtils.timeout();
+
+			expect(container.style.getPropertyValue('bottom')).toBe('var(--map-interaction-container-bottom)');
 		});
 	});
 
