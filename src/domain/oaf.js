544e6ecfb8fc7e5d64739ed680b11d34a37d964f
Thomas Aulinger <taulinger@gmx.de>
2025-04-29 14:01:24 +0200

    add enum OafQueryableType

---
+++ src/domain/oaf.js
@@ -0,0 +1,36 @@
+/**
+ * @module domain/oaf
+ */
+/**
+ * Capabilities of a {@link OafGeoResource}
+ * @typedef OafCapabilities
+ * @property {number} totalNumberOfItems
+ * @property {boolean} sampled Some meta data are bases on a a sample
+ * @property {Array<OafQueryable>} queryables List of possible `OafQueryable`
+ */
+
+/**
+ * A queryable property of a {@link OafGeoResource}
+ * @typedef OafQueryable
+ * @property {string} name The name of this queryable property
+ * @property {OafQueryableType} type The type of this queryable property
+ * @property {Array<Object>} [values] The values of this queryable property
+ * @property {boolean} [finalized] The list of values of this queryable property is final
+ * @property {number} [minValue] The min. value of all values
+ * @property {number} [maxValue] The max. values of all values
+ */
+
+/**
+ * Enum of all supported data types.
+ * @readonly
+ * @enum {String}
+ */
+export const OafQueryableType = Object.freeze({
+	STRING: 'string',
+	INTEGER: 'integer',
+	FLOAT: 'float',
+	BOOLEAN: 'boolean',
+	DATE: 'date',
+	DATETIME: 'datetime',
+	TIME: 'time'
+});
---
+++ test/domain/oaf.test.js
@@ -0,0 +1,16 @@
+import { OafQueryableType } from '../../src/domain/oaf';
+
+describe('OGC API Features', () => {
+	it('provides an enum of all known OafQueryable types', () => {
+		expect(Object.keys(OafQueryableType).length).toBe(7);
+		expect(Object.isFrozen(OafQueryableType)).toBeTrue();
+
+		expect(OafQueryableType.BOOLEAN).toBe('boolean');
+		expect(OafQueryableType.INTEGER).toBe('integer');
+		expect(OafQueryableType.FLOAT).toBe('float');
+		expect(OafQueryableType.STRING).toBe('string');
+		expect(OafQueryableType.DATE).toBe('date');
+		expect(OafQueryableType.DATETIME).toBe('datetime');
+		expect(OafQueryableType.TIME).toBe('time');
+	});
+});
