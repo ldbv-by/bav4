8f30fa8a514074d3144a3e13131ca65e5a2f5819
Schlemmer, Thilo (LDBV) <2117317+thiloSchlemmer@users.noreply.github.com>
2025-05-05 12:38:58 +0200

    replace clipboard-resource for ShareDialogContent

--- src/modules/share/components/dialog/ShareDialogContent.js
+++ src/modules/share/components/dialog/ShareDialogContent.js
@@ -3,7 +3,7 @@
  */
 import { html } from 'lit-html';
 import { $injector } from '../../../../injection';
-import clipboardIcon from './assets/clipboard.svg';
+import clipboardIcon from '../../../../assets/icons/clipboard.svg';
 import shareIcon from './assets/share.svg';
 import css from './shareDialogContent.css';
 import { emitNotification, LevelTypes } from '../../../../store/notifications/notifications.action';
