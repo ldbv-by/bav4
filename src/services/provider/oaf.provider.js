81345542f14960fcf3bef7dfb59a0e9bf056f28c
Thomas Aulinger <taulinger@gmx.de>
2025-05-15 17:49:13 +0200

    implement bvvOafFilterCapabilitiesProvider

--- src/services/provider/oaf.provider.js
+++ src/services/provider/oaf.provider.js
@@ -11,125 +11,39 @@ import { GeoResourceAuthenticationType, OafGeoResource } from '../../domain/geoR
 import { MediaType } from '../../domain/mediaTypes';
 import { $injector } from '../../injection/index';
 
-// eslint-disable-next-line no-unused-vars
 export const bvvOafFilterCapabilitiesProvider = async (oafGeoResource) => {
-	return {
-		totalNumberOfItems: 1000,
-		crs: [
-			'http://www.opengis.net/def/crs/OGC/1.3/CRS84',
-			'http://www.opengis.net/def/crs/EPSG/0/3857',
-			'http://www.opengis.net/def/crs/EPSG/0/25832',
-			'http://www.opengis.net/def/crs/EPSG/0/4258',
-			'http://www.opengis.net/def/crs/EPSG/0/4326'
-		],
-		sampled: true,
-		queryables: [
-			{
-				name: 'node_id',
-				type: 'integer',
-				values: [],
-				finalized: false
-			},
-			{
-				name: 'id',
-				type: 'integer',
-				values: [],
-				finalized: false
-			},
-			{
-				name: 'name',
-				type: 'string',
-				values: [
-					'Gasthof Pension Strohmaier',
-					'Haller-Alm',
-					'Am Guldensteig',
-					'Racheldiensthütte',
-					'Baggerloch',
-					'Gasthof zur Post',
-					'Dorfbiergarten Stark',
-					'Waldheim',
-					'Lutzgarten',
-					'Zur Schnecke',
-					'Innbräu-Biergarten',
-					'Seehaus Schreyegg',
-					'Alter Wirt Krailling',
-					'Altenmarkter Hof',
-					'Wodans Biergarten',
-					'Biergarten zur alten Münzpräge',
-					'Biergarten Zum Schiffmeister',
-					'Biergarten Cafe Waldstein',
-					'Albrechtsgarten',
-					'Wirts Kathi'
-				],
-				finalized: false
-			},
-			{
-				name: 'strasse',
-				type: 'string',
-				values: [
-					'Nebelhornstraße 16',
-					'Jamnitzerstraße 5',
-					'Lusenstraße 48',
-					'Nawiaskystraße 18',
-					'Tegernseer Platz 12',
-					'Abacostraße 11',
-					'Dorfstraße 22',
-					'Tegelbergstraße 2',
-					'Aberlestraße 15',
-					'Jakob-Klar-Straße 1',
-					'Abensbergstraße 6',
-					'Abacostraße 5',
-					'Naupliastraße 17',
-					'Aaröstraße 14',
-					'Nawiaskystraße 9',
-					'Jochensteiner Straße 5',
-					'Seestraße 34',
-					'Nederlinger Platz 20',
-					'Teisendorfer Straße 14',
-					'Jasminstraße 9'
-				],
-				finalized: false
-			},
-			{
-				name: 'plz',
-				type: 'integer',
-				values: [81247, 94556, 94160, 94130, 83471],
-				finalized: false,
-				minValue: 81247,
-				maxValue: 94556
-			},
-			{
-				name: 'ort',
-				type: 'string',
-				values: ['München', 'Neuschönau', 'Ringelai', 'Obernzell', 'Schönau am Königssee'],
-				finalized: false
-			},
-			{
-				name: 'outdoor_seating',
-				type: 'boolean',
-				values: [false, true],
-				finalized: false
-			},
-			{
-				name: 'open',
-				type: 'string',
-				values: ['11:30:00', '09:30:00', '10:30:00', '10:00:00', '09:00:00', '11:00:00'],
-				finalized: false
-			},
-			{
-				name: 'close',
-				type: 'string',
-				values: ['21:30:00', '22:00:00', '22:30:00', '20:00:00', '23:00:00', '23:30:00'],
-				finalized: false
-			},
-			{
-				name: 'geom',
-				type: 'geometry-point',
-				values: [],
-				finalized: false
-			}
-		]
+	const {
+		HttpService: httpService,
+		ConfigService: configService,
+		BaaCredentialService: baaCredentialService
+	} = $injector.inject('HttpService', 'ConfigService', 'BaaCredentialService');
+	const endpointUrl = configService.getValueAsPath('BACKEND_URL') + 'oaf/getFilterCapabilities';
+
+	const readFilterCapabilities = (oafFilterCapabilities) => {
+		// here we can do possible mapping in the future
+		return oafFilterCapabilities;
 	};
+
+	const getCredentialOrFail = (url) => {
+		const failed = () => {
+			throw new Error(`Fetching of filter capabilities failed. Credential for '${url}' not found`);
+		};
+
+		const credential = baaCredentialService.get(url);
+		return credential ? { username: credential.username, password: credential.password } : failed();
+	};
+
+	const data =
+		oafGeoResource.authenticationType === GeoResourceAuthenticationType.BAA
+			? { url: oafGeoResource.url, collectionId: oafGeoResource.collectionId, ...getCredentialOrFail(oafGeoResource.url) }
+			: { url: oafGeoResource.url, collectionId: oafGeoResource.collectionId };
+	const result = await httpService.post(endpointUrl, JSON.stringify(data), MediaType.JSON);
+	switch (result.status) {
+		case 200:
+			return readFilterCapabilities(await result.json());
+		default:
+			throw new Error(`Filter capabilities for '${oafGeoResource.url}' could not be loaded: Http-Status ${result.status}`);
+	}
 };
 
 /**
--- test/service/provider/oaf.provider.test.js
+++ test/service/provider/oaf.provider.test.js
@@ -1,6 +1,144 @@
 import { $injector } from '../../../src/injection';
-import { bvvOafGeoResourceProvider } from '../../../src/services/provider/oaf.provider';
+import { bvvOafFilterCapabilitiesProvider, bvvOafGeoResourceProvider } from '../../../src/services/provider/oaf.provider';
 import { MediaType } from '../../../src/domain/mediaTypes';
+import { GeoResourceAuthenticationType, OafGeoResource } from '../../../src/domain/geoResources';
+
+describe('bvvOafFilterCapabilitiesProvider', () => {
+	const configService = {
+		getValueAsPath() {}
+	};
+
+	const httpService = {
+		async post() {}
+	};
+
+	const baaCredentialService = {
+		get() {}
+	};
+
+	beforeAll(() => {
+		$injector
+			.registerSingleton('ConfigService', configService)
+			.registerSingleton('HttpService', httpService)
+			.registerSingleton('BaaCredentialService', baaCredentialService);
+	});
+
+	afterAll(() => {
+		$injector.reset();
+	});
+
+	const mockResponsePayload = {
+		totalNumberOfItems: 10000,
+		sampled: true,
+		queryables: [
+			{
+				name: 'h_id',
+				type: 'integer',
+				values: [5, 36, 37],
+				finalized: false,
+				minValue: 0,
+				maxValue: 1007029
+			},
+			{
+				name: 'datum',
+				type: 'date',
+				values: ['1992-01-15', '2000-01-01', '1998-09-14'],
+				finalized: false
+			},
+			{
+				name: 'gemeinde_code',
+				type: 'string',
+				values: ['671139', '674147'],
+				finalized: false
+			},
+			{
+				name: 'gemeinde_name',
+				type: 'string',
+				values: ['Laufach', 'Haßfurt', 'Schweinfurt'],
+				finalized: false
+			}
+		]
+	};
+
+	it('returns the OafFilterCapabilities for a OafGeoResource', async () => {
+		const url = 'https://some.url/oaf';
+		const collectionId = 'collectionId';
+		const oafGeoResource = new OafGeoResource('id', 'label', url, collectionId, 12345);
+		const configServiceSpy = spyOn(configService, 'getValueAsPath').withArgs('BACKEND_URL').and.returnValue('BACKEND_URL/');
+		const httpServiceSpy = spyOn(httpService, 'post')
+			.withArgs('BACKEND_URL/oaf/getFilterCapabilities', JSON.stringify({ url, collectionId }), MediaType.JSON)
+			.and.resolveTo(new Response(JSON.stringify(mockResponsePayload)));
+
+		const result = await bvvOafFilterCapabilitiesProvider(oafGeoResource);
+
+		expect(result).toEqual(mockResponsePayload);
+		expect(configServiceSpy).toHaveBeenCalled();
+		expect(httpServiceSpy).toHaveBeenCalled();
+	});
+
+	describe('OAF service has basic authorization', () => {
+		it('returns the OafFilterCapabilities for a BAA-restricted OafGeoResource', async () => {
+			const url = 'https://some.url/oaf';
+			const collectionId = 'collectionId';
+			const username = 'foo';
+			const password = 'bar';
+			const oafGeoResource = new OafGeoResource('id', 'label', url, collectionId, 12345).setAuthenticationType(GeoResourceAuthenticationType.BAA);
+			const configServiceSpy = spyOn(configService, 'getValueAsPath').withArgs('BACKEND_URL').and.returnValue('BACKEND_URL/');
+			const httpServiceSpy = spyOn(httpService, 'post')
+				.withArgs('BACKEND_URL/oaf/getFilterCapabilities', JSON.stringify({ url, collectionId, username, password }), MediaType.JSON)
+				.and.resolveTo(new Response(JSON.stringify(mockResponsePayload)));
+			const baaCredentialSpy = spyOn(baaCredentialService, 'get').withArgs(url).and.returnValue({ username, password });
+
+			const result = await bvvOafFilterCapabilitiesProvider(oafGeoResource);
+
+			expect(result).toEqual(mockResponsePayload);
+
+			expect(configServiceSpy).toHaveBeenCalled();
+			expect(httpServiceSpy).toHaveBeenCalled();
+			expect(baaCredentialSpy).toHaveBeenCalled();
+		});
+
+		it('rejects with an error when credentials are not available', async () => {
+			const url = 'https://some.url/oaf';
+			const collectionId = 'collectionId';
+			const username = 'foo';
+			const password = 'bar';
+			const oafGeoResource = new OafGeoResource('id', 'label', url, collectionId, 12345).setAuthenticationType(GeoResourceAuthenticationType.BAA);
+			const configServiceSpy = spyOn(configService, 'getValueAsPath').withArgs('BACKEND_URL').and.returnValue('BACKEND_URL/');
+			const httpServiceSpy = spyOn(httpService, 'post')
+				.withArgs('BACKEND_URL/oaf/getFilterCapabilities', JSON.stringify({ url, collectionId, username, password }), MediaType.JSON)
+				.and.resolveTo(new Response(JSON.stringify(mockResponsePayload)));
+			const baaCredentialSpy = spyOn(baaCredentialService, 'get').withArgs(url).and.returnValue(null);
+
+			await expectAsync(bvvOafFilterCapabilitiesProvider(oafGeoResource)).toBeRejectedWithError(
+				"Fetching of filter capabilities failed. Credential for 'https://some.url/oaf' not found"
+			);
+
+			expect(configServiceSpy).toHaveBeenCalled();
+			expect(httpServiceSpy).not.toHaveBeenCalled();
+			expect(baaCredentialSpy).toHaveBeenCalled();
+		});
+	});
+
+	describe('OafFilterCapabilities endpoint responds with any other HTTP status', () => {
+		it('rejects with an error ', async () => {
+			const url = 'https://some.url/oaf';
+			const collectionId = 'collectionId';
+			const oafGeoResource = new OafGeoResource('id', 'label', url, collectionId, 12345);
+			const configServiceSpy = spyOn(configService, 'getValueAsPath').withArgs('BACKEND_URL').and.returnValue('BACKEND_URL/');
+			const httpServiceSpy = spyOn(httpService, 'post')
+				.withArgs('BACKEND_URL/oaf/getFilterCapabilities', JSON.stringify({ url, collectionId }), MediaType.JSON)
+				.and.resolveTo(new Response(null, { status: 400 }));
+
+			await expectAsync(bvvOafFilterCapabilitiesProvider(oafGeoResource)).toBeRejectedWithError(
+				"Filter capabilities for 'https://some.url/oaf' could not be loaded: Http-Status 400"
+			);
+
+			expect(configServiceSpy).toHaveBeenCalled();
+			expect(httpServiceSpy).toHaveBeenCalled();
+		});
+	});
+});
 
 describe('bvvOafGeoResourceProvider', () => {
 	const configService = {
@@ -25,6 +163,10 @@ describe('bvvOafGeoResourceProvider', () => {
 			.registerSingleton('HttpService', httpService)
 			.registerSingleton('ProjectionService', projectionService)
 			.registerSingleton('BaaCredentialService', baaCredentialService);
+	});
+
+	afterAll(() => {
+		$injector.reset();
 	});
 
 	const mockResponsePayload = [
@@ -62,7 +204,6 @@ describe('bvvOafGeoResourceProvider', () => {
 
 	it('returns a OafGeoResource for each OAF collection without extra ImportOafOptions', async () => {
 		const url = 'https://some.url/oaf';
-
 		const configServiceSpy = spyOn(configService, 'getValueAsPath').withArgs('BACKEND_URL').and.returnValue('BACKEND_URL/');
 		const httpServiceSpy = spyOn(httpService, 'post')
 			.withArgs('BACKEND_URL/oaf/getCollections', JSON.stringify({ url }), MediaType.JSON)
@@ -98,7 +239,6 @@ describe('bvvOafGeoResourceProvider', () => {
 		const result = await bvvOafGeoResourceProvider(url, { ...defaultImportOafOptions, collections: ['id1'] });
 
 		expect(result).toHaveSize(1);
-
 		expect(result[0].id).toBe('http://url1/||id1');
 		expect(result[0].label).toBe('title1');
 		expect(result[0].collectionId).toBe('id1');
@@ -153,13 +293,14 @@ describe('bvvOafGeoResourceProvider', () => {
 			await expectAsync(bvvOafGeoResourceProvider(url, { ...defaultImportOafOptions, isAuthenticated: true })).toBeRejectedWithError(
 				"Import of OAF service failed. Credential for 'https://some.url/oaf' not found"
 			);
+
 			expect(configServiceSpy).toHaveBeenCalled();
 			expect(httpServiceSpy).not.toHaveBeenCalled();
 			expect(baaCredentialSpy).toHaveBeenCalled();
 		});
 	});
 
-	describe('OAF service responds with HTTP status 404', () => {
+	describe('OafCollection endpoint responds with HTTP status 404', () => {
 		it('returns an empty array', async () => {
 			const url = 'https://some.url/oaf';
 			const configServiceSpy = spyOn(configService, 'getValueAsPath').withArgs('BACKEND_URL').and.returnValue('BACKEND_URL/');
@@ -170,12 +311,11 @@ describe('bvvOafGeoResourceProvider', () => {
 			const result = await bvvOafGeoResourceProvider(url, defaultImportOafOptions);
 
 			expect(result).toHaveSize(0);
-
 			expect(configServiceSpy).toHaveBeenCalled();
 			expect(httpServiceSpy).toHaveBeenCalled();
 		});
 	});
-	describe('OAF service responds with any other HTTP status', () => {
+	describe('OafCollection endpoint responds with any other HTTP status', () => {
 		it('rejects with an error ', async () => {
 			const url = 'https://some.url/oaf';
 			const configServiceSpy = spyOn(configService, 'getValueAsPath').withArgs('BACKEND_URL').and.returnValue('BACKEND_URL/');
