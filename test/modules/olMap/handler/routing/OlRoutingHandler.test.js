512d6641e850a884e93cb113a0d18864a3936f0e
Thomas Aulinger <49945713+taulinger@users.noreply.github.com>
2025-06-17 14:29:38 +0200

    Revert "RoutingStyle: prevent rendering of large icons (#3113)" (#3139)
    This reverts commit d9894b1faf21e889c2a0d4371dcbbe4995ea382d.

--- test/modules/olMap/handler/routing/OlRoutingHandler.test.js
+++ test/modules/olMap/handler/routing/OlRoutingHandler.test.js
@@ -19,7 +19,7 @@ import { Vector } from 'ol/layer';
 import { Modify, Select, Translate } from 'ol/interaction';
 import { setCategory, setHighlightedSegments, setIntermediate, setWaypoints } from '../../../../../src/store/routing/routing.action';
 import MapBrowserEventType from 'ol/MapBrowserEventType';
-import { Feature, MapBrowserEvent, MapEvent } from 'ol';
+import { Feature, MapBrowserEvent } from 'ol';
 import Event from 'ol/events/Event';
 import { LineString, Point } from 'ol/geom';
 import { ModifyEvent } from 'ol/interaction/Modify';
@@ -170,7 +170,6 @@ describe('OlRoutingHandler', () => {
 		const getModifyOptionsSpy = spyOn(instanceUnderTest, '_getModifyOptions').and.callThrough();
 		const layer = instanceUnderTest.activate(map);
 		await TestUtils.timeout();
-		map.dispatchEvent(new MapEvent('rendercomplete', map, null));
 		return { map, instanceUnderTest, store, layer, getSelectOptionsSpy, getModifyOptionsSpy };
 	};
 
@@ -320,7 +319,6 @@ describe('OlRoutingHandler', () => {
 
 				await TestUtils.timeout();
 				expect(instanceUnderTest._catId).toBe(catId);
-				map.dispatchEvent(new MapEvent('rendercomplete', map, null));
 				expect(requestRouteFromCoordinatesSpy).toHaveBeenCalledWith(coordinates, RoutingStatusCodes.Ok);
 			});
 		});
@@ -408,13 +406,12 @@ describe('OlRoutingHandler', () => {
 					[44, 55]
 				];
 				const catId = 'catId';
-				const { map, instanceUnderTest } = await newTestInstance();
+				const { instanceUnderTest } = await newTestInstance();
 				const requestRouteFromCoordinatesSpy = spyOn(instanceUnderTest, '_requestRouteFromCoordinates');
 
 				setCategory(catId);
 				await TestUtils.timeout();
 				expect(instanceUnderTest._catId).toBe(catId);
-				map.dispatchEvent(new MapEvent('rendercomplete', map, null));
 				expect(requestRouteFromCoordinatesSpy).toHaveBeenCalledWith([], RoutingStatusCodes.Start_Destination_Missing);
 
 				requestRouteFromCoordinatesSpy.calls.reset();
@@ -422,7 +419,6 @@ describe('OlRoutingHandler', () => {
 				setWaypoints(coordinates);
 				await TestUtils.timeout();
 				expect(instanceUnderTest._catId).toBe(catId);
-				map.dispatchEvent(new MapEvent('rendercomplete', map, null));
 				expect(requestRouteFromCoordinatesSpy).toHaveBeenCalledWith(coordinates, RoutingStatusCodes.Ok);
 			});
 		});
@@ -453,7 +449,7 @@ describe('OlRoutingHandler', () => {
 					[22, 55],
 					[33, 66]
 				];
-				const { map, instanceUnderTest } = await newTestInstance({
+				const { instanceUnderTest } = await newTestInstance({
 					status: RoutingStatusCodes.Ok
 				});
 				const addIntermediateSpy = spyOn(instanceUnderTest, '_addIntermediate').and.returnValue(coordinates);
@@ -461,7 +457,6 @@ describe('OlRoutingHandler', () => {
 
 				setIntermediate([22, 55]);
 				await TestUtils.timeout();
-				map.dispatchEvent(new MapEvent('rendercomplete', map, null));
 				expect(addIntermediateSpy).toHaveBeenCalledWith([22, 55], instanceUnderTest._routeLayerCopy);
 				expect(requestRouteFromCoordinatesSpy).toHaveBeenCalledWith(coordinates, RoutingStatusCodes.Ok);
 			});
