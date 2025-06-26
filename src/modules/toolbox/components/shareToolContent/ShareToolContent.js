93f99aaf5087ca68a5cd4d1549aa2ee7c75545bb
Schlemmer, Thilo (LDBV) <2117317+thiloSchlemmer@users.noreply.github.com>
2025-06-26 13:57:08 +0200

    update doc

--- src/modules/toolbox/components/shareToolContent/ShareToolContent.js
+++ src/modules/toolbox/components/shareToolContent/ShareToolContent.js
@@ -129,7 +129,7 @@ export class ShareToolContent extends AbstractToolContent {
 
 						await this._window.navigator.share(shareData);
 					} catch (e) {
-						// In some rare cases we need a fallback. this happens when the webbrowser can basically
+						// In some rare cases we need a fallback. This happens when the webbrowser can basically
 						// use the share-API but enterprise policies on operating system level rejects the calls,
 						// due to missing user privileges.
 						if (!(e instanceof DOMException && e.name === 'AbortError')) {
