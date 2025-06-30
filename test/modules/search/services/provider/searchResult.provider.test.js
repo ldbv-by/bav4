ccbdfcb14975f9dc8fd242542e3661b7c0ef31da
herrmutig <28516408+herrmutig@users.noreply.github.com>
2025-06-30 14:42:46 +0200

    undo unwanted prettier changes

--- test/modules/search/services/provider/searchResult.provider.test.js
+++ test/modules/search/services/provider/searchResult.provider.test.js
@@ -157,7 +157,7 @@ describe('SearchResult provider', () => {
 
 			const searchResult0 = searchResults[0];
 
-			expect(searchResult0 instanceof LocationSearchResult).toBeTrue(), expect(searchResult0.label).toBe('Wasserburger Weg, Günzburg');
+			(expect(searchResult0 instanceof LocationSearchResult).toBeTrue(), expect(searchResult0.label).toBe('Wasserburger Weg, Günzburg'));
 			expect(searchResult0.labelFormatted).toBe('<b>Wasserburger</b> <b>Weg</b>, Günzburg');
 			expect(searchResult0.center).toEqual([10.270116669125855, 48.44638557638974]);
 			expect(searchResult0.extent).toEqual([10.268321055918932, 48.441788353957236, 10.271912282332778, 48.450982798822224]);
@@ -254,7 +254,7 @@ describe('SearchResult provider', () => {
 
 			const searchResult0 = searchResults[0];
 
-			expect(searchResult0 instanceof CadastralParcelSearchResult).toBeTrue(), expect(searchResult0.label).toBe('foo, bar');
+			(expect(searchResult0 instanceof CadastralParcelSearchResult).toBeTrue(), expect(searchResult0.label).toBe('foo, bar'));
 			expect(searchResult0.labelFormatted).toBe('<b>foo</b>, bar');
 			expect(searchResult0.center).toEqual([10.270116669125855, 48.44638557638974]);
 			expect(searchResult0.id).not.toBeNull();
@@ -278,7 +278,7 @@ describe('SearchResult provider', () => {
 
 			const searchResult0 = searchResults[0];
 
-			expect(searchResult0 instanceof CadastralParcelSearchResult).toBeTrue(), expect(searchResult0.label).toBe('foo, bar');
+			(expect(searchResult0 instanceof CadastralParcelSearchResult).toBeTrue(), expect(searchResult0.label).toBe('foo, bar'));
 			expect(searchResult0.labelFormatted).toBe('<b>foo</b>, bar');
 			expect(searchResult0.center).toEqual([10.270116669125855, 48.44638557638974]);
 			expect(searchResult0.extent).toEqual([0, 1, 2, 3]);
