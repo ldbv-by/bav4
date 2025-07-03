94e995bb0bf8b8f31cae1d1dc96ce87241cae480
Thomas Aulinger <49945713+taulinger@users.noreply.github.com>
2025-07-03 09:49:02 +0200

    add a test panel for VectorData styling (#3184)
    * add a test panel for VectorData styling
    
    * cleanup
    
    * cleanup

--- src/modules/utils/components/showCase/index.js
+++ src/modules/utils/components/showCase/index.js
@@ -1,4 +1,8 @@
+import { VectorDataStylingTestPanel } from './testPanels/VectorDataStylingTestPanel';
 import { ShowCase } from './ShowCase';
+if (!window.customElements.get(VectorDataStylingTestPanel.tag)) {
+	window.customElements.define(VectorDataStylingTestPanel.tag, VectorDataStylingTestPanel);
+}
 if (!window.customElements.get(ShowCase.tag)) {
 	window.customElements.define(ShowCase.tag, ShowCase);
 }
