211447df5dc22dfaedb0f13215ac5a0069fd9cc9
Thomas Aulinger <taulinger@gmx.de>
2025-09-18 15:16:54 +0200

    use ADMIN_PAGE_ACCESS_TOKEN

--- test/e2e/admin.page.spec.js
+++ test/e2e/admin.page.spec.js
@@ -4,12 +4,13 @@ const templateParameters = process.env.BACKEND_URL
 	? require(`../../src/assets/${process.env.DEFAULT_LANG || 'en'}.json`)
 	: require(`../../src/assets/standalone.json`);
 const BASE_URL = process.env.URL || 'http://localhost:8080';
+const ADMIN_PAGE_ACCESS_TOKEN = process.env.ADMIN_PAGE_ACCESS_TOKEN ?? '';
 
 test.describe('admin page', () => {
 	test.beforeEach(async ({ page }) => {
 		// Go to the starting url before each test.
 		// To avoid a redirect by our internal filters we append the corresponding query parameter
-		await page.goto(`${BASE_URL}/admin.html`);
+		await page.goto(`${BASE_URL}/admin.html?token${ADMIN_PAGE_ACCESS_TOKEN}`);
 	});
 
 	test.describe('when loaded', () => {
@@ -19,7 +20,7 @@ test.describe('admin page', () => {
 		});
 
 		test('should contain one top level ba-components', async ({ page }) => {
-			expect(await page.locator('body > *').count()).toBe(1);
+			expect(await page.locator('body > *').count()).linttoBe(1);
 
 			expect(await page.locator('ba-admin-ui').count()).toBe(1);
 		});
