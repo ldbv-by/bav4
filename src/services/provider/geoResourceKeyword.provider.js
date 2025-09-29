b180773cfcf6953fcca2e3b84bed9b05de57da8c
Thomas Aulinger <taulinger@gmx.de>
2025-09-29 19:05:23 +0200

    add keywordProvider provider and corresponding tests

---
+++ src/services/provider/geoResourceKeyword.provider.js
@@ -0,0 +1,26 @@
+/**
+ * @module services/provider/geoResourceKeyword_provider
+ */
+import { $injector } from '../../injection/index';
+
+/**
+ * Implementation of {@link module:services/GeoResourceService~keywordProvider}.
+ * @function
+ * @type {module:services/GeoResourceService~keywordProvider}
+ */
+export const getKeywordsForGeoResource = (geoResource) => {
+	const { TranslationService: translationService } = $injector.inject('TranslationService');
+	const translate = (key) => translationService.translate(key);
+
+	if (geoResource) {
+		const keywords = [...geoResource.authRoles];
+		if (geoResource.localData) {
+			keywords.push(translate('global_georesource_keyword_local'));
+		}
+		if (geoResource.isExternal()) {
+			keywords.push(translate('global_georesource_keyword_external'));
+		}
+		return keywords;
+	}
+	return [];
+};
