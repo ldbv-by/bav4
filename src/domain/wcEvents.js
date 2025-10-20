b23dbd6886c538ebca3bb541f82b1ca9c04d2e50
Thomas Aulinger <taulinger@gmx.de>
2025-10-20 18:26:30 +0200

    docs: update comments for WcEvents enum to improve clarity

--- src/domain/wcEvents.js
+++ src/domain/wcEvents.js
@@ -2,13 +2,13 @@
  * @module domain/wcEvents
  */
 /**
- * Enum which holds all events triggered by the public web component.
+ * Enum which holds all events triggered by the public Web Component
  * @readonly
  * @enum {String}
  */
 export const WcEvents = Object.freeze({
 	/**
-	 * Public web component loaded.
+	 * Web Component loaded. Technically this means the wrapped iframe was loaded successful.
 	 */
 	LOAD: 'ba-load',
 	/**
