973326d54380be0c7ab909dcd80d8afa0ac5301b
Thomas Aulinger <49945713+taulinger@users.noreply.github.com>
2025-11-12 10:44:08 +0100

    extend featureInfoItem.provider (#3398)
    * set default SRID to 4326again  in forGeoJSON method
    
    * add optional raw properties
    
    * add util toEwkt()
    
    * - use EWKT  as the data of the FeatureInfo geometry
    - include raw properties
    
    * use util toEwkt()
    
    * cleanup

--- src/domain/sourceType.js
+++ src/domain/sourceType.js
@@ -50,11 +50,10 @@ export class SourceType {
 	}
 
 	/**
-	 * @param {number} [srid=4326] The SRID of the GeoJSON. Defaults to 4326.
 	 * @returns {SourceType} SourceType for GeoJson
 	 */
-	static forGeoJSON(srid = 4326) {
-		return new SourceType(SourceTypeName.GEOJSON, null, srid);
+	static forGeoJSON() {
+		return new SourceType(SourceTypeName.GEOJSON, null, 4326);
 	}
 
 	/**
