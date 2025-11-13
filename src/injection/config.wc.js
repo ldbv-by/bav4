748ec239822330087c2c8ae236e7e5337e04694b
Thomas Aulinger <taulinger@gmx.de>
2025-11-13 13:12:19 +0100

    register ProjectionService and CoordinateService in injector configuration

--- src/injection/config.wc.js
+++ src/injection/config.wc.js
@@ -6,10 +6,16 @@ import { $injector } from '.';
 import { ProcessEnvConfigService } from '../services/ProcessEnvConfigService';
 import { wcModule } from '../modules/wc/components/injection/index';
 import { EnvironmentService } from '../services/EnvironmentService';
+import { MapService } from '../services/MapService';
+
+const noopServiceClass = {};
 
 $injector
 	.registerModule(wcModule)
 	.register('EnvironmentService', EnvironmentService)
+	.registerSingleton('ProjectionService', noopServiceClass)
+	.registerSingleton('CoordinateService', noopServiceClass)
+	.register('MapService', MapService)
 	.registerSingleton('ConfigService', new ProcessEnvConfigService())
 	.ready();
 
--- test/injection/config.wc.test.js
+++ test/injection/config.wc.test.js
@@ -6,10 +6,12 @@ import { Injector } from '../../src/injection/core/injector.js';
 describe('injector configuration', () => {
 	it('registers the expected dependencies', () => {
 		expect($injector.isReady()).toBeTrue();
-		expect($injector.count()).toBe(3);
+		expect($injector.count()).toBe(6);
 
 		expect($injector.getScope('StoreService')).toBe(Injector.SCOPE_SINGLETON);
 		expect($injector.getScope('EnvironmentService')).toBe(Injector.SCOPE_PERLOOKUP);
+		expect($injector.getScope('ProjectionService')).toBe(Injector.SCOPE_SINGLETON);
+		expect($injector.getScope('CoordinateService')).toBe(Injector.SCOPE_SINGLETON);
 		expect($injector.getScope('ConfigService')).toBe(Injector.SCOPE_SINGLETON);
 	});
 });
