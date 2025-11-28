bd76382a9601f40397576b88e8e45f1370e01c2f
herrmutig <28516408+herrmutig@users.noreply.github.com>
2025-11-28 17:18:11 +0100

    added title to object-info print button

--- test/modules/featureInfo/i18n/featureInfo.provider.test.js
+++ test/modules/featureInfo/i18n/featureInfo.provider.test.js
@@ -7,6 +7,7 @@ describe('i18n for featureInfo module', () => {
 		expect(map.featureInfo_close_button).toBe('Schließen');
 		expect(map.featureInfo_header).toBe('Objekt-Info');
 		expect(map.featureInfo_info).toBe('Keine Informationen verfügbar.<br>Bitte in die <b>Karte</b> klicken.');
+		expect(map.featureInfo_object_info_print_title).toBe('Objektinformationen drucken');
 	});
 
 	it('provides translation for en', () => {
@@ -15,10 +16,11 @@ describe('i18n for featureInfo module', () => {
 		expect(map.featureInfo_close_button).toBe('Close');
 		expect(map.featureInfo_header).toBe('Object Information');
 		expect(map.featureInfo_info).toBe('No information available.<br>Please click into the <b>map</b>.');
+		expect(map.featureInfo_object_info_print_title).toBe('Print object information');
 	});
 
 	it('contains the expected amount of entries', () => {
-		const expectedSize = 3;
+		const expectedSize = 4;
 		const deMap = provide('de');
 		const enMap = provide('en');
 
