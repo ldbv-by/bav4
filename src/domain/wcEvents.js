4233d25a5d835026afc1eaf0509aa4cefe6a14c5
Thomas Aulinger <taulinger@gmx.de>
2025-12-16 11:41:20 +0100

    add WC related definitions

--- src/domain/wcEvents.js
+++ src/domain/wcEvents.js
@@ -2,6 +2,8 @@
  * @module domain/wcEvents
  */
 
+import { QueryParameters } from './queryParameters';
+
 /**
  * Enumeration of custom DOM event names dispatched by the <bayern-atlas> web component.
  *
@@ -12,7 +14,6 @@
  *
  * @enum {string}
  * @readonly
- * @name WcEvents
  */
 export const WcEvents = Object.freeze({
 	LOAD: 'baLoad',
@@ -22,4 +23,40 @@ export const WcEvents = Object.freeze({
 	GEOMETRY_CHANGE: 'baGeometryChange',
 
 	FEATURE_SELECT: 'baFeatureSelect'
+});
+
+/**
+ * Defines a list of supported attributes
+ * @readonly
+ */
+export const WcAttributes = Object.freeze([
+	QueryParameters.LAYER,
+	QueryParameters.ZOOM,
+	QueryParameters.CENTER,
+	QueryParameters.ROTATION,
+	QueryParameters.LAYER_VISIBILITY,
+	QueryParameters.LAYER_OPACITY,
+	QueryParameters.EC_SRID,
+	QueryParameters.EC_GEOMETRY_FORMAT,
+	QueryParameters.EC_MAP_ACTIVATION,
+	QueryParameters.EC_LINK_TO_APP,
+	QueryParameters.EC_DRAW_TOOL
+]);
+
+/**
+ * Enumeration of message keys broadcast bay the public WebComponent
+ * @enum {string}
+ * @readonly
+ */
+export const WcMessageKeys = Object.freeze({
+	ADD_LAYER: 'addLayer',
+	MODIFY_LAYER: 'modifyLayer',
+	REMOVE_LAYER: 'removeLayer',
+	MODIFY_VIEW: 'modifyView',
+	ZOOM_TO_EXTENT: 'zoomToExtent',
+	ZOOM_TO_LAYER_EXTENT: 'zoomToLayerExtent',
+	ADD_MARKER: 'addMarker',
+	REMOVE_MARKER: 'removeMarker',
+	CLEAR_MARKERS: 'clearMarkers',
+	CLEAR_HIGHLIGHTS: 'clearHighlights'
 });
--- test/domain/wcEvents.test.js
+++ test/domain/wcEvents.test.js
@@ -1,12 +1,47 @@
-import { WcEvents } from '../../src/domain/wcEvents';
+import { QueryParameters } from '../../src/domain/queryParameters';
+import { WcAttributes, WcEvents, WcMessageKeys } from '../../src/domain/wcEvents';
 
 describe('WcEvents', () => {
-	it('provides an enum of all valid events pf the public web component', () => {
+	it('provides an enum of all valid events of the public web component', () => {
+		expect(Object.isFrozen(WcEvents)).toBeTrue();
 		expect(Object.keys(WcEvents).length).toBe(4);
 
 		expect(WcEvents.LOAD).toBe('baLoad');
 		expect(WcEvents.CHANGE).toBe('baChange');
 		expect(WcEvents.GEOMETRY_CHANGE).toBe('baGeometryChange');
 		expect(WcEvents.FEATURE_SELECT).toBe('baFeatureSelect');
+	});
+
+	it('provides a list of all supported attributes', () => {
+		expect(Object.isFrozen(WcAttributes)).toBeTrue();
+		expect(WcAttributes).toEqual([
+			QueryParameters.LAYER,
+			QueryParameters.ZOOM,
+			QueryParameters.CENTER,
+			QueryParameters.ROTATION,
+			QueryParameters.LAYER_VISIBILITY,
+			QueryParameters.LAYER_OPACITY,
+			QueryParameters.EC_SRID,
+			QueryParameters.EC_GEOMETRY_FORMAT,
+			QueryParameters.EC_MAP_ACTIVATION,
+			QueryParameters.EC_LINK_TO_APP,
+			QueryParameters.EC_DRAW_TOOL
+		]);
+	});
+
+	it('provides an enum of all valid message keys', () => {
+		expect(Object.isFrozen(WcMessageKeys)).toBeTrue();
+		expect(Object.keys(WcMessageKeys).length).toBe(10);
+
+		expect(WcMessageKeys.ADD_LAYER).toBe('addLayer');
+		expect(WcMessageKeys.MODIFY_LAYER).toBe('modifyLayer');
+		expect(WcMessageKeys.REMOVE_LAYER).toBe('removeLayer');
+		expect(WcMessageKeys.MODIFY_VIEW).toBe('modifyView');
+		expect(WcMessageKeys.ZOOM_TO_EXTENT).toBe('zoomToExtent');
+		expect(WcMessageKeys.ZOOM_TO_LAYER_EXTENT).toBe('zoomToLayerExtent');
+		expect(WcMessageKeys.ADD_MARKER).toBe('addMarker');
+		expect(WcMessageKeys.REMOVE_MARKER).toBe('removeMarker');
+		expect(WcMessageKeys.CLEAR_MARKERS).toBe('clearMarkers');
+		expect(WcMessageKeys.CLEAR_HIGHLIGHTS).toBe('clearHighlights');
 	});
 });
