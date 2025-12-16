d6721ef353a0c1764e698d5a124962ad4877532c
Thomas Aulinger <taulinger@gmx.de>
2025-12-16 13:40:25 +0100

    update doc

--- src/domain/queryParameters.js
+++ src/domain/queryParameters.js
@@ -174,10 +174,12 @@ export const QueryParameters = Object.freeze({
 	 */
 	EC_LINK_TO_APP: 'ec_link_to_app',
 	/**
+	 * @ignore
 	 * Designated SRID of returned coordinates (e.g. of geometries) (`integer`, e.g. 4326). Default is 4326.
 	 */
 	EC_SRID: 'ec_srid',
 	/**
+	 * @ignore
 	 * Designated Type (format) of returned features (`string`, one of `ewkt`, `kml`, `geojson`, `gpx`). Default is `ewkt`.
 	 */
 	EC_GEOMETRY_FORMAT: 'ec_geometry_format',
