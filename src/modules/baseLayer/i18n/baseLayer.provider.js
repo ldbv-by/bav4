07763de61ff511be2562d891f1a8f7f7e68f3435
alsturm <alexander_sturm@gmx.net>
2026-01-19 14:31:42 +0100

    change labels

--- src/modules/baseLayer/i18n/baseLayer.provider.js
+++ src/modules/baseLayer/i18n/baseLayer.provider.js
@@ -4,14 +4,16 @@ export const provide = (lang) => {
 			return {
 				//the first part of the snake_case key should be the name of the related module
 				baseLayer_switcher_header: 'Base maps',
-				baseLayer_container_category_raster: 'Raster',
-				baseLayer_container_category_vector: 'Vector',
 				baseLayer_container_scroll_button_next: 'Scroll next',
 				baseLayer_container_scroll_button_last: 'Scroll back',
 				baseLayer_container_category_standard: 'General',
+				baseLayer_container_scroll_button_standard: 'to General maps',
 				baseLayer_container_category_planung: 'Planning',
+				baseLayer_container_scroll_button_planung: 'to Planning maps',
 				baseLayer_container_category_freizeit: 'Leisure time',
+				baseLayer_container_scroll_button_freizeit: 'to Leisure maps',
 				baseLayer_container_category_historisch: 'Historical',
+				baseLayer_container_scroll_button_historisch: 'to Historical maps',
 				baseLayer_container_collapse_button_title: 'Collapse/Expand base map switcher'
 			};
 
@@ -19,14 +21,16 @@ export const provide = (lang) => {
 			return {
 				//the first part of the snake_case key should be the name of the related module
 				baseLayer_switcher_header: 'Basiskarten',
-				baseLayer_container_category_raster: 'Raster',
-				baseLayer_container_category_vector: 'Vektor',
 				baseLayer_container_scroll_button_next: 'Weiterblättern',
 				baseLayer_container_scroll_button_last: 'Zurückblättern',
 				baseLayer_container_category_standard: 'Allgemein',
+				baseLayer_container_scroll_button_standard: 'zu den Allgemeinen Karten',
 				baseLayer_container_category_planung: 'Planung',
+				baseLayer_container_scroll_button_planung: 'zu den Planungskarten',
 				baseLayer_container_category_freizeit: 'Freizeit',
+				baseLayer_container_scroll_button_freizeit: 'zu den Freizeitkarten',
 				baseLayer_container_category_historisch: 'Historisch',
+				baseLayer_container_scroll_button_historisch: 'zu den Historischen Karten',
 				baseLayer_container_collapse_button_title: 'Basiskarten-Umschalter ein-/ausklappen'
 			};
 
--- test/modules/baseLayer/i18n/baseLayer.provider.test.js
+++ test/modules/baseLayer/i18n/baseLayer.provider.test.js
@@ -5,15 +5,16 @@ describe('i18n for header module', () => {
 		const map = provide('de');
 
 		expect(map.baseLayer_switcher_header).toBe('Basiskarten');
-		expect(map.baseLayer_container_category_raster).toBe('Raster');
-		expect(map.baseLayer_container_category_vector).toBe('Vektor');
 		expect(map.baseLayer_container_scroll_button_next).toBe('Weiterblättern');
 		expect(map.baseLayer_container_scroll_button_last).toBe('Zurückblättern');
 		expect(map.baseLayer_container_category_standard).toBe('Allgemein');
+		expect(map.baseLayer_container_scroll_button_standard).toBe('zu den Allgemeinen Karten');
 		expect(map.baseLayer_container_category_planung).toBe('Planung');
-		expect(map.baseLayer_container_category_vector).toBe('Vektor');
+		expect(map.baseLayer_container_scroll_button_planung).toBe('zu den Planungskarten');
 		expect(map.baseLayer_container_category_freizeit).toBe('Freizeit');
+		expect(map.baseLayer_container_scroll_button_freizeit).toBe('zu den Freizeitkarten');
 		expect(map.baseLayer_container_category_historisch).toBe('Historisch');
+		expect(map.baseLayer_container_scroll_button_historisch).toBe('zu den Historischen Karten');
 		expect(map.baseLayer_container_collapse_button_title).toBe('Basiskarten-Umschalter ein-/ausklappen');
 	});
 
@@ -21,20 +22,21 @@ describe('i18n for header module', () => {
 		const map = provide('en');
 
 		expect(map.baseLayer_switcher_header).toBe('Base maps');
-		expect(map.baseLayer_container_category_raster).toBe('Raster');
-		expect(map.baseLayer_container_category_vector).toBe('Vector');
 		expect(map.baseLayer_container_scroll_button_next).toBe('Scroll next');
-		expect(map.baseLayer_container_scroll_button_last).toBe('scroll back');
+		expect(map.baseLayer_container_scroll_button_last).toBe('Scroll back');
 		expect(map.baseLayer_container_category_standard).toBe('General');
+		expect(map.baseLayer_container_scroll_button_standard).toBe('to General maps');
 		expect(map.baseLayer_container_category_planung).toBe('Planning');
-		expect(map.baseLayer_container_category_vector).toBe('Vector');
+		expect(map.baseLayer_container_scroll_button_planung).toBe('to Planning maps');
 		expect(map.baseLayer_container_category_freizeit).toBe('Leisure time');
+		expect(map.baseLayer_container_scroll_button_freizeit).toBe('to Leisure maps');
 		expect(map.baseLayer_container_category_historisch).toBe('Historical');
+		expect(map.baseLayer_container_scroll_button_historisch).toBe('to Historical maps');
 		expect(map.baseLayer_container_collapse_button_title).toBe('Collapse/Expand base map switcher');
 	});
 
 	it('contains the expected amount of entries', () => {
-		const expectedSize = 10;
+		const expectedSize = 12;
 		const deMap = provide('de');
 		const enMap = provide('en');
 
