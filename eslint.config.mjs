67a8fb28a392af850fb15b398bf9b465f70e487c
herrmutig <28516408+herrmutig@users.noreply.github.com>
2026-02-02 13:36:39 +0100

    migrated to eslint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import esImport from 'eslint-plugin-import';
+import promisePlugin from 'eslint-plugin-promise';
+import stylistic from '@stylistic/eslint-plugin';
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+	{
+		languageOptions: {
+			ecmaVersion: 2023,
+			globals: { ...globals.browser, ...globals.node, ...globals.jasmine }
+		},
+		plugins: {
+			js,
+			'@stylistic': stylistic,
+			esImport,
+			promise: promisePlugin
+		},
+		extends: [js.configs.recommended, esImport.flatConfigs.errors],
+		rules: {
+			'@stylistic/no-mixed-spaces-and-tabs': 'off',
+			'@stylistic/space-infix-ops': ['error', { int32Hint: false }],
+			'promise/prefer-await-to-then': 'error',
+			'esImport/no-default-export': 'error',
+			'no-console': ['error', { allow: ['warn', 'error'] }],
+			eqeqeq: ['error', 'smart'],
+			'no-var': 'error',
+			'prefer-const': 'error'
+		}
+	}
+]);
