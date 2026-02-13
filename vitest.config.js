f68151a57dd75afa7e9df579607bf795ef0f2817
herrmutig <28516408+herrmutig@users.noreply.github.com>
2026-02-13 12:07:48 +0100

    migrated common Button.js

---
+++ vitest.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vitest/config';
+import { playwright } from '@vitest/browser-playwright';
+import { resolve } from 'node:path';
+
+export default defineConfig({
+	test: {
+		include: ['**/components/*Button.test.js'],
+		// resets spies and mocks after each test (same mocks are shared across tests in the same file)
+		mockReset: true,
+		globals: true,
+		watch: false,
+		css: {
+			include: /.+/
+		},
+		dir: './vitest',
+		browser: {
+			provider: playwright(),
+			enabled: true,
+			headless: true,
+			instances: [{ browser: 'chromium' }, { browser: 'firefox' }, { browser: 'webkit' }],
+			screenshotFailures: false
+		}
+	},
+
+	resolve: {
+		alias: {
+			'@src': resolve(__dirname, './src'),
+			'@test': resolve(__dirname, './test')
+		}
+	}
+});
