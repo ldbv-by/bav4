118dd88a840bc243138562021cb2906cb095f971
Thomas Aulinger <taulinger@gmx.de>
2026-03-09 11:46:15 +0100

    port to vitest

--- vitest/service/provider/elevation.provider.test.js
+++ vitest/service/provider/elevation.provider.test.js
@@ -1,5 +1,5 @@
-import { $injector } from '../../../src/injection';
-import { loadBvvElevation } from '../../../src/services/provider/elevation.provider';
+import { $injector } from '@src/injection';
+import { loadBvvElevation } from '@src/services/provider/elevation.provider';
 
 describe('Elevation provider', () => {
 	describe('Bvv Elevation provider', () => {
@@ -20,22 +20,22 @@ describe('Elevation provider', () => {
 		it('loads an elevation', async () => {
 			const backendUrl = 'https://backend.url';
 			const elevationMock = { z: 5 };
-			const configServiceSpy = spyOn(configService, 'getValueAsPath').withArgs('BACKEND_URL').and.returnValue(backendUrl);
-			const httpServiceSpy = spyOn(httpService, 'get').and.returnValue(Promise.resolve(new Response(JSON.stringify(elevationMock))));
+			const configServiceSpy = vi.spyOn(configService, 'getValueAsPath').mockReturnValue(backendUrl);
+			const httpServiceSpy = vi.spyOn(httpService, 'get').mockResolvedValue(new Response(JSON.stringify(elevationMock)));
 
-			await expectAsync(loadBvvElevation(coordinateMock)).toBeResolvedTo(elevationMock.z);
-			expect(configServiceSpy).toHaveBeenCalled();
-			expect(httpServiceSpy).toHaveBeenCalled();
+			expect(loadBvvElevation(coordinateMock)).resolves.toEqual(elevationMock.z);
+			expect(configServiceSpy).toHaveBeenCalledWith('BACKEND_URL');
+			expect(httpServiceSpy).toHaveBeenCalledWith(`${backendUrl}dem/elevation/21/42`);
 		});
 
 		it('throws an error when backend cannot fulfill', async () => {
 			const backendUrl = 'https://backend.url';
-			const configServiceSpy = spyOn(configService, 'getValueAsPath').withArgs('BACKEND_URL').and.returnValue(backendUrl);
-			const httpServiceSpy = spyOn(httpService, 'get').and.returnValue(Promise.resolve(new Response(null, { status: 500 })));
+			const configServiceSpy = vi.spyOn(configService, 'getValueAsPath').mockReturnValue(backendUrl);
+			const httpServiceSpy = vi.spyOn(httpService, 'get').mockResolvedValue(new Response(null, { status: 500 }));
 
-			await expectAsync(loadBvvElevation(coordinateMock)).toBeRejectedWithError('Elevation could not be retrieved: Http-Status 500');
-			expect(configServiceSpy).toHaveBeenCalled();
-			expect(httpServiceSpy).toHaveBeenCalled();
+			expect(loadBvvElevation(coordinateMock)).rejects.toThrow('Elevation could not be retrieved: Http-Status 500');
+			expect(configServiceSpy).toHaveBeenCalledWith('BACKEND_URL');
+			expect(httpServiceSpy).toHaveBeenCalledWith(`${backendUrl}dem/elevation/21/42`);
 		});
 	});
 });
