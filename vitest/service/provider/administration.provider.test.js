193fe8eb48ec58ecd14d97b53678a68362b28669
Thomas Aulinger <taulinger@gmx.de>
2026-03-16 16:19:57 +0100

    cleanup

--- vitest/service/provider/administration.provider.test.js
+++ vitest/service/provider/administration.provider.test.js
@@ -20,16 +20,12 @@ describe('Administration provider', () => {
 		it('loads an Administration object without a parcel', async () => {
 			const backendUrl = 'https://backend.url';
 			const administrationMock = { gemeinde: 'gemeinde', gemarkung: 'gemarkung', flstBezeichnung: null };
-			const configServiceSpy = vi.spyOn(configService, 'getValueAsPath').mockImplementation((arg) => {
-				if (arg === 'BACKEND_URL') return backendUrl;
-
-				throw new Error('Invalid Argument for spy.');
-			});
+			const configServiceSpy = vi.spyOn(configService, 'getValueAsPath').mockReturnValue(backendUrl);
 			const httpServiceSpy = vi.spyOn(httpService, 'get').mockResolvedValue(new Response(JSON.stringify(administrationMock)));
 
 			const administration = await loadBvvAdministration(coordinateMock);
 
-			expect(configServiceSpy).toHaveBeenCalled();
+			expect(configServiceSpy).toHaveBeenCalledWith('BACKEND_URL');
 			expect(httpServiceSpy).toHaveBeenCalled();
 			expect(administration.community).toBe(administrationMock.gemeinde);
 			expect(administration.district).toBe(administrationMock.gemarkung);
@@ -39,16 +35,12 @@ describe('Administration provider', () => {
 		it('loads an Administration object containing a parcel', async () => {
 			const backendUrl = 'https://backend.url';
 			const administrationMock = { gemeinde: 'gemeinde', gemarkung: 'gemarkung', flstBezeichnung: '12345' };
-			const configServiceSpy = vi.spyOn(configService, 'getValueAsPath').mockImplementation((arg) => {
-				if (arg === 'BACKEND_URL') return backendUrl;
-
-				throw new Error('Invalid Argument for spy.');
-			});
+			const configServiceSpy = vi.spyOn(configService, 'getValueAsPath').mockReturnValue(backendUrl);
 			const httpServiceSpy = vi.spyOn(httpService, 'get').mockResolvedValue(new Response(JSON.stringify(administrationMock)));
 
 			const administration = await loadBvvAdministration(coordinateMock);
 
-			expect(configServiceSpy).toHaveBeenCalled();
+			expect(configServiceSpy).toHaveBeenCalledWith('BACKEND_URL');
 			expect(httpServiceSpy).toHaveBeenCalled();
 			expect(administration.community).toBe(administrationMock.gemeinde);
 			expect(administration.district).toBe(administrationMock.gemarkung);
@@ -57,30 +49,22 @@ describe('Administration provider', () => {
 
 		it('return null when for status 404', async () => {
 			const backendUrl = 'https://backend.url';
-			const configServiceSpy = vi.spyOn(configService, 'getValueAsPath').mockImplementation((arg) => {
-				if (arg === 'BACKEND_URL') return backendUrl;
-
-				throw new Error('Invalid Argument for spy.');
-			});
+			const configServiceSpy = vi.spyOn(configService, 'getValueAsPath').mockReturnValue(backendUrl);
 			const httpServiceSpy = vi.spyOn(httpService, 'get').mockResolvedValue(new Response(null, { status: 404 }));
 
 			await expect(loadBvvAdministration(coordinateMock)).resolves.toBe(null);
 
-			expect(configServiceSpy).toHaveBeenCalled();
+			expect(configServiceSpy).toHaveBeenCalledWith('BACKEND_URL');
 			expect(httpServiceSpy).toHaveBeenCalled();
 		});
 
 		it('throws error when backend request cannot be fulfilled', async () => {
 			const backendUrl = 'https://backend.url';
-			const configServiceSpy = vi.spyOn(configService, 'getValueAsPath').mockImplementation((arg) => {
-				if (arg === 'BACKEND_URL') return backendUrl;
-
-				throw new Error('Invalid Argument for spy.');
-			});
+			const configServiceSpy = vi.spyOn(configService, 'getValueAsPath').mockReturnValue(backendUrl);
 			const httpServiceSpy = vi.spyOn(httpService, 'get').mockResolvedValue(new Response(null, { status: 500 }));
 
 			await expect(loadBvvAdministration(coordinateMock)).rejects.toThrow('Administration could not be retrieved: Http-Status 500');
-			expect(configServiceSpy).toHaveBeenCalled();
+			expect(configServiceSpy).toHaveBeenCalledWith('BACKEND_URL');
 			expect(httpServiceSpy).toHaveBeenCalled();
 		});
 	});
