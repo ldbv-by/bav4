6467940595f13563a2f3c16842dae94639083cd2
Thomas Aulinger <taulinger@gmx.de>
2026-03-25 18:06:15 +0100

    Refactor Vitest configuration to support dynamic browser instances and log output to a file

--- vitest.config.js
+++ vitest.config.js
@@ -1,41 +1,56 @@
 import { defineConfig } from 'vitest/config';
 import { playwright } from '@vitest/browser-playwright';
 import { resolve } from 'node:path';
+import { appendFileSync } from 'fs';
+import { loadEnv } from 'vite';
 
-export default defineConfig({
-	test: {
-		include: ['**/*.test.js'],
-		// resets spies and mocks after each test (same mocks are shared across tests in the same file)
-		mockReset: true,
-		globals: true,
-		watch: false,
-		css: {
-			include: /.+/
-		},
-		dir: './vitest',
-		browser: {
-			provider: playwright(),
-			enabled: true,
-			headless: true,
-			instances: [{ browser: 'chromium' }, { browser: 'firefox' }, { browser: 'webkit' }],
-			screenshotFailures: false
-		},
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	const instances = env.VITEST_BROWSERS
+		? env.VITEST_BROWSERS.split(',').map((value) => ({ browser: value }))
+		: [{ browser: 'chromium' }, { browser: 'firefox' }, { browser: 'webkit' }];
 
-		alias: {
-			'@chunk': resolve(__dirname, './test/chunkUtil')
+	return {
+		test: {
+			include: ['**/*.test.js'],
+			// resets spies and mocks after each test (same mocks are shared across tests in the same file)
+			mockReset: true,
+			globals: true,
+			watch: false,
+			css: {
+				include: /.+/
+			},
+			dir: './vitest',
+			browser: {
+				provider: playwright(),
+				enabled: true,
+				headless: true,
+				instances,
+				screenshotFailures: false
+			},
+
+			alias: {
+				'@chunk': resolve(__dirname, './test/chunkUtil')
+			},
+			coverage: {
+				enabled: true,
+				provider: 'istanbul',
+				reporter: ['text-summary', 'lcov']
+			},
+			onConsoleLog(log, type) {
+				// Append logs to a file
+				if (env.TEST_SINGLE_FILE) {
+					appendFileSync('console.log', `[${type}] ${log}\n`);
+				}
+				return false;
+			}
 		},
-		coverage: {
-			enabled: true,
-			provider: 'istanbul',
-			reporter: ['text-summary', 'lcov']
+		resolve: {
+			alias: {
+				'@chunk': resolve(__dirname, './test/chunkUtil'),
+				'@src': resolve(__dirname, './src'),
+				'@test': resolve(__dirname, './test')
+			}
 		}
-	},
-
-	resolve: {
-		alias: {
-			'@chunk': resolve(__dirname, './test/chunkUtil'),
-			'@src': resolve(__dirname, './src'),
-			'@test': resolve(__dirname, './test')
-		}
-	}
+	};
 });
