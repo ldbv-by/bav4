0452768ffb83784b5f085fec359df1fb8d3a828f
Schlemmer, Thilo (LDBV) <2117317+thiloSchlemmer@users.noreply.github.com>
2026-04-28 14:18:02 +0200

    update test

--- test/modules/layerManager/i18n/layerManager.provider.test.js
+++ test/modules/layerManager/i18n/layerManager.provider.test.js
@@ -119,7 +119,7 @@ describe('i18n for layer-manager', () => {
 	});
 
 	it('contains the expected amount of entries', () => {
-		const expectedSize = 54;
+		const expectedSize = 57;
 		const deMap = provide('de');
 		const enMap = provide('en');
 
