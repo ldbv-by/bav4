4e3c9840fa06fbe0c6a00ecb3a66a27a5e27b497
Thomas Aulinger <taulinger@gmx.de>
2026-07-27 16:24:43 +0200

    add GeoResourceTypeBadge component with rendering logic and tests

---
+++ src/modules/geoResourceInfo/components/GeoResourceTypeBadge.js
@@ -0,0 +1,69 @@
+/**
+ * @module modules/geoResourceInfo/components/GeoResourceTypeBadge
+ */
+import { $injector } from '@src/injection';
+import { MvuElement } from '@src/modules/MvuElement';
+import { html, nothing } from 'lit-html';
+
+const UPDATE_GEORESOURCE_ID = 'update_georesource_id';
+
+/**
+ * Component rendering a badge for a `GeoResource` type.
+ *
+ * The component listens for a `geoResourceId` update and displays
+ * a localized badge label and tooltip based on the type of the referenced `GeoResource`.
+ * @property {String} geoResourceId - The ID of the referenced GeoResource
+ * @class
+ */
+export class GeoResourceTypeBadge extends MvuElement {
+	#translationService;
+	#geoResourceService;
+
+	constructor() {
+		super({ geoResourceId: null });
+		const { TranslationService: translationService, GeoResourceService: geoResourceService } = $injector.inject(
+			'TranslationService',
+			'GeoResourceService'
+		);
+		this.#translationService = translationService;
+		this.#geoResourceService = geoResourceService;
+	}
+	update(type, data, model) {
+		switch (type) {
+			case UPDATE_GEORESOURCE_ID:
+				return { ...model, geoResourceId: data };
+		}
+	}
+
+	/**
+	 *@override
+	 */
+	createView(model) {
+		const { geoResourceId } = model;
+		const geoResource = this.#geoResourceService.byId(geoResourceId);
+
+		if (geoResource) {
+			const type = geoResource.getType().description;
+
+			return html`
+				<ba-badge
+					.color=${'var(--text5)'}
+					.label=${this.#translationService.translate(`geoResourceInfo_typeBadge_label_${type}`)}
+					.title=${this.#translationService.translate(`geoResourceInfo_typeBadge_desc_${type}`)}
+				></ba-badge>
+			`;
+		}
+		return nothing;
+	}
+	static get tag() {
+		return 'ba-georesource-type-badge';
+	}
+
+	set geoResourceId(value) {
+		this.signal(UPDATE_GEORESOURCE_ID, value);
+	}
+
+	get geoResourceId() {
+		return this.getModel().geoResourceId;
+	}
+}
---
+++ test/modules/geoResourceInfo/components/GeoResourceTypeBadge.test.js
@@ -0,0 +1,67 @@
+import { GeoResourceTypeBadge } from '@src/modules/geoResourceInfo/components/GeoResourceTypeBadge';
+import { TestUtils } from '@test/test-utils';
+import { $injector } from '@src/injection';
+import { WmsGeoResource } from '@src/domain/geoResources';
+import { expect } from 'vitest';
+
+window.customElements.define(GeoResourceTypeBadge.tag, GeoResourceTypeBadge);
+
+describe('GeoResourceTypeBadge', () => {
+	const geoResourceServiceMock = {
+		byId() {}
+	};
+
+	const setup = () => {
+		TestUtils.setupStoreAndDi();
+		$injector.registerSingleton('GeoResourceService', geoResourceServiceMock);
+		$injector.registerSingleton('TranslationService', { translate: (key) => key });
+		return TestUtils.render(GeoResourceTypeBadge.tag);
+	};
+
+	describe('properties', () => {
+		it('provides getters', async () => {
+			await setup();
+			const element = new GeoResourceTypeBadge();
+			element.geoResourceId = '12345';
+
+			expect(element.geoResourceId).toBe('12345');
+		});
+	});
+
+	describe('when instantiated', () => {
+		it('sets a default model', async () => {
+			await setup();
+			const element = new GeoResourceTypeBadge();
+
+			expect(element.getModel()).toEqual({
+				geoResourceId: null
+			});
+		});
+	});
+
+	describe('when initialized', () => {
+		it('should render nothing when geoResourceId is null', async () => {
+			const element = await setup();
+
+			expect(element.shadowRoot.children.length).toBe(0);
+		});
+	});
+
+	describe('when geoResourceId is set (via property)', () => {
+		it('update th UI', async () => {
+			const geoResourceId = '914c9263-5312-453e-b3eb-5104db1bf788';
+			const element = await setup();
+			const geoResourceServiceSpy = vi
+				.spyOn(geoResourceServiceMock, 'byId')
+				.mockReturnValue(new WmsGeoResource(geoResourceId, 'label', 'url', 'layers', 'format'));
+
+			element.geoResourceId = '914c9263-5312-453e-b3eb-5104db1bf788';
+
+			const badges = element.shadowRoot.querySelectorAll('ba-badge');
+			expect(badges).toHaveLength(1);
+			expect(badges[0].label).toBe('geoResourceInfo_typeBadge_label_wms');
+			expect(badges[0].title).toBe('geoResourceInfo_typeBadge_desc_wms');
+			expect(geoResourceServiceSpy).toHaveBeenCalledWith(geoResourceId);
+		});
+	});
+});
