d1a3cfa1eb3a8630fa6aab5298dc25167f81ecab
Bumi A. <28516408+herrmutig@users.noreply.github.com>
2026-07-31 14:03:59 +0200

    Common implementation of GeoResourceBadge (#3933)
    * added common implementation of GeoResourceBadge
    
    * added click Action, added tests and  documentation
    
    * changed domain description for GeoResourceBadgeType
    
    * cleanup

--- src/modules/geoResourceInfo/index.js
+++ src/modules/geoResourceInfo/index.js
@@ -2,6 +2,8 @@ import './i18n';
 import { GeoResourceInfoPanel } from './components/GeoResourceInfoPanel';
 import { LastModifiedItem } from './components/LastModifiedItem';
 import { GeoResourceTypeBadge } from './components/GeoResourceTypeBadge';
+import { GeoResourceBadge } from './components/GeoResourceBadge';
+
 if (!window.customElements.get(GeoResourceInfoPanel.tag)) {
 	window.customElements.define(GeoResourceInfoPanel.tag, GeoResourceInfoPanel);
 }
@@ -10,4 +12,8 @@ if (!window.customElements.get(LastModifiedItem.tag)) {
 }
 if (!window.customElements.get(GeoResourceTypeBadge.tag)) {
 	window.customElements.define(GeoResourceTypeBadge.tag, GeoResourceTypeBadge);
+}
+
+if (!window.customElements.get(GeoResourceBadge.tag)) {
+	window.customElements.define(GeoResourceBadge.tag, GeoResourceBadge);
 }
