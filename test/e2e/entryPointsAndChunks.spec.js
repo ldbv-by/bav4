1e1e3789fb51b762d060edbe0d1d1282e5a8a123
Thomas Aulinger <taulinger@gmx.de>
2026-08-19 11:43:45 +0200

    configure maplibre-gl worker and update tests

--- test/e2e/entryPointsAndChunks.spec.js
+++ test/e2e/entryPointsAndChunks.spec.js
@@ -38,3 +38,14 @@ test.describe('chunks', () => {
 		expect(response.ok()).toBe(true);
 	});
 });
+
+test.describe('maplibre-worker configuration', () => {
+	test('should provide the ba-elevation.js', async ({ request }) => {
+		const response = await request.get(`${BASE_URL}/maplibre-gl-worker.mjs`);
+		expect(response.ok()).toBe(true);
+	});
+	test('should provide the ba-routing.js', async ({ request }) => {
+		const response = await request.get(`${BASE_URL}/maplibre-gl-shared.mjs`);
+		expect(response.ok()).toBe(true);
+	});
+});
