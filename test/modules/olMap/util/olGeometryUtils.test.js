e435ffb170f169d5f7687dcb2439692db9f8ff9d
thiloSchlemmer <2117317+thiloSchlemmer@users.noreply.github.com>
2026-09-08 09:32:25 +0200

    Mfp3Encoder: encode measure style (#3941)
    * refactor measureStyleFunction to getMeasureStyleFunction
    
    Fix behavior of measureStyleFunction while map view is rotated and a geodesic line is needed. Fixing with usage of the related layer , when calling getMeasureStyleFunction, to get the responsible layerRenderer.
    
    
    * change encoding of MeasurementFeatures
    
    ---------
    
    Co-authored-by: Thomas Aulinger <49945713+taulinger@users.noreply.github.com>

--- test/modules/olMap/util/olGeometryUtils.test.js
+++ test/modules/olMap/util/olGeometryUtils.test.js
@@ -7,6 +7,7 @@ import {
 	isValidGeometry,
 	moveParallel,
 	calculatePartitionResidualOfSegments,
+	calculateOrientedFractionCoordinates,
 	getStats,
 	getPolygonFrom,
 	getAzimuthFrom,
@@ -740,6 +741,39 @@ describe('polarStakeOut', () => {
 	});
 });
 
+describe('calculateOrientedFractionCoordinates', () => {
+	it('calculates no ticks for a Point', () => {
+		expect(calculateOrientedFractionCoordinates([[0, 0]])).toEqual([]);
+		expect(calculateOrientedFractionCoordinates([0, 0])).toEqual([]);
+	});
+
+	it('calculates ticks for a LineString', () => {
+		expect(
+			calculateOrientedFractionCoordinates(
+				[
+					[0, 0],
+					[15, 0]
+				],
+				0.2
+			)
+		).toEqual(expect.any(Array));
+	});
+
+	describe('with subdivisionFactor', () => {
+		it('calculates no ticks for subdivisionFactor > fraction ', () => {
+			expect(
+				calculateOrientedFractionCoordinates(
+					[
+						[0, 0],
+						[15, 0]
+					],
+					0.2,
+					0.1
+				)
+			).toEqual(expect.any(Array));
+		});
+	});
+});
 describe('calculatePartitionResidualOfSegments', () => {
 	it('calculates no residuals for a Point', () => {
 		expect(calculatePartitionResidualOfSegments(new Point([0, 0]))).toEqual([]);
