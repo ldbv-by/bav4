38400597949b6f03c395d42c8f8dc1646220d973
Schlemmer, Thilo (LDBV) <2117317+thiloSchlemmer@users.noreply.github.com>
2026-09-16 12:10:08 +0200

    add tests and jsdoc

--- src/modules/olMap/ol/style/NamedStyle.js
+++ src/modules/olMap/ol/style/NamedStyle.js
@@ -1,5 +1,17 @@
+/**
+ * @module modules/olMap/ol/style/NamedStyle
+ */
 import { Style } from 'ol/style';
 
+/**
+ * Extends an openlayers style with a name.
+ *
+ * For usecases where a feature with an array of styles must be
+ * changed and specific styles must be filtered.
+ *
+ * @class
+ * @author thiloSchlemmer
+ */
 export class NamedStyle extends Style {
 	#name;
 	constructor(name, options) {
