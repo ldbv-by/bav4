3eb9495e3fc887a7a0ded024d34294c5201f24f0
Thomas Aulinger <taulinger@gmx.de>
2026-09-23 15:51:04 +0200

    Fix optional chaining in getStyle for geoResource styling

--- src/store/layers/layers.reducer.js
+++ src/store/layers/layers.reducer.js
@@ -395,7 +395,7 @@ export const getStyle = (layer) => {
 	 */
 	const geoResource = geoResourceService.byId(layer.geoResourceId);
 
-	if (!layer.style && (geoResource?.isStylable() || getCluster(layer))) {
+	if (!layer.style && (geoResource?.isStylable?.() || getCluster(layer))) {
 		return geoResource?.hasStyle() ? { ...geoResource.style } : { baseColor: nextColor(layer.geoResourceId) };
 	}
 	return layer.style;
